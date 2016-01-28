b0233b707365861d39af3b3d7a1f21b0d17e5f5c
Mae Isabelle Turiana <maeisabelle.turiana@marklogic.com>
2016-01-28 22:27:23 +0800

    Made the error message more user-friendly

--- quick-start/src/main/java/com/marklogic/hub/exception/DataHubException.java
+++ quick-start/src/main/java/com/marklogic/hub/exception/DataHubException.java
@@ -5,6 +5,6 @@ public class DataHubException extends Exception {
 	private static final long serialVersionUID = 4824858930318275798L;
 
 	public DataHubException(String message, Throwable cause) {
-		super(message, cause);
+		super("Error in connecting to the Data Hub API with the following message: " + message, cause);
 	}
 }
