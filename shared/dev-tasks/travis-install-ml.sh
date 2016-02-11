84d5897632d391b9686fe31fa0f6950ab1f3f5ba
Paxton Hare <paxton@greenllama.com>
2016-02-11 13:29:20 -0500

    trying to get travis to work

--- shared/dev-tasks/travis-install-ml.sh
+++ shared/dev-tasks/travis-install-ml.sh
@@ -11,18 +11,60 @@ function successOrExit {
     fi
 }
 
-fname=MarkLogic.rpm
-fnamedeb="marklogic_"
-suff="_amd64.deb"
-fnamedeb=$fnamedeb$suff
+set | grep TRAVIS
 
-url=$MLRELEASE_URL
+test $1 && arg1=$1
+if [[ $arg1 = 'release' ]]; then
+  fname=MarkLogic-8.0-20150819.x86_64.rpm
+  ver=8.0-4
+  fnamedeb="marklogic_"
+  fnamedeb=$fnamedeb$ver
+  suff="_amd64.deb"
+  fnamedeb=$fnamedeb$suff
 
-echo "********* Downloading MarkLogic"
+  url=$MLRELEASE_URL
 
-status=$(curl -k --head --write-out %{http_code} --silent --output /dev/null $url)
-if [[ $status = 200 ]]; then
+  echo "********* Downloading MarkLogic $ver"
+
+  status=$(curl -k --head --write-out %{http_code} --silent --output /dev/null $url)
+  if [[ $status = 200 ]]; then
     successOrExit curl -k -o ./$fname $url
+
+    fname=$(pwd)/$fname
+
+    sudo apt-get update
+    sudo apt-get install wajig alien rpm lsb-base dpkg-dev debhelper build-essential
+    (cd /etc && sudo ln -s default sysconfig)
+    sudo wajig rpminstall $fname
+
+    echo "********* MarkLogic $ver installed"
+  else
+    echo "CANNOT DOWNLOAD: status = $status for version $ver (URL=secure/suppressed)"
+    exit 1
+  fi
+else
+  # find today
+  day=$(date +"%Y%m%d")
+
+  # if the user passed a day string as a param then use it instead
+  test $1 && day=$1
+  # make a version number out of the date
+  ver="8.0-$day"
+
+  echo "********* Downloading MarkLogic nightly $ver"
+
+  # fetch/install ML nightly
+  fname="MarkLogic-$ver.x86_64.rpm"
+  fnamedeb="marklogic_"
+  fnamedeb=$fnamedeb$ver
+  suff="_amd64.deb"
+  fnamedeb=$fnamedeb$suff
+
+  url="https://root.marklogic.com/nightly/builds/linux64/rh6-intel64-80-test-1.marklogic.com/b8_0/pkgs.$day/$fname"
+
+  status=$(curl -k --anyauth -u $MLBUILD_USER:$MLBUILD_PASSWORD --head --write-out %{http_code} --silent --output /dev/null $url)
+  if [[ $status = 200 ]]; then
+    successOrExit curl -k --anyauth -u $MLBUILD_USER:$MLBUILD_PASSWORD -o ./$fname $url
 
     fname=$(pwd)/$fname
 
@@ -31,8 +73,9 @@ if [[ $status = 200 ]]; then
     sudo alien -d -k $fname
     sudo dpkg -i $fnamedeb
 
-    echo "********* MarkLogic installed"
-else
-    echo "CANNOT DOWNLOAD: status = $status for version (URL=secure/suppressed)"
+    echo "********* MarkLogic nightly $ver installed"
+  else
+    echo "CANNOT DOWNLOAD: status = $status for date $day (URL=\"$url\")"
     exit 1
+  fi
 fi
