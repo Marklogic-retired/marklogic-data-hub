1506f3f9612f65b4e9635c40fb6837a4a6fe14b4
Paxton Hare <paxton@greenllama.com>
2016-02-26 11:32:29 -0500

    Fixed #62 - added spring batch to run jobs

---
+++ data-hub/src/main/java/com/marklogic/spring/batch/hub/CollectorReader.java
@@ -0,0 +1,52 @@
+package com.marklogic.spring.batch.hub;
+
+import java.util.List;
+
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemStreamException;
+import org.springframework.batch.item.ItemStreamReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+
+import com.marklogic.client.helper.LoggingObject;
+import com.marklogic.hub.collector.Collector;
+
+public class CollectorReader extends LoggingObject implements ItemStreamReader<String> {
+
+    private Collector collector;
+
+    private List<String> results;
+
+    private int index = 0;
+
+    public CollectorReader(Collector collector) {
+        this.collector = collector;
+    }
+
+    @Override
+    public void open(ExecutionContext executionContext) throws ItemStreamException {
+        this.results = collector.run();
+    }
+
+    @Override
+    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+        String result = null;
+
+        if (results.size() > this.index) {
+            result = this.results.get(this.index);
+            index++;
+        }
+
+        return result;
+    }
+
+    @Override
+    public void update(ExecutionContext executionContext) throws ItemStreamException {
+    }
+
+    @Override
+    public void close() throws ItemStreamException {
+    }
+
+}
