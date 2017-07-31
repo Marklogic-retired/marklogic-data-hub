803e7c58d22d61b9f50fc0a4c75ec3e4da07ab33
Paxton Hare <paxton@greenllama.com>
2017-07-31 14:29:05 -0400

    fixed #466

---
+++ marklogic-data-hub/src/main/java/com/marklogic/hub/util/MlcpConsumer.java
@@ -0,0 +1,54 @@
+package com.marklogic.hub.util;
+
+import com.marklogic.hub.flow.FlowStatusListener;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MlcpConsumer implements Consumer<String> {
+    private int currentPc = 0;
+    private final Pattern completedPattern = Pattern.compile("^.+completed (\\d+)%$");
+    private final Pattern successfulEventsPattern = Pattern.compile("^.+OUTPUT_RECORDS_COMMITTED:\\s+(\\d+).*$");
+    private final Pattern failedEventsPattern = Pattern.compile("^.+OUTPUT_RECORDS_FAILED\\s+(\\d+).*$");
+    private AtomicLong successfulEvents;
+    private AtomicLong failedEvents;
+    private FlowStatusListener statusListener;
+    private String jobId;
+
+    public MlcpConsumer(AtomicLong successfulEvents, AtomicLong failedEvents, FlowStatusListener statusListener,
+                        String jobId)
+    {
+        this.successfulEvents = successfulEvents;
+        this.failedEvents = failedEvents;
+        this.statusListener = statusListener;
+        this.jobId = jobId;
+    }
+
+    @Override
+    public void accept(String status) {
+        Matcher m = completedPattern.matcher(status);
+        if (m.matches()) {
+            int pc = Integer.parseInt(m.group(1));
+
+            // don't send 100% because more stuff happens after 100% is reported here
+            if (pc > currentPc && pc != 100) {
+                currentPc = pc;
+            }
+        }
+
+        m = successfulEventsPattern.matcher(status);
+        if (m.matches()) {
+            successfulEvents.addAndGet(Long.parseLong(m.group(1)));
+        }
+
+        m = failedEventsPattern.matcher(status);
+        if (m.matches()) {
+            failedEvents.addAndGet(Long.parseLong(m.group(1)));
+        }
+
+        statusListener.onStatusChange(jobId, currentPc, status);
+    }
+}
