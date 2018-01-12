b3089885ba00e9dd8a178114a0aa0fbb860295fc
Paxton Hare <paxton@greenllama.com>
2018-01-12 15:37:34 -0500

    fixed #633 added the internals for #536 because this task really needed it. Did not expose the stop on fail option to gradle or the UI

---
+++ marklogic-data-hub/src/main/java/com/marklogic/hub/error/HarmonizeFlowError.java
@@ -0,0 +1,8 @@
+package com.marklogic.hub.error;
+
+public class HarmonizeFlowError  extends Exception {
+
+    public HarmonizeFlowError(String error) {
+        super(error);
+    }
+}
--- marklogic-data-hub/src/main/java/com/marklogic/hub/flow/impl/FlowRunnerImpl.java
+++ marklogic-data-hub/src/main/java/com/marklogic/hub/flow/impl/FlowRunnerImpl.java
@@ -30,6 +30,7 @@ import com.marklogic.client.util.RequestParameters;
 import com.marklogic.hub.HubConfig;
 import com.marklogic.hub.collector.Collector;
 import com.marklogic.hub.collector.DiskQueue;
+import com.marklogic.hub.error.HarmonizeFlowError;
 import com.marklogic.hub.flow.*;
 import com.marklogic.hub.job.Job;
 import com.marklogic.hub.job.JobManager;
@@ -54,6 +55,7 @@ public class FlowRunnerImpl implements FlowRunner {
     private String destinationDatabase;
     private Map<String, Object> options;
     private int previousPercentComplete;
+    private boolean stopOnFailure = false;
 
     private List<FlowItemCompleteListener> flowItemCompleteListeners = new ArrayList<>();
     private List<FlowItemFailureListener> flowItemFailureListeners = new ArrayList<>();
@@ -96,6 +98,12 @@ public class FlowRunnerImpl implements FlowRunner {
     @Override
     public FlowRunner withDestinationDatabase(String destinationDatabase) {
         this.destinationDatabase = destinationDatabase;
+        return this;
+    }
+
+    @Override
+    public FlowRunner withStopOnFailure(boolean stopOnFailure) {
+        this.stopOnFailure = stopOnFailure;
         return this;
     }
 
@@ -200,6 +208,8 @@ public class FlowRunnerImpl implements FlowRunner {
 
         double batchCount = Math.ceil((double)uris.size() / (double)batchSize);
 
+        HashMap<String, JobTicket> ticketWrapper = new HashMap<>();
+
         QueryBatcher queryBatcher = dataMovementManager.newQueryBatcher(uris.iterator())
             .withBatchSize(batchSize)
             .withThreadCount(threadCount)
@@ -246,6 +256,14 @@ public class FlowRunnerImpl implements FlowRunner {
                             });
                         });
                     }
+
+                    if (stopOnFailure && response.errorCount > 0) {
+                        JobTicket jobTicket = ticketWrapper.get("jobTicket");
+                        if (jobTicket != null) {
+                            dataMovementManager.stopJob(jobTicket);
+                        }
+                    }
+
                 }
                 catch(Exception e) {
                     if (errorMessages.size() < MAX_ERROR_MESSAGES) {
@@ -259,6 +277,7 @@ public class FlowRunnerImpl implements FlowRunner {
             });
 
         JobTicket jobTicket = dataMovementManager.startJob(queryBatcher);
+        ticketWrapper.put("jobTicket", jobTicket);
         jobManager.saveJob(job.withStatus(JobStatus.RUNNING_HARMONIZE));
 
         runningThread = new Thread(() -> {
@@ -273,7 +292,10 @@ public class FlowRunnerImpl implements FlowRunner {
             dataMovementManager.stopJob(queryBatcher);
 
             JobStatus status;
-            if (failedEvents.get() + successfulEvents.get() != uris.size()) {
+            if (failedEvents.get() > 0 && stopOnFailure) {
+                status = JobStatus.STOP_ON_ERROR;
+            }
+            else if (failedEvents.get() + successfulEvents.get() != uris.size()) {
                 status = JobStatus.CANCELED;
             }
             else if (failedEvents.get() > 0 && successfulEvents.get() > 0) {
