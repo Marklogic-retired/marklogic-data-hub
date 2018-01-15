f72eee0acf2d2f596d606bce922fd4f928bdbe1c
Dave Cassel <dmcassel@gmail.com>
2018-01-15 15:57:41 -0800

    #504 send options along with harmonization flows

--- quick-start/src/test/java/com/marklogic/quickstart/web/EntitiesControllerTest.java
+++ quick-start/src/test/java/com/marklogic/quickstart/web/EntitiesControllerTest.java
@@ -1,21 +1,39 @@
 package com.marklogic.quickstart.web;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.marklogic.client.document.DocumentRecord;
+import com.marklogic.client.io.DocumentMetadataHandle;
+import com.marklogic.client.io.JacksonHandle;
 import com.marklogic.hub.HubConfig;
+import com.marklogic.hub.flow.CodeFormat;
+import com.marklogic.hub.flow.DataFormat;
+import com.marklogic.hub.flow.FlowType;
+import com.marklogic.hub.scaffold.Scaffolding;
+import com.marklogic.hub.util.FileUtil;
+import com.marklogic.quickstart.model.EnvironmentConfig;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @WebAppConfiguration
 public class EntitiesControllerTest extends BaseTestController {
+
+    private static String ENTITY = "test-entity";
 
     @Autowired
     private EntitiesController ec;
@@ -39,6 +57,54 @@ public class EntitiesControllerTest extends BaseTestController {
         envConfig.setMlSettings(HubConfig.hubFromEnvironment(path, null));
         Map<String, Object> options = ec.getInputFlowOptions("test-entity", "flow-name");
         JSONAssert.assertEquals("{ \"input_file_path\": \"C:\\\\some\\\\crazy\\\\path\\\\to\\\\project\" }", new ObjectMapper().writeValueAsString(options), true);
+    }
+
+    @Test
+    public void runHarmonizeFlowWithOptions() throws IOException, InterruptedException {
+        // Set up (not needed for other tests)
+        baseSetUp();
+        installHub();
+
+        Path projectDir = Paths.get(".", PROJECT_PATH);
+        Scaffolding scaffolding = new Scaffolding(projectDir.toString(), stagingClient);
+
+        scaffolding.createFlow(ENTITY, "sjs-json-harmonization-flow", FlowType.HARMONIZE,
+            CodeFormat.JAVASCRIPT, DataFormat.JSON);
+
+        Path harmonizeDir = projectDir.resolve("plugins/entities/" + ENTITY + "/harmonize");
+        FileUtil.copy(getResourceStream("flow-manager/sjs-harmonize-flow/collector.sjs"), harmonizeDir.resolve("sjs-json-harmonization-flow/collector.sjs").toFile());
+        FileUtil.copy(getResourceStream("flow-manager/sjs-harmonize-flow/content.sjs"), harmonizeDir.resolve("sjs-json-harmonization-flow/content.sjs").toFile());
+        FileUtil.copy(getResourceStream("flow-manager/sjs-harmonize-flow/headers.sjs"), harmonizeDir.resolve("sjs-json-harmonization-flow/headers.sjs").toFile());
+        FileUtil.copy(getResourceStream("flow-manager/sjs-harmonize-flow/main.sjs"), harmonizeDir.resolve("sjs-json-harmonization-flow/main.sjs").toFile());
+        FileUtil.copy(getResourceStream("flow-manager/sjs-harmonize-flow/triples.sjs"), harmonizeDir.resolve("sjs-json-harmonization-flow/triples.sjs").toFile());
+        FileUtil.copy(getResourceStream("flow-manager/sjs-harmonize-flow/writer.sjs"), harmonizeDir.resolve("sjs-json-harmonization-flow/writer.sjs").toFile());
+
+        getDataHub().installUserModules(true);
+
+        DocumentMetadataHandle meta = new DocumentMetadataHandle();
+        meta.getCollections().add(ENTITY);
+        installStagingDoc("/staged.json", meta, "flow-manager/staged.json");
+
+        EnvironmentConfig envConfig = new EnvironmentConfig(PROJECT_PATH, "local", "admin", "admin");
+        envConfig.setMlSettings(HubConfig.hubFromEnvironment(PROJECT_PATH, null));
+        setEnvConfig(envConfig);
+
+        final String OPT_VALUE = "test-value";
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode body = mapper.readTree("{\"batchSize\":1, \"threadCount\": 1, \"options\": {\"test-option\": \"" + OPT_VALUE + "\"}}");
+
+        ResponseEntity<?> responseEntity = ec.runHarmonizeFlow(ENTITY, "sjs-json-harmonization-flow", body);
+
+        Assert.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        DocumentRecord doc = finalDocMgr.read("/staged.json").next();
+        JsonNode root = doc.getContent(new JacksonHandle()).get();
+        JsonNode env = root.path("envelope");
+        JsonNode headers = env.path("headers");
+        JsonNode optionNode = headers.path("test-option");
+        Assert.assertFalse(optionNode.isMissingNode());
+        Assert.assertEquals(OPT_VALUE, optionNode.asText());
+
+        uninstallHub();
     }
 
 }
