c69081743fae06c3385668776fabb6611767377e
ayuwono <ayuwono@marklogic.com>
2018-02-12 16:26:26 -0800

    add run harmonize test

--- quick-start/e2e/specs/run/run.ts
+++ quick-start/e2e/specs/run/run.ts
@@ -4,6 +4,7 @@ import loginPage from '../../page-objects/auth/login';
 import dashboardPage from '../../page-objects/dashboard/dashboard';
 import entityPage from '../../page-objects/entities/entities';
 import flowPage from '../../page-objects/flows/flows';
+import jobsPage from '../../page-objects/jobs/jobs';
 
 export default function() {
   describe('Run Flows', () => {
@@ -21,6 +22,43 @@ export default function() {
       }));
     });
 
+    it ('should run Load Products flow', function() {
+      flowPage.entityDisclosure('Product').click();
+      browser.wait(EC.elementToBeClickable(flowPage.getFlow('Product', 'Load Products', 'INPUT')));
+      flowPage.runInputFlow('Product', 'Load Products', 'json', 1);
+    });
+
+    //run harmonize flow
+    it ('should run Harmonize Products flow', function() {
+      flowPage.entityDisclosure('Product').click();
+      browser.wait(EC.elementToBeClickable(flowPage.getFlow('Product', 'Harmonize Products', 'HARMONIZE')));
+      flowPage.getFlow('Product', 'Harmonize Products', 'HARMONIZE').click();
+      browser.wait(EC.visibilityOf(flowPage.tabs));
+      flowPage.getFlowTab('content').click();
+      flowPage.pluginTextArea().click();
+      browser.actions().sendKeys(flowPage.ctrlA(), protractor.Key.DELETE, flowPage.contentScriptWithFlowOptions()).perform();
+      browser.actions().sendKeys(flowPage.ctrlS()).perform();
+      browser.sleep(5000);
+      browser.executeScript('window.document.getElementById("jobs-tab").click()');
+      flowPage.flowsTab.click();
+      flowPage.entityDisclosure('Product').click();
+      browser.wait(EC.elementToBeClickable(flowPage.getFlow('Product', 'Harmonize Products', 'HARMONIZE')));
+      flowPage.getFlow('Product', 'Harmonize Products', 'HARMONIZE').click();
+      flowPage.getFlowTab('flowInfo').click();
+      flowPage.runHarmonizeButton().click();
+      browser.wait(EC.elementToBeClickable(flowPage.toastButton));
+      flowPage.toastButton.click();
+      flowPage.jobsTab.click();
+      jobsPage.isLoaded();
+      expect(jobsPage.finishedHarmonizedFlows.isPresent()).toBe(true);
+      jobsPage.flowsTab.click();
+      flowPage.isLoaded(); 
+    });
+
+    it ('should open the TestEntity disclosure', function() {
+      flowPage.entityDisclosure('TestEntity').click();
+    });
+    
     let flowCount = 1;
     ['sjs', 'xqy'].forEach((codeFormat) => {
       ['xml', 'json'].forEach((dataFormat) => {
@@ -32,22 +70,5 @@ export default function() {
         });
       });
     });
-
-    //run harmonize flow
-    /*it ('should run Harmonize Products flow', function() {
-      flowPage.entityDisclosure('Product').click();
-      flowPage.getFlow('Product', 'Harmonize Products', 'HARMONIZE').click();
-      browser.wait(EC.visibilityOf(flowPage.tabs));
-      browser.sleep(5000);
-      // *** this is not finding the span element ***
-      flowPage.tab('collector').click();
-      browser.sleep(5000);
-      flowPage.pluginTextArea().clear();
-      browser.sleep(5000);
-      flowPage.pluginTextArea().sendKeys(flowPage.contentScriptWithFlowOptions());
-      browser.sleep(5000);
-      flowPage.pluginSaveButton().click();
-      browser.sleep(5000);
-    });*/
   });
 }
