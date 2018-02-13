2f870f49847f3daf072618c56feae0a1a77d29af
ayuwono <ayuwono@marklogic.com>
2018-02-13 14:05:28 -0800

    add verification steps on run flows

--- quick-start/e2e/specs/run/run.ts
+++ quick-start/e2e/specs/run/run.ts
@@ -5,6 +5,8 @@ import dashboardPage from '../../page-objects/dashboard/dashboard';
 import entityPage from '../../page-objects/entities/entities';
 import flowPage from '../../page-objects/flows/flows';
 import jobsPage from '../../page-objects/jobs/jobs';
+import browsePage from '../../page-objects/browse/browse';
+import viewerPage from '../../page-objects/viewer/viewer';
 
 export default function() {
   describe('Run Flows', () => {
@@ -28,23 +30,77 @@ export default function() {
       flowPage.runInputFlow('Product', 'Load Products', 'json', 1);
     });
 
-    //run harmonize flow
-    it ('should run Harmonize Products flow', function() {
+    it('should verify the loaded data', function() {
+      //verify on jobs page
+      flowPage.jobsTab.click();
+      jobsPage.isLoaded();
+      expect(jobsPage.finishedFlows.isPresent()).toBe(true);
+      //verify on browse data page
+      jobsPage.browseDataTab.click();
+      browsePage.isLoaded();
+      expect(browsePage.resultsPagination().getText()).toContain('Showing Results 1 to 10 of 450');
+      browsePage.databaseDropDown().click();
+      browsePage.selectDatabase('STAGING').click();
+      browser.wait(EC.elementToBeClickable(browsePage.resultsUri()));
+      browsePage.searchBox().clear();
+      browsePage.searchBox().sendKeys('442403950907');
+      browsePage.searchButton().click();
+      browser.wait(EC.elementToBeClickable(browsePage.resultsUri()));
+      expect(browsePage.resultsUri().getText()).toEqual('/private/board_games_accessories.csv-0-1');
+      //verify on viewer page
+      browsePage.resultsUri().click();
+      viewerPage.isLoaded();
+      expect(viewerPage.searchResultUri().getText()).toEqual('Uri: /private/board_games_accessories.csv-0-1');
+      expect(element(by.cssContainingText('.cm-variable', 'sku')).isPresent()).toBe(true);
+      expect(element(by.cssContainingText('.cm-string', '442403950907')).isPresent()).toBe(true);
+      expect(element(by.cssContainingText('.cm-variable', 'attachments')).isPresent()).toBe(true);
+      expect(element(by.cssContainingText('.cm-variable', 'null')).isPresent()).toBe(true);
+      expect(element(by.cssContainingText('.cm-variable', 'opt1')).isPresent()).toBe(false);
+      expect(element(by.cssContainingText('.cm-string', 'world')).isPresent()).toBe(false);
+      //verfiy on dashboard page
+      viewerPage.dashboardTab.click();
+      dashboardPage.isLoaded();
+      expect(element(by.cssContainingText('div', '450')).isPresent()).toBe(true);
+      dashboardPage.flowsTab.click();
+      flowPage.isLoaded();
+    });
+
+    it ('should setup Harmonize Products flow', function() {
+      let contentWithOptionsFilePath = 'e2e/qa-data/plugins/contentWithOptions.sjs';
       flowPage.entityDisclosure('Product').click();
       browser.wait(EC.elementToBeClickable(flowPage.getFlow('Product', 'Harmonize Products', 'HARMONIZE')));
       flowPage.getFlow('Product', 'Harmonize Products', 'HARMONIZE').click();
       browser.wait(EC.visibilityOf(flowPage.tabs));
+      //change content.sjs to include flow options
       flowPage.getFlowTab('content').click();
       flowPage.pluginTextArea().click();
-      browser.actions().sendKeys(flowPage.ctrlA(), protractor.Key.DELETE, flowPage.contentScriptWithFlowOptions()).perform();
+      browser.actions().sendKeys(flowPage.ctrlA(), protractor.Key.DELETE, flowPage.readFileContent(contentWithOptionsFilePath)).perform();
       browser.actions().sendKeys(flowPage.ctrlS()).perform();
       browser.sleep(5000);
       browser.executeScript('window.document.getElementById("jobs-tab").click()');
       flowPage.flowsTab.click();
+    });
+
+    it ('should logout', function() {
+      flowPage.logout();
+      loginPage.isLoaded();
+    });
+
+    it('should login', function() {
+      loginPage.clickNext('ProjectDirTab');
+      browser.wait(EC.elementToBeClickable(loginPage.environmentTab));
+      loginPage.clickNext('EnvironmentTab');
+      browser.wait(EC.elementToBeClickable(loginPage.loginTab));
+      loginPage.login();
+    });
+
+    it('should run Harmonize Products flow', function() {
+      dashboardPage.flowsTab.click()
       flowPage.entityDisclosure('Product').click();
       browser.wait(EC.elementToBeClickable(flowPage.getFlow('Product', 'Harmonize Products', 'HARMONIZE')));
       flowPage.getFlow('Product', 'Harmonize Products', 'HARMONIZE').click();
       flowPage.getFlowTab('flowInfo').click();
+      browser.wait(EC.visibilityOf(flowPage.tabs));
       flowPage.runHarmonizeButton().click();
       browser.wait(EC.elementToBeClickable(flowPage.toastButton));
       flowPage.toastButton.click();
@@ -52,13 +108,32 @@ export default function() {
       jobsPage.isLoaded();
       expect(jobsPage.finishedHarmonizedFlows.isPresent()).toBe(true);
       jobsPage.flowsTab.click();
-      flowPage.isLoaded(); 
+      flowPage.isLoaded();
+    });
+
+    it('should verify the harmonized data', function() {
+      flowPage.browseDataTab.click();
+      expect(browsePage.resultsPagination().getText()).toContain('Showing Results 1 to 10 of 450');
+      browsePage.databaseDropDown().click();
+      browsePage.selectDatabase('FINAL').click();
+      browser.wait(EC.elementToBeClickable(browsePage.resultsUri()));
+      browsePage.searchBox().clear();
+      browsePage.searchBox().sendKeys('442403950907');
+      browsePage.searchButton().click();
+      browser.wait(EC.elementToBeClickable(browsePage.resultsUri()));
+      expect(browsePage.resultsUri().getText()).toEqual('/private/board_games_accessories.csv-0-1');
+      browsePage.resultsUri().click();
+      viewerPage.isLoaded();
+      expect(viewerPage.searchResultUri().getText()).toEqual('Uri: /private/board_games_accessories.csv-0-1');
+      expect(element(by.cssContainingText('.cm-variable', 'opt1')).isPresent()).toBe(true);
+      expect(element(by.cssContainingText('.cm-string', 'world')).isPresent()).toBe(true);
+      viewerPage.flowsTab.click();
     });
 
     it ('should open the TestEntity disclosure', function() {
       flowPage.entityDisclosure('TestEntity').click();
     });
-    
+
     let flowCount = 1;
     ['sjs', 'xqy'].forEach((codeFormat) => {
       ['xml', 'json'].forEach((dataFormat) => {
