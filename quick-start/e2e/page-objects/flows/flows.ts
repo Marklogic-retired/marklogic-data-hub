1e826ed34fac3a365d98d1c627ae93ec87899080
ayuwono <ayuwono@marklogic.com>
2018-02-13 14:05:08 -0800

    adding read custom content file

--- quick-start/e2e/page-objects/flows/flows.ts
+++ quick-start/e2e/page-objects/flows/flows.ts
@@ -2,6 +2,7 @@ import { protractor, browser, element, by, By, $, $$, ExpectedConditions as EC }
 import { AppPage } from '../appPage';
 import { pages } from '../page';
 import jobsPage from '../jobs/jobs';
+const fs = require('fs-extra');
 
 export class FlowPage extends AppPage {
 
@@ -213,83 +214,8 @@ export class FlowPage extends AppPage {
     return element(by.css(`mdl-menu-item[data-value="${value}"]`));
   }
 
-  contentScriptWithFlowOptions() {
-    return `
-    /*
-     * Create Content Plugin
-     *
-     * @param id         - the identifier returned by the collector
-     * @param options    - an object containing options. Options are sent from Java
-     *
-     * @return - your content
-     */
-    function createContent(id, options) {
-      var doc = cts.doc(id);
-      var root = doc.root.toObject();
-    
-      var source;
-      var opts;
-    
-      // for xml we need to use xpath
-      if (root && xdmp.nodeKind(root) === 'element') {
-        source = root.xpath('/*:envelope/*:instance/node()');
-      }
-      // for json we need to return the instance
-      else if (root && root.envelope && root.envelope.instance) {
-        source = root.envelope.instance;
-        opts = options;
-      }
-      // for everything else
-      else {
-        source = doc;
-      }
-    
-      return extractInstanceProduct(source, opts);
-    }
-    
-    /**
-     * Creates an object instance from some source document.
-     * @param source  A document or node that contains
-     *   data for populating a Product
-     * @return An object with extracted data and
-     *   metadata about the instance.
-     */
-    function extractInstanceProduct(source, opts) {
-      // the original source documents
-      var attachments = source;
-    
-      var sku = xs.string(source.sku || source.SKU);
-      var title = xs.string(source.title);
-      var price = xs.decimal(source.price);
-      var id = xs.string(source.id);
-      var value1 = xs.string(opts.hello);
-      var value2 = xs.string(opts.myNumber);
-      
-      // return the instance object
-      return {
-        '$attachments': attachments,
-        '$type': 'Product',
-        '$version': '0.0.1',
-        'sku': sku,
-        'title': title,
-        'price': price,
-        'opt1': value1,
-        'opt2': value2
-      }
-    };
-    
-    
-    function makeReferenceObject(type, ref) {
-      return {
-        '$type': type,
-        '$ref': ref
-      };
-    }
-    
-    module.exports = {
-      createContent: createContent
-    };
-    `;
+  readFileContent(filepath: string) {
+    return fs.readFileSync(filepath, 'utf8');
   }
 
   runInputFlow(entityName: string, flowName: string, dataFormat: string, count: number) {
