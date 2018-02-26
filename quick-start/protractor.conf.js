29aa417f6f1f9444eef402b9e6eb0f0388010e5b
ayuwono <ayuwono@marklogic.com>
2018-02-26 11:05:29 -0800

    go non headless and change the window size

--- quick-start/protractor.conf.js
+++ quick-start/protractor.conf.js
@@ -14,8 +14,8 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     chromeOptions: {
-      args: ["--headless", "--disable-gpu", "--window-size=1920,1080" ]
-      //args: ["--window-size=1600,1000"]
+      //args: ["--headless", "--disable-gpu", "--window-size=1920,1080" ]
+      args: ["--window-size=1920,1080"]
     }
   },
   directConnect: true,
