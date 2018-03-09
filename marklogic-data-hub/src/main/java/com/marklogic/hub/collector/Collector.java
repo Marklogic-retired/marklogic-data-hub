6cdc42fcb8f5c4b06e7ecc016876d3c3b2e0005d
Alexander Ebadirad <alex@ebadirad.com>
2018-03-09 11:56:07 -0700

    Add javdocs for collector interface

--- marklogic-data-hub/src/main/java/com/marklogic/hub/collector/Collector.java
+++ marklogic-data-hub/src/main/java/com/marklogic/hub/collector/Collector.java
@@ -23,14 +23,50 @@ import java.util.Map;
 
 public interface Collector {
 
+    /**
+     * Obtains the code format for the collector in use
+     * @return xqy or sjs (xquery or javascript)
+     */
     CodeFormat getCodeFormat();
+
+    /**
+     * Returns the module for the collect
+     * @return the code in string form
+     */
     String getModule();
 
+    /**
+     * Set's the hub config for the collector
+     * @param config takes in a HubConfig object
+     */
     void setHubConfig(HubConfig config);
+
+    /**
+     * Returns the current hubConfig ont he collector object
+     * @return current hub config in use by collector
+     */
     HubConfig getHubConfig();
 
+    /**
+     * Sets the client to be used to communicate with the database
+     * @param client - a databaseclient to use
+     */
     void setClient(DatabaseClient client);
+
+    /**
+     * Gets the client in use currently by the collector object
+     * @return a database client object
+     */
     DatabaseClient getClient();
 
+    /**
+     * Obtains and grabs a list of uris that match the collector code
+     * @param jobId - id of the job this is to have
+     * @param entity - name of which entity is this being run against
+     * @param flow - name of which flow
+     * @param threadCount - how many threads to use for the operation
+     * @param options - string/object map of options you want to pass to the collector
+     * @return a list of uris as strings in a diskqueue object
+     */
     DiskQueue<String> run(String jobId, String entity, String flow, int threadCount, Map<String, Object> options);
 }
