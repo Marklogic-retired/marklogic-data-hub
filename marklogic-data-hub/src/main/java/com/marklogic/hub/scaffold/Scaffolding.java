ffa37afe1374cf937f655296a66568ba6a3b9d33
Alexander Ebadirad <alex@ebadirad.com>
2018-03-12 11:33:57 -0700

    Update scaffolding java docs

--- marklogic-data-hub/src/main/java/com/marklogic/hub/scaffold/Scaffolding.java
+++ marklogic-data-hub/src/main/java/com/marklogic/hub/scaffold/Scaffolding.java
@@ -29,10 +29,22 @@ import java.util.List;
 
 public interface Scaffolding {
 
+    /**
+     * Creates and returns a Scaffolding object
+     * @param projectDir - the path to the project as a string
+     * @param databaseClient - the database client that will be used to connect
+     * @return Scaffolding object with project directory and database set
+     */
     static Scaffolding create(String projectDir, DatabaseClient databaseClient) {
         return new ScaffoldingImpl(projectDir, databaseClient);
     }
 
+    /**
+     * Grabs the absolute path of a series of strings
+     * @param first - the minimum required string path to get the absolute path from
+     * @param more - additional paths submitted
+     * @return the absolute path of each string path provided
+     */
     static String getAbsolutePath(String first, String... more) {
         StringBuilder absolutePath = new StringBuilder(first);
         for (String path : more) {
@@ -42,29 +54,96 @@ public interface Scaffolding {
         return absolutePath.toString();
     }
 
+    /**
+     * Returns the directory of the flow
+     * @param entityName - the entity name
+     * @param flowName - the name of the flow
+     * @param flowType - FlowType (sjs or xqy)
+     * @return the directory path as a Path type
+     */
     Path getFlowDir(String entityName, String flowName, FlowType flowType);
 
+    /**
+     * Creates an entity given a string name
+     * @param entityName - the name of the entity as string
+     */
     void createEntity(String entityName);
 
+    /**
+     * Creates a flow for an entity with Entity Services model as default
+     * @param entityName - name of the entity to associate the flow with
+     * @param flowName - the name of the flow as a string
+     * @param flowType - the type of flow as TypeFlow, eg: harmonize or ingest
+     * @param codeFormat - the format of the code as CodeFormat enum
+     * @param dataFormat - the format of the data (json or xml)
+     */
     void createFlow(String entityName, String flowName,
                     FlowType flowType, CodeFormat codeFormat,
                     DataFormat dataFormat);
 
+    /**
+     * Creates a flow for an entity with an additional option for using Entity Services
+     * @param entityName - name of the entity to associate the flow with
+     * @param flowName - the name of the flow as a string
+     * @param flowType - the type of flow as TypeFlow, eg: harmonize or ingest
+     * @param codeFormat - the format of the code as CodeFormat enum
+     * @param dataFormat - the format of the data (json or xml)
+     * @param useEsModel - true to use Entity Services, false not to
+     */
     void createFlow(String entityName, String flowName,
                     FlowType flowType, CodeFormat codeFormat,
                     DataFormat dataFormat, boolean useEsModel);
 
+    /**
+     * Updates a legacy flow on disk
+     * @param fromVersion - string version number of DHF
+     * @param entityName - the entity which the flow is attached
+     * @return a list of updated flows by name
+     */
     List<String> updateLegacyFlows(String fromVersion, String entityName);
 
+    /**
+     * Update a specific entity that's legacy
+     * @param entityName - name of the entity
+     */
     void updateLegacyEntity(String entityName);
 
+    /**
+     * Update a legacy flow
+     * @param fromVersion - string version number of DHF
+     * @param entityName - the entity which the flow is attached
+     * @param flowName - the name of the flow as a string
+     * @param flowType - the type of flow as TypeFlow, eg: harmonize or ingest
+     * @return
+     */
     boolean updateLegacyFlow(String fromVersion, String entityName, String flowName, FlowType flowType);
 
+    /**
+     * Creates a rest extension on disk to be deployed to server
+     * @param entityName - the entity which the flow is attached
+     * @param extensionName - the name of the extension as a string
+     * @param flowType - the type of flow as TypeFlow, eg: harmonize or ingest
+     * @param codeFormat - the format of the code as CodeFormat enum
+     * @throws ScaffoldingValidationException
+     */
     void createRestExtension(String entityName, String extensionName,
                              FlowType flowType, CodeFormat codeFormat) throws ScaffoldingValidationException;
 
+    /**
+     * Creates a rest transform on disk to be deployed to server
+     * @param entityName - the entity which the flow is attached
+     * @param transformName - the name of the transform as a string
+     * @param flowType - the type of flow as TypeFlow, eg: harmonize or ingest
+     * @param codeFormat - the format of the code as CodeFormat enum
+     * @throws ScaffoldingValidationException
+     */
     void createRestTransform(String entityName, String transformName,
                              FlowType flowType, CodeFormat codeFormat) throws ScaffoldingValidationException;
 
+    /**
+     * Returns the disk path of the entity
+     * @param entityName - name of the entity as string
+     * @return Path of the entity
+     */
     Path getEntityDir(String entityName);
 }
