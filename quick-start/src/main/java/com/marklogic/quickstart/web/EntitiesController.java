1fcb53ac0f68c276959c528886c3cf5dd849cd94
Alex Ebadirad <alex@ebadirad.com>
2018-03-21 13:26:40 -0700

    DHFPROD-386 - added paths to save harmonize flow options to disk

--- quick-start/src/main/java/com/marklogic/quickstart/web/EntitiesController.java
+++ quick-start/src/main/java/com/marklogic/quickstart/web/EntitiesController.java
@@ -189,7 +189,18 @@ class EntitiesController extends EnvironmentAware {
         return entityManagerService.validatePlugin(envConfig().getMlSettings(), entityName, flowName, plugin);
     }
 
+    @RequestMapping(value = "/entities/{entityName}/flows/harmonize/{flowName}/save-harmonize-options", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<?> saveHarmonizeFlowOptions(
+        @PathVariable String entityName,
+        @PathVariable String flowName,
+        @RequestBody JsonNode json) throws IOException {
 
+        flowManagerService.saveOrUpdateHarmonizeFlowOptionsToFile(entityName,
+            flowName, json.toString());
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
     @RequestMapping(value = "/entities/{entityName}/flows/input/{flowName}/save-input-options", method = RequestMethod.POST)
     @ResponseBody
