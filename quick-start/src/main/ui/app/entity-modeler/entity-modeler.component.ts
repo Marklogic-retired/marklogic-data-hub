28658b9a7408b69fe7b22342f696ed075071421b
mhuang-ml <michael.huang@marklogic.com>
2018-04-24 17:47:28 -0400

    Updating Issue #578: Update the deletion message Removing the double confirmation dialog and updating the deletion message that is displayed to the user to make it clearer to what they are deleting.

--- quick-start/src/main/ui/app/entity-modeler/entity-modeler.component.ts
+++ quick-start/src/main/ui/app/entity-modeler/entity-modeler.component.ts
@@ -297,12 +297,9 @@ export class EntityModelerComponent implements AfterViewChecked {
   }
 
   deleteEntity(entity: Entity) {
-    let result = this.dialogService.confirm(`Really delete ${entity.name}?`, 'No', 'Yes');
+    let result = this.dialogService.confirm(`Delete the ${entity.name} entity?\n\nAny flows associated with the entity will also be deleted.`, 'No', 'Yes');
     result.subscribe(() => {
-      let confirmResult = this.dialogService.confirm(`Are you sure really want to delete the ${entity.name} entity as it will delete the entity and all associated flows and code?`, 'No', 'Yes');
-      confirmResult.subscribe(() => {
-        this.entitiesService.deleteEntity(entity);
-      }, () => {});
+      this.entitiesService.deleteEntity(entity);
     }, () => {});
   }
 
