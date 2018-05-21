2921c2f0e4b345a4cff6edee7c928d8083262d7a
Aries Yuwono <ayuwono@marklogic.com>
2018-05-21 08:09:12 -0700

    E2e/scalable entity (#1017)
    * modify window size
    
    * use entity name for editing
    
    * modify page object for more scalable entity test

--- quick-start/protractor.conf.js
+++ quick-start/protractor.conf.js
@@ -15,7 +15,7 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     chromeOptions: {
-      args: ["--headless", "--disable-gpu", "--window-size=1920,1080"]
+      args: ["--headless", "--disable-gpu", "--window-size=1920x1080"]
     }
   },
   directConnect: true,
