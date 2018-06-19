e13fe60341fa28d0467fbfd7ba7956925a62a828
Meredith Ju <Meredith.Ju@marklogic.com>
2018-06-18 17:22:38 -0700

    My first commit

--- ml-data-hub-plugin/src/main/groovy/com/marklogic/gradle/task/HubInfoTask.groovy
+++ ml-data-hub-plugin/src/main/groovy/com/marklogic/gradle/task/HubInfoTask.groovy
@@ -12,7 +12,7 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *  
+ *
  */
 
 package com.marklogic.gradle.task
@@ -23,6 +23,7 @@ class HubInfoTask extends HubTask {
 
     @TaskAction
     void dumpHubInfo() {
-        print(this.hubConfig.toString());
+        //print(this.hubConfig.toString());
+        print("test");
     }
 }
