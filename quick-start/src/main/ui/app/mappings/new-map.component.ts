458c643e70e0ba5f672df7a702b797de853f4556
Mike Wooldridge <mike@hellion.org>
2018-07-16 18:14:09 -0700

    DHFPROD-1176 do not allow duplicate mapping names

--- quick-start/src/main/ui/app/mappings/new-map.component.ts
+++ quick-start/src/main/ui/app/mappings/new-map.component.ts
@@ -17,18 +17,21 @@ import {Entity} from "../entities";
 export class NewMapComponent {
   actions: any;
   entity: Entity;
-  mapName: string;
+  mapName: string = '';
   mapDesc: string = '';
+  mappings: Array<Mapping>;
 
   constructor(
     private dialog: MdlDialogReference,
     private envService: EnvironmentService,
     private mapService: MapService,
     @Inject('actions') actions: any,
-    @Inject('entity') entity: Entity
+    @Inject('entity') entity: Entity,
+    @Inject('mappings') mappings: Array<Mapping>
   ) {
     this.actions = actions;
     this.entity = entity;
+    this.mappings = mappings;
   }
 
   ngOnInit() {
@@ -45,8 +48,25 @@ export class NewMapComponent {
     this.cancel();
   }
 
+  nameEmpty() {
+    let result = true;
+    if (this.mapName) {
+      result = this.mapName.length === 0;
+    }
+    return result;
+  }
+
+  nameDuplicate() {
+    let result;
+    if (this.mappings && this.mapName) {
+      let name = this.mapName;
+      result =  _.findIndex(this.mappings, function(m) { return m.name === name; });
+    }
+    return result > -1;
+  }
+
   create() {
-    if (this.mapName && this.mapName.length > 0) {
+    if (!this.nameEmpty() && !this.nameDuplicate()) {
       this.hide();
       if (this.actions && this.actions.save) {
         this.actions.save(this.mapName, this.mapDesc);
