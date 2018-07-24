d46f16fcde5f6daef9a9c12632e6a2d25be5672e
ayuwono <ayuwono@marklogic.com>
2018-07-24 13:08:01 -0700

    add functions for createHarmonizeFlow, createInputFlow

--- quick-start/e2e/page-objects/flows/flows.ts
+++ quick-start/e2e/page-objects/flows/flows.ts
@@ -82,6 +82,10 @@ export class FlowPage extends AppPage {
     return element(by.cssContainingText('app-select-list td', 'XML'));
   }
 
+  useMapButton(mapName: string) {
+    return element(by.cssContainingText('app-select-list td', mapName));
+  }
+
   get createFlowButton() {
     return element(by.buttonText('Create'));
   }
@@ -134,24 +138,6 @@ export class FlowPage extends AppPage {
 
   pluginTextArea() {
     return element(by.css('mdl-tabs > mdl-tab-panel.mdl-tabs__panel.is-active > mdl-tab-panel-content > div > div.plugin-codemirror > app-codemirror > div > div.CodeMirror-scroll > div.CodeMirror-sizer > div > div > div > div.CodeMirror-code'));
-  }
-
-  ctrlA() {
-    let osName = process.platform;
-    if(osName = 'darwin') {
-      return protractor.Key.chord(protractor.Key.COMMAND, "a");
-    }
-    else
-      return protractor.Key.chord(protractor.Key.CONTROL, "a");
-  }
-
-  ctrlS() {
-    let osName = process.platform;
-    if(osName = 'darwin') {
-      return protractor.Key.chord(protractor.Key.COMMAND, "s");
-    }
-    else
-      return protractor.Key.chord(protractor.Key.CONTROL, "s");
   }
 
   getInputFlow(entityName: string, flowName: string) {
@@ -291,22 +277,20 @@ export class FlowPage extends AppPage {
     this.flowsTab.click();
 
     this.isLoaded();
-
-    // let message = `${entityName}: ${flowName} starting...`;
-    // browser.wait(EC.visibilityOf(this.toast));
-    // expect(this.toast.isDisplayed()).toBe(true);
-
-    // browser.wait(EC.visibilityOf(this.jobProgress));
-    // expect(element(by.css('.job-progress')).isDisplayed()).toBe(true);
-    // browser.wait(EC.not(EC.visibilityOf(this.jobProgress)));
-    // expect(element(by.css('.job-progress')).isPresent()).toBe(false);
   }
 
-  // this function has more flexibility on choosing the folder and input file type
-  // we will change it to runInputFlow in the future
-  // we need to change the old runInputFlow as it's hardcoded to products folder 
+  /*
+  * Run input flow -- this will replace the old runInputFlow that has hardcoded input folder
+  * @entityName: entity name
+  * @flowName: flow name
+  * @dataFormat: json | xml
+  * @dataFolderName: folder name under input directory
+  * @inputFileType: aggregates | archive | delimited_text | delimited_json | documents
+  *   | forest | rdf | sequencefile
+  * @uriSuffix: document uri suffix to append
+  */
   runInputFlowWithFolder(entityName: string, flowName: string, dataFormat: string, 
-      dataFolderName: string, inputFileType: string) {
+      dataFolderName: string, inputFileType: string, uriSuffix: string) {
     console.log(`running flow: ${entityName}: ${flowName}: ${dataFormat}`)
     this.getFlow(entityName, flowName, 'INPUT').click();
 
@@ -326,21 +310,22 @@ export class FlowPage extends AppPage {
     browser.wait(EC.elementToBeClickable(this.mlcpDropdown('input_file_type')));
     this.mlcpDropdown('input_file_type').click();
 
-    // click delimited text
+    // select input file type
     browser.wait(EC.elementToBeClickable(this.menuItem(inputFileType)));
     this.menuItem(inputFileType).click();
 
     // click Document Type
     browser.wait(EC.elementToBeClickable(this.mlcpDropdown('document_type')));
     this.mlcpDropdown('document_type').click();
+
+    // select document type
     browser.wait(EC.elementToBeClickable(this.menuItem(dataFormat)));
     browser.actions().mouseMove(this.menuItem(dataFormat)).perform();
     this.menuItem(dataFormat).click();
 
     // set output uri suffix
     this.mlcpInput('output_uri_suffix').clear();
-    // verify that uri can contain character &
-    this.mlcpInput('output_uri_suffix').sendKeys('?doc=yes&type=foo');
+    this.mlcpInput('output_uri_suffix').sendKeys(uriSuffix);
  
     if(inputFileType === 'delimited_text') {
       // click delimited text options
@@ -403,8 +388,14 @@ export class FlowPage extends AppPage {
     expect(this.newFlowDialog.isPresent()).toBe(false);
   }
   
-  // we will separate creating flow from general function createFlow
-  // it is because harmonized flow has new mapping selection parameter
+  /*
+  * Create input flow
+  * @entityName: entity name
+  * @flowName: flow name
+  * @dataFormat: json | xml
+  * @codeFormat: sjs | xqy
+  * @useEs: false - use blank template | true - use entity service
+  */
   createInputFlow(
     entityName: string, flowName: string, dataFormat: string,
     codeFormat: string, useEs: boolean)
@@ -439,8 +430,53 @@ export class FlowPage extends AppPage {
     browser.wait(EC.not(EC.presenceOf(this.newFlowDialog)));
     expect(this.newFlowDialog.isPresent()).toBe(false);
   }
-}
 
+  /*
+  * Create harmonize flow
+  * @entityName: entity name
+  * @flowName: flow name
+  * @dataFormat: json | xml
+  * @codeFormat: sjs | xqy
+  * @useEs: false - use blank template | true - use entity service
+  * @mapName: mapping name, default is None. Only valid when useEs is true
+  */
+  createHarmonizeFlow(
+    entityName: string, flowName: string, dataFormat: string,
+    codeFormat: string, useEs: boolean, mapName = 'None')
+  {
+    this.harmonizeFlowButton(entityName).click();
+    
+    browser.wait(EC.visibilityOf(this.newFlowDialog));
+    expect(this.newFlowDialog.isDisplayed()).toBe(true);
+
+    this.newFlowName.sendKeys(flowName);
+
+    if (useEs) {
+      this.esTemplateButton.click();
+    } else {
+      this.blankTemplateButton.click();
+    }
+
+    if (codeFormat === 'sjs') {
+      this.sjsFlowTypeButton.click();
+    } else {
+      this.xqyFlowTypeButton.click();
+    }
+
+    if (dataFormat === 'json') {
+      this.jsonDataFormatButton.click();
+    } else {
+      this.xmlDataFormatButton.click();
+    }
+
+    this.useMapButton(mapName).click();
+
+    this.createFlowButton.click();
+
+    browser.wait(EC.not(EC.presenceOf(this.newFlowDialog)));
+    expect(this.newFlowDialog.isPresent()).toBe(false);
+  }  
+}
 
 var flowPage = new FlowPage();
 export default flowPage;
