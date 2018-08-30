66945345cf59516afffd06640370f3b07f9ce1ba
Alex Ebadirad <alex@ebadirad.com>
2018-08-30 14:32:42 -0700

    object native tostring =/= apache IO tostring method for an input stream

--- marklogic-data-hub/src/main/java/com/marklogic/hub/deploy/commands/DeployHubAmpsCommand.java
+++ marklogic-data-hub/src/main/java/com/marklogic/hub/deploy/commands/DeployHubAmpsCommand.java
@@ -26,6 +26,7 @@ import com.marklogic.hub.DatabaseKind;
 import com.marklogic.hub.HubConfig;
 import com.marklogic.hub.error.DataHubConfigurationException;
 import com.marklogic.mgmt.ManageConfig;
+import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
@@ -56,7 +57,7 @@ public class DeployHubAmpsCommand extends DeployAmpsCommand {
         //new AmpsInstaller(securityStagingClient).installAmps(stagingModulesDatabaseName);
         ServerEvaluationCall call = installerClient.newServerEval();
         try (InputStream is = new ClassPathResource("installer-util/install-amps.xqy").getInputStream()) {
-            String ampCall = new InputStreamHandle(is).toString();
+            String ampCall = IOUtils.toString(is, "utf-8");
             is.close();
             ampCall.replace("data-hub-staging-MODULES", stagingModulesDatabaseName);
             call.xquery(ampCall);
@@ -81,7 +82,7 @@ public class DeployHubAmpsCommand extends DeployAmpsCommand {
         //new AmpsInstaller(securityStagingClient).unInstallAmps(stagingModulesDatabaseName);
         ServerEvaluationCall call = installerClient.newServerEval();
         try (InputStream is = new ClassPathResource("installer-util/uninstall-amps.xqy").getInputStream()) {
-            String ampCall = new InputStreamHandle(is).toString();
+            String ampCall = IOUtils.toString(is, "utf-8");
             is.close();
             ampCall.replace("data-hub-staging-MODULES", stagingModulesDatabaseName);
             call.xquery(ampCall);
