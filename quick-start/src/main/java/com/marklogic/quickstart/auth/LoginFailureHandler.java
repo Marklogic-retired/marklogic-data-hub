803a2f5796c439189d597ebb1f2bb47743c9ed7c
Alex Ebadirad <alex@ebadirad.com>
2018-08-29 23:53:39 -0700

    Pass at update, todo: check 2x update runs on flows and make sure update can happen despite not having a staging modules db

--- quick-start/src/main/java/com/marklogic/quickstart/auth/LoginFailureHandler.java
+++ quick-start/src/main/java/com/marklogic/quickstart/auth/LoginFailureHandler.java
@@ -1,9 +1,6 @@
 package com.marklogic.quickstart.auth;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
-import okhttp3.internal.connection.ConnectInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
@@ -11,7 +8,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -24,15 +20,12 @@ public class LoginFailureHandler implements AuthenticationFailureHandler {
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
                                         AuthenticationException exception) throws IOException, ServletException {
 
-        Map<String, String> output = new LinkedHashMap<String, String>();
+        Map<String, String> output = new LinkedHashMap<>();
         output.put("message", exception.getMessage());
         String json = new Gson().toJson(output);
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.getWriter().write(json);
-
-
     }
-
 }
