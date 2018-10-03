46bf5bac3fe3162b6615b126c8feb3e1616b6f9a
ssambasu <ssambasu@marklogic.com>
2018-10-03 11:26:44 -0700

    Removing hardcoding of "data-hub-role"

--- marklogic-data-hub/src/test/java/com/marklogic/hub/flow/FlowManagerTest.java
+++ marklogic-data-hub/src/test/java/com/marklogic/hub/flow/FlowManagerTest.java
@@ -109,7 +109,7 @@ public class FlowManagerTest extends HubTestBase {
         clearDatabases(HubConfig.DEFAULT_STAGING_NAME, HubConfig.DEFAULT_FINAL_NAME);
         DocumentMetadataHandle meta = new DocumentMetadataHandle();
         meta.getCollections().add("tester");
-        meta.getPermissions().add("data-hub-role", READ, UPDATE, EXECUTE);
+        meta.getPermissions().add(getHubAdminConfig().getHubRoleName(), READ, UPDATE, EXECUTE);
         installStagingDoc("/employee1.xml", meta, "flow-manager-test/input/employee1.xml");
         installStagingDoc("/employee2.xml", meta, "flow-manager-test/input/employee2.xml");
     }
@@ -118,7 +118,7 @@ public class FlowManagerTest extends HubTestBase {
         clearDatabases(HubConfig.DEFAULT_STAGING_NAME, HubConfig.DEFAULT_FINAL_NAME);
         DocumentMetadataHandle meta = new DocumentMetadataHandle();
         meta.getCollections().add("tester");
-        meta.getPermissions().add("data-hub-role", READ, UPDATE, EXECUTE);
+        meta.getPermissions().add(getHubAdminConfig().getHubRoleName(), READ, UPDATE, EXECUTE);
         installFinalDoc("/employee1.xml", meta, "flow-manager-test/input/employee1.xml");
         installFinalDoc("/employee2.xml", meta, "flow-manager-test/input/employee2.xml");
     }
