8e543afa09a841fe48074e1047cb47358c25067f
Alexander Smirnoff <alexander.smirnoff@marklogic.com>
2018-10-03 15:14:14 -0700

    fixed build error and mdl css import moved to angular.json

---
+++ quick-start/src/main/ui/app/shared/stories/utils/utils.module.ts
@@ -0,0 +1,7 @@
+import {NgModule} from "@angular/core";
+import {StoryCardComponent} from "./story-card/story-card.component";
+
+@NgModule({
+  declarations: [StoryCardComponent]
+})
+export class UtilsModule {}
