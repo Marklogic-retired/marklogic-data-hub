cf188dcd478ad8d394e17726fc560964ad496b14
Bruce An <bruce.an@marklogic.com>
2018-10-11 09:06:57 -0700

    moved component

---
+++ quick-start/src/main/ui/app/shared/stories/components/new-entity/new-entity.component.stories.ts
@@ -0,0 +1,36 @@
+import {moduleMetadata, storiesOf} from '@storybook/angular';
+import {object, text, boolean, withKnobs} from '@storybook/addon-knobs';
+import {withNotes} from '@storybook/addon-notes';
+import {action} from '@storybook/addon-actions';
+import {linkTo} from '@storybook/addon-links';
+import {centered} from '@storybook/addon-centered/angular';
+import {StoryCardComponent} from '../../utils/story-card/story-card.component';
+import {NewEntityComponent} from '../../../components/new-entity/new-entity.component';
+import {ThemeModule} from '../../../components/theme/theme.module';
+import {Component, Input} from '@angular/core';
+
+storiesOf('Components|New Entity', module)
+    .addDecorator(withKnobs)
+    .addDecorator(centered)
+    .addDecorator(
+        moduleMetadata({
+            imports: [
+                ThemeModule
+            ],
+            declarations: [NewEntityComponent, StoryCardComponent]
+        })
+    )
+    .add('New Entity Component', () => ({
+        template: `
+            <mlui-dhf-theme>
+                <mlui-story-card [width]="500" [height]="150">
+                    <app-new-entity
+                      
+                    ></app-new-entity>
+                </mlui-story-card>
+            </mlui-dhf-theme>
+        `,
+        props: {
+            
+        },
+    }));
