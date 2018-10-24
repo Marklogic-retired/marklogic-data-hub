562e99a4433c4467e1eb06103a6818472eec5b58
Scott D Brooks <clockworked24.7@gmail.com>
2018-10-24 15:49:38 -0700

    Settings components wrap-up

--- quick-start/src/main/ui/app/shared/stories/components/settings/settings.component.stories.ts
+++ quick-start/src/main/ui/app/shared/stories/components/settings/settings.component.stories.ts
@@ -3,14 +3,10 @@ import {HttpModule} from '@angular/http';
 import {storiesOf, moduleMetadata} from '@storybook/angular';
 import {centered} from '@storybook/addon-centered/angular';
 import { MdlDialogService } from '@angular-mdl/core';
+import { EventEmitter } from '@angular/core';
 import {
-  object,
-  text,
-  boolean,
-  number,
   withKnobs
 } from '@storybook/addon-knobs';
-import {action} from '@storybook/addon-actions';
 import {StoryCardComponent} from '../../utils';
 import {ThemeModule} from "../../../components";
 import {SettingsComponent} from "../../../components";
@@ -18,8 +14,84 @@ import { SettingsService } from '../../../services/settings/settings.service';
 import { InstallService } from '../../../services/installer';
 import { ProjectService } from '../../../services/projects';
 import { STOMPService } from '../../../services/stomp/stomp.service';
-import { RouterTestingModule } from '@angular/router/testing';
 
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs';
+
+
+/**
+ * Create a mock of each service
+ */
+class MockSettingsService {
+
+  private _mlcpPath: string = '';
+  traceEnabled: boolean = false;
+  debugEnabled: boolean = false;
+
+  constructor() { }
+
+  get mlcpPath(): string {
+    return this._mlcpPath;
+  }
+
+  set mlcpPath(path: string) { this._mlcpPath = path; }
+
+  validateMlcpPath(path: string): Observable<Data[]> { return Observable.of([]) }
+
+  toggleDebugging() {
+    if (this.debugEnabled) {
+      this.disableDebugging();
+    } else {
+      this.enableDebugging();
+    }
+  }
+
+  toggleTracing() {
+    if (this.traceEnabled) {
+      this.disableTracing();
+    } else {
+      this.enableTracing();
+    }
+  }
+
+  enableDebugging() { 
+    this.debugEnabled = true;
+  }
+
+  disableDebugging() { 
+    this.debugEnabled = false;
+  }
+
+  enableTracing() {
+    this.traceEnabled = true;
+  }
+
+  disableTracing() {
+    this.traceEnabled = false;
+  }
+
+}
+
+class MockInstallService {
+  messageEmitter: EventEmitter<any> = new EventEmitter<any>();
+
+  install() {}
+  uninstall() {}
+}
+
+class MockProjectService {
+
+}
+
+class MockSTOMPService {
+
+}
+
+class MockMdlDialogService {
+  confirm(question: string, declineText?: string, confirmText?: string, title?: string): Observable<void> {
+    return Observable.of();
+  };
+}
 
 storiesOf('Components|Settings', module)
   .addDecorator(withKnobs)
@@ -32,23 +104,22 @@ storiesOf('Components|Settings', module)
         StoryCardComponent
       ],
       entryComponents: [],
-      providers: [MdlDialogService, SettingsService, InstallService, ProjectService, STOMPService]
+      providers: [
+        { provide: MdlDialogService, useValue: new MockMdlDialogService() }, 
+        { provide: SettingsService, useValue: new MockSettingsService() }, 
+        { provide: InstallService, useValue: new MockInstallService() }, 
+        { provide: ProjectService, useValue: new MockProjectService() }, 
+        { provide: STOMPService, useValue: new MockSTOMPService() }
+      ]
     })
   )
   .addDecorator(centered)
   .add('Settings Component', () => ({
     template: `
            <mlui-dhf-theme>
-             <mlui-story-card [width]="'500px'" [height]="'250px'">
+             <mlui-story-card [width]="'640px'" [height]="'675px'">
                 <app-settings></app-settings>
              </mlui-story-card>
            </mlui-dhf-theme>`,
-    props: {
-      installStatus: text('installStatus', ''),
-      uninstallStatus: text('uninstallStatus', ''),
-      percentComplete: number('percentComplete', 0),
-      isInstalling: boolean('isInstalling', false),
-      isUninstalling: boolean('isUninstalling', false),
-      isMlcpPathValid: boolean('isMlcpPathValid', false)
-    }
+    props: { }
   }));
