e83ccc4256a4c77274bc5b421d1e14376af67079
Bruce An <bruce.an@marklogic.com>
2018-10-26 11:32:30 -0700

    removed entity box export

--- quick-start/src/main/ui/app/entity-modeler/index.ts
+++ quick-start/src/main/ui/app/entity-modeler/index.ts
@@ -1,5 +1,4 @@
 export * from './entity-modeler.component';
-export * from '../shared/components/entity-box/entity-box.component';
 export * from './entity-editor.component';
 export * from './externaldef-dialog.component';
 export * from './choose-collation.component';
