17ab35b5b0115caa9379b3b3d23ae6ecdb99b4a0
Charles Greer <cgreer@marklogic.com>
2018-10-30 09:04:11 -0700

    DHFPROD-1302 Issues in trace ui binary asset loading

--- marklogic-data-hub/src/main/java/com/marklogic/com/marklogic/client/ext/file/CacheBusterDocumentFileProcessor.java
+++ marklogic-data-hub/src/main/java/com/marklogic/com/marklogic/client/ext/file/CacheBusterDocumentFileProcessor.java
@@ -19,6 +19,7 @@ import com.marklogic.client.ext.file.DocumentFile;
 import com.marklogic.client.ext.file.DocumentFileProcessor;
 import com.marklogic.client.ext.helper.FilenameUtil;
 import com.marklogic.client.ext.helper.LoggingObject;
+import com.marklogic.client.io.Format;
 import org.springframework.core.io.Resource;
 import org.springframework.util.FileCopyUtils;
 
@@ -29,29 +30,33 @@ public class CacheBusterDocumentFileProcessor extends LoggingObject implements D
 
     @Override
     public DocumentFile processDocumentFile(DocumentFile documentFile) {
-        String text = documentFile.getModifiedContent();
-        if (text == null) {
-            Resource resource = documentFile.getResource();
-            if (resource != null) {
-                try {
-                    text = new String(FileCopyUtils.copyToByteArray(resource.getInputStream()));
-                } catch (IOException ie) {
-                    logger.warn("Unable to replace tokens in file: " + documentFile.getUri() + "; cause: " + ie.getMessage());
+        if (documentFile.getFormat() == Format.BINARY) {
+            return documentFile;
+        } else {
+            String text = documentFile.getModifiedContent();
+            if (text == null) {
+                Resource resource = documentFile.getResource();
+                if (resource != null) {
+                    try {
+                        text = new String(FileCopyUtils.copyToByteArray(resource.getInputStream()));
+                    } catch (IOException ie) {
+                        logger.warn("Unable to replace tokens in file: " + documentFile.getUri() + "; cause: " + ie.getMessage());
+                    }
                 }
             }
-        }
-        if (text != null) {
-            Resource resource = documentFile.getResource();
-            String comment = "";
-            if (FilenameUtil.isJavascriptFile(resource.getFilename())) {
-                comment = "// cache buster: " + UUID.randomUUID().toString() + "\n";
-            } else if (FilenameUtil.isXqueryFile(resource.getFilename())) {
-                comment = "(: cache buster: " + UUID.randomUUID().toString() + " :)\n";
-            }
+            if (text != null) {
+                Resource resource = documentFile.getResource();
+                String comment = "";
+                if (FilenameUtil.isJavascriptFile(resource.getFilename())) {
+                    comment = "// cache buster: " + UUID.randomUUID().toString() + "\n";
+                } else if (FilenameUtil.isXqueryFile(resource.getFilename())) {
+                    comment = "(: cache buster: " + UUID.randomUUID().toString() + " :)\n";
+                }
 
-            text = comment + text;
-            documentFile.setModifiedContent(text);
+                text = comment + text;
+                documentFile.setModifiedContent(text);
+            }
+            return documentFile;
         }
-        return documentFile;
     }
 }
