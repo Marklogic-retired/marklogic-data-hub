0bd3d83cd6840ffba89783326882c3c8d527d793
Peng Zhou <peng.zhou@marklogic.com>
2018-11-09 17:25:48 -0800

    add @types/clipboard to make clipboard load correctly in storybook

--- quick-start/src/main/ui/app/clipboard/clipboard.directive.ts
+++ quick-start/src/main/ui/app/clipboard/clipboard.directive.ts
@@ -1,6 +1,6 @@
 import { Directive, ElementRef, Input, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
-import Clipboard from 'clipboard';
-
+import { Options } from 'clipboard';
+import * as Clipboard from 'clipboard';
 @Directive({
   selector: '[appClipboard]'
 })
@@ -18,7 +18,7 @@ export class ClipboardDirective implements OnInit, OnDestroy {
   constructor(private elmRef: ElementRef) { }
 
   ngOnInit() {
-    let option: ClipboardOptions;
+    let option: Options;
     option = !!this.appClipboard ? { target: () => <any>this.appClipboard } : { text: () => this.cbContent };
     this.clipboard = new Clipboard(this.elmRef.nativeElement, option);
     this.clipboard.on('success', (e) => this.onSuccess.emit(true));
