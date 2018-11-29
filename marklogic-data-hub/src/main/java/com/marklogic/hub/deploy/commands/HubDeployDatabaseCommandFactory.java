3de06c424a77fe5fef562b9d5d8be52805385cd3
Rob Rudin <rob.rudin@marklogic.com>
2018-11-29 11:39:17 -0500

    1546 Using DeployOtherDatabasesCommand to deploy all databases
    The set of staging commands no longer has the database commands; since the final commands are executed first, they have the database commands, and DeployOtherDatabasesCommand is modified to use a custom impl of DeployDatabaseCommand that accounts for some DHF-specific stuff. 
    
    Will soon have a single list of commands a single AppConfig.

---
+++ marklogic-data-hub/src/main/java/com/marklogic/hub/deploy/commands/HubDeployDatabaseCommandFactory.java
@@ -0,0 +1,27 @@
+package com.marklogic.hub.deploy.commands;
+
+import com.marklogic.appdeployer.command.databases.DeployDatabaseCommand;
+import com.marklogic.appdeployer.command.databases.DeployDatabaseCommandFactory;
+import com.marklogic.hub.HubConfig;
+
+import java.io.File;
+
+/**
+ * Hub-specific factory implementation that's used to construct the hub-specific command for deploying a database.
+ */
+public class HubDeployDatabaseCommandFactory implements DeployDatabaseCommandFactory {
+
+    private HubConfig hubConfig;
+
+    public HubDeployDatabaseCommandFactory(HubConfig hubConfig) {
+        this.hubConfig = hubConfig;
+    }
+
+    @Override
+    public DeployDatabaseCommand newDeployDatabaseCommand(File databaseFile) {
+        final String filename = databaseFile != null ? databaseFile.getName() : null;
+        DeployHubDatabaseCommand c = new DeployHubDatabaseCommand(hubConfig, filename);
+        c.setDeployDatabaseCommandFactory(this);
+        return c;
+    }
+}
