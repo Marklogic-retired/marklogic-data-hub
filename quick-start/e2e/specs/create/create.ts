778f24c760ca585b3e05735592b009803bd81a67
Aries Yuwono <ayuwono@marklogic.com>
2018-12-11 15:30:30 -0800

    add sleep on entity dialog display

--- quick-start/e2e/specs/create/create.ts
+++ quick-start/e2e/specs/create/create.ts
@@ -352,6 +352,7 @@ export default function(tmpDir) {
       browser.get('http://localhost:8080/#/entities');
       await entityPage.toolsButton.click();
       await entityPage.newEntityButton.click();
+      browser.sleep(5000);
       expect(entityPage.entityEditor.isDisplayed()).toBe(true);
       await entityPage.entityTitle.sendKeys('TestEntity');
       await entityPage.saveEntity.click();
