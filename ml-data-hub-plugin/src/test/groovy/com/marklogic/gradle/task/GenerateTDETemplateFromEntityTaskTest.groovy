808d75764c09d4b04896e9a28939afc2b37f7225
ssambasu <ssambasu@marklogic.com>
2019-01-28 19:09:35 -0800

    Adding tests for ES-style entity and entity without reference

--- ml-data-hub-plugin/src/test/groovy/com/marklogic/gradle/task/GenerateTDETemplateFromEntityTaskTest.groovy
+++ ml-data-hub-plugin/src/test/groovy/com/marklogic/gradle/task/GenerateTDETemplateFromEntityTaskTest.groovy
@@ -20,6 +20,7 @@ class GenerateTDETemplateFromEntityTaskTest extends BaseTest {
         given:
         def pluginDir = Paths.get(hubConfig().hubProject.projectDirString).resolve("plugins")
 
+        //DHF style nested entities (references in separate file)
         def entitiesDir1 = pluginDir.resolve("entities").resolve("Order")
         def entitiesDir2 = pluginDir.resolve("entities").resolve("Item")
         def entitiesDir3 = pluginDir.resolve("entities").resolve("Customer")
@@ -28,14 +29,27 @@ class GenerateTDETemplateFromEntityTaskTest extends BaseTest {
         entitiesDir2.toFile().mkdirs()
         entitiesDir3.toFile().mkdirs()
 
+        //ES style nested entities(references in the same file)
+
+        def entitiesDir4 = pluginDir.resolve("entities").resolve("Entity1")
+        entitiesDir4.toFile().mkdirs()
+
+        //DHF style without references
+
+        def entitiesDir5 = pluginDir.resolve("entities").resolve("e2eentity")
+        entitiesDir5.toFile().mkdirs()
+
         FileUtils.copyFile(new File("src/test/resources/tde-template/Order.entity.json"), entitiesDir1.resolve('Order.entity.json').toFile())
         FileUtils.copyFile(new File("src/test/resources/tde-template/Item.entity.json"), entitiesDir2.resolve('Item.entity.json').toFile())
         FileUtils.copyFile(new File("src/test/resources/tde-template/Customer.entity.json"), entitiesDir3.resolve('Customer.entity.json').toFile())
 
+        FileUtils.copyFile(new File("src/test/resources/tde-template/Order1.entity.json"), entitiesDir4.resolve('Order1.entity.json').toFile())
+        FileUtils.copyFile(new File("src/test/resources/tde-template/e2eentity.entity.json"), entitiesDir5.resolve('e2eentity.entity.json').toFile())
+
         runTask("hubDeployUserArtifacts")
 
         when:
-        def result = runTask('hubGenerateTDETemplates')
+        def result = runTask('hubGenerateTDETemplates', '-PentityNames=Order,Order1,e2eentity,nonexistentEntity')
 
         then:
         notThrown(UnexpectedBuildFailure)
@@ -43,8 +57,9 @@ class GenerateTDETemplateFromEntityTaskTest extends BaseTest {
 
         Path tdePath = Paths.get(testProjectDir.root.toString(), "src", "main", "ml-schemas", "tde")
         tdePath.resolve("Order-1.0.0.tdex").toFile().exists() == true
-        tdePath.resolve("Item-1.0.0.tdex").toFile().exists() == true
-        tdePath.resolve("Customer-1.0.0.tdex").toFile().exists() == true
+        tdePath.resolve("Order1-1.0.0.tdex").toFile().exists() == true
+        tdePath.resolve("e2eentity-0.0.1.tdex").toFile().exists() == true
+        tdePath.resolve("nonexistentEntity-0.0.1.tdex").toFile().exists() == false
 
     }
 }
