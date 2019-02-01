46285ad675597630e0308f8aa64ea3b49179a686
Akshay Sonvane <akshay.sonvane93@gmail.com>
2019-02-01 15:27:55 -0800

    Make "local" default envName Add test for timestamp file

--- ml-data-hub-plugin/src/main/groovy/com/marklogic/gradle/DataHubPlugin.groovy
+++ ml-data-hub-plugin/src/main/groovy/com/marklogic/gradle/DataHubPlugin.groovy
@@ -200,9 +200,10 @@ class DataHubPlugin implements Plugin<Project> {
                 hubConfig.refreshProject(new ProjectPropertySource(project).getProperties(), false)
             }
 
-            if (project.hasProperty("environmentName")) {
-                hubConfig.withPropertiesFromEnvironment(project.property("environmentName").toString())
-            }
+            // By default, DHF uses gradle-local.properties for your local environment.
+            def envNameProp = project.hasProperty("environmentName") ? project.property("environmentName") : "local"
+            hubConfig.withPropertiesFromEnvironment(envNameProp.toString())
+
             hubConfig.setAppConfig(extensions.getByName("mlAppConfig"))
             hubConfig.setAdminConfig(extensions.getByName("mlAdminConfig"))
             hubConfig.setAdminManager(extensions.getByName("mlAdminManager"))
