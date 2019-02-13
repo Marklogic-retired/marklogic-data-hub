66c9eee59f96e0bfa699f7cf4fec151c42a9f811
Alexander Smirnoff <alexander.smirnoff@marklogic.com>
2019-02-13 11:25:31 -0800

    Merge branch 'develop' into DHFPROD-1783

--- quick-start/src/main/ui/app/components/flows/stories/flows-ui.compoment.stories.ts
+++ quick-start/src/main/ui/app/components/flows/stories/flows-ui.compoment.stories.ts
@@ -6,6 +6,7 @@ import {centered} from '@storybook/addon-centered/angular';
 import {boolean, object, text, withKnobs} from '@storybook/addon-knobs';
 import {moduleMetadata, storiesOf} from '@storybook/angular';
 import {RouterTestingModule} from '@angular/router/testing';
+import {TooltipModule} from 'ngx-bootstrap';
 
 import {ClipboardDirective} from '../../../directives/clipboard';
 import {CodemirrorComponent} from "../../codemirror";
@@ -39,7 +40,8 @@ storiesOf('Components|Flows', module)
         MdlModule,
         CommonModule,
         RouterTestingModule,
-        HttpModule
+        HttpModule,
+        TooltipModule.forRoot()
       ],
       declarations: [
         FlowsUiComponent,
@@ -264,6 +266,7 @@ const SampleEntities = [
         'flowName': 'hl7',
         'codeFormat': 'XQUERY',
         'dataFormat': 'XML',
+        'transformModulePath': () => action('cancel/close clicked'),
         'plugins': [
           {
             'hasShown': false,
