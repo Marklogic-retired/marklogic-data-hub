24348541cf8db6a61f09961d1457a09968a3045e
Alexander Smirnoff <alexander.smirnoff@marklogic.com>
2019-02-25 10:54:05 -0800

    added HttpClient to flows-manage services and some cleanup

--- quick-start/src/main/ui/app/components/manage-flows/manage-flows/stories/flows-page.stories.ts
+++ quick-start/src/main/ui/app/components/manage-flows/manage-flows/stories/flows-page.stories.ts
@@ -4,7 +4,7 @@ import {boolean, object, text, withKnobs} from '@storybook/addon-knobs';
 import {moduleMetadata, storiesOf} from '@storybook/angular';
 import {ManageFlowsModule} from "../../manage-flows.module";
 import {ThemeModule} from "../../../index";
-import {StoryCardComponent} from "../../../../utils/index";
+import {StoryCardComponent} from "../../../../utils";
 import {FlowsPageUiComponent} from "../ui/flows-page-ui.component";
 import {
   MatButtonModule,
@@ -21,21 +21,8 @@ import {
   MatTableModule,
   MatTabsModule
 } from "@angular/material";
-import {RouterModule, Routes} from "@angular/router";
-import {FlowsPageService} from "../../services/flow-page.service";
-import {FlowsPageComponent} from "../flows-page.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {flowsModelArray} from "./flows-page.data";
-
-const routes: Routes = [
-  {
-    path: 'flows',
-    component: FlowsPageComponent,
-    resolve: {
-      data: FlowsPageService
-    }
-  }
-];
 
 storiesOf('Components|Flows', module)
   .addDecorator(withKnobs)
