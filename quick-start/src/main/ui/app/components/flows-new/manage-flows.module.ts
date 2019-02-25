324c283063ed2da0663d675e17a8ddc2994ca19d
Mike Wooldridge <mike@hellion.org>
2019-02-25 14:16:02 -0800

    Add step-icons component

--- quick-start/src/main/ui/app/components/flows-new/manage-flows.module.ts
+++ quick-start/src/main/ui/app/components/flows-new/manage-flows.module.ts
@@ -5,19 +5,23 @@ import {MaterialModule} from "../theme/material.module";
 import {ManageFlowsUiComponent} from "./manage-flows/ui/manage-flows-ui.component";
 import {ConfirmationDialogComponent} from "../common";
 import {NewFlowDialogComponent} from "./manage-flows/ui/new-flow-dialog.component";
+import {StepIconsUiComponent} from "./manage-flows/ui/step-icons-ui.component";
 import {HttpClientModule} from "@angular/common/http";
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
     ConfirmationDialogComponent,
     NewFlowDialogComponent,
     ManageFlowsUiComponent,
-    ManageFlowsComponent
+    ManageFlowsComponent,
+    StepIconsUiComponent
   ],
   imports     : [
     MaterialModule,
     RouterModule,
-    HttpClientModule
+    HttpClientModule,
+    CommonModule
   ],
   providers   : [
   ],
