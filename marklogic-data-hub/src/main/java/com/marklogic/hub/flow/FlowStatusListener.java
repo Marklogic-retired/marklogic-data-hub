446c999778799b8f9fe8ea14392f14eca0ef0900
ssambasu <ssambasu@marklogic.com>
2019-03-19 00:02:25 -0700

    Adding FlowStatusListener, refactoring and changes to FlowRunnerImpl

---
+++ marklogic-data-hub/src/main/java/com/marklogic/hub/flow/FlowStatusListener.java
@@ -0,0 +1,16 @@
+package com.marklogic.hub.flow;
+
+import com.marklogic.hub.step.Step;
+
+public interface FlowStatusListener {
+
+    /**
+     *
+     * @param jobId - the id of the running job
+     * @param step -  the current running step
+     * @param percentComplete - the percentage of completeness expressed as an int
+     * @param message - the message you'd like to send along with it
+     */
+
+    void onStatusChange(String jobId, Step step, int percentComplete, String message) ;
+}
