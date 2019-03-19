e3b2eea7e02f0a1f2fddf1af5e379ff91cd2300c
ssambasu <ssambasu@marklogic.com>
2019-03-19 00:02:25 -0700

    Adding FlowStatusListener, refactoring and changes to FlowRunnerImpl

--- marklogic-data-hub/src/main/java/com/marklogic/hub/flow/impl/FlowRunnerImpl.java
+++ marklogic-data-hub/src/main/java/com/marklogic/hub/flow/impl/FlowRunnerImpl.java
@@ -2,76 +2,129 @@ package com.marklogic.hub.flow.impl;
 
 import com.marklogic.hub.flow.Flow;
 import com.marklogic.hub.flow.FlowRunner;
+import com.marklogic.hub.flow.FlowStatusListener;
 import com.marklogic.hub.impl.FlowManagerImpl;
 import com.marklogic.hub.impl.HubConfigImpl;
+import com.marklogic.hub.step.Step;
 import com.marklogic.hub.step.StepRunner;
+import com.marklogic.hub.step.StepRunnerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.*;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+@Component
 public class FlowRunnerImpl implements FlowRunner{
+
     @Autowired
     private HubConfigImpl hubConfig;
+
     @Autowired
     private FlowManagerImpl flowManager;
-    private ThreadPoolExecutor threadPool;
+
     private AtomicBoolean isRunning;
-    private String currentJob;
-    private Map<String, List<String>> flowMap = new HashMap<>();
-    private LinkedBlockingQueue jobQueue = new LinkedBlockingQueue();
-    private Thread runningThread = null;
+
+    private String runningJobId;
+    private Step runningStep;
+    private Flow runningFlow;
+
+    private StepRunner stepRunner;
+
+    private Map<String, List<Step>> stepsMap = new HashMap<>();
+    private Map<String, Flow> flowMap = new HashMap<>();
+    private ConcurrentLinkedQueue jobQueue = new ConcurrentLinkedQueue();
+
+    private List<FlowStatusListener> flowStatusListeners = new ArrayList<>();
+
     private Calendar jobStartTime;
     private Calendar jobEndTime;
 
+    private ExecutorService threadPool;
 
-    public String runFlow(String flowName, List<String> steps) {
+
+    public String runFlow(String flowName, List<String> stepNums) {
         Flow flow = flowManager.getFlow(flowName);
         if (flow == null){
-
+            throw new RuntimeException("Flow not found");
         }
-        //TODO - get steps from flow and make sure they exist
+        Iterator<String> stepItr = stepNums.iterator();
+        List<Step> steps = new ArrayList<>();
+        while(stepItr.hasNext()) {
+            Step tmpStep = flow.getStep(stepItr.next());
+            if(tmpStep == null){
+                throw new RuntimeException("Step not found");
+            }
+            steps.add(tmpStep);
+        }
+
         String jobId = UUID.randomUUID().toString();
-        flowMap.put(jobId, steps);
+        stepsMap.put(jobId, steps);
+        flowMap.put(jobId, flow);
         jobQueue.add(jobId);
         if(! isRunning.get()){
-            initializeTask();
+            initializeFlow(jobId);
         }
         return jobId;
     }
 
+    private void initializeFlow(String jobId) {
+        isRunning.set(true);
+        jobStartTime = Calendar.getInstance();
+        runningJobId = jobId;
+        runningFlow = flowMap.get(runningJobId);
+        threadPool = Executors.newFixedThreadPool(1);
+        threadPool.submit(new FlowRunnerTask(runningFlow, runningJobId));
+    }
+
     public void stopJob(String jobId) {
-        if (jobId.equals(currentJob)) {
+        if (jobId.equals(runningJobId)) {
             jobEndTime = Calendar.getInstance();
             isRunning.set(false);
-            if ( threadPool != null ) threadPool.shutdownNow();
         }
+        stepRunner.stop();
+        threadPool.shutdown();
     }
 
-    private void initializeTask() {
-        isRunning.set(true);
-
-    }
-
-    private void launchNextStep(){
-
-    }
-
-    private class StepRunnerTask implements Runnable {
-        private StepRunner stepRunner;
+    private class FlowRunnerTask implements Runnable {
         private String jobId;
+        private Flow flow;
+
+        public FlowRunnerTask(Flow flow, String jobId) {
+            this.jobId = jobId;
+            this.flow = flow;
+        }
 
         @Override
         public void run() {
-            while(! jobQueue.isEmpty()){
-                List stepList = flowMap.get(jobQueue.peek());
-                if(! stepList.isEmpty()) {
-                    launchNextStep();
-                }
+            List<Step> stepList = stepsMap.get(jobQueue.peek());
+            Iterator<Step> itr = stepList.iterator();
+            while ((itr.hasNext())){
+                Step step = itr.next();
+                runningStep = step;
+                stepRunner = new StepRunnerFactory().getStepRunner(step)
+                    .withJobId(jobId)
+                    .onItemFailed((jobId, itemId)-> {
+                        if(flow.isStopOnError()){
+                            stopJob(jobId);
+                        }
+                    })
+                    .onStatusChanged((jobId, percentComplete, message) ->{
+                        flowStatusListeners.forEach((FlowStatusListener listener) -> {
+                            listener.onStatusChange(jobId, step, percentComplete, step.getName() + " " + message);
+                        });
 
+                    });
+                stepRunner.run();
+                stepRunner.awaitCompletion();
+            }
+            jobQueue.remove(jobQueue.peek());
+            if(!jobQueue.isEmpty()) {
+                initializeFlow((String) jobQueue.peek());
             }
         }
     }
@@ -85,8 +138,8 @@ public class FlowRunnerImpl implements FlowRunner{
     }
 
     public void awaitCompletion(long timeout, TimeUnit unit) throws InterruptedException {
-        if (runningThread != null) {
-            runningThread.join(unit.convert(timeout, TimeUnit.MILLISECONDS));
+        if (threadPool != null) {
+            threadPool.awaitTermination(timeout, unit);
         }
     }
 }
--- marklogic-data-hub/src/main/java/com/marklogic/hub/step/StepRunner.java
+++ marklogic-data-hub/src/main/java/com/marklogic/hub/step/StepRunner.java
@@ -20,125 +20,136 @@ import com.marklogic.hub.flow.*;
 import com.marklogic.hub.job.Job;
 
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
- * Executes a flow with options
+ * Executes a step with options
  */
 public interface StepRunner {
 
     /**
-     * Sets the flow to be used with the flow runner
+     * Sets the flow to be used with the step runner
      * @param flow the flow object to be used
-     * @return the flow runner object
+     * @return the step runner object
      */
     StepRunner withFlow(Flow flow);
 
     /**
-     * Sets the batch size for the flow runner
+     * Sets the batch size for the step runner
      * @param batchSize - the size of the batch in integer form
-     * @return the flow runner object
+     * @return the step runner object
      */
     StepRunner withBatchSize(int batchSize);
 
     /**
-     * Sets the batch size for the flow runner
+     * Sets the batch size for the step runner
      * @param step - the step to be run in the flow
-     * @return the flow runner object
+     * @return the step runner object
      */
 
     StepRunner withStep(int step) ;
 
     /**
-     * Sets the batch size for the flow runner
+     * Sets the batch size for the step runner
      * @param jobId - the id of the job
-     * @return the flow runner object
+     * @return the step runner object
      */
 
     StepRunner withJobId(String jobId) ;
 
     /**
      * Sets the thread count for the flowrunner
-     * @param threadCount - the number of threads for the flow runner to use
-     * @return the flow runner object
+     * @param threadCount - the number of threads for the step runner to use
+     * @return the step runner object
      */
     StepRunner withThreadCount(int threadCount);
 
     /**
-     * Sets the source client on the flow runner. The source client determines which database to run against for building the envelope.
+     * Sets the source client on the step runner. The source client determines which database to run against for building the envelope.
      * @param sourceClient - the client that will be used
-     * @return  the flow runner object
+     * @return  the step runner object
      */
     StepRunner withSourceClient(DatabaseClient sourceClient);
 
     /**
      * Sets the database where flow output data will be persisted to
      * @param destinationDatabase - the name of the destination database
-     * @return the flow runner object
+     * @return the step runner object
      */
     StepRunner withDestinationDatabase(String destinationDatabase);
 
     /**
      * Sets the options to be passed into the xqy or sjs flow in the $options or options variables of main.
      * @param options - the object map of options as string/object pair
-     * @return the flow runner object
+     * @return the step runner object
      */
     StepRunner withOptions(Map<String, Object> options);
 
     /**
      * Sets if this will stop the job on a failure, or if it will continue on
      * @param stopOnFailure - true to stop the job if a failure happens
-     * @return the flow runner object
+     * @return the step runner object
      */
     StepRunner withStopOnFailure(boolean stopOnFailure);
 
     /**
      * Sets a listener on each item completing
      * @param listener the listen object to set
-     * @return the flow runner object
+     * @return the step runner object
      */
     StepRunner onItemComplete(StepItemCompleteListener listener);
 
     /**
      * Sets the failure listener for each item in the flow
      * @param listener the listener for the failures in the flow
-     * @return the flow runner object
+     * @return the step runner object
      */
     StepRunner onItemFailed(StepItemFailureListener listener);
 
     /**
      * Sets the status change listener on the flowrunner object
      * @param listener - the listener for when the status changes
-     * @return the flow runner object
+     * @return the step runner object
      */
     StepRunner onStatusChanged(StepStatusListener listener);
 
     /**
      * Sets the finished listener for when the item has processed (similar to a finally)
      * @param listener - the listener for the flow item when it finishes
-     * @return the flow runner object
+     * @return the step runner object
      */
     StepRunner onFinished(StepFinishedListener listener);
 
     /**
-     * Blocks until the job is complete.
+     * Blocks until the step execution is complete.
      */
     void awaitCompletion();
 
     /**
-     * Blocks until the job is complete.
+     * Blocks until the step execution is complete.
      *
      * @param timeout the maximum time to wait
      * @param unit the time unit of the timeout argument
      *
      * @throws InterruptedException if interrupted while waiting
      */
-    void awaitCompletion(long timeout, TimeUnit unit) throws InterruptedException;
+    void awaitCompletion(long timeout, TimeUnit unit) throws InterruptedException, TimeoutException;
 
     /**
      * Runs the flow and creates the job
      * @return Job object for the flow that is run
      */
     Job run();
+
+    /**
+     * Runs the flow and creates the job. This bypasses the collector
+     *
+     * @params uris the ids to pass to the harmonization flow
+     *
+     * @return Job object for the flow that is run
+     */
+    Job run(Collection<String> uris);
 }
