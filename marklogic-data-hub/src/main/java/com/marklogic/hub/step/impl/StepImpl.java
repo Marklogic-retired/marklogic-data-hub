c8ebf4a834aa042e92621a88aab204b563faf3f2
ssambasu <ssambasu@marklogic.com>
2019-03-28 18:01:32 -0700

    Remove todos

--- marklogic-data-hub/src/main/java/com/marklogic/hub/step/impl/StepImpl.java
+++ marklogic-data-hub/src/main/java/com/marklogic/hub/step/impl/StepImpl.java
@@ -36,7 +36,6 @@ public class StepImpl implements Step {
     private String modulePath;
     private String identifier;
     private int retryLimit;
-    //TODO:Set default values
     private int batchSize;
     private int threadCount;
     private String sourceDatabase;
