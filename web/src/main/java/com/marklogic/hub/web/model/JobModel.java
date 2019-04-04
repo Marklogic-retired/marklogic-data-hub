6c9bb7e9ad2cd9ce67ccac8b12c530a69e80b246
Hao Liu <hao.liu@marklogic.com>
2019-04-04 11:24:02 -0700

    DHFPROD-2069: create api/jobs with static data

---
+++ web/src/main/java/com/marklogic/hub/web/model/JobModel.java
@@ -0,0 +1,37 @@
+package com.marklogic.hub.web.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.List;
+
+public class JobModel {
+    public String jobId;
+    public String name; //flowName
+    public String targetEntity;
+    public String user;
+    public String lastAttemptedStep;
+    public String lastCompletedStep;
+    public String status;
+    public String startTime;
+    public String endTime;
+    public long successfulEvents;
+    public long failedEvents;
+
+    public static class JobStep {
+        public int stepNumber;
+        public String type;
+        public String name;
+        public String id;
+        public String identifier;
+        public int retryLimit;
+        public JsonNode options;
+        public String status;
+        public String startTime;
+        public String endTime;
+        public long successfulEvents;
+        public long failedEvents;
+    }
+
+    @JsonProperty("steps")
+    public List<JobStep> stepModels;
+}
