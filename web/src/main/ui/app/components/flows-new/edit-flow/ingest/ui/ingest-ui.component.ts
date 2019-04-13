7220ab12ec34c14f5b4aaf3521acf7c0226cf034
Alexander Smirnoff <alexander.smirnoff@marklogic.com>
2019-04-12 18:19:30 -0700

    added id for error message in the dialog

--- web/src/main/ui/app/components/flows-new/edit-flow/ingest/ui/ingest-ui.component.ts
+++ web/src/main/ui/app/components/flows-new/edit-flow/ingest/ui/ingest-ui.component.ts
@@ -3,15 +3,11 @@ import {Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
 const settings = {
   inputFilePath: {
     label: 'Input File Path',
-    field: 'inputFilePath',
-    type: 'string',
     description: 'Fhe filesystem location(s) to use for input. Default is current project path relative to the server location',
     value: '.'
   },
   fileTypes: {
     label: 'Input File Type',
-    field: 'inputFileType',
-    type: 'type',
     description: 'The input file type. Accepted value: txt, json, xml, binary, csv, or all.\nDefault: json.',
     options: [
       {
@@ -33,18 +29,11 @@ const settings = {
       {
         label: 'CSV',
         value: 'csv',
-      },
-      {
-        label: 'All',
-        value: 'all',
       }
-    ],
-    value: 'json'
+    ]
   },
   outputDocTypes: {
     label: 'Output File Type',
-    field: 'outputFileType',
-    type: 'type',
     description: 'The type of document to create. Accepted values: xml, json. Default: json.',
     options: [
       {
@@ -55,20 +44,16 @@ const settings = {
         label: 'XML',
         value: 'xml',
       }
-    ],
-    value: 'json'
+    ]
   },
   outputPermissions: {
     label: 'Output Permissions',
-    field: 'outputPermissions',
     description: 'A comma separated list of (role,capability) pairs to apply to loaded documents.\nDefault: The default permissions associated with the user inserting the document.\n\nExample: role1,read,role2,update',
     value: 'rest-reader,read,rest-writer,update',
   },
   outputURIReplacement: {
     label: 'Output URI Replacement',
-    field: 'outputURIReplacement',
-    description: 'Specify a prefix to prepend to the default URI. Used to construct output document URIs. For details, see Controlling Database URIs During Ingestion.',
-    value: ''
+    description: 'Specify a prefix to prepend to the default URI. Used to construct output document URIs. For details, see Controlling Database URIs During Ingestion.'
   }
 };
 
