2af099b0e2087b1a9a26a178699d2cff8014e2e7
Bruce <32427979+brucean52@users.noreply.github.com>
2019-04-18 13:25:45 -0700

    added sourceCollection to step model fixed custom step creation (#2279)

--- web/src/main/ui/app/components/flows-new/models/mapping-options.model.ts
+++ web/src/main/ui/app/components/flows-new/models/mapping-options.model.ts
@@ -1,5 +1,6 @@
 export class MappingOptions {
   public sourceQuery: string = '';
+  public sourceCollection: string = '';
   public sourceDatabase: string = '';
   public targetDatabase: string;
   public mapping: any;
