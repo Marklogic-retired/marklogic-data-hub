57b5e108b3528b80e1ebd0f48e72b6c1205f881d
Scott D Brooks <clockworked24.7@gmail.com>
2019-04-19 11:10:02 -0700

    match https://docs.marklogic.com/cts.collectionQuery syntax

--- web/src/main/ui/app/components/common/form-validators/existing-step-name-validator.ts
+++ web/src/main/ui/app/components/common/form-validators/existing-step-name-validator.ts
@@ -4,7 +4,7 @@ import {Flow} from "../../flows-new/models/flow.model";
 export class ExistingStepNameValidator {
   static forbiddenName(flow: Flow, currentStepName: String): ValidatorFn {
     return (control: FormControl): { [key: string]: any } | null => {
-      const forbiddenName = flow.steps.find((step => (step.name === control.value && (currentStepName ? currentStepName && currentStepName !== step.name : true)  ) ));
+      const forbiddenName = flow.steps.find((step => (step.name === control.value && (currentStepName ? currentStepName !== step.name : true)  ) ));
       return forbiddenName ? {'forbiddenName': {value: control.value}} : null;
     }
   }
