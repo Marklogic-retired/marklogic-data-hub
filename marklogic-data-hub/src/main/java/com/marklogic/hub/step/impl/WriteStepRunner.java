b9719095e789b017ba02be0e57cfbda9a4c6d955
Ryan Dew <ryan.j.dew@gmail.com>
2019-05-02 22:38:10 -0700

    DHFPROD-2400: Prefix metadata with datahub

--- marklogic-data-hub/src/main/java/com/marklogic/hub/step/impl/WriteStepRunner.java
+++ marklogic-data-hub/src/main/java/com/marklogic/hub/step/impl/WriteStepRunner.java
@@ -475,12 +475,12 @@ public class WriteStepRunner implements StepRunner {
         }
         // Set metadata values
         DocumentMetadataHandle.DocumentMetadataValues metadataValues = metadataHandle.getMetadataValues();
-        metadataValues.add("createdByJob", jobId);
-        metadataValues.add("createdInFlow", flow.getName());
-        metadataValues.add("createdByStep", flow.getStep(step).getStepDefinitionName());
+        metadataValues.add("datahubCreatedByJob", jobId);
+        metadataValues.add("datahubCreatedInFlow", flow.getName());
+        metadataValues.add("datahubCreatedByStep", flow.getStep(step).getStepDefinitionName());
         // TODO createdOn/createdBy data may not be accurate enough. Unfortunately REST transforms don't allow for writing metadata
-        metadataValues.add("createdOn", DATE_TIME_FORMAT.format(new Date()));
-        metadataValues.add("createdBy", this.hubConfig.getFlowOperatorUserName());
+        metadataValues.add("datahubCreatedOn", DATE_TIME_FORMAT.format(new Date()));
+        metadataValues.add("datahubCreatedBy", this.hubConfig.getFlowOperatorUserName());
         writeBatcher.withDefaultMetadata(metadataHandle);
         Format format = null;
         switch (inputFileType.toLowerCase()) {
