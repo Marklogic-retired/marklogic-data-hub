47a7b0a300a455dc403cfe5cb3d0ff18b330f2ac
wooldridge <mike@hellion.org>
2019-06-11 18:38:53 -0700

    DHFPROD-2383: Matching tooltip edits
    - Move Reduce Properties tooltip up to section label.
    - Make title attributes empty (no space) strings.
    - Nudge the tooltip containers to the right and widen container for match weight tooltip.
    - Add tooltip for Action in Match Thresholds dialog.
    - Edit tooltip content, mostly for length.

--- web/src/main/ui/app/components/flows-new/tooltips/flows.tooltips.ts
+++ web/src/main/ui/app/components/flows-new/tooltips/flows.tooltips.ts
@@ -32,20 +32,21 @@ export class FlowsTooltips {
     targetDatabase: 'The database where to store the processed data. For mastering, choose the FINAL database where you want to store mastered data. Default is data-hub-FINAL. IMPORTANT: For mastering, the source database and the target database must be the same. If you want the target database to be different, create a custom step with a custom module to override the default.',
 
     matching: {
-        weightOption: 'A factor that signifies the relative importance of the rule.',
-        thesaurusURI: 'The path to a thesaurus that is stored in a MarkLogic Server database and used to determine synonyms.',
-        filter: 'A node in the thesaurus to use as a filter. For example, <thsr:qualifier>birds</thsr:qualifier>.',
-        dictionaryURI: 'The path to a phonetic dictionary that is stored in a database and used when comparing words phonetically. ',
-        distanceThresh: 'The threshold below which the phonetic difference (distance) between two strings is considered insignificant; i.e., the strings are similar to each other.',
-        collation: 'The URI to the collation to use. A collation specifies the order for sorting strings.',
-        zip5matches9: 'The weight to use if, given one 9-digit zip code and one 5-digit zip code, the first five digits of the zip codes match.',
-        zip9matches5: 'The weight to use if, given two 9-digit zip codes, the first five digits match and the last four do not. To add a weight when all nine digits match, use the Exact matching type to compare the zip codes as strings.',
-        propsReduce: 'One or more properties whose values to compare.',
-        weightReduce: 'A positive integer that denotes how much to reduce the weight of a match.',
+        weightOption: 'A factor signifying the relative importance of the rule.',
+        thesaurusURI: 'The path to the thesaurus used to determine synonyms.',
+        filter: 'A node in the thesaurus to use as a filter. Example: <thsr:qualifier>birds</thsr:qualifier>',
+        dictionaryURI: 'The path to the dictionary used to compare words phonetically.',
+        distanceThresh: 'The phonetic distance below which two strings are considered similar.',
+        collation: 'The URI for a collation, which specifies the order for sorting strings.',
+        zip5matches9: 'The weight to add to the exact match weight when a 5-digit zip code matches the first five digits of a 9-digit zip code.',
+        zip9matches5: 'The weight to apply when the first five digits of a 9-digit zip code match a 5-digit zip code.',
+        propsReduce: 'One or more properties to compare.',
+        weightReduce: 'How much to reduce the weight when all compared properties match.',
         customURI: 'The path to the custom module.',
         customFunc: 'The name of the custom function within the custom module.',
-        customNS: 'The namespace of the library module where the custom function is. Blank, if the custom function is JavaScript code.',
-        weightThreshold: 'The threshold with which to compare the total weight of the matches.'
+        customNS: 'The namespace of the custom module. Leave blank if the custom module is JavaScript.',
+        weightThreshold: 'The threshold the sum of match weights must meet for documents to be considered a match.',
+        actionThreshold: 'The action to take when the threshold is met.'
     },
 
     merging: {
