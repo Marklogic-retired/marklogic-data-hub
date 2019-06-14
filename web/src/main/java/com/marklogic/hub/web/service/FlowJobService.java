d3398f51c3a46ded6c77d6a7339631a2a1db3c17
Hao Liu <hao.liu@marklogic.com>
2019-06-14 16:19:03 -0700

    DHFPROD-2587: Reduce latency of getFlows API call on the backend server

--- web/src/main/java/com/marklogic/hub/web/service/FlowJobService.java
+++ web/src/main/java/com/marklogic/hub/web/service/FlowJobService.java
@@ -1,4 +1,21 @@
+/*
+ * Copyright 2012-2019 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.marklogic.hub.web.service;
+
+import static com.marklogic.hub.job.JobStatus.RUNNING_PREFIX;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.cache.Cache;
@@ -11,20 +28,23 @@ import com.marklogic.client.io.Format;
 import com.marklogic.client.io.JacksonHandle;
 import com.marklogic.client.io.StringHandle;
 import com.marklogic.client.util.RequestParameters;
-import com.marklogic.hub.FlowManager;
 import com.marklogic.hub.flow.Flow;
 import com.marklogic.hub.flow.impl.FlowRunnerImpl;
 import com.marklogic.hub.impl.HubConfigImpl;
 import com.marklogic.hub.job.JobStatus;
 import com.marklogic.hub.step.impl.Step;
 import com.marklogic.hub.util.json.JSONObject;
+import com.marklogic.hub.util.metrics.tracer.JaegerConfig;
 import com.marklogic.hub.web.model.FlowJobModel.FlowJobs;
 import com.marklogic.hub.web.model.FlowJobModel.LatestJob;
+import io.opentracing.Scope;
+import io.opentracing.Span;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+import javax.annotation.PreDestroy;
 import javax.xml.bind.DatatypeConverter;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -32,16 +52,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import static com.marklogic.hub.job.JobStatus.RUNNING_PREFIX;
-
 @Service
 public class FlowJobService extends ResourceManager {
     private static final String ML_JOBS_NAME = "ml:jobs";
 
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    @Autowired
-    private FlowManager flowManager;
 
     @Autowired
     private FlowRunnerImpl flowRunner;
@@ -64,47 +79,101 @@ public class FlowJobService extends ResourceManager {
 
     private void setupClient() {
         client = hubConfig.newJobDbClient();
+        client.init(ML_JOBS_NAME, this);
     }
 
-    public FlowJobs getJobs(String flowName, boolean forceRefresh) {
+    @PreDestroy
+    public void destroy() {
+        logger.info("release the job database client.");
+        this.release();
+    }
+
+    public FlowJobs getJobs(Flow flow, boolean forceRefresh, Span parentSpan) {
+        String flowName = flow.getName();
         if (forceRefresh) {
-            FlowJobs flowJobs = retrieveJobsByFlowName(flowName);
+            FlowJobs flowJobs = retrieveJobsByFlowName(flow, parentSpan);
             cachedJobsByFlowName.put(flowName, flowJobs);
             return flowJobs;
         }
-        try {
-            return cachedJobsByFlowName.get(flowName, () -> retrieveJobsByFlowName(flowName));
+
+        Span span = JaegerConfig.buildSpanFromMethod(new Object() {}, parentSpan)
+            .withTag("flowName", flowName)
+            .start();
+
+        try (Scope ignored = JaegerConfig.activate(span)) {
+            return cachedJobsByFlowName.get(flowName, () -> retrieveJobsByFlowName(flow, span));
         } catch (Exception e) {
             logger.error(e.getMessage());
         } finally {
             if (cachedJobsByFlowName.getIfPresent(flowName) == null) {
                 cachedJobsByFlowName.invalidate(flowName);
             }
+            span.finish();
         }
 
         return cachedJobsByFlowName.getIfPresent(flowName);
     }
 
-    private FlowJobs retrieveJobsByFlowName(String flowName) {
-        if (client == null) {
-            setupClient();
+    private FlowJobs retrieveJobsByFlowName(Flow flow, Span parentSpan) {
+        String flowName = flow.getName();
+        Span span = JaegerConfig.buildSpanFromMethod(new Object() {
+        }, parentSpan)
+            .withTag("flowName", flowName)
+            .start();
+
+        JsonNode jsonNode;
+        try (Scope ignored = JaegerConfig.activate(span)) {
+            span.setTag("setupClient", true);
+            span.setTag("beforeInitialClient",
+                client == null ? "client is null" : "client is not null");
+            if (client == null) {
+                setupClient();
+            }
+            span.setTag("AfterInitialClient", client.getClientImplementation().toString());
+        } catch (Exception e) {
+            logger.error("Failed to init client!!!");
+        } finally {
+            span.finish();
         }
-        client.init(ML_JOBS_NAME, this);
-        RequestParameters params = new RequestParameters();
-        if (StringUtils.isNotEmpty(flowName)) {
-            params.add("flow-name", flowName);
+        Span span2 = JaegerConfig.buildSpanFromMethod(new Object() {
+        }, parentSpan)
+            .withTag("flowName", flowName)
+            .start();
+
+        ServiceResultIterator resultItr;
+        try (Scope ignored = JaegerConfig.activate(span2)) {
+            span2.setTag("getJobCall", true);
+            RequestParameters params = new RequestParameters();
+            if (StringUtils.isNotEmpty(flowName)) {
+                params.add("flow-name", flowName);
+            }
+
+            resultItr = this.getServices().get(params);
+            if (resultItr == null || !resultItr.hasNext()) {
+                throw new RuntimeException("No jobs found for flow with name: " + flowName);
+            }
+        } finally {
+            span2.finish();
         }
 
-        ServiceResultIterator resultItr = this.getServices().get(params);
-        if (resultItr == null || !resultItr.hasNext()) {
-            throw new RuntimeException("No jobs found for flow with name: " + flowName);
-        }
-        ServiceResult res = resultItr.next();
-        JsonNode jsonNode = res.getContent(new JacksonHandle()).get();
+        Span span3 = JaegerConfig.buildSpanFromMethod(new Object() {
+        }, parentSpan)
+            .withTag("flowName", flowName)
+            .start();
 
-        Flow flow = flowManager.getFlow(flowName);
+        try (Scope ignored = JaegerConfig.activate(span3)) {
+            span3.setTag("getContent", true);
+            ServiceResult res = resultItr.next();
+            jsonNode = res.getContent(new JacksonHandle()).get();
+        } finally {
+            span3.finish();
+        }
+
+        return retrieveJobsByFlowName(flow, jsonNode);
+    }
+
+    private FlowJobs retrieveJobsByFlowName(Flow flow, JsonNode jsonNode) {
         Map<String, Step> steps = flow.getSteps();
-
         List<String> jobs = new ArrayList<>();
         LatestJob latestJob = new LatestJob();
         FlowJobs flowJobs = new FlowJobs(jobs, latestJob);
@@ -193,7 +262,6 @@ public class FlowJobService extends ResourceManager {
             updateJobStaleStates(staleStateJobIds);
         }
 
-        this.release();
         return flowJobs;
     }
 
--- web/src/main/java/com/marklogic/hub/web/web/NewJobController.java
+++ web/src/main/java/com/marklogic/hub/web/web/NewJobController.java
@@ -1,15 +1,22 @@
 package com.marklogic.hub.web.web;
 
+import com.marklogic.hub.util.metrics.tracer.JaegerConfig;
 import com.marklogic.hub.web.exception.DataHubException;
 import com.marklogic.hub.web.model.JobModel;
 import com.marklogic.hub.web.service.NewJobService;
+import io.opentracing.Scope;
+import io.opentracing.Span;
 import java.io.IOException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value="/api/jobs")
@@ -22,12 +29,16 @@ public class NewJobController {
     @ResponseBody
     public ResponseEntity<?> getJobs(@RequestParam(value = "flowName", required = false) String flowName) throws IOException {
         List<JobModel> jobModels;
-        try {
+        Span span = JaegerConfig.buildSpan("getJobs").start();
+        try (Scope ignored = JaegerConfig.activate(span)) {
             jobService.setupClient();
             jobModels = jobService.getJobs(flowName);
+            span.setTag("flowName", flowName != null ? flowName : "");
             jobService.release();
         } catch (Exception ex) {
             throw new DataHubException(ex.getMessage(), ex);
+        } finally {
+            span.finish();
         }
         return new ResponseEntity<>(jobModels, HttpStatus.OK);
     }
