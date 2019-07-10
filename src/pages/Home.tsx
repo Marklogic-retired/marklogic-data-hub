526c4a37e2b4c0ef8c873e75a1bbd14c9cccb573
Bruce An <Bruce.An@marklogic.com>
2019-07-10 16:18:04 -0700

    setup apollo client, old mock api and npm scripts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,63 @@
 import React from 'react';
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
 import Counter from '../components/test';
 
+interface Data {
+  getEntityByTitle: {
+    title: string,
+    baseUri: string,
+    entityDefinitions: Array<{ name: string, properties: any }>;
+  }
+};
+
+interface Variables {
+  title: string;
+};
+
+
+const GET_ENTITY = gql`
+  query getEntityModel($title: String!) { 
+    getEntityByTitle(title: $title){
+      title
+      baseUri
+      entityDefinitions {
+        name
+        properties {
+          name
+          description
+          datatype
+        }
+      }
+    }
+  }
+  `;
+
 const Home: React.FC = () => {
+  const title = 'Race';
   return (
     <div>
       Home View
       <Counter initial={10}/>
+      <Query<Data, Variables> query={GET_ENTITY} variables={{ title }}>
+        {({ loading, error, data }) => {
+          if (loading) return "Loading...";
+          if (error) return `Error! ${error.message}`;
+    
+          if (data !== undefined) {
+            console.log('data response', data);
+            return (
+              <div>
+                <h1>{data.getEntityByTitle.title}</h1>
+              </div>
+            );
+          }
+        }}
+      </Query> 
     </div>
   );
 }
+
+
 
 export default Home;
