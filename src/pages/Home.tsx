0bb966d2cc030f637d89640210fb37b056cc1ea0
Brian Tang <briantang98@gmail.com>
2019-08-05 16:30:38 -0700

    UI tweaks to landing page and header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import LoginForm from '../components/login-form/login-form';
+import DatahubIcon from '../components/datahub-icon/datahub-icon';
+import styles from './Home.module.scss';
+import {Row, Col, Typography} from 'antd';
+
 
 type Props = { checkLogin: any};
+const {Title} = Typography;
+
 const Home: React.FC<Props> = props => {
   return (
     <>
-    <LoginForm checkLogin={props.checkLogin}/>    
+      
+      <div className={styles.icon}>
+        <DatahubIcon size={890} fill='lightgrey' view='0 0 55 55'/>
+      </div>   
+      
+      <Row className={styles.grid}>
+        <Col span={12} offset={3}><Title level={2}>Sign In</Title><LoginForm checkLogin={props.checkLogin}/></Col>
+      </Row>
+  
     </>
   );
 }
