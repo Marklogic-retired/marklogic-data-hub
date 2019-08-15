2d22e91d1b4d94b5f1eabc2d1d9dfe19cad64842
ayuwono <ayuwono@marklogic.com>
2019-08-15 12:18:54 -0700

    added search bar and pagination

--- web/src/main/explorer-ui/src/pages/Browse.tsx
+++ web/src/main/explorer-ui/src/pages/Browse.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Sidebar from '../components/sidebar/sidebar';
+import SearchBar from '../components/search-bar/search-bar';
+import SearchPagination from '../components/search-pagination/search-pagination';
 
 const Browse: React.FC = () => {
   return (
     <div>
       <Sidebar />
+      <SearchBar />
+      <SearchPagination />
     </div>
   );
 }
-
-
 
 export default Browse;
