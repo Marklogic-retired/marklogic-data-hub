3a639ff41af4e0dffd6ebf64edaef35c5d47ca15
Nikhil Shrivastava <nikhil.shrivastava@marklogic.com>
2019-08-14 17:03:03 -0700

    Fixing the step color issue in Manage flows pane.

--- web/src/main/ui/app/components/flows-new/manage-flows/ui/step-icons-ui.component.ts
+++ web/src/main/ui/app/components/flows-new/manage-flows/ui/step-icons-ui.component.ts
@@ -8,35 +8,5 @@ import {StepType} from '../../models/step.model';
 })
 export class StepIconsUiComponent {
   @Input() steps: Array<Object> = new Array<Object>();
-
-  createStepHeader(step: any): string {
-    if (step.stepDefinitionType === StepType.INGESTION){
-      if(step.stepDefinitionName === 'default-ingestion'){
-        return 'INGESTION';
-      }
-      else{
-        return 'CUSTOM';
-      }
-    }
-    else if (step.stepDefinitionType === StepType.MAPPING){
-      if(step.stepDefinitionName === 'default-mapping'){
-        return 'MAPPING';
-      }
-      else{
-        return 'CUSTOM';
-      }
-    }
-    else if (step.stepDefinitionType === StepType.MASTERING){
-      if(step.stepDefinitionName === 'default-mastering'){
-        return 'MASTERING';
-      }
-      else{
-        return 'CUSTOM';
-      }
-    }
-    else {
-        return 'CUSTOM';
-    }
-  }
-
+  
 }
