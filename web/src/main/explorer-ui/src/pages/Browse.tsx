1fc98185994e0f9b32808a48142e3f76316cfc79
Aries Yuwono <ayuwono@marklogic.com>
2019-08-19 13:50:26 -0700

    cleanup

--- web/src/main/explorer-ui/src/pages/Browse.tsx
+++ web/src/main/explorer-ui/src/pages/Browse.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Layout } from 'antd';
 import Sidebar from '../components/sidebar/sidebar';
 import SearchBar from '../components/search-bar/search-bar';
 import SearchPagination from '../components/search-pagination/search-pagination';
-import { Layout, Menu, Icon } from 'antd';
 import SearchSummary from '../components/search-summary/search-summary';
 import SearchResults from '../components/search-results/search-results';
 
 const Browse: React.FC = () => {
   const { Content, Sider } = Layout;
+
   return (
     <Layout>
       <Sider width={300} style={{ background: '#f3f3f3' }}>
