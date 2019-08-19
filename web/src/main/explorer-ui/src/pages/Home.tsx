99822b12eab361a8d3478a0495bcf16d600141bb
Timur Isangulov <timur.isangulov@marklogic.com>
2019-08-19 14:49:53 -0700

    added antd layout for home page

--- web/src/main/explorer-ui/src/pages/Home.tsx
+++ web/src/main/explorer-ui/src/pages/Home.tsx
@@ -2,32 +2,37 @@ import React from 'react';
 import LoginForm from '../components/login-form/login-form';
 import DatahubIcon from '../components/datahub-icon/datahub-icon';
 import styles from './Home.module.scss';
-import { Row, Col, Typography } from 'antd';
+import { Layout, Row, Col, Typography } from 'antd';
 
 const { Title, Text } = Typography;
 
 const Home: React.FC = () => {
+  const { Content, Footer } = Layout;
   return (
-    <>
+    <Layout className={styles.container}>
+      <Content>
       <div className={styles.icon}>
-        <DatahubIcon size={780} fill='lightgrey' view='0 0 60 50' />
+        <DatahubIcon size={760} fill='lightgrey' view='0 0 55 55' />
       </div>
-      <Row className={styles.grid}>
-        <Col span={12} offset={3}>
-          <Title level={2}>Sign In</Title>
-          <LoginForm />
-          <div className={styles.policy}>
-            <Text type="secondary">
-              We're committed to processing your personal data in{<br />}
-              compliance with our <a href="https://www.marklogic.com/privacy/" target="_blank">Privacy Statement</a> while providing{<br />}
-              you with transparent notice about our practices.</Text>
-          </div>
-        </Col>
-      </Row>
-      <div className={styles.copyright}>
-        <Text type="secondary">Copyright @ 2019 MarkLogic Corporation. All Rights Reserved | <a href="https://s3-us-west-2.amazonaws.com/marklogic-services-resources/legal/ServiceTerms.pdf" target="_blank">Terms and Conditions</a> | <a href="https://www.marklogic.com/privacy/" target="_blank">Policies</a></Text>
-      </div>
-    </>
+        <Row className={styles.grid}>
+          <Col span={12} offset={3}>
+            <Title level={2}>Sign In</Title>
+            <LoginForm />
+            <div className={styles.policy}>
+              <Text type="secondary">
+                We're committed to processing your personal data in{<br />}
+                compliance with our <a href="https://www.marklogic.com/privacy/" target="_blank">Privacy Statement</a> while providing{<br />}
+                you with transparent notice about our practices.</Text>
+            </div>
+          </Col>
+        </Row>
+      </Content>
+      <Footer className={styles.footer}>
+        <div className={styles.copyright}>
+          <Text type="secondary">Copyright @ 2019 MarkLogic Corporation. All Rights Reserved | <a href="https://s3-us-west-2.amazonaws.com/marklogic-services-resources/legal/ServiceTerms.pdf" target="_blank">Terms and Conditions</a> | <a href="https://www.marklogic.com/privacy/" target="_blank">Policies</a></Text>
+        </div>
+      </Footer>
+    </Layout>
   );
 }
 
