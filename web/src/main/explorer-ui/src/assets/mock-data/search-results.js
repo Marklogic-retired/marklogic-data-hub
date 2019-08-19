9a35c25d2aa9e0e6881cc8c5312be4300962ce51
Aries Yuwono <ayuwono@marklogic.com>
2019-08-19 13:48:19 -0700

    add mock data for search results

---
+++ web/src/main/explorer-ui/src/assets/mock-data/search-results.js
@@ -0,0 +1,95 @@
+const searchResults = 
+    [
+        {
+            collection: 'Customer',
+            id: 123,
+            created: '2019-09-09T12:03:07.665187-07:00',
+            source: 'AdvantageFlow',
+            fileType: 'JSON',
+            user: 'admin',
+            content: 'John Smith 28 124 Cascade Blvd San Franscisco CA 98765 655-907-8899 655-887-9898 john.smith@hellomail.com'
+        },
+        {
+            collection: 'Customer',
+            id: 234,
+            created: '2019-06-19T12:03:07.665187-07:00',
+            source: 'AdvantageFlow',
+            fileType: 'JSON',
+            user: 'admin',
+            content: 'Mary Lou'
+        },
+        {
+            collection: 'MapCust',
+            id: 1002,
+            created: '2005-03-22T12:03:07.665187-07:00',
+            source: 'BedrockFlow',
+            fileType: 'CSV',
+            user: 'admin',
+            content: 'Some data here...'
+        },
+        {
+            collection: 'Product',
+            id: 342,
+            created: '2009-03-12T12:03:07.665187-07:00',
+            source: 'ProdFlow',
+            fileType: 'XML',
+            user: 'admin',
+            content: 'Andrew Deer'
+        },
+        {
+            collection: 'Product',
+            id: 5678,
+            created: '2019-09-09T12:03:07.665187-07:00',
+            source: 'TestFlow',
+            fileType: 'JSON',
+            user: 'admin',
+            content: 'Test test test'
+        },
+        {
+            collection: 'Customer',
+            id: 9008,
+            created: '2019-11-13T12:03:07.665187-07:00',
+            source: 'AdvantageFlow',
+            fileType: 'JSON',
+            user: 'admin',
+            content: 'DeAndre Gold'
+        },
+        {
+            collection: 'MapProd',
+            id: 2377,
+            created: '2013-05-30T12:03:07.665187-07:00',
+            source: 'Prod2Flow',
+            fileType: 'XML',
+            user: 'admin',
+            content: 'This is the product for...'
+        },
+        {
+            collection: 'Product',
+            id: 4566,
+            created: '2014-03-02T12:03:07.665187-07:00',
+            source: 'MyProdFlow',
+            fileType: 'CSV',
+            user: 'admin',
+            content: 'Air Conditioned'
+        },
+        {
+            collection: 'Customer',
+            id: 123,
+            created: '2019-09-11T12:03:07.665187-07:00',
+            source: 'AdvantageFlow',
+            fileType: 'JSON',
+            user: 'admin',
+            content: 'Stephanie Laurie'
+        },
+        {
+            collection: 'MapCust',
+            id: 123,
+            created: '2014-03-23T12:03:07.665187-07:00',
+            source: 'MappingCustFlow',
+            fileType: 'CSV',
+            user: 'admin',
+            content: 'Mapping to be done here'
+        }
+    ];
+
+export default searchResults;
