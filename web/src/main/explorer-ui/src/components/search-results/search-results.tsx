21d8aaf99f24c87bba72562bb27a59034ce3ee6c
Aries Yuwono <ayuwono@marklogic.com>
2019-08-22 09:17:54 -0700

    passing props uri and hardcoded db name, implement Descriptions Item

--- web/src/main/explorer-ui/src/components/search-results/search-results.tsx
+++ web/src/main/explorer-ui/src/components/search-results/search-results.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { List } from 'antd';
+import { List, Descriptions } from 'antd';
 import styles from './search-results.module.scss';
 
 type Props = {
@@ -18,16 +18,21 @@ const SearchResults:React.FC<Props> = (props) => {
                 renderItem={item => (
                     <List.Item>
                         <List.Item.Meta
-                            title={<Link to="/detail">{"Customer > uri: " + item.uri}</Link>}
+                            title={
+                                <Link to={{ pathname: "/detail", state: { uri: item.uri, database: "data-hub-FINAL" }}}>
+                                    {"Customer > uri: " + item.uri}
+                                </Link>
+                            }
                             description={
-                                <p>
-                                    <label className={styles.label}>Created: </label>{"2019-2019-09-09T12:03:07.665187-07:00-05"}
-                                    <label className={styles.label}>&nbsp; &nbsp; Source: </label>{"AdvantageFlow"}
-                                    <label className={styles.label}>&nbsp; &nbsp; File Type: </label>{item.format}
-                                    <label className={styles.label}>&nbsp; &nbsp; User: </label>{"admin"}
+                                <Descriptions title="Content">
+                                    <Descriptions.Item label="Created" className={styles.label}>2019-09-09</Descriptions.Item>
+                                    <Descriptions.Item label="Source" className={styles.label}>AdvantageFlow</Descriptions.Item>
+                                    <Descriptions.Item label="File Type" className={styles.label}>{item.format}</Descriptions.Item>
+                                    <Descriptions.Item label="User" className={styles.label}>admin</Descriptions.Item>
                                     <br />
-                                    {"Lorem ipsum dolor sit amet, eos ei utamur scriptorem, omnesque efficiendi interesset vis an. Illud ullum vim te, sit atqui dolore cu, vix te modus lorem sadipscing."}
-                                </p>
+                                    <br />
+                                    <Descriptions.Item span={3}>Lorem ipsum dolor sit amet, eos ei utamur scriptorem, omnesque efficiendi interesset vis an. Illud ullum vim te, sit atqui dolore cu, vix te modus lorem sadipscing.</Descriptions.Item>
+                                </Descriptions>
                             }
                         />
                     </List.Item>
