3087db5576e71b02c26e0f89b1e645ff28682bd5
Hao Liu <hao.liu@marklogic.com>
2019-08-29 17:46:53 -0700

    Initial endpoints for Entitiy Model

---
+++ explorer/src/main/java/com/marklogic/hub/explorer/web/ModelController.java
@@ -0,0 +1,35 @@
+package com.marklogic.hub.explorer.web;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.marklogic.hub.explorer.service.ModelService;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("datahub/v2/models")
+public class ModelController {
+
+    @Autowired
+    protected ModelService modelService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<?> getModels() throws IOException {
+        JsonNode json = modelService.getModels();
+        return new ResponseEntity<>(json, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{modelName}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<?> getModel(@PathVariable String modelName) throws IOException {
+        String json = modelService.getModel(modelName);
+        return new ResponseEntity<>(json, HttpStatus.OK);
+    }
+}
