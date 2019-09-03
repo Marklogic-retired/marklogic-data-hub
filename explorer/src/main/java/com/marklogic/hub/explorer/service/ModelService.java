32414cb2d675180ec2fa97b81e8c1c65bb5d57e3
Hao Liu <hao.liu@marklogic.com>
2019-09-03 16:36:45 -0700

    Simplify retrieve entity models

--- explorer/src/main/java/com/marklogic/hub/explorer/service/ModelService.java
+++ explorer/src/main/java/com/marklogic/hub/explorer/service/ModelService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.document.DocumentRecord;
 import com.marklogic.client.document.GenericDocumentManager;
 import com.marklogic.client.io.Format;
 import com.marklogic.client.io.StringHandle;
@@ -11,11 +12,9 @@ import com.marklogic.client.query.QueryManager;
 import com.marklogic.client.query.StructuredQueryBuilder;
 import com.marklogic.client.query.StructuredQueryDefinition;
 import com.marklogic.hub.explorer.util.DatabaseClientHolder;
-import com.marklogic.hub.util.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -35,45 +34,22 @@ public class ModelService {
     public JsonNode getModels() throws IOException {
         DatabaseClient dbClient = dbClientHolder.getDatabaseClient();
         QueryManager queryMgr = dbClient.newQueryManager();
-
         queryMgr.setPageLength(Integer.MAX_VALUE);
         StructuredQueryBuilder sb = queryMgr.newStructuredQueryBuilder("default");
 
         StringHandle sh = new StringHandle();
         sh.setFormat(Format.JSON);
-        String res = queryMgr.search(sb.collection(ENTITY_MODEL_COLLECTION_NAME), sh, 0).get();
-
         JsonNode jsonRes = JsonNodeFactory.instance.arrayNode();
-        if (StringUtils.isEmpty(res)) {
-            return jsonRes;
-        }
-
-        JSONObject json = new JSONObject(res);
-        JsonNode results = json.getNode("results");
-        if (results == null) {
-            return jsonRes;
-        }
-
-        List<String> uris = new ArrayList<>();
-        if (results.isArray()) {
-            results.forEach(e -> {
-                String uri = e.get("uri").asText(null);
-                if (StringUtils.isNotEmpty(uri)) {
-                    uris.add(uri);
-                }
-            });
-        } else {
-            return jsonRes;
-        }
-        if (uris.isEmpty()) {
-            return jsonRes;
-        }
 
         GenericDocumentManager docMgr = dbClient.newDocumentManager();
-        for (String uri : uris) {
-            String content = docMgr.readAs(uri, String.class);
-            ((ArrayNode) jsonRes).add(content);
+        List<DocumentRecord> documentRecords = new ArrayList<>();
+        docMgr.search(sb.collection(ENTITY_MODEL_COLLECTION_NAME), 0 , sh).forEach(documentRecords::add);
+        StringHandle handle = new StringHandle();
+        for (DocumentRecord record : documentRecords) {
+            record.getContent(handle);
+            ((ArrayNode) jsonRes).add(handle.get());
         }
+
         return jsonRes;
     }
 
@@ -93,26 +69,17 @@ public class ModelService {
 
         StringHandle sh = new StringHandle();
         sh.setFormat(Format.JSON);
-        String res = queryMgr.search(sbd, sh, 0).get();
-        if (StringUtils.isEmpty(res)) {
-            throw new RuntimeException("No entity " + modelName);
-        }
-        JSONObject json = new JSONObject(res);
-        JsonNode results = json.getNode("results");
-        if (results == null) {
-            throw new RuntimeException("No entity " + modelName);
-        }
-        final String[] uri = new String[1];
-        if (results.isArray()) {
-            results.forEach(e -> {
-                JSONObject jobJson = new JSONObject(e);
-                uri[0] = e.get("uri") != null ? e.get("uri").asText() : null;
-                return;
-            });
-        }
-        
+
         GenericDocumentManager docMgr = dbClient.newDocumentManager();
-        return docMgr.readAs(uri[0], String.class);
+        List<DocumentRecord> documentRecords = new ArrayList<>();
+        docMgr.search(sbd, 0 , sh).forEach(documentRecords::add);
+
+        StringHandle handle = new StringHandle();
+        if (!documentRecords.isEmpty()) {
+            documentRecords.get(0).getContent(handle);
+        }
+
+        return handle.get();
     }
 }
 
