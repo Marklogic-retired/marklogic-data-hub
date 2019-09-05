e83fd32994847e37451693f1284616ee90fce2fd
rahulvudutala <rahulbharadwaj.v@gmail.com>
2019-09-05 14:46:10 -0700

    DHFPROD-3042: Adding orQuery constraint on facetValues for each facetType

--- explorer/src/main/java/com/marklogic/hub/explorer/service/SearchService.java
+++ explorer/src/main/java/com/marklogic/hub/explorer/service/SearchService.java
@@ -8,6 +8,7 @@ import com.marklogic.client.query.StructuredQueryBuilder;
 import com.marklogic.client.query.StructuredQueryDefinition;
 import com.marklogic.hub.explorer.model.SearchQuery;
 import com.marklogic.hub.explorer.util.DatabaseClientHolder;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
@@ -18,7 +19,7 @@ import java.util.List;
 @Service
 public class SearchService {
 
-    private static final String SEARCH_QUERY_OPTIONS = "final-entity-options";
+    private static final String QUERY_OPTIONS = "final-entity-options";
 
     @Autowired
     private DatabaseClientHolder databaseClientHolder;
@@ -29,36 +30,38 @@ public class SearchService {
         QueryManager queryMgr = client.newQueryManager();
         queryMgr.setPageLength(searchQuery.getPageLength());
 
-        StructuredQueryBuilder searchQueryBuilder = queryMgr.newStructuredQueryBuilder(SEARCH_QUERY_OPTIONS);
+        StructuredQueryBuilder queryBuilder = queryMgr.newStructuredQueryBuilder(QUERY_OPTIONS);
 
         // Creating queries object
         List<StructuredQueryDefinition> queries = new ArrayList<>();
 
         // Filtering search results for docs related to an entity
-        if (! CollectionUtils.isEmpty(searchQuery.getEntityNames())) {
+        if (!CollectionUtils.isEmpty(searchQuery.getEntityNames())) {
             String[] collections = searchQuery.getEntityNames().toArray(new String[0]);
-            queries.add(searchQueryBuilder.collection(collections));
+            queries.add(queryBuilder.collection(collections));
         }
 
         // Filtering by facets
         if (searchQuery.getFacets() != null) {
-            searchQuery.getFacets().forEach((facetType, facetValues) -> facetValues.forEach(facetValue -> {
+            searchQuery.getFacets().forEach((facetType, facetValues) -> {
                 StructuredQueryDefinition facetDef = null;
-                if (facetType.equals("Collection")) {
-                    facetDef = searchQueryBuilder.collectionConstraint(facetType, facetValue);
-                } else {
-                    facetDef = searchQueryBuilder.rangeConstraint(facetType, StructuredQueryBuilder.Operator.EQ, facetValue);
-                }
+                facetDef = facetType.equals("Collection") ? queryBuilder.collectionConstraint(facetType,
+                    facetValues.toArray(new String[0])) : queryBuilder.rangeConstraint(facetType,
+                    StructuredQueryBuilder.Operator.EQ, facetValues.toArray(new String[0]));
 
                 if (facetDef != null) {
                     queries.add(facetDef);
                 }
-            }));
+            });
         }
 
         // And between all the queries
-        StructuredQueryDefinition finalQueryDef = searchQueryBuilder.and(queries.toArray(new StructuredQueryDefinition[0]));
-        finalQueryDef.setCriteria(searchQuery.getQuery());
+        StructuredQueryDefinition finalQueryDef = queryBuilder.and(queries.toArray(new StructuredQueryDefinition[0]));
+
+        // Setting search string if provided by user
+        if(StringUtils.isNotEmpty(searchQuery.getQuery())) {
+            finalQueryDef.setCriteria(searchQuery.getQuery());
+        }
 
         // Setting criteria and searching
         StringHandle resultHandle = new StringHandle();
