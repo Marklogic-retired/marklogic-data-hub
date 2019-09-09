48f322132d18692156bc2094910f0708becc5385
Aries Yuwono <ayuwono@marklogic.com>
2019-09-09 11:55:07 -0700

    handle 0 document found

--- explorer-ui/src/components/search-summary/search-summary.tsx
+++ explorer-ui/src/components/search-summary/search-summary.tsx
@@ -13,9 +13,17 @@ const SearchSummary = (props) => {
     }
   }
 
+  const isNoDocuments = () => {
+    if(props.total == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   return (
     <div className={styles.searchSummaryContainer}>
-      <Text>Showing</Text> <span className={styles.summaryValue}>{(props.start-1) * props.length + 1}-{isEndOfPage() ? props.total : props.start * props.length}</span> <Text>of</Text> <span className={styles.summaryValue}>{props.total}</span> <Text>documents</Text>
+      <Text>Showing</Text> <span className={styles.summaryValue}>{isNoDocuments() ? 0 : (props.start-1) * props.length + 1}-{isEndOfPage() ? props.total : props.start * props.length}</span> <Text>of</Text> <span className={styles.summaryValue}>{props.total}</span> <Text>documents</Text>
     </div>
   );
 }
