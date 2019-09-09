6969050da07cdc1e3a6e92bba8bf1c3d873035ec
rahulvudutala <rahulbharadwaj.v@gmail.com>
2019-09-09 16:02:08 -0700

    Removing wildcard imports

--- explorer/src/main/java/com/marklogic/hub/explorer/web/SearchController.java
+++ explorer/src/main/java/com/marklogic/hub/explorer/web/SearchController.java
@@ -9,7 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 @RequestMapping(value = "/v2/search")
