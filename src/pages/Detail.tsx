c709ba83c34b4b60730d4ce0196d1bccad776fb4
Timur Isangulov <timur.isangulov@marklogic.com>
2019-09-13 11:08:14 -0700

    tmp commit

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, Component } from 'react';
 import axios from 'axios';
 import { RouteComponentProps, Link } from 'react-router-dom';
 import { AuthContext } from '../util/auth-context';
@@ -7,6 +7,8 @@ import TableView from '../components/table-view/table-view';
 import JsonView from '../components/json-view/json-view';
 import DocumentHeader from '../components/detail-header/detail-header';
 import { Layout, Menu, PageHeader, Spin } from 'antd';
+
+import XmlView from '../components/xml-view/xml-view';
 
 interface Props extends RouteComponentProps<any> { }
 
@@ -18,6 +20,7 @@ const Detail: React.FC<Props> = ({ history }) => {
   const [data, setData] = useState();
   const [query, setQuery] = useState(history.location.state.uri);
   const [isLoading, setIsLoading] = useState(false);
+  const [contentType, setContentType] = useState();
 
   let database = history.location.state.database;
 
@@ -30,6 +33,16 @@ const Detail: React.FC<Props> = ({ history }) => {
         );
 
         setData(JSON.parse(result.data.content));
+        const contentType = result.headers.get("content-type");
+        if (contentType && contentType.indexOf("application/json") !== -1) {
+          setContentType('json')
+        } else if (contentType && contentType.indexOf("application/xml") !== -1) {
+          setContentType('xml')
+        }
+
+        console.log('CONTENT IS ', contentType)
+
+        setData(result.data);
         setIsLoading(false);
       } catch (error) {
         // console.log('error', error.response);
@@ -44,6 +57,36 @@ const Detail: React.FC<Props> = ({ history }) => {
 
   const handleClick = (event) => {
     setSelected(event.key);
+  }
+
+  const getXmlObject = (xmlData) => {
+    var parser = require('fast-xml-parser');
+    var he = require('he');
+
+    var options = {
+      attributeNamePrefix : "@_",
+      attrNodeName: "attr", //default is 'false'
+      textNodeName : "#text",
+      ignoreAttributes : true,
+      ignoreNameSpace : false,
+      allowBooleanAttributes : false,
+      parseNodeValue : true,
+      parseAttributeValue : false,
+      trimValues: true,
+      cdataTagName: "__cdata", //default is 'false'
+      cdataPositionChar: "\\c",
+      localeRange: "", //To support non english character in tag/attribute values.
+      parseTrueNumberOnly: false,
+      attrValueProcessor: a => he.decode(a, {isAttributeValue: true}),//default is a=>a
+      tagValueProcessor : a => he.decode(a) //default is a=>a
+  };
+
+    if( parser.validate(xmlData) === true) { 
+    var jsonObj = parser.parse(xmlData,options);
+
+    console.log(jsonObj);
+    return jsonObj;
+}
   }
 
   return (
@@ -71,8 +114,14 @@ const Detail: React.FC<Props> = ({ history }) => {
           {
             isLoading ? <Spin tip="Loading..." style={{ margin: '100px auto', width: '100%' }} />
               :
-              selected === 'instance' ? (data && <TableView document={data} />) : (data && <JsonView document={data} />)
-          }        </div>
+              // selected === 'instance' ? (data && <TableView document={data} />) : (data && <JsonView document={data} />)
+              // selected === 'instance' ? (data && <TableView document={data} />) : (data && <XmlView />)
+              contentType === 'json' ?
+                selected === 'instance' ? (data && <TableView document={data} />) : (data && <JsonView document={data} />)
+                :
+                selected === 'instance' ? (data && <TableView document={data} />) : (data && <XmlView document={data} />)
+          }
+        </div>
       </Content>
     </Layout>
   );
