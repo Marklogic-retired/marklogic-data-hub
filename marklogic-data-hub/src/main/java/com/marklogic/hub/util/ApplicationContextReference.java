afdc41909d17c97e93ef4e31ec7041fe22cf0726
ssambasu <ssambasu@marklogic.com>
2019-10-03 12:17:36 -0700

    Removing 'Versions' as class variable,test for case when it's null

--- marklogic-data-hub/src/main/java/com/marklogic/hub/util/ApplicationContextReference.java
+++ marklogic-data-hub/src/main/java/com/marklogic/hub/util/ApplicationContextReference.java
@@ -7,13 +7,21 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BeanUtil implements ApplicationContextAware {
+public class ApplicationContextReference implements ApplicationContextAware {
     private static ApplicationContext context;
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         context = applicationContext;
     }
+
+    public ApplicationContext getApplicationContext(){
+        return context;
+    }
+
     public static <T> T getBean(Class<T> beanClass) {
-        return context.getBean(beanClass);
+        if(context != null) {
+            return context.getBean(beanClass);
+        }
+        return null;
     }
 }
