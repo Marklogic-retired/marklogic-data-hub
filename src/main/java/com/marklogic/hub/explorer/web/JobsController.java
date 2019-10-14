9bfa6b264de1d8f0e4b52c8b8f5f217871bfc16a
Akshay Sonvane <akshay.sonvane93@gmail.com>
2019-10-14 10:36:08 -0700

    Fail silently and log if data-service module not found

--- src/main/java/com/marklogic/hub/explorer/web/JobsController.java
+++ src/main/java/com/marklogic/hub/explorer/web/JobsController.java
@@ -3,10 +3,15 @@
  */
 package com.marklogic.hub.explorer.web;
 
+import java.util.List;
+
+import com.marklogic.client.FailedRequestException;
 import com.marklogic.hub.explorer.service.JobsService;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,19 +25,42 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/v2/jobs")
 public class JobsController {
 
+  private static final Logger logger = LoggerFactory.getLogger(JobsController.class);
+
   @Autowired
   private JobsService jobsService;
 
   @RequestMapping(value = "/models", method = RequestMethod.GET)
   @ResponseBody
   public ResponseEntity<?> getLatestJobInfoForAllModels() {
-    return new ResponseEntity<>(jobsService.getLatestJobInfoForAllModels(), HttpStatus.OK);
+    List<JsonNode> modelList = null;
+    try {
+      modelList = jobsService.getLatestJobInfoForAllModels();
+    } catch (FailedRequestException e) {
+      /*
+       * As MarkLogic server throws a FailedRequestException for both data-service module not found
+       * and if the user is unauthorized to access the module; we need to fail silently and log the
+       * issue.
+       */
+      logger.error(e.getMessage());
+    }
+    return new ResponseEntity<>(modelList, HttpStatus.OK);
   }
 
   @RequestMapping(value = "/models/{modelName}", method = RequestMethod.GET)
   @ResponseBody
   public ResponseEntity<?> getLatestJobInfo(@PathVariable String modelName) {
-    JsonNode json = jobsService.getLatestJobInfo(modelName);
+    JsonNode json = null;
+    try {
+      json = jobsService.getLatestJobInfo(modelName);
+    } catch (FailedRequestException e) {
+      /*
+       * As MarkLogic server throws a FailedRequestException for both data-service module not found
+       * and if the user is unauthorized to access the module; we need to fail silently and log the
+       * issue.
+       */
+      logger.error(e.getMessage());
+    }
     return new ResponseEntity<>(json, HttpStatus.OK);
   }
 }
