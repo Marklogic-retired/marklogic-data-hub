5a6e28503242fe31e7e62500bbe3e5733cd5cc40
sanjeevani vishaka <sanjeevani.vishaka@gmail.com>
2019-10-22 17:33:11 -0700

    unit test for selected facets

---
+++ src/components/selected-facets/selected-facets.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SelectedFacets from './selected-facets';
+
+describe("Selected Facets", () => {
+    let wrapper;
+
+
+
+    it('Should show selected facets', () => {
+
+    })
+
+    it('Should clear all selected facets', () => {
+
+    })
+
+
+})
