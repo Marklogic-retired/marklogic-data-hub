23b52567f1d8593fc22f1a0d812ffdc625a95cb9
sanjeevani vishaka <sanjeevani.vishaka@gmail.com>
2019-10-28 14:03:33 -0700

    Indentation and spaces

--- src/pages/no-match-redirect.tsx
+++ src/pages/no-match-redirect.tsx
@@ -6,19 +6,19 @@ import { AuthContext } from '../util/auth-context';
 
 const NoMatchRedirect = ({history}) => {
 
-    const { user } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
-    const backToHomePage = () => {
-        return user.authenticated ? history.push('/view') : history.push('/');
-    }
-    return (
-        <Result
-            status="404"
-            title="404"
-            subTitle="Sorry, the page you visited does not exist."
-            extra={<Button type="primary" onClick={backToHomePage}>Back Home</Button>}
-        />
-    )
+  const backToHomePage = () => {
+    return user.authenticated ? history.push('/view') : history.push('/');
+  }
+  return (
+      <Result
+          status="404"
+          title="404"
+          subTitle="Sorry, the page you visited does not exist."
+          extra={<Button type="primary" onClick={backToHomePage}>Back Home</Button>}
+      />
+  )
 }
 
 export default withRouter(NoMatchRedirect);
