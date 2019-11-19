1b23b54e70b4c4cc8516de5ec03de5cd9f59bc27
Rob Rudin <rob.rudin@marklogic.com>
2019-11-18 21:36:35 -0500

    DHFPROD-3655: New flow chooses the right mapping based on ML version

--- marklogic-data-hub/src/test/java/com/marklogic/hub/impl/ScaffoldingImplTest.java
+++ marklogic-data-hub/src/test/java/com/marklogic/hub/impl/ScaffoldingImplTest.java
@@ -2,6 +2,7 @@ package com.marklogic.hub.impl;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -11,24 +12,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScaffoldingImplTest {
 
-    @Test
-    public void buildFlowFromDefaultFlow() throws Exception {
-        Map<String, String> customTokens = new HashMap<>();
+    private Map<String, String> customTokens;
+
+    @BeforeEach
+    public void setup() {
+        customTokens = new HashMap<>();
         customTokens.put("%%mlStagingDbName%%", "my-staging");
         customTokens.put("%%mlFinalDbName%%", "my-final");
         customTokens.put("%%mlFlowName%%", "my-flow");
+    }
 
-        String flow = new ScaffoldingImpl().buildFlowFromDefaultFlow(customTokens);
+    @Test
+    public void buildFlowFromDefaultFlow() throws Exception {
+        String flow = new ScaffoldingImpl().buildFlowFromDefaultFlow(customTokens, true);
         JsonNode json = new ObjectMapper().readTree(flow);
 
         assertEquals("my-flow", json.get("name").asText());
 
-        JsonNode mappingOptions = json.get("steps").get("2").get("options");
+        JsonNode stepTwo = json.get("steps").get("2");
+        assertEquals("entity-services-mapping", stepTwo.get("stepDefinitionName").asText());
+
+        JsonNode mappingOptions = stepTwo.get("options");
         assertEquals("my-staging", mappingOptions.get("sourceDatabase").asText());
         assertEquals("my-final", mappingOptions.get("targetDatabase").asText());
         assertEquals(false, mappingOptions.get("validateEntity").asBoolean(),
             "Per DHFPROD-2811, we now want to scaffold out a mapping step with " +
                 "validateEntity:false. That way, entity validation is not turned on, but a " +
                 "user can see that the feature is available.");
+    }
+
+    @Test
+    public void buildFlowWhenNotSupportingEntityServicesMapping() throws Exception {
+        String flow = new ScaffoldingImpl().buildFlowFromDefaultFlow(customTokens, false);
+        JsonNode json = new ObjectMapper().readTree(flow);
+
+        JsonNode stepTwo = json.get("steps").get("2");
+        assertEquals("default-mapping", stepTwo.get("stepDefinitionName").asText(),
+            "When ES mapping is not supported, the mapping step should use the 'default-mapping' step definition");
     }
 }
