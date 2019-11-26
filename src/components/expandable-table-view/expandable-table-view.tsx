3839f375fd7a0bc38c004e35bd053719fd6d4e5e
sanjeevani vishaka <sanjeevani.vishaka@gmail.com>
2019-11-26 09:50:35 -0800

    header for nested entities

--- src/components/expandable-table-view/expandable-table-view.tsx
+++ src/components/expandable-table-view/expandable-table-view.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table } from 'antd';
+import { Table, Icon } from 'antd';
 import { xmlParser } from "../../util/xml-parser";
 
 interface Props {
@@ -15,9 +15,9 @@ const ExpandableTableView: React.FC<Props> = (props) => {
     let parsedData = new Array();
     for (var i in obj) {
       if (obj[i] !== null && typeof (obj[i]) === "object") {
-        parsedData.push({ key: counter++, property: i, children: parseJson(obj[i]) });
+        parsedData.push({key: counter++, property: i, children: obj[i]});
       } else {
-        parsedData.push({ key: counter++, property: i, value: typeof obj[i] === 'boolean' ? obj[i].toString() : obj[i] });
+        parsedData.push({key: counter++, property: i, value: typeof obj[i] === 'boolean' ? obj[i].toString() : obj[i]});
       }
     }
     return parsedData;
@@ -39,6 +39,43 @@ const ExpandableTableView: React.FC<Props> = (props) => {
     })
   }
 
+  function expandIcon({expanded, expandable, record, onExpand}) {
+    if (!expandable || record.hasOwnProperty('children') === false) return null;
+
+    return (
+        <a style={{color:'black'}} onClick={e => onExpand(record, e)}>
+          {expanded ? <Icon type="down"/> : <Icon type="right"/>}
+        </a>
+    );
+  }
+
+  const expandedRowRender = (propertyValues) => {
+    let data: any[] = [];
+    const columns = [
+      {title: 'Property', dataIndex: 'property', width: '20%'},
+      {title: 'Value', dataIndex: 'value', width: '40%'},
+      {title: 'View', dataIndex: 'view', width: '40%'}
+    ];
+
+    for (let obj in propertyValues.children) {
+      for (let i in propertyValues.children[obj]) {
+        let property = {
+          property: i,
+          value: propertyValues.children[obj][i],
+          view: null
+        }
+        data.push(property)
+      }
+    }
+
+    return <Table
+        rowKey="property"
+        columns={columns}
+        dataSource={data}
+        pagination={false}
+    />;
+  }
+
 
   const columns = [
     {
@@ -49,15 +86,23 @@ const ExpandableTableView: React.FC<Props> = (props) => {
     {
       title: 'Value',
       dataIndex: 'value',
-      width: '80%',
+      width: '40%',
+    },
+    {
+      title: 'View',
+      dataIndex: 'view',
+      width: '40%',
     }
+
   ];
 
   return (
       <Table
           rowKey="key"
           dataSource={data}
+          expandIcon={expandIcon}
           columns={columns}
+          expandedRowRender={expandedRowRender}
           pagination={false}
           data-cy="expandable-table-view"
       />
