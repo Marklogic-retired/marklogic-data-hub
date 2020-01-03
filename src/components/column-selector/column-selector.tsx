f0ce9d2d139ca23df480d6d2e9c2dca4a29ba505
Timur Isangulov <timur.isangulov@marklogic.com>
2020-01-03 14:32:55 -0800

    fix nested reordering

--- src/components/column-selector/column-selector.tsx
+++ src/components/column-selector/column-selector.tsx
@@ -60,48 +60,50 @@ const ColumnSelector: React.FC<Props> = (props) => {
     const dropPosition = info.dropPosition - Number(dropPos[dropPos.length - 1]);
     let data = tree;
 
-    if (dropPosition !== 0) {
-      const loop = (data, key, callback) => {
-        data.forEach((item, index, arr) => {
-          if (item.key === key) {
-            return callback(item, index, arr);
-          }
-          if (item.children) {
-            return loop(item.children, key, callback);
-          }
+    if(dragKey.length === dropKey.length){
+      if (dropPosition !== 0) {
+        const loop = (data, key, callback) => {
+          data.forEach((item, index, arr) => {
+            if (item.key === key) {
+              return callback(item, index, arr);
+            }
+            if (item.children) {
+              return loop(item.children, key, callback);
+            }
+          });
+        };
+  
+        let dragObj;
+        loop(data, dragKey, (item, index, arr) => {
+          arr.splice(index, 1);
+          dragObj = item;
         });
-      };
-
-      let dragObj;
-      loop(data, dragKey, (item, index, arr) => {
-        arr.splice(index, 1);
-        dragObj = item;
-      });
-
-      let ar;
-      let i;
-      loop(data, dropKey, (item, index, arr) => {
-        ar = arr;
-        i = index;
-      });
-      if (dropPosition === -1) {
-        ar.splice(i, 0, dragObj);
-      } else {
-        ar.splice(i + 1, 0, dragObj);
-      }
-    }
-
-    let col = new Array();
-    for(let i of checkedKeys) {
-      for(let j of allKeys) {
-        if(i === j || j.startsWith(i+'-')) {
-          col.push(j)
+  
+        let ar;
+        let i;
+        loop(data, dropKey, (item, index, arr) => {
+          ar = arr;
+          i = index;
+        });
+        if (dropPosition === -1) {
+          ar.splice(i, 0, dragObj);
+        } else {
+          ar.splice(i + 1, 0, dragObj);
         }
       }
+  
+      let col = new Array();
+      for(let i of checkedKeys) {
+        for(let j of allKeys) {
+          if(i === j || j.startsWith(i+'-')) {
+            col.push(j)
+          }
+        }
+      }
+  
+      props.headerRender(reconstructHeader(deepCopy(data), col));
+      setTree(data)
     }
-
-    props.headerRender(reconstructHeader(deepCopy(data), col));
-    setTree(data)
   };
 
   const content = (
