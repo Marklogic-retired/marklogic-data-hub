511122e04ebb86a565829e239c4959c0fd91ab46
Sameera PriyathamTadikonda <preethu4476@gmail.com>
2020-01-14 10:52:22 -0800

    DHFPROD-3918: Bench/Flows components

---
+++ one-ui/ui/src/components/flows/new-flow-dialog/new-flow-dialog.tsx
@@ -0,0 +1,159 @@
+import { Modal, Form, Input, Button, Tooltip, Icon, Progress, Upload, Select } from "antd";
+import React, { useState, useEffect } from "react";
+import styles from './new-flow-dialog.module.scss';
+import {NewFlowTooltips} from '../../../config/tooltips.config';
+
+const NewFlowDialog = (props) => {
+
+  const [flowName, setFlowName] = useState('');
+  const [description, setDescription] = useState(props.flowData && props.flowData != {} ? props.flowData.description : '');
+
+  const [isFlowNameTouched, setFlowNameTouched] = useState(false);
+  const [isDescriptionTouched, setDescriptionTouched] = useState(false);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [tobeDisabled, setTobeDisabled] = useState(false);
+
+  useEffect(() => {
+    if (props.flowData && JSON.stringify(props.flowData) != JSON.stringify({}) && props.title === 'Edit Flow') {
+      setFlowName(props.flowData.name);
+      setDescription(props.flowData.description);
+      setIsLoading(true);
+      setTobeDisabled(true);
+    } else {
+      setFlowName('');
+      setFlowNameTouched(false);
+      setDescription('');
+    }
+
+    return (() => {
+      setFlowName('');
+      setFlowNameTouched(false);
+      setDescription('');
+      setTobeDisabled(false);
+    })
+
+  }, [props.title, props.newFlow]);
+
+  const onCancel = () => {
+    props.setNewFlow(false);
+  }
+
+  const onOk = () => {
+    props.setNewFlow(false);
+  }
+
+  const handleSubmit = async (event: { preventDefault: () => void; }) => {
+    if (event) event.preventDefault();
+
+    let dataPayload = {
+        name: flowName,
+        description: description
+      }
+    setIsLoading(true);
+
+    //Call create data load artifact API function
+    if (props.title === 'Edit Flow') {
+      props.updateFlow(dataPayload, flowName);
+    } else {
+      props.createFlow(dataPayload);
+    }
+
+    props.setNewFlow(false);
+  }
+
+  const handleChange = (event) => {
+    if (event.target.id === 'name') {
+      if (event.target.value === ' ') {
+        setFlowNameTouched(false);
+      }
+      else {
+        setFlowNameTouched(true);
+        setFlowName(event.target.value);
+      }
+    }
+    if (event.target.id === 'name' && event.target.value.length == 0) {
+      console.log('setIsLoading(false);');
+      setIsLoading(false);
+    }
+
+    if (event.target.id === 'description') {
+      setDescription(event.target.value)
+    }
+  }
+
+  const formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 7 },
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 15 },
+    },
+  };
+
+  return (<Modal visible={props.newFlow}
+    title={null}
+    width="700px"
+    onCancel={() => onCancel()}
+    onOk={() => onOk()}
+    okText="Save"
+    className={styles.modal}
+    footer={null}>
+
+    <p className={styles.title}>{props.title}</p>
+    <br />
+    <div className={styles.newFlowForm}>
+      <Form {...formItemLayout} onSubmit={handleSubmit} colon={false}>
+        <Form.Item label={<span>
+          Name:&nbsp;<span className={styles.asterisk}>*</span>&nbsp;
+              <Tooltip title={NewFlowTooltips.name}>
+            <Icon type="question-circle" className={styles.questionCircle} theme="filled" />
+          </Tooltip>
+          &nbsp;
+            </span>} labelAlign="left"
+          validateStatus={(flowName || !isFlowNameTouched) ? '' : 'error'}
+          help={(flowName || !isFlowNameTouched) ? '' : 'Name is required'}>
+          <Input
+            id="name"
+            placeholder="Enter name"
+            value={flowName}
+            onChange={handleChange}
+            disabled={tobeDisabled}
+          />
+        </Form.Item>
+        <Form.Item label={<span>
+          Description:&nbsp;
+              <Tooltip title={NewFlowTooltips.description}>
+            <Icon type="question-circle" className={styles.questionCircle} theme="filled" />
+          </Tooltip>
+          &nbsp;
+            </span>} labelAlign="left">
+          <Input
+            id="description"
+            placeholder="Enter description"
+            value={description}
+            onChange={handleChange}
+          />
+        </Form.Item>
+        <Form.Item className={styles.submitButtonsForm}>
+          <div className={styles.submitButtons}>
+            <Button onClick={() => onCancel()}>Cancel</Button>
+            &nbsp;&nbsp;
+            <Button 
+              type="primary" 
+              htmlType="submit" 
+              disabled={!flowName}
+              onClick={handleSubmit}
+            >
+              Save
+            </Button>
+          </div>
+        </Form.Item>
+      </Form>
+    </div>
+  </Modal>)
+}
+
+export default NewFlowDialog;
--- one-ui/ui/src/components/load-data/load-data-card.tsx
+++ one-ui/ui/src/components/load-data/load-data-card.tsx
@@ -8,24 +8,31 @@ import sourceFormatOptions from '../../config/formats.config';
 import NewDataLoadDialog from './new-data-load-dialog/new-data-load-dialog';
 import { convertDateFromISO } from '../../util/conversionFunctions';
 import LoadDataSettingsDialog from './load-data-settings/load-data-settings-dialog';
+import { ContextMenu, ContextMenuTrigger, MenuItem, SubMenu } from "react-contextmenu";
 
 interface Props {
     data: any;
+    flows: any;
     deleteLoadDataArtifact: any;
     createLoadDataArtifact: any;
     canReadOnly: any;
     canReadWrite: any;
-  }
+    addStepToFlow: any;
+    addStepToNew: any;
+}
+
+const MENU_TYPE = 'contextMenu';
 
 const LoadDataCard: React.FC<Props> = (props) => {
     const [newDataLoad, setNewDataLoad] = useState(false);
     const [title, setTitle] = useState('');
     const [stepData, setStepData] = useState({});
     const [dialogVisible, setDialogVisible] = useState(false);
+    const [addDialogVisible, setAddDialogVisible] = useState(false);
     const [loadArtifactName, setLoadArtifactName] = useState('');
+    const [flowName, setFlowName] = useState('');
 
     const [openLoadDataSettings, setOpenLoadDataSettings] = useState(false);
-
 
     const OpenAddNewDialog = () => {
         setTitle('New Data Load');
@@ -45,7 +52,7 @@ const LoadDataCard: React.FC<Props> = (props) => {
         console.log('Open settings', openLoadDataSettings)
     }
 
-    //Custom CSS for source Format
+    // Custom CSS for source Format
     const sourceFormatStyle = (sourceFmt) => {
         let customStyles: CSSProperties = {
             float: 'left',
@@ -59,7 +66,7 @@ const LoadDataCard: React.FC<Props> = (props) => {
         return customStyles;
     }
 
-    //Truncate a string (Step Name) to desired no. of characters
+    // Truncate a string (Step Name) to desired no. of characters
     const getInitialChars = (str, num, suffix) => {
         suffix = suffix ? suffix : '...';
         let result = str;
@@ -72,42 +79,124 @@ const LoadDataCard: React.FC<Props> = (props) => {
     const handleCardDelete = (name) => {
         setDialogVisible(true);
         setLoadArtifactName(name);
-      }
+    }
 
-      const onOk = (name) => {
+    const onDeleteOk = (name) => {
         props.deleteLoadDataArtifact(name)
         setDialogVisible(false);
-      }
+    }
 
-      const onCancel = () => {
+    const handleStepAdd = (event, data) => {
+        console.log('handleStepAdd', data )
+        setAddDialogVisible(true);
+        setLoadArtifactName(data.loadDataArtifact.name);
+        setFlowName(data.flowName);
+    }
+
+    const onAddOk = (lName, fName) => {
+        props.addStepToFlow(lName, fName)
+        setAddDialogVisible(false);
+    }
+
+    const onCancel = () => {
         setDialogVisible(false);
-      }  
+        setAddDialogVisible(false);
+    }  
 
-    const deleteConfirmation = <Modal
-        visible={dialogVisible}
-        okText='Yes'
-        cancelText='No'
-        onOk={() => onOk(loadArtifactName)}
-        onCancel={() => onCancel()}
-        width={350}
-        maskClosable={false}
+    const deleteConfirmation = (
+        <Modal
+            visible={dialogVisible}
+            okText='Yes'
+            cancelText='No'
+            onOk={() => onDeleteOk(loadArtifactName)}
+            onCancel={() => onCancel()}
+            width={350}
+            maskClosable={false}
         >
-        <span style={{fontSize: '16px'}}>Are you sure you want to delete this?</span>
-        </Modal>;
+            <div style={{fontSize: '16px', padding: '10px'}}>
+                Are you sure you want to delete "{loadArtifactName}"?
+            </div>
+        </Modal>
+    );   
+        
+    const addConfirmation = (
+        <Modal
+            visible={addDialogVisible}
+            okText='Yes'
+            cancelText='No'
+            onOk={() => onAddOk(loadArtifactName, flowName)}
+            onCancel={() => onCancel()}
+            width={350}
+            maskClosable={false}
+        >
+            <div style={{fontSize: '16px', padding: '10px'}}>
+                Are you sure you want to add "{loadArtifactName}" to flow "{flowName}"?
+            </div>
+        </Modal>
+    );   
+        
+    // Pass Load Data name in attributes as workaround for unresolved typescript issue:
+    // https://github.com/vkbansal/react-contextmenu/issues/270
+    const collect = (menuProps) => {
+        console.log('collect', menuProps.attributes);
+        return { 
+            loadDataArtifact: props.data.find(loadData => loadData.name === menuProps.attributes.className) 
+        };
+    }
 
-    return (
-        <div className={styles.loaddataContainer}>
-            <Row gutter={16} type="flex" >
-                {props.canReadWrite ? <Col >
+    // const contextualMenu = (
+    //     <ContextMenu id={MENU_TYPE}>
+    //         <MenuItem onClick={props.addStepToNew}>Add a step to a new flow</MenuItem>
+    //         <SubMenu title="Add a step to an existing flow">
+    //             {props && props.flows.length > 0 ? props.flows.map((flow) => (
+    //                 <MenuItem onClick={handleStepAdd} data={{ flowName: flow.name }}>{flow.name}</MenuItem>
+    //             )) : null }
+    //         </SubMenu>
+    //     </ContextMenu>
+    // );
+
+    const createContextualMenus = (propsData) => {
+        let menus: any = [];
+        for (let index = 0; index < propsData.length; index++) {
+            let elem = propsData[index];
+            menus.push(<ContextMenu id={MENU_TYPE+index.toString()}>
+                <MenuItem onClick={props.addStepToNew}>Add a step to a new flow</MenuItem>
+                <SubMenu title="Add a step to an existing flow">
+                    {props && props.flows.length > 0 ? props.flows.map((flow) => (
+                        <MenuItem onClick={handleStepAdd} data={{ 
+                            flowName: flow.name, 
+                            loadDataName: elem.name,
+                            index: index
+                        }}>{flow.name}</MenuItem>
+                    )) : null }
+                </SubMenu>
+            </ContextMenu>)
+        }
+        return menus
+    }
+
+    let contextTrigger;
+    const toggleMenu = e => {
+        if(contextTrigger) {
+            contextTrigger.handleContextClick(e);
+        }
+        e.preventDefault();
+    };
+
+    const createCards = (propsData) => {
+        let cards: any = [];
+        for (let i = 0; i < propsData.length; i++) {
+            let elem = propsData[i];
+            console.log('making menu', MENU_TYPE+i.toString());
+            cards.push(<Col key={i}>
+                <ContextMenuTrigger 
+                    id={MENU_TYPE+i.toString()}
+                    attributes={{className: elem.name}}
+                    collect={collect}
+                    ref={c => contextTrigger = c}
+                >
                     <Card
-                        size="small"
-                        className={styles.addNewCard}>
-                        <div><Icon type="plus-circle" className={styles.plusIcon} theme="filled" onClick={OpenAddNewDialog}/></div>
-                        <br />
-                        <p className={styles.addNewContent}>Add New</p>
-                    </Card>
-                </Col> : ''}{props && props.data.length > 0 ? props.data.map((elem,index) => (
-                    <Col key={index}><Card
+                        onContextMenu={toggleMenu}
                         actions={[
                             <span>{elem.filesNeedReuploaded ? (
                                 <Popover
@@ -115,8 +204,8 @@ const LoadDataCard: React.FC<Props> = (props) => {
                                     trigger="click"
                                     placement="bottom"
                                 ><i><FontAwesomeIcon icon={faExclamationCircle} className={styles.popover} size="lg" /></i></Popover>) : ''}</span>,
-                            <Tooltip title={'Settings'} placement="bottom"><Icon type="setting" key="setting" onClick={() => OpenLoadDataSettingsDialog(index)}/></Tooltip>,
-                            <Tooltip title={'Edit'} placement="bottom"><Icon type="edit" key="edit" onClick={() => OpenEditStepDialog(index)}/></Tooltip>,
+                            <Tooltip title={'Settings'} placement="bottom"><Icon type="setting" key="setting" onClick={() => OpenLoadDataSettingsDialog(i)}/></Tooltip>,
+                            <Tooltip title={'Edit'} placement="bottom"><Icon type="edit" key="edit" onClick={() => OpenEditStepDialog(i)}/></Tooltip>,
                             props.canReadWrite ? <Tooltip title={'Delete'} placement="bottom"><i><FontAwesomeIcon icon={faTrashAlt} className={styles.deleteIcon} size="lg" onClick={() => handleCardDelete(elem.name)}/></i></Tooltip> : <i><FontAwesomeIcon icon={faTrashAlt} onClick={(event) => event.preventDefault()} className={styles.disabledDeleteIcon} size="lg"/></i>,
                         ]}
                         className={styles.cardStyle}
@@ -130,18 +219,44 @@ const LoadDataCard: React.FC<Props> = (props) => {
                         <div className={styles.fileCount}>{elem.fileCount}</div>
                         <span className={styles.stepNameStyle}>{getInitialChars(elem.name, 27, '...')}</span>
                         <p className={styles.lastUpdatedStyle}>Last Updated: {convertDateFromISO(elem.lastUpdated)}</p>
-                    </Card></Col>
-                )) : <span></span> }</Row>
-                <NewDataLoadDialog 
+                    </Card>
+                </ContextMenuTrigger>
+            </Col>)
+        }
+        return cards
+    }
+
+    return (
+        <div id="load-data-card-view" className={styles.loaddataContainer}>
+            <Row gutter={16} type="flex" >
+                {props.canReadWrite ? <Col >
+                    <Card
+                        size="small"
+                        className={styles.addNewCard}>
+                        <div><Icon type="plus-circle" className={styles.plusIcon} theme="filled" onClick={OpenAddNewDialog}/></div>
+                        <br />
+                        <p className={styles.addNewContent}>Add New</p>
+                    </Card>
+                </Col> : ''}
+                { props && props.data.length > 0 ? createCards(props.data) : <span></span> }
+            </Row>
+            <NewDataLoadDialog 
                 newLoad={newDataLoad} 
                 title={title} 
                 setNewLoad={setNewDataLoad} 
                 createLoadDataArtifact={props.createLoadDataArtifact} 
                 stepData={stepData}
                 canReadWrite={props.canReadWrite}
-                canReadOnly={props.canReadOnly}/>
-                {deleteConfirmation}
-                <LoadDataSettingsDialog openLoadDataSettings={openLoadDataSettings} setOpenLoadDataSettings={setOpenLoadDataSettings} stepData={stepData}/>
+                canReadOnly={props.canReadOnly}
+            />
+            {deleteConfirmation}
+            {addConfirmation}
+            <LoadDataSettingsDialog 
+                openLoadDataSettings={openLoadDataSettings} 
+                setOpenLoadDataSettings={setOpenLoadDataSettings} 
+                stepData={stepData}
+            />
+            {createContextualMenus(props.data)}
         </div>
     );
 
