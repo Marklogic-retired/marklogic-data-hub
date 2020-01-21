e4f3b9d92a939557aef87158dbc4f75620dc736b
Ryan Dew <ryan.j.dew@gmail.com>
2020-01-21 11:57:05 -0800

    DHFPROD-4166: Move one-ui to marklogic data hub

---
+++ one-ui/src/main/java/com/marklogic/hub/curation/controllers/AbstractArtifactController.java
@@ -0,0 +1,41 @@
+package com.marklogic.hub.curation.controllers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.hub.dataservices.ArtifactService;
+import com.marklogic.hub.oneui.models.HubConfigSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public abstract class AbstractArtifactController {
+    @Autowired
+    HubConfigSession hubConfig;
+
+    protected ResponseEntity<JsonNode> getArtifacts() {
+        return new ResponseEntity<>(getArtifactService().getList(this.getArtifactType()), HttpStatus.OK);
+    }
+
+    protected ResponseEntity<JsonNode> updateArtifact(String artifactName, JsonNode loadDataJson) {
+        return new ResponseEntity<>(getArtifactService().setArtifact(this.getArtifactType(), artifactName, loadDataJson), HttpStatus.OK);
+    }
+
+    protected ResponseEntity<JsonNode> getArtifact(String artifactName) {
+        return new ResponseEntity<>(getArtifactService().getArtifact(this.getArtifactType(), artifactName), HttpStatus.OK);
+    }
+
+    protected ResponseEntity<JsonNode> deleteArtifact(String artifactName) {
+        return new ResponseEntity<>(getArtifactService().deleteArtifact(this.getArtifactType(), artifactName), HttpStatus.OK);
+    }
+
+    protected ResponseEntity<JsonNode> validateArtifact(String artifactName, JsonNode artifactJson) {
+        return new ResponseEntity<>(getArtifactService().validateArtifact(this.getArtifactType(), artifactName, artifactJson), HttpStatus.OK);
+    }
+
+    protected ArtifactService getArtifactService() {
+        DatabaseClient dataServicesClient = hubConfig.newStagingClient(null);
+        return ArtifactService.on(dataServicesClient);
+    }
+
+    protected abstract String getArtifactType();
+}
---
+++ one-ui/src/main/java/com/marklogic/hub/oneui/exceptions/CustomExceptionHandler.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2012-2020 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.marklogic.hub.oneui.exceptions;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.marklogic.client.FailedRequestException;
+import com.marklogic.client.impl.FailedRequest;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+    ObjectMapper mapper = new ObjectMapper();
+
+    @ExceptionHandler(FailedRequestException.class)
+    protected ResponseEntity<JsonNode> handleFailedRequestExceptionRequest(
+        FailedRequestException failedRequestException) {
+        FailedRequest failedRequest = failedRequestException.getFailedRequest();
+        ObjectNode errJson = mapper.createObjectNode();
+        errJson.put("code", failedRequest.getStatusCode());
+        errJson.put("message", failedRequest.getStatus());
+        errJson.put("details", failedRequest.getMessage());
+        return new ResponseEntity<>(errJson, HttpStatus.valueOf(failedRequest.getStatusCode()));
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    protected ResponseEntity<JsonNode> handleHttpClientErrorExceptionRequest(
+            HttpClientErrorException httpClientErrorException) {
+        ObjectNode errJson = mapper.createObjectNode();
+        errJson.put("code", httpClientErrorException.getRawStatusCode());
+        errJson.put("message", httpClientErrorException.getMessage());
+        return new ResponseEntity<>(errJson, HttpStatus.valueOf(httpClientErrorException.getRawStatusCode()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<JsonNode> handleExceptionRequest(
+            Exception exception) {
+        ObjectNode errJson = mapper.createObjectNode();
+        errJson.put("code", 500);
+        errJson.put("message", exception.getMessage());
+        return new ResponseEntity<>(errJson, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
