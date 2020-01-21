b59c7c5f42e1b3aedd674693744a600d07561231
Ryan Dew <ryan.j.dew@gmail.com>
2020-01-21 11:57:05 -0800

    DHFPROD-4166: Move one-ui to marklogic data hub

---
+++ one-ui/ui/src/components/login-form/login-form.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import LoginForm from './login-form';
+
+
+describe('Login page', () => {
+  let wrapper: any;
+
+  beforeEach(() => {
+    wrapper = shallow(<LoginForm />)
+  });
+
+  test('login fields renders ', () => {
+    expect(wrapper.find('#username').length).toEqual(1)
+    expect(wrapper.find('#password').length).toEqual(1)
+    expect(wrapper.find('#submit').length).toEqual(1)
+    expect(wrapper.find('[data-cy="forgot"]').length).toEqual(1)
+  });
+
+});
