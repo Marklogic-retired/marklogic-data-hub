f9c6e27abee8483f160dab690c47cdcf09389e7d
Ryan Dew <ryan.j.dew@gmail.com>
2020-01-22 15:09:54 -0800

    DHFPROD-3764: Fixing tests for Java update

--- web/src/test/java/com/marklogic/hub/web/service/AbstractServiceTest.java
+++ web/src/test/java/com/marklogic/hub/web/service/AbstractServiceTest.java
@@ -2,21 +2,23 @@ package com.marklogic.hub.web.service;
 
 import com.marklogic.hub.HubTestBase;
 import com.marklogic.hub.web.auth.ConnectionAuthenticationToken;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import javax.annotation.PostConstruct;
 
+public class AbstractServiceTest extends HubTestBase implements InitializingBean {
 
-public class AbstractServiceTest extends HubTestBase {
-
-    @PostConstruct
     protected void setupEnv() {
         createProjectDir();
         adminHubConfig.refreshProject();
         ConnectionAuthenticationToken authenticationToken = new ConnectionAuthenticationToken("admin", "admin", "localhost", 1, "local");
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         installHubModules();
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        super.afterPropertiesSet();
+        setupEnv();
     }
 
 }
