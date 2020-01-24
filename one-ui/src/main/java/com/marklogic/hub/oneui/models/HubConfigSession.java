292926e393ad2860a0213815a6f9a96831118758
Hao Liu <hao.liu@marklogic.com>
2020-01-24 14:40:07 -0800

    DHFPROD-4099: add a new test class

--- one-ui/src/main/java/com/marklogic/hub/oneui/models/HubConfigSession.java
+++ one-ui/src/main/java/com/marklogic/hub/oneui/models/HubConfigSession.java
@@ -1067,4 +1067,9 @@ public class HubConfigSession implements HubConfig, InitializingBean, Disposable
             clientsByKindAndDatabaseName.clear();
         }
     }
+
+    //only for test purpose
+    public Map<DatabaseKind, Map<String, DatabaseClient>> getAllDatabaseClients() {
+        return clientsByKindAndDatabaseName;
+    }
 }
