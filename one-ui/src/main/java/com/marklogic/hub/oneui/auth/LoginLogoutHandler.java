6de32d142047708a35f7932adc865eeef0a97b36
Hao Liu <hao.liu@marklogic.com>
2020-01-23 22:29:10 -0800

    DHFPROD-4099: Support configurable session timeout (5 min as default)

--- one-ui/src/main/java/com/marklogic/hub/oneui/auth/LoginLogoutHandler.java
+++ one-ui/src/main/java/com/marklogic/hub/oneui/auth/LoginLogoutHandler.java
@@ -21,6 +21,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Controller;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -28,6 +29,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+@Controller
 public class LoginLogoutHandler implements AuthenticationSuccessHandler, LogoutSuccessHandler {
 
     @Override
