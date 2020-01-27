83cae1f8798a1be345e38752414a5912a9a5baf7
Hao Liu <hao.liu@marklogic.com>
2020-01-27 13:52:55 -0800

    DHFPROD-4099: Replaced an annotation Controller with Component

--- one-ui/src/main/java/com/marklogic/hub/oneui/auth/LoginLogoutHandler.java
+++ one-ui/src/main/java/com/marklogic/hub/oneui/auth/LoginLogoutHandler.java
@@ -19,6 +19,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
 import javax.servlet.ServletException;
@@ -27,7 +28,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@Controller
+@Component
 public class LoginLogoutHandler implements AuthenticationSuccessHandler, LogoutSuccessHandler {
 
     @Override
