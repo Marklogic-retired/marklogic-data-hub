4102c836db7f404b5afd322c45820b622724cb47
Nikhil Shrivastava <nikhil.shrivastava@marklogic.com>
2020-01-29 13:02:26 -0800

    DHFPROD-4097: Added Custom Hook and MLCP Command initial layout

--- one-ui/ui/src/components/load-data/load-data-settings/load-data-settings-dialog.tsx
+++ one-ui/ui/src/components/load-data/load-data-settings/load-data-settings-dialog.tsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect, useContext } from "react";
 import styles from './load-data-settings-dialog.module.scss';
 import { LoadDataSettings } from '../../../config/tooltips.config';
 import Axios from "axios";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-regular-svg-icons';
 
 import { RolesContext } from "../../../util/roles";
 
@@ -118,6 +120,7 @@ const getSettingsArtifact = async () => {
       setUser('');
       setRunBefore(false);
   }
+  const provGranOptions = ['coarse-grained', 'OFF'];
 
 }
 
