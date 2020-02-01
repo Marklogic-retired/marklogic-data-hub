4c30a6dc4691d4d2c20f97575b72b090ff4460eb
Nikhil Shrivastava <nikhil.shrivastava@marklogic.com>
2020-01-31 16:22:28 -0800

    DHFPROD-3916: Final changes for Roles based privileges for load data artifacts. This involves stories 3916, 3917 & 3915.

--- one-ui/ui/src/components/load-data/new-data-load-dialog/new-data-load-dialog.tsx
+++ one-ui/ui/src/components/load-data/new-data-load-dialog/new-data-load-dialog.tsx
@@ -651,7 +651,7 @@ const NewDataLoadDialog = (props) => {
           <Icon type="question-circle" className={styles.questionCircle} theme="filled" />
         </Tooltip>
         </Form.Item>
-        {props.canReadWrite ? <Form.Item label={<span>
+        <Form.Item label={<span>
           Files:&nbsp;
             </span>} labelAlign="left">
           <span className={styles.upload}><Upload
