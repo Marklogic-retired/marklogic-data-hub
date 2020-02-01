409e8907ea17ea6cb1d79881c2a0cfbdefab04c4
ssambasu <ssambasu@marklogic.com>
2020-01-31 16:18:10 -0800

    DHFPROD-4258:Provide host, stagingPort info to UI and some refactoring

--- one-ui/ui/src/pages/ProjectInfo.tsx
+++ one-ui/ui/src/pages/ProjectInfo.tsx
@@ -1,21 +1,15 @@
-import React, { CSSProperties } from 'react';
+import React, { useContext, CSSProperties } from 'react';
 import styles from './ProjectInfo.module.scss';
 import {Card, Button} from 'antd';
 import { Row, Col } from 'antd';
 import axios from 'axios';
+import {getEnvironment} from '../util/environment';
 
-class ProjectInfo extends React.Component {
-  state: any = {};
-  cardCss:CSSProperties = {backgroundColor: '#F6F8FF', borderColor: '#44499C'};
-  divCss:CSSProperties = { padding: '1em 6em'};
-  componentDidMount() {
-    axios.get('/api/environment/project-info')
-      .then(res => {
-        const resp =res.data;
-        this.setState(resp);
-      })
-  }
-  download = () => {
+const ProjectInfo: React.FC = () => {
+  const cardCss:CSSProperties = {backgroundColor: '#F6F8FF', borderColor: '#44499C'};
+  const divCss:CSSProperties = { padding: '1em 6em'};
+
+  const download = () => {
     axios({
       url: '/api/environment/project-download',
       method: 'GET',
@@ -32,75 +26,75 @@ class ProjectInfo extends React.Component {
         link.click();
    });
   }
-  render(){
-    return (
-      <div style={this.divCss}>
-        <Row gutter={[30,20]}>
-          <Card title={<span className={styles.projTitle}>Manage Project</span>} className={styles.pageTitle}
-          headStyle= {this.cardCss}>
-            <Col span={12}>
-              <Card  className={styles.bigCard} bordered={false}>
-              <br/>
-                <Row>
-                  <Col span={6}>
-                  <p className={styles.alignleft}>Data Hub Version:</p>
-                  </Col>
-                  <Col  span={12}>
-                  <p className={styles.alignleftresults}>{this.state.dhfVersion}</p>
-                  </Col>
-                </Row>
+  const env = getEnvironment();
+  return (
+    <div style={divCss}>
+      <Row gutter={[30,20]}>
+        <Card title={<span className={styles.projTitle}>Manage Project</span>} className={styles.pageTitle}
+        headStyle= {cardCss}>
+          <Col span={12}>
+            <Card  className={styles.bigCard} bordered={false}>
+            <br/>
+              <Row>
+                <Col span={6}>
+                <p className={styles.alignleft}>Data Hub Version:</p>
+                </Col>
+                <Col  span={12}>
+                <p className={styles.alignleftresults}>{env.dataHubVersion}</p>
+                </Col>
+              </Row>
 
-                <Row>
-                  <Col span={6}>
-                  <p className={styles.alignleft}>MarkLogic Version:</p>
-                  </Col>
-                  <Col  span={12}>
-                  <p className={styles.alignleftresults}>{this.state.mlVersion}</p>
-                  </Col>
-                </Row>
-                <br/><br/>
-                <br/><br/>
-                <Row>
-                  <Col span={6}>
-                  <p className={styles.alignleft}>Project name:</p>
-                  </Col>
-                  <Col  span={12}>
-                  <p className={styles.alignleftresults}>{this.state.projectName}</p>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col  span={6}>
-                  <p className={styles.alignleft}>Project directory:</p>
-                  </Col>
-                  <Col  span={12}>
-                  <p className={styles.alignleftresults}>{this.state.projectDir}</p>
-                  </Col>
-                </Row>
-              </Card>
-            </Col>
-            <Col span={12}>
+              <Row>
+                <Col span={6}>
+                <p className={styles.alignleft}>MarkLogic Version:</p>
+                </Col>
+                <Col  span={12}>
+                <p className={styles.alignleftresults}>{env.marklogicVersion}</p>
+                </Col>
+              </Row>
+              <br/><br/>
+              <br/><br/>
+              <Row>
+                <Col span={6}>
+                <p className={styles.alignleft}>Project name:</p>
+                </Col>
+                <Col  span={12}>
+                <p className={styles.alignleftresults}>{env.projectName}</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col  span={6}>
+                <p className={styles.alignleft}>Project directory:</p>
+                </Col>
+                <Col  span={12}>
+                <p className={styles.alignleftresults}>{env.projectDir}</p>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
+          <Col span={12}>
 
-              <Card size="small" className={styles.smallCard} bordered={false}>
-                <br/><br/>
-                <div>
-                  <p className={styles.aligncenter}>Download project as .zip file</p>
-                  <br/>
-                  <Button type="primary" className={styles.addNewButton} onClick = {this.download}>Download</Button>
-                </div>
-              </Card>
-              <br/>
-              <Card size="small" className={styles.smallCard} bordered={false}>
-                <br/><br/>
-                <div> <p className={styles.aligncenter}>Upload project as .zip file</p>
+            <Card size="small" className={styles.smallCard} bordered={false}>
+              <br/><br/>
+              <div>
+                <p className={styles.aligncenter}>Download project as .zip file</p>
                 <br/>
-                <Button type="primary" className={styles.addNewButton} >Upload</Button></div>
-              </Card>
-            </Col>
-          </Card>
-        </Row>
-    </div>
-    )
-  }
+                <Button type="primary" className={styles.addNewButton} onClick = {download}>Download</Button>
+              </div>
+            </Card>
+            <br/>
+            <Card size="small" className={styles.smallCard} bordered={false}>
+              <br/><br/>
+              <div> <p className={styles.aligncenter}>Upload project as .zip file</p>
+              <br/>
+              <Button type="primary" className={styles.addNewButton} >Upload</Button></div>
+            </Card>
+          </Col>
+        </Card>
+      </Row>
+  </div>
+  )
+  
 }
 
 export default ProjectInfo;
