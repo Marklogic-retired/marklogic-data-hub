e72ab278e79a563ce87211d3d92470d7e9eb4400
Nikhil Shrivastava <nikhil.shrivastava@marklogic.com>
2020-02-04 17:51:42 -0800

    DHFPROD-4097: Removed MLCP Command for now and added Unit test and Role based access support.

--- one-ui/ui/src/config/tooltips.config.js
+++ one-ui/ui/src/config/tooltips.config.js
@@ -10,6 +10,19 @@ const NewLoadTooltips = {
     'targetURIPreview': 'The URI of an example ingested document.' 
 }
 
+const LoadDataSettings = {
+    'targetDatabase': 'The database where to store the processed data. For ingestion, choose the STAGING database where you want to store the ingested data. Default is data-hub-STAGING.', 
+    'additionalCollections': 'One or more collection tags to add to the default union of tags.', 
+    'targetPermissions' : 'The comma-separated roles required to access the ingested data.', 
+    'provGranularity': 'The value of Provenance Granularity for this Load Data Configuration.', 
+    'module': 'The path to your custom hook module.', 
+    'cHParameters': 'Parameters, as key-value pairs, to pass to your custom hook module.', 
+    'user': 'The user account to use to run the module. Default is the user running the flow; e.g., flow-operator.', 
+    'runBefore': 'For a pre-step hook, set to true. For a post-step hook, set to false.',
+    'mlcpCommand': 'To ingest files using MLCP, copy and paste this complete command to a command-line prompt . Note: MLCP and QuickStart generate two different URIs for the same delimited file.'
+}
+
 export {
-    NewLoadTooltips
+    NewLoadTooltips,
+    LoadDataSettings
 }
