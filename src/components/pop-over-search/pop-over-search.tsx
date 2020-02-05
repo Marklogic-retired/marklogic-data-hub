b25ef866da49acdf3533e122b577e6fcd9703c92
sanjeevani vishaka <sanjeevani.vishaka@gmail.com>
2020-02-05 14:14:41 -0800

    multipopover fix

--- src/components/pop-over-search/pop-over-search.tsx
+++ src/components/pop-over-search/pop-over-search.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, { useState } from 'react';
 import {Popover, Input, Checkbox, Icon} from 'antd';
 import styles from './pop-over-search.module.scss';
 import axios from "axios";
@@ -16,10 +16,6 @@ const PopOverSearch: React.FC<Props> = (props) => {
   const [options, setOptions] = useState<any[]>([]);
   const [checkedValues, setCheckedValues] = useState<any[]>([]);
   const [popOverVisibility, setPopOverVisibilty] = useState(false);
-  const [open, setOpen] = useState(true);
-
-  const popOverRef = useRef<HTMLDivElement>(null);
-  const checkRef = useRef<HTMLDivElement>(null);
 
   const getFacetValues = async (param) => {
     if (param.target.value.length >= 2 && param.target.value.toLowerCase()) {
@@ -53,53 +49,38 @@ const PopOverSearch: React.FC<Props> = (props) => {
 
   const addFacetValues = () => {
     props.checkFacetValues(checkedValues);
+    setPopOverVisibilty(false);
   }
 
-  /*const searchPopover = () => {
+  const searchPopover = () => {
     setPopOverVisibilty(true);
-  }*/
-
-  /*const closePopover = () => {
-    setPopOverVisibilty(false);
   }
-*/
-  const handleClick = e => {
-    if (popOverRef.current && popOverRef.current.contains(e.target)) {
-      console.log(popOverRef);
-      setPopOverVisibilty(true);
-    }
-    if (checkRef.current && checkRef.current.contains(e.target)) {
-      console.log(checkRef);
-      setPopOverVisibilty(false);
-    }
-    // outside click
-    setPopOverVisibilty(false);
-  };
 
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClick);
-    return () => {
-      document.removeEventListener("mousedown", handleClick);
-    };
-  }, []);
+  const handleChange = (visible) => {
+    setPopOverVisibilty(visible);
+  }
 
 
   const content = (
     <div className={styles.popover}>
       <Input placeholder="Search" allowClear={true} onChange={getFacetValues}/>
-      <div className={styles.scrollOptions} ref={popOverRef}>
+      <div className={styles.scrollOptions}>
         <Checkbox.Group options={options} onChange={onSelectCheckboxes}></Checkbox.Group>
       </div>
       <hr/>
-      <div className={styles.checkIcon} ref={checkRef}>
+      <div className={styles.checkIcon}>
         <Icon type="check-square-o" className={styles.popoverIcons} onClick={addFacetValues}/>
       </div>
     </div>
   )
 
   return (
-    <Popover placement="leftTop" content={content} trigger="click">
+    <Popover
+      placement="leftTop"
+      content={content}
+      trigger="click"
+      onVisibleChange={handleChange}
+      visible={popOverVisibility}>
       <div className={styles.search}>Search</div>
     </Popover>
   )
