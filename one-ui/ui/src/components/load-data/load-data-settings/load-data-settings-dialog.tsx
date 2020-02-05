4b0ff130efc8a256b1ab29d82fe990106f97c405
Nikhil Shrivastava <nikhil.shrivastava@marklogic.com>
2020-02-05 11:53:05 -0800

    DHFPROD-4097: Making the dialog non-closable when clicking outside.

--- one-ui/ui/src/components/load-data/load-data-settings/load-data-settings-dialog.tsx
+++ one-ui/ui/src/components/load-data/load-data-settings/load-data-settings-dialog.tsx
@@ -427,7 +427,8 @@ const getSettingsArtifact = async () => {
       onOk={() => onOk()}
       okText="Save"
       className={styles.SettingsModal}
-      footer={null}>
+      footer={null}
+      maskClosable={false}>
       <p className={styles.title}>Load Data Settings</p>
       <br />
       <div className={styles.newDataLoadForm}>
