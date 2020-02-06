abccebc58081bc45581e3445b0174f68e09690c1
Ryan Dew <ryan.j.dew@gmail.com>
2020-02-05 21:27:39 -0800

    DHFPROD-4350: Ensure config directories in AppConfig get changed properly on install

--- one-ui/src/test/java/com/marklogic/hub/oneui/TestHelper.java
+++ one-ui/src/test/java/com/marklogic/hub/oneui/TestHelper.java
@@ -3,10 +3,15 @@ package com.marklogic.hub.oneui;
 import com.marklogic.hub.oneui.auth.MarkLogicAuthenticationManager;
 import com.marklogic.hub.oneui.models.EnvironmentInfo;
 import com.marklogic.hub.oneui.models.HubConfigSession;
+import com.marklogic.hub.oneui.services.EnvironmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Configuration
 @PropertySource("classpath:application-test.properties")
@@ -22,11 +27,36 @@ public class TestHelper {
     @Value("${test.dataHubDeveloperPassword:data-hub-developer-user}")
     public String dataHubDeveloperPassword;
 
+    @Value("${test.dataHubEnvironmentManagerUsername:data-hub-environment-manager-user}")
+    public String dataHubEnvironmentManagerUsername;
+    @Value("${test.dataHubEnvironmentManagerPassword:data-hub-environment-manager-user}")
+    public String dataHubEnvironmentManagerPassword;
+
+    public Path tempProjectDirectory = Files.createTempDirectory("one-ui-hub-project");
+
     @Autowired
     private HubConfigSession hubConfig;
+
+    @Autowired
+    private EnvironmentService environmentService;
+
+    public TestHelper() throws IOException {
+    }
 
     public void authenticateSession() {
        EnvironmentInfo environmentInfo = new EnvironmentInfo(mlHost, "DIGEST", 8000,"DIGEST", 8002,"DIGEST", 8010, "DIGEST", 8011);
        hubConfig.setCredentials(environmentInfo, dataHubDeveloperUsername, dataHubDeveloperPassword);
+    }
+
+    public void authenticateSessionAsEnvironmentManager() {
+        EnvironmentInfo environmentInfo = new EnvironmentInfo(mlHost, "DIGEST", 8000,"DIGEST", 8002,"DIGEST", 8010, "DIGEST", 8011);
+        hubConfig.setCredentials(environmentInfo, dataHubEnvironmentManagerUsername, dataHubEnvironmentManagerPassword);
+    }
+
+    public void setHubProjectDirectory() {
+        environmentService.setProjectDirectory(tempProjectDirectory.toAbsolutePath().toString());
+        if (!hubConfig.getHubProject().isInitialized()) {
+            hubConfig.initHubProject();
+        }
     }
 }
