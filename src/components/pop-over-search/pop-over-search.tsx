1291e9516c218087e9ea00116a1d542299e3b1f0
sanjeevani vishaka <sanjeevani.vishaka@gmail.com>
2020-02-06 21:28:07 -0800

    reset search options

--- src/components/pop-over-search/pop-over-search.tsx
+++ src/components/pop-over-search/pop-over-search.tsx
@@ -41,6 +41,9 @@ const PopOverSearch: React.FC<Props> = (props) => {
         console.log(error)
       }
     }
+    else{
+      setOptions([]);
+    }
   }
 
   const onSelectCheckboxes = (checkedValues) => {
