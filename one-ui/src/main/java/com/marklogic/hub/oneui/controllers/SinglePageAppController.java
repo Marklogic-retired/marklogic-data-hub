2c6ef3175e8632fc4ae69bd0f3d93c5398fb3085
Ryan Dew <ryan.j.dew@gmail.com>
2020-02-06 17:00:54 -0800

    DHFPROD-4351: Fix error page so other error aren't masked by 500 error

--- one-ui/src/main/java/com/marklogic/hub/oneui/controllers/SinglePageAppController.java
+++ one-ui/src/main/java/com/marklogic/hub/oneui/controllers/SinglePageAppController.java
@@ -1,12 +1,24 @@
 package com.marklogic.hub.oneui.controllers;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class SinglePageAppController {
-    @RequestMapping(value = {"/", "/home", "/load-data", "/install", "/login", "/error"})
+public class SinglePageAppController implements ErrorController {
+    @RequestMapping(value = {"/", "/home", "/load-data", "/install", "/login"})
     public String index() {
+        return "forward:index.html";
+    }
+
+    /**
+     * Returns the path of the error page.
+     *
+     * @return the error path
+     */
+    @Override
+    @RequestMapping("/error")
+    public String getErrorPath() {
         return "forward:index.html";
     }
 }
