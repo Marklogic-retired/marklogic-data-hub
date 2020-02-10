3f3544bb2d716b8b8841a40da737f0021cc3fa34
Nikhil Shrivastava <nikhil.shrivastava@marklogic.com>
2020-02-10 14:36:09 -0800

    DHFPROD-3991: Creating initial layout of Entity tiles

--- one-ui/ui/src/pages/Home.tsx
+++ one-ui/ui/src/pages/Home.tsx
@@ -3,16 +3,21 @@ import styles from './Home.module.scss';
 import { Tooltip, Icon } from 'antd';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCubes } from '@fortawesome/free-solid-svg-icons';
+import { faCubes, faObjectUngroup } from '@fortawesome/free-solid-svg-icons';
 const Home: React.FC = () => {
 
   return (
     <div>
       <div className={styles.content}>
         <div style={{ color: '#666', fontSize: '20px'}}>Authenticated!</div>
-        <Tooltip title="Click to navigate to Load Data Screen" placement="bottom">
+        <Tooltip title="Click to navigate to Load Data Screen" placement="left">
           <button className={styles.loadDataIcon}>
             <Link to="/load-data"><i><FontAwesomeIcon icon={faCubes} size="3x"/></i></Link>
+          </button>
+        </Tooltip>
+        <Tooltip title="Click to navigate to Entities Screen" placement="left">
+          <button className={styles.entityIcon}>
+            <Link to="/entity-tiles"><i><FontAwesomeIcon icon={faObjectUngroup } size="2x"/></i></Link>
           </button>
         </Tooltip>
         <Tooltip title="Click to reset host name and log out" placement="bottom">
