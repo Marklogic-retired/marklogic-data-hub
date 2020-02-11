c31f92031c0497a1387ca88579d78533680f6e71
Ryan Dew <ryan.j.dew@gmail.com>
2020-02-10 17:13:02 -0800

    DHFPROD-4310: Keep filesystem project in sync when making updates

---
+++ marklogic-data-hub/src/main/java/com/marklogic/hub/ArtifactManager.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2012-2020 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.marklogic.hub;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+/**
+ * Handles writing artifacts to ML server and project directory and reading artifacts from ML server
+ */
+public interface ArtifactManager {
+
+    /**
+     * Gets artifacts of a given type
+     *
+     * @param artifactType - type of artifact the operation is dealing with
+     * @return ObjectNode of the artifacts matching a type
+     */
+    public ArrayNode getArtifacts(String artifactType);
+
+    /**
+     * Updates an artifact of a given type and name.
+     *
+     * @param artifactType - type of artifact the operation is dealing with
+     * @param artifactName - identifier of the artifact
+     * @param artifactJson - JSON of artifact to persist
+     * @return ObjectNode of the artifact matching a type
+     */
+    public ObjectNode updateArtifact(String artifactType, String artifactName, JsonNode artifactJson);
+
+    /**
+     * Retrieves an artifact of a given type and name.
+     *
+     * @param artifactType - type of artifact the operation is dealing with
+     * @param artifactName - identifier of the artifact
+     * @return ObjectNode of the artifact matching a type and name
+     */
+    public ObjectNode getArtifact(String artifactType, String artifactName);
+
+    /**
+     * Deletes an artifact of a given type and name.
+     *
+     * @param artifactType - type of artifact the operation is dealing with
+     * @param artifactName - identifier of the artifact
+     */
+    public void deleteArtifact(String artifactType, String artifactName);
+
+    /**
+     * Validates an artifact of a given type and name.
+     *
+     * @param artifactType - type of artifact the operation is dealing with
+     * @param artifactName - identifier of the artifact
+     * @param artifactJson - JSON of artifact to persist
+     * @return ObjectNode with validation details
+     */
+    public ObjectNode validateArtifact(String artifactType, String artifactName, JsonNode artifactJson);
+
+    /**
+     * Retrieves settings of an artifact of a given type and name.
+     *
+     * @param artifactType - type of artifact the operation is dealing with
+     * @param artifactName - identifier of the artifact
+     * @return ObjectNode of the artifact settings matching a type and name
+     */
+    public ObjectNode getArtifactSettings(String artifactType, String artifactName);
+
+    /**
+     * Updates settings of an artifact of a given type and name.
+     *
+     * @param artifactType - type of artifact the operation is dealing with
+     * @param artifactName - identifier of the artifact
+     * @param settings - JSON of artifact settings to persist
+     * @return JsonNode of the artifact settings matching a type and name
+     */
+    public ObjectNode updateArtifactSettings(String artifactType, String artifactName, JsonNode settings);
+
+}
