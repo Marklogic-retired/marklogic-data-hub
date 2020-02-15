fc4c3ad28f4fc3d23c3189ff1db505fb5e45cc69
Nikhil Shrivastava <nikhil.shrivastava@marklogic.com>
2020-02-14 16:38:25 -0800

    DHFPROD-4301: Fixing the Upload discrepancy error between Firefox and Chrome

--- one-ui/ui/src/config/tooltips.config.js
+++ one-ui/ui/src/config/tooltips.config.js
@@ -2,7 +2,7 @@ const NewLoadTooltips = {
 
     'name': 'The name of this data load configuration.', 
     'description': 'The description of this data load configuration.', 
-    'files' : 'Click *Upload* to select the source files.', 
+    'files' : 'Click *Upload* to select the source files. The total size of the files must be 100MB or less.', 
     'sourceFormat': 'The format of the source files to be ingested: Text, JSON, XML, Binary, or Delimited Text', 
     'fieldSeparator': 'The delimiter in source files. Required if *Source Format* is *Delimited Text*.', 
     'targetFormat': 'The format of the processed record: Text, JSON, XML, or Binary.', 
