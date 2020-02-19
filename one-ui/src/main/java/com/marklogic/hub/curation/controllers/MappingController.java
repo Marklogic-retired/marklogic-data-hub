c3fbefec75469975b20de2eef3aa3864f182ab98
Hao Liu <hao.liu@marklogic.com>
2020-02-19 10:30:19 -0800

    DHFPROD-4308: Middle-Tier: Create extension to existing mapping artifact API

---
+++ one-ui/src/main/java/com/marklogic/hub/curation/controllers/MappingController.java
@@ -0,0 +1,38 @@
+package com.marklogic.hub.curation.controllers;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.IOException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api/artifacts/mapping")
+public class MappingController extends AbstractArtifactController {
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<ArrayNode> getArtifacts() {
+        return super.getArtifacts();
+    }
+
+    @RequestMapping(value = "/{artifactName}", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<ObjectNode> updateArtifact(@PathVariable String artifactName, @RequestBody ObjectNode mappingJson) {
+        return super.updateArtifact(artifactName, mappingJson);
+    }
+
+    @RequestMapping(value = "/{artifactName}", method = RequestMethod.DELETE)
+    public void deleteLoadDataConfig(@PathVariable String artifactName) throws IOException {
+        super.deleteArtifact(artifactName);
+    }
+
+    @Override
+    protected String getArtifactType() {
+        return "mapping";
+    }
+}
