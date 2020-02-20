c743c109af5d48156c154b52714f5d679d74b565
Alex Ebadirad <alex@ebadirad.com>
2020-02-19 18:19:08 -0700

    DHFPROD-3854: just need to finalize the write for the test in the final db

--- marklogic-data-hub/src/test/java/com/marklogic/hub/flow/FlowRunnerTest.java
+++ marklogic-data-hub/src/test/java/com/marklogic/hub/flow/FlowRunnerTest.java
@@ -214,13 +214,9 @@ public class FlowRunnerTest extends HubTestBase {
         opts.put("collections", coll);
         opts.put("sourceQuery", "cts.values(cts.collectionReference(), '', [], cts.collectionQuery('test-values-collection'))");
         String jobid = UUID.randomUUID().toString();
-        RunFlowResponse resp = runFlow("testValuesFlow", "1", jobid, opts, null);
+        RunFlowResponse resp = runFlow("testValuesFlow", "1,2", jobid, opts, null);
         flowRunner.awaitCompletion();
-        Assertions.assertTrue(getDocCount(HubConfig.DEFAULT_FINAL_NAME, "test-values-collection") == 1);
         System.out.println(resp.toJson());
-        resp = runFlow("testValuesFlow", "2", jobid, opts, null);
-        System.out.println(resp.toJson());
-        flowRunner.awaitCompletion();
         Assertions.assertTrue(getDocCount(HubConfig.DEFAULT_FINAL_NAME, "test-values-collection") == 2);
         Assertions.assertTrue(JobStatus.FINISHED.toString().equalsIgnoreCase(resp.getJobStatus()));
     }
