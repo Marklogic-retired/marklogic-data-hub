51daee6b781c6496a834e9e636d49bbaea0f9623
Hao Liu <hao.liu@marklogic.com>
2020-02-19 19:37:39 -0800

    DHFPROD-4308: changed entityName to targetEntity & simplify javascript query func

--- one-ui/src/main/java/com/marklogic/hub/curation/services/CollectionsManager.java
+++ one-ui/src/main/java/com/marklogic/hub/curation/services/CollectionsManager.java
@@ -29,7 +29,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CollectionsService {
+public class CollectionsManager {
 
     @Autowired
     private HubConfigSession hubConfig;
