594e90ec4f719f840437b6de62aaa48a51f3726b
wooldridge <mike@hellion.org>
2020-02-24 17:51:42 -0800

    DHFPROD-3918: Update flows test

--- one-ui/ui/src/components/flows/flows.test.tsx
+++ one-ui/ui/src/components/flows/flows.test.tsx
@@ -3,10 +3,26 @@ import { shallow } from 'enzyme';
 import Flows from './flows';
 
 describe('Flows component', () => {
-    let data = [];
-    let createFlow = () => null;
+    let flows = [];
+    let loads = [];
     let deleteFlow = () => null;
+    let createFlow = () => null;
+    let updateFlow = () => null;
+    let runStep = () => null;
+    let deleteStep = () => null;
+    let canReadFlows = false;
+    let canWriteFlows = false;
     it('should render correctly', () => {
-        shallow(<Flows data={data} createFlow={createFlow} deleteFlow={deleteFlow} />);
+        shallow(<Flows 
+            flows={flows} 
+            loads={loads} 
+            deleteFlow={deleteFlow} 
+            createFlow={createFlow} 
+            updateFlow={updateFlow} 
+            runStep={runStep} 
+            deleteStep={deleteStep} 
+            canReadFlows={canReadFlows} 
+            canWriteFlows={canWriteFlows}
+        />);
     });
 });
