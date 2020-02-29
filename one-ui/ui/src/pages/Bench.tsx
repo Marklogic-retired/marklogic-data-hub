846606ecf3358a2fb66602c30caeb06e355cb803
wooldridge <mike@hellion.org>
2020-02-28 16:01:06 -0800

    DHFPROD-3922: Enable step running in Bench/Flows view
    Includes confirmation dialogs and status spinner.

--- one-ui/ui/src/pages/Bench.tsx
+++ one-ui/ui/src/pages/Bench.tsx
@@ -1,18 +1,39 @@
 import React, { useState, useEffect, useContext } from 'react';
 import styles from './Bench.module.scss';
 import Flows from '../components/flows/flows';
+import { Modal } from 'antd';
 import axios from 'axios'
 import { RolesContext } from "../util/roles";
+
+interface PollConfig {
+    interval: number;
+    retryLimit: number;
+}
+
+const Statuses = {
+    'FINISHED': 'finished',
+    'CANCELED': 'canceled',
+    'FAILED': 'failed'
+}
 
 const Bench: React.FC = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [flows, setFlows] = useState<any[]>([]);
     const [loads, setLoads] = useState<any[]>([]);
+    const [runStarted, setRunStarted] = useState<any>({});
+    const [runEnded, setRunEnded] = useState<any>({});
+    const [running, setRunning] = useState<any[]>([]);
 
     // For role-based privileges
     const roleService = useContext(RolesContext);
     const canReadFlows = roleService.canReadFlows();
     const canWriteFlows = roleService.canWriteFlows();
+    const hasOperatorRole = roleService.hasOperatorRole();
+
+    const pollConfig: PollConfig = {
+        interval: 1000, // In millseconds
+        retryLimit: 10  // Timeout after retries
+    }
 
     useEffect(() => {
         getFlows();
@@ -22,6 +43,16 @@ const Bench: React.FC = () => {
             setLoads([]);
         })
     }, [isLoading]);
+
+    useEffect(() => {
+        setRunning([...running, runStarted]);
+    }, [runStarted]);
+
+    useEffect(() => {
+        setRunning([...running].filter(
+            r => (r.flowId !== runEnded.flowId || r.stepId !== runEnded.stepId)
+        ));
+    }, [runEnded]);
 
     const getFlows = async () => {
         try {
@@ -110,17 +141,89 @@ const Bench: React.FC = () => {
         }
     }
 
-    const runStep = async (stepName) => {
+    function showSuccess(stepName, stepType) {
+        Modal.success({
+        title: <p>{stepType} "{stepName}" ran successfully</p>,
+            okText: 'Close',
+            mask: false
+        });
+    }
+
+    function showErrors(stepName, stepType, errors) {
+        Modal.error({
+            title: <p>{stepType} "{stepName}" completed with errors</p>,
+            content: (
+                <div>
+                    <ul>{errors.map(e => { return <li>{e}</li> })}</ul>
+                </div>
+            ),
+            okText: 'Close',
+            mask: false
+        });
+    }
+
+    // Poll status for running flow
+    function poll(fn, interval) {
+        let tries = 0;
+        let checkStatus = (resolve, reject) => { 
+            let promise = fn();
+            promise.then(function(response){
+                let status = response.data.jobStatus;
+                console.log('Flow status: ', status);
+                if (status === Statuses.FINISHED || status === Statuses.CANCELED || status === Statuses.FAILED) {
+                    // Non-running status, resolve promise
+                    resolve(status);
+                } else {
+                    // Still running, poll again
+                    setTimeout(checkStatus, interval, resolve, reject);
+                }
+            }).catch(function(error) {
+                if (tries++ > pollConfig.retryLimit) {
+                    // Retry limit reached, reject promise
+                    reject(new Error('Over limit, error for ' + fn + ': ' + arguments));
+                } else {
+                    // Poll again
+                    setTimeout(checkStatus, interval, resolve, reject);
+                }
+            });;
+        };
+        return new Promise(checkStatus);
+    }
+
+    // POST /api/flows/{flowId}/run
+    const runStep = async (flowId, stepId, stepName, stepType) => {
+        setRunStarted({flowId: flowId, stepId: stepId});
         try {
             setIsLoading(true);
-            //let response = await axios.delete(`/api/flows/${name}`);
-            //if (response.status === 200) {
-                console.log('POST run step', stepName);
-                setIsLoading(false);
-            //} 
+            let response = await axios.post('/api/flows/' + flowId + '/run', [stepId]);
+            if (response.status === 200) {
+                console.log('Flow started: ' + flowId);
+                let jobId = response.data.jobId;
+                await setTimeout( function(){ 
+                    poll(function() {
+                        return axios.get('/api/jobs/' + jobId);
+                    }, pollConfig.interval)
+                    .then(function(status) {
+                        setRunEnded({flowId: flowId, stepId: stepId});
+                        if (status === 'finished') {
+                            console.log('Flow complete: ' + flowId);
+                            showSuccess(stepName, stepType);
+                        } else {
+                            console.log('Flow ' + status + ': ' + flowId);
+                            // TODO Handle errors DHFPROD-4025
+                            showErrors(stepName, stepType, ['error1', 'error2', 'error3']);
+                        }
+                        setIsLoading(false);
+                    }).catch(function(error) {
+                        console.log('Flow timeout', error);
+                        setRunEnded({flowId: flowId, stepId: stepId});
+                        setIsLoading(false);
+                    });
+                }, pollConfig.interval);
+            } 
         } catch (error) {
-            //let message = error.response.data.message;
             console.log('Error running step', error);
+            setRunEnded({flowId: flowId, stepId: stepId});
             setIsLoading(false);
         }
     }
@@ -154,6 +257,8 @@ const Bench: React.FC = () => {
                 deleteStep={deleteStep}
                 canReadFlows={canReadFlows}
                 canWriteFlows={canWriteFlows}
+                hasOperatorRole={hasOperatorRole}
+                running={running}
             />
         </div>
     </div>
