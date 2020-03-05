a163c6539752380342d889e44b432f0495b8fbd8
Akshay Sonvane <akshay.sonvane93@gmail.com>
2020-03-05 14:01:12 -0800

    DHFPROD-4519: Add Health endpoint

--- one-ui/src/main/java/com/marklogic/hub/oneui/auth/WebSecurityConfiguration.java
+++ one-ui/src/main/java/com/marklogic/hub/oneui/auth/WebSecurityConfiguration.java
@@ -69,6 +69,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new String[]{
             "/api/environment/initialized",
             "/websocket/**",
+            "/actuator/**",
             "/",
             "/*.js",
             "/*.ttf",
