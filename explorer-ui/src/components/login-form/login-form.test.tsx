074ba91f6a05472985f99c58ba2b42bf3ac00295
Bruce An <Bruce.An@marklogic.com>
2020-03-10 10:53:36 -0700

    unit tests for login form and selected facets

--- explorer-ui/src/components/login-form/login-form.test.tsx
+++ explorer-ui/src/components/login-form/login-form.test.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import LoginForm from './login-form';
 
+test('Login Form renders', async () => {
+  const { getByTestId } = render(
+    <LoginForm/>,
+  )
 
-describe('Login page', () => {
-  let wrapper;
+  let username = getByTestId('username');
+  let password = getByTestId('password');
 
-  beforeEach(() => {
-    wrapper = shallow(<LoginForm />)
-  });
+  await userEvent.type(username, 'user-explorer');
+  await userEvent.type(password, 'password-explorer');
 
-  test('login fields renders ', () => {
-    expect(wrapper.find('#username').length).toEqual(1)
-    expect(wrapper.find('#password').length).toEqual(1)
-    expect(wrapper.find('#submit').length).toEqual(1)
-    expect(wrapper.find('[data-cy="forgot"]').length).toEqual(1)
-  });
-
+  expect(username).toHaveAttribute('value', 'user-explorer');
+  expect(password).toHaveAttribute('value', 'password-explorer');
 });
