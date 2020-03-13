4dc50f8879bbffac211d60f208b4f80bff8b87ca
Nikhil Shrivastava <nikhil.shrivastava@marklogic.com>
2020-03-13 01:00:35 -0700

    Adding Test and clear 1

---
+++ one-ui/ui/src/components/common/dropdown-with-search/dropdownWithSearch.tsx
@@ -0,0 +1,69 @@
+import { Menu, Select } from "antd"
+import React, { useState, useEffect, useRef, useCallback, CSSProperties } from 'react';
+import styles from './dropdownWithSearch.module.scss';
+
+const DropDownWithSearch = (props) => {
+
+    const node: any = useRef();
+    const [menuVisible, setMenuVisible] = useState(false);
+    const [selList, setSelList] = useState(false);
+
+    //handle callback from event listeners
+    const handleOuterClick = useCallback(
+        e => {
+            if (node.current && !node.current.contains(e.target)) {
+                setSelList(false);
+                setMenuVisible(false);
+                props.setDisplaySelectList(false);
+                props.setDisplayMenu(false);
+            }
+        }, []
+    );
+
+    //CSS Styles for the select list
+    const listStyle:CSSProperties = {
+        width: '12vw'
+    }
+
+    const dropDownStyle: CSSProperties = { 
+        maxHeight: '30vh', 
+        overflow: 'auto',
+    }
+    
+    useEffect(() => {
+        setSelList(props.displaySelectList);
+        setMenuVisible(props.displayMenu);
+    }, [props.displaySelectList, props.displayMenu]);
+
+    //Handling click event outside the Dropdown Menu
+    useEffect(() => {
+        if (menuVisible && selList) {
+            document.addEventListener('click', handleOuterClick);
+        }
+
+        return () => {
+            document.removeEventListener('click', handleOuterClick)
+        };
+    });
+
+    return (
+
+        <div ref={node}>
+            {menuVisible && <Menu>
+                <Select
+                    open={selList}
+                    showArrow={false}
+                    showSearch
+                    style={listStyle}
+                    dropdownStyle={dropDownStyle}
+                    value={props.itemValue}
+                    onChange={props.onItemSelect}
+                >
+                    {props.srcData.map(num => <Select.Option className={styles.selectOptionsStyle} key={num}>{num}</Select.Option>)}
+                </Select>
+            </Menu>}
+        </div>
+    );
+}
+
+export default DropDownWithSearch;
