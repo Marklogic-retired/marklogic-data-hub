e607d299a38cd11beb5497d3ae286d787d8ecb35
Akshay Sonvane <akshay.sonvane93@gmail.com>
2020-03-18 17:35:39 -0700

    DHFPROD-4519: Remove explorer codebase

--- explorer/integral/src/main/java/com/marklogic/explorer/integral/AbstractStep.java
+++
@@ -1,9 +0,0 @@
-package com.marklogic.explorer.integral;
-
-public class AbstractStep {
-  public enum Access { GET, POST };
-
-  public String composeServer(String base, String port) {
-    return base + ((port.equals("80") || port.equals("443")) ? "" : ":"+port);
-  }
-}
