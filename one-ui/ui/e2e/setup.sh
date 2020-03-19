233dbf647e96cc0789f65185f72d2441daa92f98
bsrikan <srikanth.balasubramanian@marklogic.com>
2020-03-19 16:53:46 -0700

    DHFPROD-4522: Update package.json and configurations to run tests in pipeline and generate reports

---
+++ one-ui/ui/e2e/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set e+x
+
+cd qa-project
+
+./gradlew hubInit
+./gradlew hubSaveIndexes
+./gradlew mlDeploy
+
+./gradlew hubRunFlow -PflowName=AdvantageFlow -PentityName=Customer -PbatchSize=100 -PthreadCount=4 -Psteps="1,2"
+./gradlew hubRunFlow -PflowName=PersonFlow -PentityName=Person -PbatchSize=100 -PthreadCount=4 -Psteps="1,2"
+./gradlew hubRunFlow -PflowName=PersonXMLFlow -PentityName=PersonXML -PbatchSize=100 -PthreadCount=4 -Psteps="1,2"
