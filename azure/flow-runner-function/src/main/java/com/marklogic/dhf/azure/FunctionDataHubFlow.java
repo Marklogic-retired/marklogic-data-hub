8f2f4567f5671d0ed06da24817bf084aed540934
vgouraba <vasu.gourabathina@marklogic.com>
2020-04-03 17:57:38 -0700

    DHFPROD-4532: impl for invoking the flow by taking params from req body
    DHFPROD-4532: added asserts for test methods
    
    DHRPROD-4532: rename the method to buildFlowInputs
    
    DHFPROD-4532: use jackson for json; change attributes names for consistency

--- azure/flow-runner-function/src/main/java/com/marklogic/dhf/azure/FunctionDataHubFlow.java
+++ azure/flow-runner-function/src/main/java/com/marklogic/dhf/azure/FunctionDataHubFlow.java
@@ -7,6 +7,10 @@ import com.microsoft.azure.functions.annotation.HttpTrigger;
 import com.microsoft.azure.functions.annotation.QueueOutput;
 
 import java.util.Optional;
+import java.util.Arrays;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.marklogic.hub.flow.FlowInputs;
 import com.marklogic.hub.flow.FlowRunner;
@@ -25,41 +29,107 @@ public class FunctionDataHubFlow {
     @FunctionName("HttpExampleDataHubFlow")
     public HttpResponseMessage run(
             @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
-            @QueueOutput(name = "msg", queueName = "dhf-queue", connection = "AzureWebJobsStorage") OutputBinding<String> msg,
+            @QueueOutput(name = "msg", queueName = "dhf-queue-az", connection = "AzureWebJobsStorage") OutputBinding<String> msg,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
+        HttpStatus httpCode = HttpStatus.OK; // set default return code to OK
 
-        // Parse query parameter
-        String query = request.getQueryParameters().get("name");
-        String name = request.getBody().orElse(query);
+        context.getLogger().info("Preparing jsonNode.");
+        ObjectMapper objmapper = new ObjectMapper();
+        JsonNode jsonNode = null;
+        try {
+            jsonNode = objmapper.readTree(request.getBody().orElse(null));
+        } catch (java.io.IOException ex) {
+            httpCode = HttpStatus.BAD_REQUEST;
+            return request.createResponseBuilder(httpCode).body(ex.getMessage()).build();
+        }
 
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+        //we are able to process incoming request body
+        context.getLogger().info("Done Preparing jsonNode <" + jsonNode + ">");
+        context.getLogger().info("json < " + jsonNode + ">");  //don't do this to avoid logging password
+
+        // Parse request parameters
+        String mlHost = getValueFromJson(jsonNode, "host");
+        String mlUser = getValueFromJson(jsonNode, "username");
+        String mlPassword = getValueFromJson(jsonNode, "password");
+        String flowName = getValueFromJson(jsonNode, "flowName");
+        String flowSteps = getValueFromJson(jsonNode, "steps");
+
+        String inputData = "host <" + mlHost + "> username <" + mlUser + "> flowName <" + flowName +
+            "> steps <" + flowSteps + ">";
+        context.getLogger().info("Input data: <" + inputData + ">");
+
+        // flow-steps is optional, so no need to check
+        if ((mlHost == null) || (mlUser == null) || (mlPassword == null) || (flowName == null)) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(
+                "Please pass values for host/username/password/flowName in the request body").build();
         } else {
-            // Write the name to the message queue.
-            msg.setValue(name);
-            String runFlowMessage = "Flow <" + name + "> successfully executed."; //default message
+            // Write the input values to the message queue (not logging the password though)
+            String inputMsg = "host <" + mlHost + "> username <" + mlUser + "> flowName <" + flowName + ">";
+            msg.setValue(inputMsg);
+
+            String runFlowMessage = "";
             try {
-                runFlow(name);
+                runFlowMessage = runFlow(mlHost, mlUser, mlPassword, flowName, flowSteps);
             } catch (Exception ex) {
                 context.getLogger().severe("Exception caught: " + ex.getMessage());
                 runFlowMessage = ex.getMessage(); //set to exception
+                httpCode = HttpStatus.BAD_REQUEST;
             }
-            return request.createResponseBuilder(HttpStatus.OK).body(runFlowMessage).build();
+
+            //return the response
+            return request.createResponseBuilder(httpCode).body(runFlowMessage).build();
         }
     }
 
-    // Run DataHub flow
-    public void runFlow(String flowName) {
-        System.out.println ("**** Inside AzureFunction runFlow method");
-        // Create a FlowRunner instance.
-        FlowRunner flowRunner = new FlowRunnerImpl("localhost", "ml-admin-user", "ml-admin-pwd");
+    // simple utility method to retrieve attribute values
+    protected String getValueFromJson(JsonNode jsonNode, String key) {
+        if (jsonNode == null) {
+            return null;
+        }
+        if (jsonNode.has(key)) {
+            return jsonNode.get(key).asText();
+        } else {
+            return null;
+        }
+    }
+
+    // Build flow inputs object based on the inputs received from the request payload
+    // Returns a valid FlowInputs object OR null in case of invalid inputs
+    protected FlowInputs buildFlowInputs(String flowName, String flowSteps) {
+        // supports flowname and flowsteps. Do we need to also support JobId and Options?
+
+        //validdate inputs first - look for valid flow name
+        if ( (flowName == null) || (flowName.trim().length() <= 0) ) {
+            return null;
+        }
 
         // Specify the flow to run.
         FlowInputs inputs = new FlowInputs(flowName);
 
-        // To run only a subset of the steps in the flow, uncomment the following line and specify the sequence numbers of the steps to run.
-        // inputs.setSteps(Arrays.asList("2,3,4"));
+        // If flow steps are provided, then set them as inputs.
+        // Q: Are the flow steps always Integers?
+        // Ans: Per Rob, lets treat them as strings for now. So, not adding additional test cases
+        // inputs.setSteps(Arrays.asList("2,3,4"))
+        if (flowSteps != null) {
+            inputs.setSteps(Arrays.asList(flowSteps.split(",")));
+        }
+
+        return inputs;
+    }
+
+    // Run DataHub flow
+    public String runFlow(String mlHost, String mlUser, String mlPassword, String flowName, String flowSteps) {
+        System.out.println ("**** Inside AzureFunction runFlow method");
+        // Create a FlowRunner instance.
+        //FlowRunner flowRunner = new FlowRunnerImpl("localhost", "ml-admin-user", "ml-admin-pwd");
+        FlowRunner flowRunner = new FlowRunnerImpl(mlHost, mlUser, mlPassword);
+
+        // Specify the flow to run.
+        FlowInputs inputs = buildFlowInputs(flowName, flowSteps);
+        if (inputs == null) {
+            return "Invalid flow inputs. Please check.";
+        }
 
         // Run the flow.
         RunFlowResponse response = flowRunner.runFlowWithoutProject(inputs);
@@ -67,7 +137,8 @@ public class FunctionDataHubFlow {
         // Wait for the flow to end.
         flowRunner.awaitCompletion();
 
-        // Display the response.
-        System.out.println("Response: " + response);
+        // Return the response.
+        //System.out.println("runFlow Response: " + response);
+        return response.toString();
     }
 }
--- azure/flow-runner-function/src/test/java/com/marklogic/dhf/azure/FunctionDataHubFlowTest.java
+++ azure/flow-runner-function/src/test/java/com/marklogic/dhf/azure/FunctionDataHubFlowTest.java
@@ -11,9 +11,13 @@ import java.util.Optional;
 import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+import com.marklogic.hub.flow.FlowInputs;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Unit test for Function class.
@@ -22,23 +26,27 @@ public class FunctionDataHubFlowTest {
     /**
      * Unit test for HttpExampleDataHubFlow method.
      */
-    @Test
-    public void testHttpTriggerJava() throws Exception {
-        // Setup
-        @SuppressWarnings("unchecked")
-        final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
 
+    //@Test
+    // Utility method used by juni tests
+    public HttpStatusType testRun(String reqBody) throws Exception {
+        // Setup
+        @SuppressWarnings("unchecked") final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
+
+        //this is not used currently by the flow code
         final Map<String, String> queryParams = new HashMap<>();
         queryParams.put("name", "Azure");
         doReturn(queryParams).when(req).getQueryParameters();
 
-        final Optional<String> queryBody = Optional.empty();
+        System.out.println("Processing <" + reqBody + ">");
+        final Optional<String> queryBody = Optional.of(reqBody);
         doReturn(queryBody).when(req).getBody();
 
         doAnswer(new Answer<HttpResponseMessage.Builder>() {
             @Override
             public HttpResponseMessage.Builder answer(InvocationOnMock invocation) {
                 HttpStatus status = (HttpStatus) invocation.getArguments()[0];
+                System.out.println("Invocation Status <" + status + ">");
                 return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
             }
         }).when(req).createResponseBuilder(any(HttpStatus.class));
@@ -46,19 +54,71 @@ public class FunctionDataHubFlowTest {
         final ExecutionContext context = mock(ExecutionContext.class);
         doReturn(Logger.getGlobal()).when(context).getLogger();
 
-        @SuppressWarnings("unchecked")
-        final OutputBinding<String> msg = (OutputBinding<String>)mock(OutputBinding.class);
+        @SuppressWarnings("unchecked") final OutputBinding<String> msg = (OutputBinding<String>) mock(OutputBinding.class);
 
         // Invoke
         // Running invalid flow throws Exception. The test fails without the try/catch block.
+        HttpStatusType retCode = HttpStatus.OK;
         try {
             final HttpResponseMessage ret = new FunctionDataHubFlow().run(req, msg, context);
+            System.out.println(ret.getBody());
+            retCode = ret.getStatus();
         } catch (java.lang.RuntimeException ex) {
-            System.out.println (ex.getMessage());
+            System.out.println("testHttpTriggerJava ret <" + ex.getMessage() + ">");
         }
 
-        //TO-DO: Fix this
-        // Verify - forcing OK to account for RuntimeException.
-        assertEquals(HttpStatus.OK, HttpStatus.OK);
+        return retCode;
     }
+
+    // Test with missing flow name in the request body
+    @Test
+    public void testRunNoFlow() throws Exception {
+        System.out.println(">>>Test testRunNoFlow");
+        String reqBody = "{ \"host\":\"dummy\", \"username\":\"dummy\", \"password\":\"dummy\" }";
+        HttpStatusType retCode = testRun(reqBody);
+        assertEquals (HttpStatus.BAD_REQUEST, retCode);
+    }
+
+    // Test with incorrect host name (should get java.net.UnknownHostException)
+    // Note: If you pass valid values, this could actually run the flow correctly.
+    @Test
+    public void testRunWithFlow() throws Exception {
+        System.out.println(">>>Test testRunWithFlow");
+        String reqBody = "{ \"host\":\"dummy\", \"username\":\"dummy\", \"password\":\"dummy\", \"flowName\":\"dummy\" }";
+        HttpStatusType retCode = testRun(reqBody);
+        assertEquals (HttpStatus.BAD_REQUEST, retCode);
+    }
+
+    @Test
+    public void testBuildFlowInputs() throws Exception {
+        System.out.println(">>>Test testBuildFlow");
+
+        // check for invalid flow name - empty string (simulate {"flow-name": ""} input
+        FlowInputs flowInputs = new FunctionDataHubFlow().buildFlowInputs("", "2,3,5");
+        assertTrue(flowInputs == null);
+
+        // check for invalid flow name - empty string (simulate missing flow-name entry in the payload
+        flowInputs = new FunctionDataHubFlow().buildFlowInputs(null, "2,3,5");
+        assertTrue(flowInputs == null);
+
+        //valid flow name
+        flowInputs = new FunctionDataHubFlow().buildFlowInputs("flowname", "2,3,5");
+        assertTrue(flowInputs != null);
+    }
+
+    @Test
+    public void testGetValueFromJson() throws Exception {
+        System.out.println(">>>Test testGetValueFromJson");
+        String reqBody = "{ \"host\":\"dummy-host\", \"username\":\"dummy-user\", \"password\":\"dummy-pwd\", " +
+            "\"flowName\":\"dummy-flow\" }";
+
+        FunctionDataHubFlow hubFlow = new FunctionDataHubFlow();
+        ObjectMapper objmapper = new ObjectMapper();
+        JsonNode jsonNode = objmapper.readTree(reqBody);
+        assertEquals("dummy-host", hubFlow.getValueFromJson(jsonNode, "host"));
+        assertEquals("dummy-user", hubFlow.getValueFromJson(jsonNode, "username"));
+        assertEquals("dummy-pwd", hubFlow.getValueFromJson(jsonNode, "password"));
+        assertEquals("dummy-flow", hubFlow.getValueFromJson(jsonNode, "flowName"));
+    }
+
 }
