6dca99e7d84856df179ac5523988c7adda78970c
bsrikan <srikanth.balasubramanian@marklogic.com>
2020-04-21 20:10:47 -0700

    DHFPROD-4025: unit test for failed jobStatus and some refactoring

--- marklogic-data-hub-central/ui/src/config/bench.config.ts
+++ marklogic-data-hub-central/ui/src/config/bench.config.ts
@@ -48,11 +48,11 @@ class MockRolesService implements IRolesContextInterface{
 }
 
 const response = {"data":{"jobId": "350da405-c1e9-4fa7-8269-d9aefe3b4b9a"}, "status": 200};
-const jobResp = {
+const jobRespFailedWithError = {
   "data": {
     "jobId": "350da405-c1e9-4fa7-8269-d9aefe3b4b9a",
     "flow": "testFlow",
-    "user": "admin",
+    "user": "dh-dev",
     "lastAttemptedStep": "1",
     "lastCompletedStep": "1",
     "timeStarted": "2020-04-04T01:17:44.918282-07:00",
@@ -80,6 +80,43 @@ const jobResp = {
       }
     },
     "jobStatus": "finished_with_errors"
+  },
+  "status" : 200
+};
+
+const jobRespFailed = {
+  "data": {
+    "jobId": "350da405-c1e9-4fa7-8269-d9aefe3b4b9a",
+    "flow": "testFlow",
+    "user": "dh-dev",
+    "lastAttemptedStep": "1",
+    "lastCompletedStep": "1",
+    "timeStarted": "2020-04-04T01:17:44.918282-07:00",
+    "timeEnded": "2020-04-04T01:17:45.012137-07:00",
+    "stepResponses": {
+      "1": {
+        "flowName": "testFlow",
+        "stepName": "failedIngest",
+        "stepDefinitionName": "default-ingestion",
+        "stepDefinitionType": "ingestion",
+        "stepOutput": [
+          "Local message: failed to apply resource at documents: Bad Request. Server Message: XDMP-JSONDOC: xdmp:multipart-decode(\"85edd62b-0e89-4373-80ed-64a144bdb9ba\", binary{\"2d2d38356564643632622d306538392d343337332d383065642d363461313434...\"}) -- Document is not JSON",
+          "Local message: failed to apply resource at documents: Bad Request. Server Message: XDMP-JSONDOC: xdmp:multipart-decode(\"9fed61d9-76ed-45e6-8639-66d8631b61d2\", binary{\"2d2d39666564363164392d373665642d343565362d383633392d363664383633...\"}) -- Document is not JSON",
+          "Local message: failed to apply resource at documents: Bad Request. Server Message: XDMP-JSONDOC: xdmp:multipart-decode(\"8d14ada9-d143-429f-b029-4e254828312f\", binary{\"2d2d38643134616461392d643134332d343239662d623032392d346532353438...\"}) -- Document is not JSON"
+        ],
+        "fullOutput": null,
+        "status": "completed step 2",
+        "totalEvents": 3,
+        "successfulEvents": 0,
+        "failedEvents": 3,
+        "successfulBatches": 0,
+        "failedBatches": 3,
+        "success": false,
+        "stepStartTime": "2020-04-04T01:17:44.936121-07:00",
+        "stepEndTime": "2020-04-04T01:17:45.012137-07:00"
+      }
+    },
+    "jobStatus": "failed"
   },
   "status" : 200
 };
@@ -138,7 +175,8 @@ const data = {
     flows: flows,
     response: response,
     loads: loads,
-    jobResp: jobResp,
+    jobRespFailedWithError: jobRespFailedWithError,
+    jobRespFailed: jobRespFailed,
     mockRoleService: new MockRolesService()
 };
 
