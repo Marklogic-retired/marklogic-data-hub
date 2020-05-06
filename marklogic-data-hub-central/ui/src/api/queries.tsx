66900cac843e189280f6d0d315df789bb11d4e8b
Timur Isangulov <timur.isangulov@marklogic.com>
2020-05-05 17:15:34 -0700

    DHFPROD-4224: Start export of search results directly from the table viz. in the UI

--- marklogic-data-hub-central/ui/src/api/queries.tsx
+++ marklogic-data-hub-central/ui/src/api/queries.tsx
@@ -32,3 +32,34 @@ export const removeQuery = async (query) => {
     url: `/api/entitySearch/savedQueries/query?id=${query.savedQuery.id}`
   });
 }
+
+export const exportQuery = (query, limit) => {
+  let queryString = JSON.stringify(query)
+  const mapForm = document.createElement("form");
+  mapForm.target ="_self"||"_blank";
+  mapForm.id="exportForm";
+  mapForm.method = "POST";
+  mapForm.action = "/api/entitySearch/export";
+  const mapInput = document.createElement("input");
+  mapInput.type = "hidden";
+  mapInput.name = "fileType";
+  mapInput.value = "csv";
+  const mapInput2 = document.createElement("input");
+  mapInput2.type = "hidden";
+  mapInput2.name = "limit";
+  mapInput2.value = limit === Number.MAX_SAFE_INTEGER || limit < 1 ? '' : limit;
+  const mapInput3 = document.createElement("input");
+  mapInput3.type = "hidden";
+  mapInput3.name = "queryDocument";
+  mapInput3.value = queryString;
+  mapForm.appendChild(mapInput);
+  mapForm.appendChild(mapInput2);
+  mapForm.appendChild(mapInput3);
+  document.body.appendChild(mapForm);
+  mapForm.submit();
+  mapForm.reset();
+}
+
+export const exportSavedQuery = (id, limit) => {
+  window.open(`/api/entitySearch/export/query/${id}?fileType=csv&limit=${limit === Number.MAX_SAFE_INTEGER || limit < 1 ? '' : limit}`, '_self');
+}
