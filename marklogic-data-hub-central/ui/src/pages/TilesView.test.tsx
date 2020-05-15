5896fba2e7a0f5974253d2439375b8a6595bcfd1
bsrikan <srikanth.balasubramanian@marklogic.com>
2020-05-15 00:49:53 -0700

    DHFPROD-4772: Test for run tile

--- marklogic-data-hub-central/ui/src/pages/TilesView.test.tsx
+++ marklogic-data-hub-central/ui/src/pages/TilesView.test.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { render, fireEvent, waitForElement, cleanup, getAllByRole } from '@testing-library/react'
+import { render, fireEvent, waitForElement, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import TilesView from './TilesView';
+import {AuthoritiesContext} from '../util/authorities';
 import axiosMock from 'axios';
 import curateData from '../config/bench.config';
+import authorities from '../config/authorities.config';
 
 jest.mock('axios');
+
+const mockDevRolesService = authorities.DeveloperRolesService;
 
 describe('TilesView component', () => {
 
@@ -15,7 +19,7 @@ describe('TilesView component', () => {
     })
     
     test('Verify TilesView renders with the toolbar', async () => {
-        const {getByLabelText, getAllByRole, debug} = render(<TilesView/>);
+        const {getByLabelText} = render(<TilesView/>);
 
         expect(getByLabelText("toolbar")).toBeInTheDocument();
 
@@ -34,7 +38,7 @@ describe('TilesView component', () => {
     test('Verify Curate tile displays from toolbar', async () => {
         const {getByLabelText, getByText, queryByText} = render(<TilesView/>);
 
-        // Tile not shown initially
+        // Curate tile not shown initially
         expect(queryByText("icon-curate")).not.toBeInTheDocument();
         expect(queryByText("title-curate")).not.toBeInTheDocument();
 
@@ -52,10 +56,39 @@ describe('TilesView component', () => {
         }) 
         fireEvent.click(getByLabelText("tool-curate"));
         
-        // Curate Tile shown with entityTypes after click
+        // Curate tile shown with entityTypes after click
         expect(await(waitForElement(() => getByLabelText("icon-curate")))).toBeInTheDocument();
         expect(getByLabelText("title-curate")).toBeInTheDocument();
         expect(getByText('Customer')).toBeInTheDocument();
+    });
+
+    test('Verify Run tile displays from toolbar', async () => {
+        const {getByLabelText, getByText, queryByText} = await render(<AuthoritiesContext.Provider value={ mockDevRolesService}><TilesView/></AuthoritiesContext.Provider>);
+
+        // Run tile not shown initially
+        expect(queryByText("icon-run")).not.toBeInTheDocument();
+        expect(queryByText("title-run")).not.toBeInTheDocument();
+
+        await axiosMock.get['mockImplementation']((url) => {
+            switch (url) {
+                case '/api/flows':
+                    return Promise.resolve(curateData.flows)
+                case '/api/artifacts/loadData':
+                    return Promise.resolve(curateData.loads)
+                case '/api/artifacts/mapping':
+                    return Promise.resolve(curateData.mappings)
+                default:
+                    return Promise.reject(new Error('not found'))
+            }
+        }) 
+        fireEvent.click(getByLabelText("tool-run"));
+        
+        // Run tile shown with entityTypes after click
+        expect(await(waitForElement(() => getByLabelText("icon-run")))).toBeInTheDocument();
+        expect(getByLabelText("title-run")).toBeInTheDocument();
+        expect(document.querySelector('#flows-container')).toBeInTheDocument();
+        expect(getByText('Create Flow')).toBeInTheDocument();
+        expect(getByText('testFlow')).toBeInTheDocument();
     });
 
 });
