0de7e8de879c3bb85155d1e6feec7f8273dca8ab
Nikhil Shrivastava <nikhil.shrivastava@marklogic.com>
2020-06-08 14:30:48 -0700

    DHFPROD-4723: Fixing the source format labels in the load cards

--- marklogic-data-hub-central/ui/src/config/formats.config.js
+++ marklogic-data-hub-central/ui/src/config/formats.config.js
@@ -1,9 +1,11 @@
 //This file contains an object which stores source format options and its related properties. It can be used across the project.
 
 const sourceFormatOptions = {
-    json: { color: '#ffbf00' },
-    xml: { color: '#5bd171' },
-    csv: { color: '#531dab' },
+    json: { label: 'JSON', color: '#ffbf00' },
+    xml: { label: 'XML', color: '#5bd171' },
+    csv: { label: 'CSV', color: '#531dab' },
+    text: { label: 'TXT', color: '#1254f9'},
+    binary : { label: 'BIN', color: '#444444'},
     default: {color: '#44499C'}
 }
 
