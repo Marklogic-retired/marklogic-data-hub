e6d145a8c0cb959e2b75e052a27217c802e08a67
bsrikan <srikanth.balasubramanian@marklogic.com>
2020-06-11 15:28:36 -0700

    DHFPROD-5164: DH project for e2e tests in HC

--- marklogic-data-hub-central/ui/e2e/setup.sh
+++ marklogic-data-hub-central/ui/e2e/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-cd qa-project;
 
 if [[ $1 != *"dhs"* || $2 != *"mlHost"* ]]
 then
@@ -9,24 +8,27 @@ then
 fi
 DHS=`echo $1 | cut -d'=' -f 2`
 mlHost=`echo $2 | cut -d'=' -f 2`
-env='local'
 
 credentials='-PmlUsername=hc-developer -PmlPassword=password'
 
-./gradlew hubInit
-./gradlew hubMigrateFlows -Pconfirm=true
-
 if $DHS
 then
+        cd hc-qa-project
+        ./gradlew hubInit
+        ./gradlew hubSaveIndexes -PmlHost=$mlHost --info --stacktrace
         ./gradlew hubDeployAsDeveloper  --info --stacktrace $credentials -PenvironmentName=dhs -PmlHost=$mlHost
-        env='dhs'
 else
+        cd qa-project
+        ./gradlew hubInit
+        ./gradlew hubMigrateFlows -Pconfirm=true
         cp ../cypress/fixtures/users/* src/main/ml-config/security/users/
+
         ./gradlew mlDeploy -PmlHost=$mlHost --info --stacktrace
         ./gradlew hubSaveIndexes -PmlHost=$mlHost --info --stacktrace
         ./gradlew hubDeployAsDeveloper $credentials -PmlHost=$mlHost --info --stacktrace
-fi
 
-./gradlew hubRunFlow $credentials -PenvironmentName=$env -PflowName=AdvantageFlow -PentityName=Customer -PbatchSize=100 -PthreadCount=4 -Psteps='1,2' -PmlHost=$mlHost  --info --stacktrace
-./gradlew hubRunFlow $credentials -PenvironmentName=$env -PflowName=PersonFlow -PentityName=Person -PbatchSize=100 -PthreadCount=4 -Psteps='1,2' -PmlHost=$mlHost  --info --stacktrace
-./gradlew hubRunFlow $credentials -PenvironmentName=$env -PflowName=PersonXMLFlow -PentityName=PersonXML -PbatchSize=100 -PthreadCount=4 -Psteps='1,2' -PmlHost=$mlHost  --info --stacktrace
+        ./gradlew hubRunFlow $credentials -PflowName=AdvantageFlow -PentityName=Customer -PbatchSize=100 -PthreadCount=4 -Psteps='1,2' -PmlHost=$mlHost  --info --stacktrace
+        ./gradlew hubRunFlow $credentials -PflowName=PersonFlow -PentityName=Person -PbatchSize=100 -PthreadCount=4 -Psteps='1,2' -PmlHost=$mlHost  --info --stacktrace
+        ./gradlew hubRunFlow $credentials -PflowName=PersonXMLFlow -PentityName=PersonXML -PbatchSize=100 -PthreadCount=4 -Psteps='1,2' -PmlHost=$mlHost  --info --stacktrace
+
+fi
