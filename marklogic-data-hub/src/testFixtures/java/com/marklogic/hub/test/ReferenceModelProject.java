e1d13b13852e9d5b30a37e97b478775bc9009890
rahulvudutala <rahulbharadwaj.v@gmail.com>
2020-06-28 21:53:07 -0700

    DHFPROD-5114: Fixing 500 Error for XML Documents on clicking Details Link

--- marklogic-data-hub/src/testFixtures/java/com/marklogic/hub/test/ReferenceModelProject.java
+++ marklogic-data-hub/src/testFixtures/java/com/marklogic/hub/test/ReferenceModelProject.java
@@ -1,15 +1,23 @@
 package com.marklogic.hub.test;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.marklogic.client.document.GenericDocumentManager;
 import com.marklogic.client.document.JSONDocumentManager;
 import com.marklogic.client.io.DocumentMetadataHandle;
+import com.marklogic.client.io.Format;
+import com.marklogic.client.io.InputStreamHandle;
 import com.marklogic.client.io.JacksonHandle;
 import com.marklogic.hub.HubClient;
 import com.marklogic.hub.flow.FlowInputs;
 import com.marklogic.hub.flow.FlowRunner;
 import com.marklogic.hub.flow.RunFlowResponse;
 import com.marklogic.hub.flow.impl.FlowRunnerImpl;
+
+import java.io.ByteArrayInputStream;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class ReferenceModelProject extends TestObject {
 
@@ -33,31 +41,56 @@ public class ReferenceModelProject extends TestObject {
     }
 
     public void createCustomerInstance(Customer customer) {
+        createCustomerInstance(customer, Format.JSON);
+    }
+
+    public void createCustomerInstance(Customer customer, Format contentFormat) {
         String customerEntityType = "Customer";
 
-        JSONDocumentManager mgr = hubClient.getFinalClient().newJSONDocumentManager();
-        ObjectNode customerProps = objectMapper.createObjectNode();
+        Map<String, Object> infoMap = new LinkedHashMap<>();
+        infoMap.put("title", customerEntityType);
+        infoMap.put("version", "0.0.1");
+        infoMap.put("baseUri", "http://example.org/");
+
+        Map<String, Object> customerProps = new LinkedHashMap<>();
         customerProps.put("customerId", customer.customerId);
         customerProps.put("name", customer.name);
         customerProps.put("customerNumber", customer.customerNumber);
         customerProps.put("customerSince", customer.customerSince);
 
-        ObjectNode infoProp = objectMapper.createObjectNode();
-        infoProp.put("title", customerEntityType);
-        infoProp.put("version", "0.0.1");
-        infoProp.put("baseUri", "http://example.org/");
+        Map<String, Object> instanceMap = new LinkedHashMap<>();
+        instanceMap.put("info", infoMap);
+        instanceMap.put(customerEntityType, customerProps);
 
-        ObjectNode instanceProps = objectMapper.createObjectNode();
-        instanceProps.set("info", infoProp);
-        instanceProps.set(customerEntityType, customerProps);
+        Map<String, Object> entityInstanceMap = new LinkedHashMap<>();
+        entityInstanceMap.put("instance", instanceMap);
 
-        JsonNode instance = objectMapper.createObjectNode().set("instance", instanceProps);
-        JsonNode envelope = objectMapper.createObjectNode().set("envelope", instance);
+        byte[] instanceBytes;
+        ByteArrayInputStream instanceByteStream = null;
+        String fileExtension;
+        GenericDocumentManager mgr = hubClient.getFinalClient().newDocumentManager();
+        mgr.setContentFormat(contentFormat);
+
+        try {
+            if(contentFormat.getDefaultMimetype().equals("application/xml")) {
+                XmlMapper xmlMapper = new XmlMapper();
+                instanceBytes = xmlMapper.writer().withRootName("envelope").writeValueAsBytes(entityInstanceMap);
+                instanceByteStream = new ByteArrayInputStream(instanceBytes);
+                fileExtension = ".xml";
+            } else {
+                ObjectMapper mapper = new ObjectMapper();
+                instanceBytes = mapper.writer().withRootName("envelope").writeValueAsBytes(entityInstanceMap);
+                instanceByteStream = new ByteArrayInputStream(instanceBytes);
+                fileExtension = ".json";
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
 
         DocumentMetadataHandle metadata = new DocumentMetadataHandle()
-            .withCollections(customerEntityType)
-            .withPermission("data-hub-common", DocumentMetadataHandle.Capability.READ, DocumentMetadataHandle.Capability.UPDATE);
-        mgr.write("/" + customerEntityType + customer.customerId + ".json", metadata, new JacksonHandle(envelope));
+                .withCollections(customerEntityType)
+                .withPermission("data-hub-common", DocumentMetadataHandle.Capability.READ, DocumentMetadataHandle.Capability.UPDATE);
+        mgr.write("/" + customerEntityType + customer.customerId + fileExtension, metadata, new InputStreamHandle(instanceByteStream));
     }
 
     public RunFlowResponse runFlow(FlowInputs flowInputs) {
