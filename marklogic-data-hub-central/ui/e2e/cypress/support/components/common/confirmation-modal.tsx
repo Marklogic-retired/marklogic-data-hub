cdc0db7a2a8d4703196f40b43d2037f30c2e5a24
Bruce An <Bruce.An@marklogic.com>
2020-06-30 09:24:16 -0700

    DHFPROD-5314: Structured types can have same property name as Entity type

--- marklogic-data-hub-central/ui/e2e/cypress/support/components/common/confirmation-modal.tsx
+++ marklogic-data-hub-central/ui/e2e/cypress/support/components/common/confirmation-modal.tsx
@@ -13,6 +13,18 @@ class ConfirmationModal {
   getToggleStepsButton() {
     return cy.findByLabelText('toggle-steps');
   }
+  getSaveEntityText() {
+    return cy.get('#save-text', {timeout: 25000});
+  }
+  getDeleteEntityText() {
+    return cy.get('#delete-text', {timeout: 15000});
+  }
+  getDeleteEntityRelationshipText() {
+    return cy.get('#delete-relationship-text', {timeout: 15000});
+  }
+  getDeleteEntityStepText() {
+    return cy.get('#delete-step-text', {timeout: 15000});
+  }
 }
 
 const confirmationModal = new ConfirmationModal();
