024eaae258ba4965021e3f66a9f5d2786af1dce9
Timur Isangulov <timur.isangulov@marklogic.com>
2020-07-09 14:26:36 -0700

    DHFPROD-5349: Various roles can access tiles they are not supposed to

--- marklogic-data-hub-central/ui/src/components/tiles/toolbar.test.tsx
+++ marklogic-data-hub-central/ui/src/components/tiles/toolbar.test.tsx
@@ -24,4 +24,24 @@ describe('Toolbar component', () => {
 
     });
 
+    it('verify rendering of disabled tile icons', () => {
+        let disabledTiles = ['load', 'model', 'curate', 'run', 'explore']
+        const {getByLabelText} = render(<Router history={history}><Toolbar tiles={tiles} enabled={[]}/></Router>);
+        expect(getByLabelText("toolbar")).toBeInTheDocument();
+
+        disabledTiles.forEach((tile, i) => {
+            expect(getByLabelText("tool-" + tile)).toHaveStyle('color: grey; opacity: 0.5; cursor: not-allowed;')
+        })
+    });
+
+    it('verify rendering of enabled tile icons', () => {
+        let enabledTiles = ['load', 'model', 'curate', 'run', 'explore']
+        const {getByLabelText} = render(<Router history={history}><Toolbar tiles={tiles} enabled={enabledTiles}/></Router>);
+        expect(getByLabelText("toolbar")).toBeInTheDocument();
+
+        enabledTiles.forEach((tile, i) => {
+            expect(getByLabelText("tool-" + tile)).not.toHaveStyle('color: grey; opacity: 0.5; cursor: not-allowed;')
+        })
+    });
+
 });
