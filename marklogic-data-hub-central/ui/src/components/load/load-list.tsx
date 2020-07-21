c20b9250073c795b53ecbb68201653796fc2abde
Brian Tang <briantang98@gmail.com>
2020-07-21 00:04:10 -0700

    DHFPROD-5048: Add steps to flow from Load TABLE view

--- marklogic-data-hub-central/ui/src/components/load/load-list.tsx
+++ marklogic-data-hub-central/ui/src/components/load/load-list.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from "react-router-dom";
+import { Link, useLocation, useHistory } from "react-router-dom";
 import styles from './load-list.module.scss';
-import {Table, Icon, Button, Tooltip, Popover, Modal} from 'antd';
+import './load-list.scss';
+import {Table, Icon, Button, Tooltip, Popover, Modal, Menu, Select, Dropdown} from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
 import {faTrashAlt} from '@fortawesome/free-regular-svg-icons';
@@ -11,14 +12,20 @@ import { convertDateFromISO } from '../../util/conversionFunctions';
 import AdvancedSettingsDialog from "../advanced-settings/advanced-settings-dialog";
 import {AdvLoadTooltips} from "../../config/tooltips.config";
 import { MLTooltip } from '@marklogic/design-system';
+import { OmitProps } from 'antd/lib/transfer/renderListBody';
 
+const {Option} = Select;
 
 interface Props {
     data: any;
+    flows: any;
+    canWriteFlow: any;
     deleteLoadArtifact: any;
     createLoadArtifact: any;
     canReadWrite: any;
     canReadOnly: any;
+    addStepToFlow: any;
+    addStepToNew: any;
   }
 
 const LoadList: React.FC<Props> = (props) => {
@@ -29,6 +36,8 @@ const LoadList: React.FC<Props> = (props) => {
     const [newDataLoad, setNewDataLoad] = useState(false);
     const [title, setTitle] = useState('');
     const [dialogVisible, setDialogVisible] = useState(false);
+    const [addDialogVisible, setAddDialogVisible] = useState(false);
+    const [flowName, setFlowName] = useState('');
     const [loadArtifactName, setLoadArtifactName] = useState('');
     const [stepData,setStepData] = useState({});
     const [openLoadSettings, setOpenLoadSettings] = useState(false);
@@ -41,6 +50,8 @@ const LoadList: React.FC<Props> = (props) => {
         setPage(Math.floor(stepIndex / pageSize) + 1);
       }
     }, [location, props.data]);
+
+    let history = useHistory();
 
     const OpenAddNewDialog = () => {
         setNewDataLoad(true);
@@ -70,8 +81,82 @@ const LoadList: React.FC<Props> = (props) => {
 
     const onCancel = () => {
         setDialogVisible(false);
+        setAddDialogVisible(false);
     }
 
+
+    function handleSelect(obj) {
+        handleStepAdd(obj.loadName, obj.flowName);
+    }
+
+
+    const handleStepAdd = (loadName, flowName) => {
+        setAddDialogVisible(true);
+        setLoadArtifactName(loadName);
+        setFlowName(flowName);
+    }
+
+    const onAddOk = async (lName, fName) => {
+        await props.addStepToFlow(lName, fName)
+        setAddDialogVisible(false);
+
+        history.push({
+            pathname: '/tiles/run/add',
+            state: {
+                flowName: fName,
+                flowsDefaultKey: [props.flows.findIndex(el => el.name === fName)],
+                existingFlow: true
+            }
+        })
+    }
+
+    const addConfirmation = (
+        <Modal
+            visible={addDialogVisible}
+            okText={<div aria-label="Yes">Yes</div>}
+            cancelText={<div aria-label="No">No</div>}
+            onOk={() => onAddOk(loadArtifactName, flowName)}
+            onCancel={() => onCancel()}
+            width={350}
+            maskClosable={false}
+        >
+            <div style={{fontSize: '16px', padding: '10px'}}>
+                Are you sure you want to add "{loadArtifactName}" to flow "{flowName}"?
+            </div>
+        </Modal>
+    );
+
+    const menu = (name) => (
+        <Menu style={{right: '80px'}}>
+            <Menu.Item key="0">
+                { <Link data-testid="link" id="tiles-run-add" to={
+                                        {pathname: '/tiles/run/add',
+                                        state: {
+                                            stepToAdd : name,
+                                            stepDefinitionType : 'ingestion',
+                                            existingFlow : false
+                                        }}}><div className={styles.stepLink} data-testid={`${name}-toNewFlow`}>Add step to a new flow</div></Link>}
+            </Menu.Item>
+            <Menu.Item key="1">
+                <div className={styles.stepLinkExisting} data-testid={`${name}-toExistingFlow`}>Add step to an existing flow 
+                    <div className={styles.stepLinkSelect} onClick={(event) => { event.stopPropagation(); event.preventDefault(); }}>
+                        <Select
+                            style={{ width: '100%' }}
+                            onChange={(flowName) => handleSelect({flowName: flowName, loadName: name})}
+                            placeholder="Select Flow"
+                            defaultActiveFirstOption={false}
+                            disabled={!props.canWriteFlow}
+                            data-testid={`${name}-flowsList`}
+                        >
+                            { props.flows && props.flows.length > 0 ? props.flows.map((f,i) => (
+                                <Option aria-label={f.name} value={f.name} key={i}>{f.name}</Option>
+                            )) : null}
+                        </Select>
+                    </div>
+                </div>
+            </Menu.Item>
+        </Menu>
+    );
     const deleteConfirmation = <Modal
         visible={dialogVisible}
         okText={<div aria-label="Yes">Yes</div>}
@@ -134,6 +219,9 @@ const LoadList: React.FC<Props> = (props) => {
             key: 'actions',
             render: (text, row) => (
                 <span>
+                    <Dropdown data-testid={`${row.name}-dropdown`} overlay={menu(row.name)} trigger={['hover']} disabled = {!props.canWriteFlow}>
+                        {props.canWriteFlow ? <span className={'AddToFlowIcon'} aria-label = {row.name+'-add-icon'}></span> : <MLTooltip title={'Add to Flow'} placement="bottom"><span aria-label = {row.name+'-disabled-add-icon'} className={'disabledAddToFlowIcon'}></span></MLTooltip>}
+                    </Dropdown>
                     <MLTooltip title={'Settings'} placement="bottom"><Icon type="setting" data-testid={row.name+'-settings'} onClick={() => OpenLoadSettingsDialog(row)} className={styles.settingsIcon} /></MLTooltip>
                     &nbsp;&nbsp;
                     {props.canReadWrite ? <MLTooltip title={'Delete'} placement="bottom"><i aria-label="icon: delete"><FontAwesomeIcon icon={faTrashAlt} data-testid={row.name+'-delete'} onClick={() => {showDeleteConfirm(row.name)}} className={styles.deleteIcon} size="lg"/></i></MLTooltip> :
@@ -180,6 +268,7 @@ const LoadList: React.FC<Props> = (props) => {
             stepData={stepData}
             canWrite={props.canReadWrite}
         />
+        {addConfirmation} 
     </div>
    );
 }
