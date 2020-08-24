a6424c98a62191a66a3ba7cf4d76cd22ec239418
bsrikan <srikanth.balasubramanian@marklogic.com>
2020-08-24 10:49:24 -0700

    DHFPROD-5793: match/merge/master steps are now visible in edit property modal
    Fix typo in writer.spec

--- marklogic-data-hub-central/ui/e2e/cypress/integration/modeling/writer.spec.tsx
+++ marklogic-data-hub-central/ui/e2e/cypress/integration/modeling/writer.spec.tsx
@@ -125,6 +125,8 @@ describe('Entity Modeling: Writer Role', () => {
     propertyTable.editProperty('fname');
     cy.waitUntil(() => propertyModal.getToggleStepsButton().should('exist')).click();
     cy.contains('mapPersonJSON').should('be.visible');
+    cy.contains('match-person').should('be.visible');
+    cy.contains('merge-person').should('be.visible');
     cy.contains('master-person').should('be.visible');
     cy.contains('Hide Steps...').should('be.visible');
 
@@ -135,6 +137,8 @@ describe('Entity Modeling: Writer Role', () => {
     cy.contains('Show Steps...').should('be.visible');
 
     cy.contains('mapPersonJSON').should('not.be.visible');
+    cy.contains('match-person').should('not.be.visible');
+    cy.contains('merge-person').should('not.be.visible');
     cy.contains('master-person').should('not.be.visible');
     cy.contains('Hide Steps...').should('not.be.visible');
     propertyModal.getCancelButton().click();
