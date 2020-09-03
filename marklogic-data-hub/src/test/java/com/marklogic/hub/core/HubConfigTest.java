c4738537e776de26c2e661f03f42e63d2ebc417e
Rob Rudin <rob.rudin@marklogic.com>
2020-09-03 15:52:48 -0400

    DHFPROD-5745: Can now run tests in parallel
    Notes:
    
    - Tests now run against a HubConfigImpl tied to a specific host; this is enabled via HubConfigInterceptor and HubConfigObjectFactory. Just set mlHost to multiple hosts, separated by commas (where each host is part of a different cluster).
    - HubConfigImpl and HubProjectImpl are no longer Components, which greatly complicated Spring config in tests. Instead, HubConfigImpl is a Bean in ApplicationConfig and WebApplication so as to preserve existing functionality.
    - Because HubProjectImpl is no longer Spring-managed but still depends on a FlowManager, a FlowManager is required by the method that depends on it
    - ScaffoldingValidator is no longer a Spring component - instead, it's a simple class whose constructor depends on a HubProject
    - StepDefinitionManagerImpl implements StepDefinitionProvider, allowing ProjectStepDefinitionProvider to be deleted.
    - StepRunnerFactory is no longer a Spring component; it requires either a HubConfig or HubClient to be instantiated. With a HubConfig, it constructs a StepDefinitionManagerImpl to act as its StepDefinitionProvider. 
    - Reworked FlowRunnerImpl so that FlowRunnerTask does not depend on a Spring-managed StepRunnerFactory, which didn't work with parallel tests. Now, a non-Spring-managed StepRunnerFactory is passed to FlowRunnerTask.
    - Installer is now a regular main program that can also run against multiple hosts at once
    - Deleted RunMappingTestsWithoutProjectTest, which has outlived its purpose; we have many tests now that run tests without a project
    - Deleted EndToEndAPITest in QS, as the test hasn't been run in many months (it was excluded in build.gradle) and isn't working

--- marklogic-data-hub/src/test/java/com/marklogic/hub/core/HubConfigTest.java
+++ marklogic-data-hub/src/test/java/com/marklogic/hub/core/HubConfigTest.java
@@ -7,10 +7,9 @@ import com.marklogic.client.ext.SecurityContextType;
 import com.marklogic.hub.AbstractHubCoreTest;
 import com.marklogic.hub.DatabaseKind;
 import com.marklogic.hub.HubConfig;
-import com.marklogic.hub.error.DataHubConfigurationException;
+import com.marklogic.hub.impl.HubConfigImpl;
+import com.marklogic.hub.impl.HubProjectImpl;
 import com.marklogic.mgmt.util.SimplePropertySource;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Properties;
@@ -19,14 +18,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class HubConfigTest extends AbstractHubCoreTest {
 
-    @AfterEach
-    public void cleanup() {
-        resetProperties();
-    }
-
     @Test
     public void testAppConfigDefaultProps() {
-        AppConfig config = adminHubConfig.getAppConfig();
+        HubConfigImpl hubConfig = new HubConfigImpl();
+        hubConfig.applyProperties(new Properties());
+        AppConfig config = hubConfig.getAppConfig();
         assertEquals(HubConfig.DEFAULT_FINAL_NAME, config.getContentDatabaseName());
         assertEquals(HubConfig.DEFAULT_FINAL_TRIGGERS_DB_NAME, config.getTriggersDatabaseName());
         assertEquals(HubConfig.DEFAULT_FINAL_SCHEMAS_DB_NAME, config.getSchemasDatabaseName());
@@ -35,25 +31,16 @@ public class HubConfigTest extends AbstractHubCoreTest {
 
     @Test
     public void applyFinalConnectionPropsToDefaultRestConnection() {
-        Assumptions.assumeFalse(getDataHubAdminConfig().getIsProvisionedEnvironment());
-
-        AppConfig config = adminHubConfig.getAppConfig();
+        HubConfigImpl hubConfig = new HubConfigImpl(getHubProject());
+        hubConfig.applyProperties(new Properties());
+        AppConfig config = hubConfig.getAppConfig();
 
         assertEquals((Integer) 8011, config.getRestPort(),
             "The final port should be used as restPort so that any ml-gradle feature that depends on mlRestPost " +
                 "ends up talking to the final app server");
-        if (!(isCertAuth() || isSslRun())) {
-            assertNull(config.getRestSslContext(), "Should be null because neither mlSimpleSsl nor mlFinalSimpleSsl were set to true");
-            assertNull(config.getRestSslHostnameVerifier(), "Should be null because neither mlSimpleSsl nor mlFinalSimpleSsl were set to true");
-            assertNull(config.getRestTrustManager(), "Should be null because neither mlSimpleSsl nor mlFinalSimpleSsl were set to true");
-        }
-        //get the old values
-        String port = adminHubConfig.getPort(DatabaseKind.FINAL).toString();
-        String authMethod = adminHubConfig.getAuthMethod(DatabaseKind.FINAL);
-        String certFile = adminHubConfig.getCertFile(DatabaseKind.FINAL);
-        String certPassword = adminHubConfig.getCertPassword(DatabaseKind.FINAL);
-        String extName = adminHubConfig.getExternalName(DatabaseKind.FINAL);
-        Boolean sslMethod = adminHubConfig.getSimpleSsl(DatabaseKind.FINAL);
+        assertNull(config.getRestSslContext(), "Should be null because neither mlSimpleSsl nor mlFinalSimpleSsl were set to true");
+        assertNull(config.getRestSslHostnameVerifier(), "Should be null because neither mlSimpleSsl nor mlFinalSimpleSsl were set to true");
+        assertNull(config.getRestTrustManager(), "Should be null because neither mlSimpleSsl nor mlFinalSimpleSsl were set to true");
 
         Properties props = new Properties();
         props.put("mlFinalAuth", "basic");
@@ -63,10 +50,10 @@ public class HubConfigTest extends AbstractHubCoreTest {
         props.put("mlFinalExternalName", "somename");
         props.put("mlFinalSimpleSsl", "true");
 
-        resetProperties();
-        adminHubConfig.loadConfigurationFromProperties(props, true);
+        hubConfig.applyDefaultPropertyValues();
+        hubConfig.loadConfigurationFromProperties(props, true);
 
-        config = adminHubConfig.getAppConfig();
+        config = hubConfig.getAppConfig();
 
         assertEquals(SecurityContextType.BASIC, config.getRestSecurityContextType());
         assertEquals((Integer) 8123, config.getRestPort());
@@ -76,63 +63,33 @@ public class HubConfigTest extends AbstractHubCoreTest {
         assertNotNull(config.getRestSslContext(), "Should have been set because mlFinalSimpleSsl=true");
         assertNotNull(config.getRestSslHostnameVerifier(), "Should have been set because mlFinalSimpleSsl=true");
         assertNotNull(config.getRestTrustManager(), "Should have been set because mlFinalSimpleSsl=true");
-
-        props = new Properties();
-        //reset them
-        props.put("mlFinalAuth", authMethod);
-        props.put("mlFinalPort", port);
-
-        //these values not set by dhf-default, so checking for null
-        if (certFile != null)
-            props.put("mlFinalCertFile", certFile);
-        if (certPassword != null)
-            props.put("mlFinalCertPassword", certPassword);
-        if (extName != null)
-            props.put("mlFinalExternalName", extName);
-        props.put("mlFinalSimpleSsl", sslMethod);
-        //if sslContext is set , it is assumed that it is a secure connection, hence unsetting them
-        if (!sslMethod) {
-            adminHubConfig.setSslContext(DatabaseKind.FINAL, null);
-            adminHubConfig.setSslHostnameVerifier(DatabaseKind.FINAL, null);
-            adminHubConfig.setTrustManager(DatabaseKind.FINAL, null);
-        }
-
-
     }
 
     @Test
     public void testConfigAppName() {
+        HubConfigImpl hubConfig = new HubConfigImpl();
+        hubConfig.applyProperties(new Properties());
         // When not set, app name should default to "DHF"
-        assertEquals("DHF", adminHubConfig.getAppConfig().getName());
+        assertEquals("DHF", hubConfig.getAppConfig().getName());
 
         String appName = "data-hub";
         Properties props = new Properties();
         props.setProperty("mlAppName", appName);
-        adminHubConfig.applyProperties(new SimplePropertySource(props));
-        assertEquals(appName, adminHubConfig.getAppConfig().getName());
+        hubConfig.applyProperties(new SimplePropertySource(props));
+        assertEquals(appName, hubConfig.getAppConfig().getName());
     }
 
 
     @Test
-    public void testHubInfo() {
-
-        HubConfig config = adminHubConfig;
+    public void testHubInfo() throws Exception {
+        HubConfigImpl config = new HubConfigImpl(getHubProject());
+        config.applyProperties(new Properties());
         ObjectMapper objmapper = new ObjectMapper();
 
-        try {
-
-            JsonNode jsonNode = objmapper.readTree(config.getInfo());
-
-            assertEquals(jsonNode.get("stagingDbName").asText(), config.getDbName(DatabaseKind.STAGING));
-
-            assertEquals(jsonNode.get("stagingHttpName").asText(), config.getHttpName(DatabaseKind.STAGING));
-
-            assertEquals(jsonNode.get("finalForestsPerHost").asInt(), (int) config.getForestsPerHost(DatabaseKind.FINAL));
-
-            assertEquals(jsonNode.get("finalPort").asInt(), (int) config.getPort(DatabaseKind.FINAL));
-
-        } catch (Exception e) {
-            throw new DataHubConfigurationException("Your datahub configuration could not serialize");
-        }
+        JsonNode jsonNode = objmapper.readTree(config.getInfo());
+        assertEquals(jsonNode.get("stagingDbName").asText(), config.getDbName(DatabaseKind.STAGING));
+        assertEquals(jsonNode.get("stagingHttpName").asText(), config.getHttpName(DatabaseKind.STAGING));
+        assertEquals(jsonNode.get("finalForestsPerHost").asInt(), (int) config.getForestsPerHost(DatabaseKind.FINAL));
+        assertEquals(jsonNode.get("finalPort").asInt(), (int) config.getPort(DatabaseKind.FINAL));
     }
 }
