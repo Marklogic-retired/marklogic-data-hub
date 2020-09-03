1882407b76b7a5d52530a6c80d8fdac911794c85
Rob Rudin <rob.rudin@marklogic.com>
2020-09-03 15:52:48 -0400

    DHFPROD-5745: Can now run tests in parallel
    Notes:
    
    - Tests now run against a HubConfigImpl tied to a specific host; this is enabled via HubConfigInterceptor and HubConfigObjectFactory. Just set mlHost to multiple hosts, separated by commas (where each host is part of a different cluster).
    - HubConfigImpl and HubProjectImpl are no longer Components, which greatly complicated Spring config in tests. Instead, HubConfigImpl is a Bean in ApplicationConfig and WebApplication so as to preserve existing functionality.
    - Because HubProjectImpl is no longer Spring-managed but still depends on a FlowManager, a FlowManager is required by the method that depends on it
    - ScaffoldingValidator is no longer a Spring component - instead, it's a simple class whose constructor depends on a HubProject
    - StepDefinitionManagerImpl implements StepDefinitionProvider, allowing ProjectStepDefinitionProvider to be deleted.
    - StepRunnerFactory is no longer a Spring component; it requires either a HubConfig or HubClient to be instantiated. With a HubConfig, it constructs a StepDefinitionManagerImpl to act as its StepDefinitionProvider. 
    - Reworked FlowRunnerImpl so that FlowRunnerTask does not depend on a Spring-managed StepRunnerFactory, which didn't work with parallel tests. Now, a non-Spring-managed StepRunnerFactory is passed to FlowRunnerTask.
    - Installer is now a regular main program that can also run against multiple hosts at once
    - Deleted RunMappingTestsWithoutProjectTest, which has outlived its purpose; we have many tests now that run tests without a project
    - Deleted EndToEndAPITest in QS, as the test hasn't been run in many months (it was excluded in build.gradle) and isn't working

--- web/src/test/java/com/marklogic/hub/web/web/HubConfigJsonTest.java
+++ web/src/test/java/com/marklogic/hub/web/web/HubConfigJsonTest.java
@@ -20,86 +20,70 @@ package com.marklogic.hub.web.web;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.marklogic.hub.ApplicationConfig;
 import com.marklogic.hub.HubConfig;
 import com.marklogic.hub.impl.HubConfigImpl;
-import com.marklogic.hub.web.WebApplication;
+import com.marklogic.hub.web.AbstractWebTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
-import org.springframework.boot.test.autoconfigure.json.JsonTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = {WebApplication.class, ApplicationConfig.class, HubConfigJsonTest.class, JsonTest.class})
 @AutoConfigureJsonTesters
-public class HubConfigJsonTest {
+public class HubConfigJsonTest extends AbstractWebTest {
 
     @Autowired
     JacksonTester<HubConfig> json;
 
-    @Autowired
-    HubConfigImpl hubConfig;
-
     @Test
     public void testSerialize() throws IOException {
-        String originalUsername = hubConfig.getMlUsername();
-        String originalPassword = hubConfig.getMlPassword();
-        try {
-            hubConfig.setMlUsername("mluser");
-            hubConfig.setMlPassword("mlpassword");
+        HubConfigImpl hubConfig = new HubConfigImpl(getHubProject());
+        hubConfig.setMlUsername("test-user");
+        hubConfig.setMlPassword("test-password");
 
-            String expected = "{\n" +
-                "  \"stagingDbName\": \"data-hub-STAGING\",\n" +
-                "  \"stagingHttpName\": \"data-hub-STAGING\",\n" +
-                "  \"stagingForestsPerHost\": 1,\n" +
-                "  \"stagingPort\": 8010,\n" +
-                "  \"stagingAuthMethod\": \"digest\",\n" +
-                "  \"finalDbName\": \"data-hub-FINAL\",\n" +
-                "  \"finalHttpName\": \"data-hub-FINAL\",\n" +
-                "  \"finalForestsPerHost\": 1,\n" +
-                "  \"finalPort\": 8011,\n" +
-                "  \"finalAuthMethod\": \"digest\",\n" +
-                "  \"jobDbName\": \"data-hub-JOBS\",\n" +
-                "  \"jobHttpName\": \"data-hub-JOBS\",\n" +
-                "  \"jobForestsPerHost\": 1,\n" +
-                "  \"jobPort\": 8013,\n" +
-                "  \"jobAuthMethod\": \"digest\",\n" +
-                "  \"modulesDbName\": \"data-hub-MODULES\",\n" +
-                "  \"stagingTriggersDbName\": \"data-hub-staging-TRIGGERS\",\n" +
-                "  \"stagingSchemasDbName\": \"data-hub-staging-SCHEMAS\",\n" +
-                "  \"finalTriggersDbName\": \"data-hub-final-TRIGGERS\",\n" +
-                "  \"finalSchemasDbName\": \"data-hub-final-SCHEMAS\",\n" +
-                "  \"modulesForestsPerHost\": 1,\n" +
-                "  \"stagingTriggersForestsPerHost\": 1,\n" +
-                "  \"stagingSchemasForestsPerHost\": 1,\n" +
-                "  \"finalTriggersForestsPerHost\": 1,\n" +
-                "  \"finalSchemasForestsPerHost\": 1,\n" +
-                "  \"flowOperatorRoleName\": \"flow-operator-role\",\n" +
-                "  \"flowOperatorUserName\": \"flow-operator\",\n" +
-                "  \"customForestPath\": \"forests\",\n" +
-                "  \"jarVersion\": \"" + hubConfig.getJarVersion() + "\"\n" +
-                "}";
+        String expected = "{\n" +
+            "  \"stagingDbName\": \"data-hub-STAGING\",\n" +
+            "  \"stagingHttpName\": \"data-hub-STAGING\",\n" +
+            "  \"stagingForestsPerHost\": 3,\n" +
+            "  \"stagingPort\": 8010,\n" +
+            "  \"stagingAuthMethod\": \"digest\",\n" +
+            "  \"finalDbName\": \"data-hub-FINAL\",\n" +
+            "  \"finalHttpName\": \"data-hub-FINAL\",\n" +
+            "  \"finalForestsPerHost\": 3,\n" +
+            "  \"finalPort\": 8011,\n" +
+            "  \"finalAuthMethod\": \"digest\",\n" +
+            "  \"jobDbName\": \"data-hub-JOBS\",\n" +
+            "  \"jobHttpName\": \"data-hub-JOBS\",\n" +
+            "  \"jobForestsPerHost\": 4,\n" +
+            "  \"jobPort\": 8013,\n" +
+            "  \"jobAuthMethod\": \"digest\",\n" +
+            "  \"modulesDbName\": \"data-hub-MODULES\",\n" +
+            "  \"stagingTriggersDbName\": \"data-hub-staging-TRIGGERS\",\n" +
+            "  \"stagingSchemasDbName\": \"data-hub-staging-SCHEMAS\",\n" +
+            "  \"finalTriggersDbName\": \"data-hub-final-TRIGGERS\",\n" +
+            "  \"finalSchemasDbName\": \"data-hub-final-SCHEMAS\",\n" +
+            "  \"modulesForestsPerHost\": 1,\n" +
+            "  \"stagingTriggersForestsPerHost\": 1,\n" +
+            "  \"stagingSchemasForestsPerHost\": 1,\n" +
+            "  \"finalTriggersForestsPerHost\": 1,\n" +
+            "  \"finalSchemasForestsPerHost\": 1,\n" +
+            "  \"flowOperatorRoleName\": \"flow-operator-role\",\n" +
+            "  \"flowOperatorUserName\": \"flow-operator\",\n" +
+            "  \"customForestPath\": \"forests\",\n" +
+            "  \"jarVersion\": \"" + hubConfig.getJarVersion() + "\"\n" +
+            "}";
 
-            assertThat(json.write(hubConfig)).isEqualToJson(expected);
+        assertThat(json.write(hubConfig)).isEqualToJson(expected);
 
-            // Verify some fields were not serialized
-            JsonNode actualJson = new ObjectMapper().readTree(json.write(hubConfig).getJson());
-            assertFalse(actualJson.has("mlUsername"), "mlUsername and mlPassword were previously serialized because they " +
-                "had public getters in HubConfigImpl, but this seems like an unintended error, as we wouldn't want to " +
-                "serialize passwords out into a JSON string");
-            assertFalse(actualJson.has("mlPassword"));
-        } finally {
-            hubConfig.setMlUsername(originalUsername);
-            hubConfig.setMlPassword(originalPassword);
-        }
+        // Verify some fields were not serialized
+        JsonNode actualJson = new ObjectMapper().readTree(json.write(hubConfig).getJson());
+        assertFalse(actualJson.has("mlUsername"), "mlUsername and mlPassword were previously serialized because they " +
+            "had public getters in HubConfigImpl, but this seems like an unintended error, as we wouldn't want to " +
+            "serialize passwords out into a JSON string");
+        assertFalse(actualJson.has("mlPassword"));
     }
 }
