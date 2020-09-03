ccff5f12165ce1d31eefbc77b0a6add09e697425
Rob Rudin <rob.rudin@marklogic.com>
2020-09-03 15:52:48 -0400

    DHFPROD-5745: Can now run tests in parallel
    Notes:
    
    - Tests now run against a HubConfigImpl tied to a specific host; this is enabled via HubConfigInterceptor and HubConfigObjectFactory. Just set mlHost to multiple hosts, separated by commas (where each host is part of a different cluster).
    - HubConfigImpl and HubProjectImpl are no longer Components, which greatly complicated Spring config in tests. Instead, HubConfigImpl is a Bean in ApplicationConfig and WebApplication so as to preserve existing functionality.
    - Because HubProjectImpl is no longer Spring-managed but still depends on a FlowManager, a FlowManager is required by the method that depends on it
    - ScaffoldingValidator is no longer a Spring component - instead, it's a simple class whose constructor depends on a HubProject
    - StepDefinitionManagerImpl implements StepDefinitionProvider, allowing ProjectStepDefinitionProvider to be deleted.
    - StepRunnerFactory is no longer a Spring component; it requires either a HubConfig or HubClient to be instantiated. With a HubConfig, it constructs a StepDefinitionManagerImpl to act as its StepDefinitionProvider. 
    - Reworked FlowRunnerImpl so that FlowRunnerTask does not depend on a Spring-managed StepRunnerFactory, which didn't work with parallel tests. Now, a non-Spring-managed StepRunnerFactory is passed to FlowRunnerTask.
    - Installer is now a regular main program that can also run against multiple hosts at once
    - Deleted RunMappingTestsWithoutProjectTest, which has outlived its purpose; we have many tests now that run tests without a project
    - Deleted EndToEndAPITest in QS, as the test hasn't been run in many months (it was excluded in build.gradle) and isn't working

--- marklogic-data-hub/src/test/java/com/marklogic/hub/flow/RunFlowWithInvalidStepDefTest.java
+++ marklogic-data-hub/src/test/java/com/marklogic/hub/flow/RunFlowWithInvalidStepDefTest.java
@@ -1,27 +1,23 @@
 package com.marklogic.hub.flow;
 
 import com.marklogic.hub.AbstractHubCoreTest;
+import com.marklogic.hub.flow.impl.FlowRunnerImpl;
+import com.marklogic.hub.impl.FlowManagerImpl;
 import com.marklogic.hub.job.JobStatus;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RunFlowWithInvalidStepDefTest extends AbstractHubCoreTest {
 
-    @Autowired
-    FlowRunner flowRunner;
-
-    /**
-     * In order to reproduce this error, we have to use the Spring-managed FlowRunner, as that does not use
-     * MarkLogicStepDefinitionProvider, which will immediately provide a nice error message when a step definition
-     * cannot be found.
-     */
     @Test
     void test() {
         installProjectInFolder("test-projects/flow-with-invalid-step-def");
 
+        // Need to use a FlowRunner as constructed by Spring, which does not use MarkLogicStepDefinitionProvider, which
+        // will immediately provide a nice error message when a step definition cannot be found
+        FlowRunnerImpl flowRunner = new FlowRunnerImpl(getHubConfig(), new FlowManagerImpl(getHubConfig()));
         RunFlowResponse response = flowRunner.runFlow("invalidStepDef");
         flowRunner.awaitCompletion();
 
