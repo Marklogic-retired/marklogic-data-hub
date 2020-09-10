aa8b118e43b8072ba32e5b520243e059736947ff
srinathgit <ssambasu@marklogic.com>
2020-09-10 10:22:20 -0700

    DHFPROD-5898:Create hub-central roles to be shown in service portal

---
+++ marklogic-data-hub/src/test/java/com/marklogic/hub/security/VerifyHubCentralRolesTest.java
@@ -0,0 +1,35 @@
+package com.marklogic.hub.security;
+
+import com.marklogic.hub.AbstractHubCoreTest;
+import com.marklogic.hub.impl.HubConfigImpl;
+import com.marklogic.mgmt.api.API;
+import com.marklogic.mgmt.api.security.Role;
+import com.marklogic.mgmt.mapper.DefaultResourceMapper;
+import com.marklogic.mgmt.mapper.ResourceMapper;
+import com.marklogic.mgmt.resource.security.RoleManager;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VerifyHubCentralRolesTest extends AbstractHubCoreTest {
+
+    @Test
+    void verifyRoles(){
+        verifyRole("hub-central-curator", "hub-central-flow-writer", "hub-central-load-writer", "hub-central-mapping-writer",
+            "hub-central-custom-reader", "hub-central-entity-model-reader");
+        verifyRole("hub-central-explorer",  "hub-central-saved-query-user", "hub-central-entity-exporter");
+        verifyRole("hub-central-modeler","hub-central-entity-model-writer");
+    }
+
+    private void verifyRole(String roleName, String ... assignedRoles){
+        HubConfigImpl adminHubConfig = runAsAdmin();
+        RoleManager roleManager = new RoleManager(adminHubConfig.getManageClient());
+        ResourceMapper resourceMapper = new DefaultResourceMapper(new API(adminHubConfig.getManageClient()));
+
+        Role r = resourceMapper.readResource(roleManager.getAsJson(roleName), Role.class);
+        assertNotNull(r);
+        for(String role: assignedRoles){
+            assertTrue(r.getRole().contains(role));
+        }
+    }
+}
