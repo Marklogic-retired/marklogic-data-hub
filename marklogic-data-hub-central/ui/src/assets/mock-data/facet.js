a9a86ba542aba7044e161bcd688a06a6e6879a5d
wooldridge <mike@hellion.org>
2020-09-09 22:21:37 -0700

    DHFPROD-5597: Show search link when facets > 20
    Use default facet props object.
    Refactor facet tests
    Fixing the failing e2e tests

--- marklogic-data-hub-central/ui/src/assets/mock-data/facet.js
+++ marklogic-data-hub-central/ui/src/assets/mock-data/facet.js
@@ -1,3 +1,79 @@
 export const numericRange = { "data": { "min": 11, "max": 110 } };
 
 export const stringSearchResponse = ["Adams Cole"];
+
+export const facetProps = {
+    name: "sales_region",
+    constraint: "sales_region",
+    key: "",
+    tooltip: "",
+    facetType: "xs:string",
+    facetCategory: "entity",
+    updateSelectedFacets: jest.fn(),
+    addFacetValues: jest.fn(),
+    referenceType: "element",
+    entityTypeId: "",
+    propertyPath: "sales_region",
+    facetValues: [
+    {
+      "name": "Customer",
+      "count": 50,
+      "value": "Customer"
+    },
+    {
+      "name": "ProductGroupLicense",
+      "count": 1200,
+      "value": "ProductGroupLicense"
+    },
+    {
+      "name": "OrderDetail",
+      "count": 15000,
+      "value": "OrderDetail"
+    },
+    {
+      "name": "ItemType",
+      "count": 300,
+      "value": "ItemType"
+    },
+    {
+      "name": "ProductDetail",
+      "count": 4095,
+      "value": "ProductDetail"
+    },
+    {
+      "name": "OrderType",
+      "count": 1034,
+      "value": "OrderType"
+    },
+    {
+      "name": "Order",
+      "count": 2334,
+      "value": "Order"
+    },
+    {
+      "name": "ProductGroup",
+      "count": 2346,
+      "value": "ProductGroup"
+    },
+    {
+      "name": "Protein",
+      "count": 607,
+      "value": "Protein"
+    },
+    {
+      "name": "Provider",
+      "count": 12584,
+      "value": "Provider"
+    },
+    {
+      "name": "TestEntityForMapping",
+      "count": 100,
+      "value": "TestEntityForMapping"
+    },
+    {
+      "name": "CustomerType",
+      "count": 999,
+      "value": "CustomerType"
+    }
+  ]
+}
