96037635534a544b56acaeece760c0b01aa458c8
Bruce An <Bruce.An@marklogic.com>
2020-09-18 10:40:35 -0700

    DHFPROD-5532: e2e test for load and map step with custom header

--- marklogic-data-hub-central/ui/e2e/cypress/integration/curation/curate/mapping.spec.tsx
+++ marklogic-data-hub-central/ui/e2e/cypress/integration/curation/curate/mapping.spec.tsx
@@ -9,6 +9,7 @@ import loadPage from "../../../support/pages/load";
 import browsePage from "../../../support/pages/browse";
 import curatePage from "../../../support/pages/curate";
 import runPage from "../../../support/pages/run";
+import detailPage from "../../../support/pages/detail";
 import 'cypress-wait-until';
 
 describe('Mapping', () => {
@@ -27,9 +28,9 @@ describe('Mapping', () => {
 
   after(() => {
     cy.loginAsDeveloper().withRequest();
-    cy.deleteSteps('ingestion', 'loadOrder');
-    cy.deleteSteps('mapping', 'mapOrder');
-    cy.deleteFlows( 'orderFlow');
+    cy.deleteSteps('ingestion', 'loadOrder', 'loadOrderCustomHeader');
+    cy.deleteSteps('mapping', 'mapOrder', 'mapOrderCustomHeader');
+    cy.deleteFlows( 'orderFlow', 'orderCustomHeaderFlow');
   })
 
   it('can create load step with processors & custom hook, can create mapping step with processors & custom hook, can create new flow, run both steps, and verify processors & custom hooks', () => {
@@ -126,4 +127,91 @@ describe('Mapping', () => {
     cy.contains('primaryKey');
     cy.contains('uriFromCustomHook');
   })
+
+  it('can create a load step with a custom header, can create a mapping step with a custom header, and run both steps and verify in the detail view, ', () => {
+    const flowName = 'orderCustomHeaderFlow'
+    const loadStep = 'loadOrderCustomHeader';
+    const mapStep = 'mapOrderCustomHeader';
+    // create load step
+    toolbar.getLoadToolbarIcon().click();
+    cy.waitUntil(() => loadPage.stepName('ingestion-step').should('be.visible'));
+    loadPage.addNewButton('card').click();
+    loadPage.stepNameInput().type(loadStep);
+    loadPage.stepDescriptionInput().type('load order with a custom header');
+    loadPage.confirmationOptions('Save').click();
+    cy.findByText(loadStep).should('be.visible');
+    loadPage.stepSettings(loadStep).click();
+
+    // add custom header to load step
+    advancedSettingsDialog.setHeaderContent('loadTile/customHeader');
+    advancedSettingsDialog.saveSettings(loadStep).click();
+    advancedSettingsDialog.saveSettings(loadStep).should('not.be.visible');
+    
+    // add step to a new flow
+    loadPage.addStepToNewFlow(loadStep);
+    cy.findByText('New Flow').should('be.visible');
+    runPage.setFlowName(flowName);
+    runPage.setFlowDescription(`${flowName} description`);
+    loadPage.confirmationOptions('Save').click();
+    cy.verifyStepAddedToFlow('Load', loadStep);
+
+    //Run the ingest with JSON
+    runPage.runStep(loadStep).click();
+    cy.uploadFile('input/10260.json');
+    cy.verifyStepRunResult('success','Ingestion', loadStep);
+    tiles.closeRunMessage().click();
+
+    // create mapping step
+    toolbar.getCurateToolbarIcon().click();
+    cy.waitUntil(() => curatePage.getEntityTypePanel('Customer').should('be.visible'));
+    curatePage.toggleEntityTypeId('Order');
+    cy.waitUntil(() => curatePage.addNewMapStep().click());
+
+    createEditMappingDialog.setMappingName(mapStep);
+    createEditMappingDialog.setMappingDescription('An order mapping with custom header');
+    createEditMappingDialog.setSourceRadio('Query');
+    createEditMappingDialog.setQueryInput(`cts.collectionQuery(['${loadStep}'])`)
+    createEditMappingDialog.saveButton().click(); 
+    curatePage.verifyStepNameIsVisible(mapStep);
+
+    // add custom header
+    curatePage.stepSettings(mapStep).click();
+    advancedSettingsDialog.setHeaderContent('curateTile/customHeader');
+    advancedSettingsDialog.saveSettings(mapStep).click();
+    advancedSettingsDialog.saveSettings(mapStep).should('not.be.visible');
+
+    // map source to entity
+    curatePage.openSourceToEntityMap('Order', mapStep);
+    cy.waitUntil(() => sourceToEntityMap.expandCollapseEntity().should('be.visible')).click();
+    sourceToEntityMap.setXpathExpressionInput('orderId', 'OrderID');
+    sourceToEntityMap.setXpathExpressionInput('address', '/');
+    sourceToEntityMap.setXpathExpressionInput('city', 'ShipCity');
+    sourceToEntityMap.setXpathExpressionInput('state', 'ShipAddress');
+    sourceToEntityMap.setXpathExpressionInput('orderDetails', '/');
+    sourceToEntityMap.setXpathExpressionInput('productID', 'OrderDetails/ProductID');
+    sourceToEntityMap.setXpathExpressionInput('unitPrice', 'head(OrderDetails/UnitPrice)');
+    sourceToEntityMap.setXpathExpressionInput('quantity', 'OrderDetails/Quantity');
+    sourceToEntityMap.setXpathExpressionInput('discount', 'head(OrderDetails/Discount)');
+    sourceToEntityMap.setXpathExpressionInput('shipRegion', 'ShipRegion');
+    sourceToEntityMap.setXpathExpressionInput('shippedDate', 'ShippedDate');
+    // close modal
+    cy.get('body').type('{esc}');
+
+    curatePage.openExistingFlowDropdown('Order', mapStep);
+    curatePage.getExistingFlowFromDropdown(flowName).click();
+    curatePage.addStepToFlowConfirmationMessage(mapStep, flowName);
+    curatePage.confirmAddStepToFlow(mapStep, flowName)
+
+    runPage.runStep(mapStep).click();
+    cy.verifyStepRunResult('success','Mapping', mapStep);
+    runPage.explorerLink().click()
+    browsePage.getTableViewInstanceIcon().click();
+
+    detailPage.getDocumentSource().should('contain', 'backup-ABC123');
+    detailPage.getDocumentTimestamp().should('not.exist');
+
+    detailPage.getSourceView().click();
+    cy.contains('accessLevel');
+    cy.contains('999ABC');
+  });
 })
