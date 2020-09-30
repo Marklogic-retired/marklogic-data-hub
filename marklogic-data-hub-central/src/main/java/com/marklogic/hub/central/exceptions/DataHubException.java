6e88879f0e2022f28776d1173166ad87f4f6dc8e
rahulvudutala <rahulbharadwaj.v@gmail.com>
2020-09-30 15:33:30 -0700

    DHFPROD-4732: Explore All Data

--- marklogic-data-hub-central/src/main/java/com/marklogic/hub/central/exceptions/DataHubException.java
+++ marklogic-data-hub-central/src/main/java/com/marklogic/hub/central/exceptions/DataHubException.java
@@ -4,10 +4,6 @@ public class DataHubException extends RuntimeException {
 
     private static final long serialVersionUID = 4824858930318275798L;
 
-    public DataHubException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
     public DataHubException(String message) {
         super(message);
     }
