362a143b8c17e94287c66eeb879fff0a0c8e52ee
Bruce An <Bruce.An@marklogic.com>
2020-10-02 10:08:26 -0700

    DHFPROD-5252: Create match ruleset form and match detail view

--- marklogic-data-hub-central/ui/src/components/entities/matching/matching-card.test.tsx
+++ marklogic-data-hub-central/ui/src/components/entities/matching/matching-card.test.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import userEvent from "@testing-library/user-event";
+
 import MatchingCard from './matching-card';
 
+import { matchingStep } from '../../../assets/mock-data/matching';
+import { customerEntityDef} from '../../../assets/mock-data/entity-definitions-mock';
+import { MatchingStep } from '../../../types/curation-types'
+
+const matchingStepsArray: MatchingStep[] = matchingStep.artifacts
+
+
 describe('Matching cards view component', () => {
-  it('should render correctly', () => {
-    shallow(<MatchingCard data deleteMatchingArtifact entityName
-      createMatchingArtifact
-      canReadMatchMerge
-      canWriteMatchMerge/>);
+  it('can render matching steps', () => {
+    const { getByText } =  render(
+      <Router>
+        <MatchingCard 
+          matchingStepsArray={matchingStepsArray}
+          flows={[]}
+          entityName={customerEntityDef.info.title}
+          deleteMatchingArtifact={jest.fn()}
+          createMatchingArtifact={jest.fn()}
+          canReadMatchMerge={true}
+          canWriteMatchMerge={true}
+          entityModel={customerEntityDef.definitions}
+          canWriteFlow={true}
+          addStepToFlow={jest.fn()}
+          addStepToNew={jest.fn()}
+        />
+      </Router>
+    )
+
+    expect(getByText('matchCustomers')).toBeInTheDocument()
+    expect(getByText('matchCustomersEmpty')).toBeInTheDocument()
   });
 });
