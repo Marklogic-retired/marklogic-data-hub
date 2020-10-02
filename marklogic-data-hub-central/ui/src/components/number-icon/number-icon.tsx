02794caccaf2eee63c114f1d529d9a07881d4db9
Bruce An <Bruce.An@marklogic.com>
2020-10-02 10:08:26 -0700

    DHFPROD-5252: Create match ruleset form and match detail view

---
+++ marklogic-data-hub-central/ui/src/components/number-icon/number-icon.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './number-icon.module.scss'
+
+type Props = {
+  value: number
+}
+
+/**
+  * Circular number icon that matches the Step Number Icon CSS from the Ant Design Steps component
+  * https://3x.ant.design/components/steps/
+  * @param value = Step number, works for single digit 0 - 9 only
+  * @example 1
+  **/
+const NumberIcon: React.FC<Props> = (props) => {
+  return (
+    <div className={styles.numberIcon}>
+      <span className={styles.numberValue}>{props.value}</span>
+    </div>
+  )
+}
+
+export default NumberIcon;
