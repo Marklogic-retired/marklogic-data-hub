cc9c11248bc4d5904140c414911b258feacd6d3e
Bruce An <Bruce.An@marklogic.com>
2020-10-02 10:08:26 -0700

    DHFPROD-5252: Create match ruleset form and match detail view

---
+++ marklogic-data-hub-central/ui/src/components/entity-property-tree-select/entity-property-tree-select.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+
+import EntityPropertyTreeSelect from './entity-property-tree-select';
+import { customerEntityDef} from '../../assets/mock-data/entity-definitions-mock';
+import { definitionsParser } from '../../util/data-conversion';
+
+const customerEntityDefsArray = definitionsParser(customerEntityDef.definitions);
+const entityTypeDefinition = customerEntityDefsArray.find( entityDefinition => entityDefinition.name === 'Customer')
+
+describe('Entity Property Tree Select component', () => {
+  it('can render and can select a value', () => {
+    const mockOnValueSelected = jest.fn()
+
+    const { getByText } =  render(
+      <EntityPropertyTreeSelect 
+        propertyDropdownOptions={entityTypeDefinition?.properties || []}
+        entityDefinitionsArray={customerEntityDefsArray}
+        value={undefined}
+        onValueSelected={mockOnValueSelected}
+      />
+    )
+
+    userEvent.click(getByText('Select property'));
+    userEvent.click(getByText('customerId'));
+    expect(mockOnValueSelected.mock.calls[0][0]).toBe('customerId');
+  });
+});
