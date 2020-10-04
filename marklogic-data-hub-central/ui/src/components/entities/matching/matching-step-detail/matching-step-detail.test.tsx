b7221fd6a314f74398390223f30de2d85711ca77
sanjeevani vishaka <sanjeevani.vishaka@marklogic.com>
2020-10-04 16:40:16 -0500

    DHFPROD-5230-slider enhancements

--- marklogic-data-hub-central/ui/src/components/entities/matching/matching-step-detail/matching-step-detail.test.tsx
+++ marklogic-data-hub-central/ui/src/components/entities/matching/matching-step-detail/matching-step-detail.test.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen, fireEvent, waitForElement} from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
-
 import MatchingStepDetail from './matching-step-detail';
 
 import { CurationContext } from '../../../../util/curation-context';
 import { customerMatchingStep, customerMatchingStepEmpty } from '../../../../assets/mock-data/curation-context-mock';
 
+
 describe('Matching Step Detail view component', () => {
   it('can render matching step with no rulesets or thresholds and click less/more text', () => {
 
-    const { getByLabelText, getByText, queryByLabelText } =  render(
+    const { getByLabelText, queryByLabelText, getByTestId } =  render(
       <CurationContext.Provider value={customerMatchingStepEmpty}>
         <MatchingStepDetail/>
       </CurationContext.Provider>
@@ -18,6 +18,10 @@ describe('Matching Step Detail view component', () => {
 
     expect(queryByLabelText('threshold-more')).toBeNull();
     expect(queryByLabelText('ruleset-more')).toBeNull();
+    expect(getByTestId('threshold-slider-rail')).toBeInTheDocument();
+    expect(getByTestId('threshold-slider-options')).toBeInTheDocument();
+    expect(getByTestId('ruleSet-slider-rail')).toBeInTheDocument()
+    expect(getByTestId('ruleSet-slider-options')).toBeInTheDocument();
 
     userEvent.click(getByLabelText('threshold-less'));
     expect(queryByLabelText('threshold-more')).toBeInTheDocument();
@@ -26,9 +30,9 @@ describe('Matching Step Detail view component', () => {
     expect(queryByLabelText('ruleset-more')).toBeInTheDocument();
   });
 
-  it('can render matching step with rulesets and thresholds and click add single ruleset', () => {
+  it('can render matching step with rulesets and thresholds and click add single ruleset', async() => {
 
-    const { getByLabelText, getByText, queryByLabelText } =  render(
+    const { getByLabelText, getByText, queryByLabelText, getByTestId, debug } =  render(
       <CurationContext.Provider value={customerMatchingStep}>
         <MatchingStepDetail/>
       </CurationContext.Provider>
@@ -36,9 +40,28 @@ describe('Matching Step Detail view component', () => {
 
     expect(queryByLabelText('threshold-more')).toBeInTheDocument();
     expect(queryByLabelText('ruleset-more')).toBeInTheDocument();
+    //Verify threshold options are rendered on the slider
+    expect(getByText('sameThreshold')).toBeInTheDocument();
+    expect(getByText('similarThreshold')).toBeInTheDocument();
+    expect(getByText('household')).toBeInTheDocument();
 
     userEvent.click(getByLabelText('add-ruleset'));
     userEvent.click(getByLabelText('single-ruleset-menu'));
+    //Verify ruleset options are rendered on the slider
+    expect(getByText('name')).toBeInTheDocument();
+    expect(getByText('lastName')).toBeInTheDocument();
+    expect(getByText('billingAddress')).toBeInTheDocument();
+    expect(getByText('shippingAddress')).toBeInTheDocument();
+
+    //Verify tooltips are highlighted on hover
+    //TODO in e2e
+   /* userEvent.hover(getByTestId('sameThreshold-tooltip'));
+    await(waitForElement(() => expect(getByTestId('sameThreshold-tooltip')).toHaveStyle({backgroundColor:'#444'})))
+    expect(getByTestId('sameThreshold-tooltip')).toHaveStyle('background-color: rgb(233, 247, 254)');*/
+
+    //Verify handles are draggable
+    fireEvent.dragStart(screen.getByTestId('sameThreshold-active'), { clientX: 0, clientY: 0 });
+    fireEvent.drop(screen.getByTestId('sameThreshold-active'),{ clientX: 0, clientY: 1 });
     expect(screen.getByText('Select property')).toBeInTheDocument();
   });
 });
