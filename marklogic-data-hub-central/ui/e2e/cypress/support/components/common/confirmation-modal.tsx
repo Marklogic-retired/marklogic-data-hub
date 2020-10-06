9f436dcb841b6522ee946f6608d5576d930c83db
Bruce An <Bruce.An@marklogic.com>
2020-10-06 13:41:13 -0700

    DHFPROD-6093: Modeling e2e test improvements

--- marklogic-data-hub-central/ui/e2e/cypress/support/components/common/confirmation-modal.tsx
+++ marklogic-data-hub-central/ui/e2e/cypress/support/components/common/confirmation-modal.tsx
@@ -14,45 +14,48 @@ class ConfirmationModal {
     return cy.findByLabelText('toggle-steps');
   }
   getSaveEntityText() {
-    return cy.findByLabelText('save-text', {timeout: 25000});
+    return cy.findByLabelText('save-text', { timeout: 20000 });
   }
   getSaveAllEntityText() {
-    return cy.findByLabelText('save-all-text', {timeout: 25000});
+    return cy.findByLabelText('save-all-text', { timeout: 30000 });
   }
 
   getRevertEntityText() {
-    return cy.findByLabelText('revert-text', {timeout: 25000});
+    return cy.findByLabelText('revert-text');
   }
 
   getRevertAllEntityText() {
-    return cy.findByLabelText('revert-all-text', {timeout: 25000});
+    return cy.findByLabelText('revert-all-text');
   }
   getDeleteEntityText() {
-    return cy.findByLabelText('delete-text', {timeout: 15000});
+    return cy.findByLabelText('delete-text' , { timeout: 20000 });
   }
   getDeleteEntityRelationshipText() {
-    return cy.findByLabelText('delete-relationship-text', {timeout: 15000});
+    return cy.findByLabelText('delete-relationship-text', { timeout: 30000 });
   }
 
   getDeleteEntityRelationshipEditText() {
-    return cy.findByLabelText('delete-relationship-edit-text', {timeout: 25000});
+    return cy.findByLabelText('delete-relationship-edit-text', { timeout: 30000 });
   }
 
   getDeleteEntityNoRelationshipEditText() {
-    return cy.findByLabelText('delete-no-relationship-edit-text', {timeout: 25000});
+    return cy.findByLabelText('delete-no-relationship-edit-text', { timeout: 30000 });
   }
 
   getDeleteEntityStepText() {
-    return cy.findByLabelText('delete-step-text', {timeout: 15000});
+    return cy.findByLabelText('delete-step-text', { timeout: 30000 });
   }
   getDeletePropertyWarnText() {
-    return cy.findByLabelText('delete-property-text', {timeout: 15000});
+    return cy.findByLabelText('delete-property-text');
   }
   getDeletePropertyStepWarnText() {
-    return cy.findByLabelText('delete-property-step-text', {timeout: 15000});
+    return cy.findByLabelText('delete-property-step-text');
   }
   getNavigationWarnText() {
     return cy.findByLabelText('navigation-warn-text');
+  }
+  getIdentifierText() {
+    return cy.findByLabelText('identifier-text');
   }
 }
 
