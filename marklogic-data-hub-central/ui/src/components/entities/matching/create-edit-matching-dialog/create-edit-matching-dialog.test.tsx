0402fc24d10fdda1eebaac8d7756e70e63a4226b
Brian Tang <briantang98@gmail.com>
2020-10-19 15:06:32 -0700

    DHFPROD-5437: Typeahead search for collection source query field in mapping step

--- marklogic-data-hub-central/ui/src/components/entities/matching/create-edit-matching-dialog/create-edit-matching-dialog.test.tsx
+++ marklogic-data-hub-central/ui/src/components/entities/matching/create-edit-matching-dialog/create-edit-matching-dialog.test.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { render, fireEvent, cleanup } from '@testing-library/react';
+import { render, fireEvent, cleanup, wait } from '@testing-library/react';
 import CreateEditMatchingDialog from './create-edit-matching-dialog';
 import data from "../../../../assets/mock-data/curation/matching.data";
+import axiosMock from 'axios';
+import {stringSearchResponse} from "../../../../assets/mock-data/explore/facet-props";
 
+
+jest.mock('axios');
 describe('Create/Edit Matching artifact component', () => {
 
-    afterEach(cleanup);
-
+  afterEach(() => {
+    cleanup();
+    jest.clearAllMocks();
+});
     test('Verify Edit Matching dialog renders correctly for a read only user', () => {
         const { getByText, getByPlaceholderText, getByLabelText } = render(<CreateEditMatchingDialog {...data.editMatching} canReadOnly={true} canReadWrite={false}/>);
 
@@ -17,7 +23,8 @@ describe('Create/Edit Matching artifact component', () => {
         expect(getByLabelText('Collection')).toBeChecked();
         expect(getByLabelText('Collection')).toBeDisabled();
         expect(getByLabelText('Query')).toBeDisabled();
-        expect(getByPlaceholderText('Enter collection name')).toBeDisabled();
+        const collInput = document.querySelector(('#collList .ant-input'))
+        expect(collInput).toBeDisabled();
 
         expect(getByText('Save')).toBeDisabled();
         expect(getByText('Cancel')).toBeEnabled();
@@ -32,7 +39,7 @@ describe('Create/Edit Matching artifact component', () => {
         expect(getByPlaceholderText('Enter description')).toBeInTheDocument();
         expect(getByLabelText('Collection')).toBeInTheDocument();
         expect(getByLabelText('Query')).toBeInTheDocument();
-        expect(getByPlaceholderText('Enter collection name')).toBeInTheDocument();
+        expect(getByLabelText('collection-input')).toBeInTheDocument();
         expect(getByText('Save')).toBeDisabled();
         expect(getByText('Cancel')).toBeEnabled();
         //Collection radio button should be selected by default
@@ -53,18 +60,40 @@ describe('Create/Edit Matching artifact component', () => {
         expect(saveButton).toBeDisabled();
     });
 
-    test('Verify able to type in input fields', () => {
+    test('Verify able to type in input fields and typeahead search in collections field', async () => {
+        axiosMock.post['mockImplementationOnce'](jest.fn(() => Promise.resolve({status: 200, data: stringSearchResponse})));
         const { getByText, getByLabelText, getByPlaceholderText } = render(<CreateEditMatchingDialog {...data.newMatching} />);
 
         const descInput = getByPlaceholderText('Enter description');
-        const collInput = getByPlaceholderText('Enter collection name');
-        expect(collInput).toBeInTheDocument();
+        const collInput = document.querySelector(('#collList .ant-input'))
         const saveButton = getByText('Save');
         saveButton.onclick = jest.fn();
 
         fireEvent.change(descInput, { target: {value: 'test description'}});
         expect(descInput).toHaveValue('test description');
-        fireEvent.change(collInput, { target: {value: 'testCollection'}});
+        await wait(() => {
+            if(collInput){
+              fireEvent.change(collInput, { target: {value: 'ada'} });
+            }
+          });
+          let url = "/api/entitySearch/facet-values?database=staging";
+          let payload = {
+              'referenceType':"collection",
+              'entityTypeId':" ",
+              'propertyPath':" ",
+              'limit':10,
+              'dataType':"string",
+              'pattern':'ada'
+          };
+          expect(axiosMock.post).toHaveBeenCalledWith(url, payload);
+          expect(axiosMock.post).toHaveBeenCalledTimes(1);
+          expect(getByText('Adams Cole')).toBeInTheDocument();
+          
+        await wait(() => {
+            if(collInput){
+              fireEvent.change(collInput, { target: {value: 'testCollection'} });
+            }
+          });
         expect(collInput).toHaveValue('testCollection');
         fireEvent.click(getByLabelText('Query'));
         const queryInput = getByPlaceholderText('Enter source query');
@@ -118,7 +147,8 @@ describe('Create/Edit Matching artifact component', () => {
         expect(getByPlaceholderText('Enter description')).toHaveValue('Description of testMatching');
 
         expect(getByLabelText('Collection')).toBeChecked();
-        expect(getByPlaceholderText('Enter collection name')).toHaveValue('matching-collection');
+        const collInput = document.querySelector(('#collList .ant-input'))
+        expect(collInput).toHaveValue('matching-collection');
 
         fireEvent.click(getByLabelText('Query'));
         expect(getByPlaceholderText('Enter source query')).toHaveTextContent("cts.collectionQuery(['matching-collection'])");
