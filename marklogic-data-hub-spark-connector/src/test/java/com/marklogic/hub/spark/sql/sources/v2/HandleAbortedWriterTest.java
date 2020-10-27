7865c818f1a5333d4d0a2d45290eb54a1675d9f4
Rob Rudin <rob.rudin@marklogic.com>
2020-10-26 20:13:06 -0400

    DHFPROD-6096: Spark connector now handles write failures and aborts
    Also removed the concept of batchSize - the BulkInputCaller already supports this itself, we don't need to maintain a list of records in HubDataWriter. 
    
    As a result of this, rewrote a lot of the tests so that HubDataWriter.commit is called after all the fruit rows are written. This ensures that any pending writes are flushed.

---
+++ marklogic-data-hub-spark-connector/src/test/java/com/marklogic/hub/spark/sql/sources/v2/HandleAbortedWriterTest.java
@@ -0,0 +1,37 @@
+package com.marklogic.hub.spark.sql.sources.v2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HandleAbortedWriterTest extends AbstractSparkConnectorTest {
+
+    @Test
+    void dataWriterIsAborted() {
+        initializeDataWriter(newFruitOptions());
+        assertEquals("started", getJobDocumentStatus());
+
+        hubDataWriter.write(buildRow("apple", "red"));
+        hubDataWriter.abort();
+        logger.info("This test is only verifying that abort doesn't throw an error. It is expected to interrupt " +
+            "the BulkInputCaller, but it's not clear what impact that has - as of Java Client 5.3.0, it's possible to " +
+            "keep using the caller by calling accept and awaitCompletion on it.");
+
+        assertEquals("started", getJobDocumentStatus(), "Aborting the DataWriter won't impact the job document; it's " +
+            "still on the DataSourceWriter to take care of updating the job doc");
+    }
+
+    @Test
+    void dataSourceWriterIsAborted() {
+        initializeDataWriter(newFruitOptions());
+        assertEquals("started", getJobDocumentStatus());
+
+        writeRows(buildRow("apple", "red"), buildRow("blueberry", "blue"));
+        assertEquals(2, getFruitCount(), "Just verifying the two fruits were written");
+
+        dataSourceWriter.abort(null);
+        assertEquals("canceled", getJobDocumentStatus(), "If the DataSourceWriter is aborted for any reason, the job " +
+            "document should have a status of 'canceled'. Note that this does not imply whether any writes failed. " +
+            "But that is a limitation of the JobStatus class in DHF.");
+    }
+}
