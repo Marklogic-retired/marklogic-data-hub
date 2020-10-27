7a67647477abf6601d156dad98dfd9b19381c175
Rob Rudin <rob.rudin@marklogic.com>
2020-10-27 15:21:05 -0400

    DHFPROD-6161: User can now customize job endpoints

--- marklogic-data-hub-api/src/testFixtures/java/com/marklogic/hub/test/TestObject.java
+++ marklogic-data-hub-api/src/testFixtures/java/com/marklogic/hub/test/TestObject.java
@@ -4,8 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.marklogic.client.ext.helper.LoggingObject;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.FileCopyUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Base class for any object that helps with writing tests. Should only contain the most generic helper methods.
@@ -19,6 +23,14 @@ public abstract class TestObject extends LoggingObject {
             Thread.sleep(ms);
         } catch (InterruptedException ex) {
             logger.warn("Unexpected InterruptedException: " + ex.getMessage());
+        }
+    }
+
+    protected InputStream readInputStreamFromClasspath(String path) {
+        try {
+            return new ClassPathResource(path).getInputStream();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
         }
     }
 
