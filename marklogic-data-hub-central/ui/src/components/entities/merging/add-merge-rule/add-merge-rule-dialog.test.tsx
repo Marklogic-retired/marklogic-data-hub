a69de5c1fbc6a7300edad62e10e35dae007a13dc
sanjeevani vishaka <sanjeevani.vishaka@marklogic.com>
2020-11-02 10:13:38 -0600

    DHFPROD-5245: add merge rule property specific mergetype

--- marklogic-data-hub-central/ui/src/components/entities/merging/add-merge-rule/add-merge-rule-dialog.test.tsx
+++ marklogic-data-hub-central/ui/src/components/entities/merging/add-merge-rule/add-merge-rule-dialog.test.tsx
@@ -1,63 +1,96 @@
 import React from 'react';
-import { render, cleanup, fireEvent } from '@testing-library/react';
+import {render, cleanup, fireEvent} from '@testing-library/react';
 import AddMergeRuleDialog from './add-merge-rule-dialog';
 import data from "../../../../assets/mock-data/curation/merging.data";
-import { CurationContext } from '../../../../util/curation-context';
-import { customerMergingStep } from '../../../../assets/mock-data/curation/curation-context-mock';
+import {CurationContext} from '../../../../util/curation-context';
+import {customerMergingStep} from '../../../../assets/mock-data/curation/curation-context-mock';
 
 describe('Add Merge Rule Dialog component', () => {
 
-  afterEach(cleanup);
+    afterEach(cleanup);
 
-  test('Verify Add Merge Rule dialog renders correctly', () => {
-    const { getByText, getByTestId, getByLabelText, queryByLabelText } = render(
-    <CurationContext.Provider value={customerMergingStep}>
-        <AddMergeRuleDialog
-          {...data.mergingDataProps}
-        />
-      </CurationContext.Provider>
-    );
+    test('Verify Add Merge Rule dialog with custom mergeType renders correctly', () => {
+        const {getByText, getByTestId, getByLabelText, queryByLabelText, getByPlaceholderText} = render(
+            <CurationContext.Provider value={customerMergingStep}>
+                <AddMergeRuleDialog
+                    {...data.mergingDataProps}
+                />
+            </CurationContext.Provider>
+        );
 
-    expect(getByText('Add Merge Rule')).toBeInTheDocument();
-    expect(getByText('Select the property and the merge type for this merge rule. When you select a structured type property, the merge rule is applied to all the properties within that structured type property as well.')).toBeInTheDocument();
-    expect(getByTestId('multipleIconLegend')).toBeInTheDocument();
-    expect(getByTestId('structuredIconLegend')).toBeInTheDocument();
-    expect(getByLabelText('formItem-Property')).toBeInTheDocument();
+        expect(getByText('Add Merge Rule')).toBeInTheDocument();
+        expect(getByText('Select the property and the merge type for this merge rule. When you select a structured type property, the merge rule is applied to all the properties within that structured type property as well.')).toBeInTheDocument();
+        expect(getByTestId('multipleIconLegend')).toBeInTheDocument();
+        expect(getByTestId('structuredIconLegend')).toBeInTheDocument();
+        expect(getByLabelText('formItem-Property')).toBeInTheDocument();
 
-    fireEvent.click(getByText('Select property'));
-    fireEvent.click(getByText('customerId'));
+        fireEvent.click(getByText('Select property'));
+        fireEvent.click(getByText('customerId'));
 
-    //Confirming that URI, function and namespace fields are not available now, because Custom merge type is not selected yet.
-    expect(queryByLabelText('uri-input')).not.toBeInTheDocument();
-    expect(queryByLabelText('function-input')).not.toBeInTheDocument();
-    expect(queryByLabelText('namespace-input')).not.toBeInTheDocument();
+        //Confirming that URI, function and namespace fields are not available now, because Custom merge type is not selected yet.
+        expect(queryByLabelText('uri-input')).not.toBeInTheDocument();
+        expect(queryByLabelText('function-input')).not.toBeInTheDocument();
+        expect(queryByLabelText('namespace-input')).not.toBeInTheDocument();
 
-    //Selecting the merge type to Custom
-    fireEvent.click(getByLabelText('mergeType-select'));
-    fireEvent.click(getByTestId('mergeTypeOptions-Custom'));
+        //Selecting the merge type to Custom
+        fireEvent.click(getByLabelText('mergeType-select'));
+        fireEvent.click(getByTestId('mergeTypeOptions-Custom'));
 
-    //Initializing the required elements to be re-used later.
-    let uri = getByLabelText('uri-input');
-    let functionValue = getByLabelText('function-input');
-    let saveButton = getByText('Save');
+        //Initializing the required elements to be re-used later.
+        let uri = getByLabelText('uri-input');
+        let functionValue = getByLabelText('function-input');
+        let saveButton = getByText('Save');
 
-    //Checking if URI, function and namespace fields are available now, since merge type is Custom.
-    expect(uri).toBeInTheDocument();
-    expect(functionValue).toBeInTheDocument();
-    expect(getByLabelText('namespace-input')).toBeInTheDocument();
+        //Checking if URI, function and namespace fields are available now, since merge type is Custom.
+        expect(uri).toBeInTheDocument();
+        expect(functionValue).toBeInTheDocument();
+        expect(getByLabelText('namespace-input')).toBeInTheDocument();
 
-    fireEvent.click(saveButton); //Will throw an error because URI and Function are mandatory fields.
+        fireEvent.click(saveButton); //Will throw an error because URI and Function are mandatory fields.
 
-    //verify if the below error messages are displayed properly
-    expect(getByText('URI is required')).toBeInTheDocument();
-    expect(getByText('Function is required')).toBeInTheDocument();
+        //verify if the below error messages are displayed properly
+        expect(getByText('URI is required')).toBeInTheDocument();
+        expect(getByText('Function is required')).toBeInTheDocument();
 
-    //Enter the values for URI and Function fields and see if the save button gets enabled.
-    fireEvent.change(uri, { target: {value: 'Customer/Cust1.json'}});
-    fireEvent.change(functionValue, { target: {value: 'Compare'}});
+        //Enter the values for URI and Function fields and see if the save button gets enabled.
+        fireEvent.change(uri, {target: {value: 'Customer/Cust1.json'}});
+        fireEvent.change(functionValue, {target: {value: 'Compare'}});
 
-    fireEvent.click(saveButton); //Modal will close now
-    expect(data.mergingDataProps.setOpenAddMergeRuleDialog).toHaveBeenCalledTimes(1);
-});
+        fireEvent.click(saveButton); //Modal will close now
+        expect(data.mergingDataProps.setOpenAddMergeRuleDialog).toHaveBeenCalledTimes(1);
+
+    });
+
+    test('Verify Add Merge Rule dialog with property-specific mergeType renders correctly', () => {
+        const {getByText, getByTestId, getByLabelText, queryByLabelText, getByPlaceholderText} = render(
+            <CurationContext.Provider value={customerMergingStep}>
+                <AddMergeRuleDialog
+                    {...data.mergingDataProps}
+                />
+            </CurationContext.Provider>
+        );
+
+        fireEvent.click(getByText('Select property'));
+        fireEvent.click(getByText('customerId'));
+
+        //Confirming that max values, max sources and add button are not available now, because property-specific merge type is not selected yet.
+        expect(queryByLabelText('Enter max values')).not.toBeInTheDocument();
+        expect(queryByLabelText('Enter max sources')).not.toBeInTheDocument();
+        expect(queryByLabelText('add-slider-button')).not.toBeInTheDocument();
+
+        let saveButton = getByText('Save');
+
+        //Selecting the merge type to Property-Specific
+        fireEvent.click(getByLabelText('mergeType-select'));
+        fireEvent.click(getByTestId('mergeTypeOptions-Property-specific'));
+
+        //Checking if max values, max sources, priority slider and add button are available now, since merge type is property-specific.
+        expect(getByPlaceholderText('Enter max values')).toBeInTheDocument();
+        expect(getByPlaceholderText('Enter max sources')).toBeInTheDocument();
+        expect(getByTestId('priorityOrderSlider')).toBeInTheDocument();
+        expect(getByLabelText('add-slider-button')).toBeInTheDocument();
+
+        fireEvent.click(saveButton); //Modal will close now
+    });
 
 });
