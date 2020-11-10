924a04d95536694963a126dbe76e9ce366f37088
srinathgit <ssambasu@marklogic.com>
2020-11-10 15:45:39 -0800

    DHFPROD-6194:Recognize errors in Target Permissions on field blur

--- marklogic-data-hub-central/ui/src/config/messages.config.ts
+++ marklogic-data-hub-central/ui/src/config/messages.config.ts
@@ -1,6 +1,6 @@
 const AdvancedSettingsMessages = {
     'targetPermissions': {
-        'incorrectFormat' : 'The format of the string is incorrect. The required format is role,capability,role,capability,....',
+        'incorrectFormat' : 'The format of the string is incorrect. The required format is role,capability,role,capability,...',
         'invalidCapabilities' : 'The string contains invalid capabilities. Capabilities must be read, insert, update or execute.'
     }
 };
