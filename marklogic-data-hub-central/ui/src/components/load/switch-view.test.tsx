83299595a69deadfc410678b4b6d4066e0460469
Jaker Yang <jyang@hpzbookg5-6095.marklogic.com>
2020-11-25 14:16:10 -0600

    DHFPROD-5623: added focus to switch view buttons in load tile

--- marklogic-data-hub-central/ui/src/components/load/switch-view.test.tsx
+++ marklogic-data-hub-central/ui/src/components/load/switch-view.test.tsx
@@ -30,4 +30,46 @@ describe('Switch view component', () => {
     expect(getByLabelText('switch-view-card')).toHaveStyle('color: rgb(127, 134, 181');
   });
 
+  test('Verify integrity of enter and arrow keys navigation', () => {
+
+    const { getByLabelText } = render(
+      <SwitchView handleSelection={() => null} defaultView={INITIAL_VIEW} />
+    );
+
+    expect(getByLabelText('switch-view')).toBeInTheDocument();
+    getByLabelText('switch-view').firstChild.focus();
+
+    // verify default selection
+    expect(getByLabelText('switch-view-card')).toHaveProperty('checked', true);
+    expect(getByLabelText('switch-view-list')).toHaveProperty('checked', false);
+
+    // verify pressing enter switches the view to list
+    fireEvent.keyDown(getByLabelText('switch-view'), { key: 'Enter', code: 'Enter' });
+    expect(getByLabelText('switch-view-card')).toHaveProperty('checked', false);
+    expect(getByLabelText('switch-view-list')).toHaveProperty('checked', true);
+
+    // verify pressing enter again switches the view back to card
+    fireEvent.keyDown(getByLabelText('switch-view'), { key: 'Enter', code: 'Enter' });
+    expect(getByLabelText('switch-view-card')).toHaveProperty('checked', true);
+    expect(getByLabelText('switch-view-list')).toHaveProperty('checked', false);
+
+    // verify pressing right arrow when on card switches view to list
+    fireEvent.keyDown(getByLabelText('switch-view'), { key: 'ArrowRight', code: 'ArrowRight' });
+    expect(getByLabelText('switch-view-card')).toHaveProperty('checked', false);
+    expect(getByLabelText('switch-view-list')).toHaveProperty('checked', true);
+
+    // verify pressing right arrow when on list does not switch view
+    fireEvent.keyDown(getByLabelText('switch-view'), { key: 'ArrowRight', code: 'ArrowRight' });
+    expect(getByLabelText('switch-view-list')).toHaveProperty('checked', true);
+
+    // verify pressing left arrow when on list switches view to card
+    fireEvent.keyDown(getByLabelText('switch-view'), { key: 'ArrowLeft', code: 'ArrowLeft' });
+    expect(getByLabelText('switch-view-card')).toHaveProperty('checked', true);
+    expect(getByLabelText('switch-view-list')).toHaveProperty('checked', false);
+
+    // verify pressing left arrow when on card does not switch view
+    fireEvent.keyDown(getByLabelText('switch-view'), { key: 'ArrowLeft', code: 'ArrowLeft' });
+    expect(getByLabelText('switch-view-card')).toHaveProperty('checked', true);
+  });
+
 });
