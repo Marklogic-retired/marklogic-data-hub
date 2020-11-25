a6c4704ec9213e61d3be3afb26784eb741a9989f
Jaker Yang <jyang@hpzbookg5-6095.marklogic.com>
2020-11-25 14:16:10 -0600

    DHFPROD-5623: added focus to switch view buttons in load tile

--- marklogic-data-hub-central/ui/src/components/load/switch-view.tsx
+++ marklogic-data-hub-central/ui/src/components/load/switch-view.tsx
@@ -17,19 +17,58 @@ const SwitchView: React.FC<Props> = (props) => {
         props.handleSelection(val);
     };
 
+    const radioKeyDownHandler = (event) => {
+        if (event.key === 'Enter') {
+            switch (view) {
+                case 'list':
+                    onChange('card');
+                    break;
+                case 'card':
+                    onChange('list');
+                    break;
+                default:
+                    // should not ever reach this code
+                    // do nothing
+                    break;
+            }
+        }
+
+        if (event.key == 'ArrowLeft') {
+            // stops default react radio button controls
+            event.preventDefault();
+
+            if (view === 'list')
+                onChange('card');
+        }
+
+        if (event.key == 'ArrowRight') {
+            event.preventDefault();
+            
+            if (view === 'card')
+                onChange('list');
+        }
+
+        // disable up and down arrow default controls on radio buttons
+        if (event.key == 'ArrowUp' || event.key == 'ArrowDown')
+            event.preventDefault();
+    };
+
     return (
-        <div id="switch-view" aria-label="switch-view">
+        <div id="switch-view" aria-label="switch-view" onKeyDown={radioKeyDownHandler}>
             <MLRadio.MLGroup
                 buttonStyle="outline"
+                className={'radioGroupView'}
                 defaultValue={view}
                 name="radiogroup"
                 onChange={e => onChange(e.target.value)}
                 size="large"
+                style={{ color: '#999' }}
+                tabIndex={0}
             >
-                <MLRadio.MLButton aria-label="switch-view-card" value={'card'}>
+                <MLRadio.MLButton aria-label="switch-view-card" value={'card'} checked={view === 'card'}>
                     <i>{<FontAwesomeIcon icon={faThLarge} />}</i>
                 </MLRadio.MLButton>
-                <MLRadio.MLButton aria-label="switch-view-list" value={'list'}>
+                <MLRadio.MLButton aria-label="switch-view-list" value={'list'} checked={view === 'list'}>
                     <i>{<FontAwesomeIcon icon={faTable} />}</i>
                 </MLRadio.MLButton>
             </MLRadio.MLGroup>
