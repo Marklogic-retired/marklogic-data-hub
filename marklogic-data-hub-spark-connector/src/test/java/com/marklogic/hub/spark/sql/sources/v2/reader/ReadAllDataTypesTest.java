9c101f4a884c091cba773e84a411308f5298400d
Rob Rudin <rob.rudin@marklogic.com>
2020-12-01 16:48:23 -0500

    DHFPROD-6237: Can now read arrays and maps with Spark connector
    Reworked HubInputPartitionReader to use Scala's JacksonParser to read a string of JSON into an InternalRow; resulted in a lot of code being deleted from HubInputPartitionReader. 
    
    Made a couple tweaks to ReadAllDataTypesTest too:
    
    - The timestamp is being returned as a long by JacksonParser, so verifying the correct long is returned
    - JacksonParser is not able to convert a hexBinary into a byte array, so readLib was updated to associate hexBinary with string

--- marklogic-data-hub-spark-connector/src/test/java/com/marklogic/hub/spark/sql/sources/v2/reader/ReadAllDataTypesTest.java
+++ marklogic-data-hub-spark-connector/src/test/java/com/marklogic/hub/spark/sql/sources/v2/reader/ReadAllDataTypesTest.java
@@ -3,46 +3,36 @@ package com.marklogic.hub.spark.sql.sources.v2.reader;
 import com.marklogic.client.io.DocumentMetadataHandle;
 import com.marklogic.client.io.Format;
 import com.marklogic.client.io.InputStreamHandle;
-import com.marklogic.hub.HubClient;
 import com.marklogic.hub.spark.sql.sources.v2.Options;
-import com.marklogic.io.Base64;
-import org.apache.commons.codec.binary.Hex;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.types.DataTypes;
 import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
-public class ReadAllDataTypesTest extends AbstractSparkReadTest{
+public class ReadAllDataTypesTest extends AbstractSparkReadTest {
 
     @Test
-    void readAllDataTypes() throws Exception{
+    void readAllDataTypes() throws Exception {
         runAsDataHubDeveloper();
         loadTDE("User");
-
-        HubClient client = runAsDataHubOperator();
-        InputStreamHandle handle = new InputStreamHandle(readInputStreamFromClasspath("entityInstances/User/user1.json"));
-        handle.withFormat(Format.JSON);
-        client.getFinalClient().newJSONDocumentManager().write("/doc1.json", new DocumentMetadataHandle()
-                .withCollections("User")
-                .withPermission("data-hub-operator", DocumentMetadataHandle.Capability.READ, DocumentMetadataHandle.Capability.UPDATE),
-            handle);
-
+        runAsDataHubOperator();
+        writeSingleUserToFinal();
 
         Options options = newOptions().withView("User").withNumPartitions("2");
         HubDataSourceReader dataSourceReader = new HubDataSourceReader(options.toDataSourceOptions());
         List<InternalRow> rows = readRows(dataSourceReader);
         assertEquals(1, rows.size());
-        //There is only one row of data.
-        InternalRow row =  rows.get(0);
+        InternalRow row = rows.get(0);
 
         //The comment in the assertions will have the datatype of the property in TDE template
         assertEquals(1, row.getInt(0), "integer");
         assertEquals(1, row.getInt(1), "int");
-        assertEquals(Byte.decode("1"), row.getByte(2),"byte");
-        assertEquals(1, row.getInt(3),"positiveInteger");
+        assertEquals(Byte.decode("1"), row.getByte(2), "byte");
+        assertEquals(1, row.getInt(3), "positiveInteger");
         assertEquals(1, row.getInt(4), "nonNegativeInteger");
         assertEquals(1L, row.getLong(5), "long");
         assertEquals(1L, row.getLong(6), "unsignedLong");
@@ -53,8 +43,12 @@ public class ReadAllDataTypesTest extends AbstractSparkReadTest{
         assertEquals("John", row.getString(11), "string");
         assertEquals(6218, row.get(12, DataTypes.DateType), "date; Spark accepts java.sql.Date." +
             "1987-01-10 translates to 6218");
-        assertEquals(537336758000000L, row.get(13, DataTypes.TimestampType), "dateTime; Spark accepts java.sql.TimeStamp." +
-            "1987-01-10T20:12:38.643728-08:00 translates to 537336758000000L milliseconds");
+
+        final String expectedDateTimeOfBirth = "1987-01-10T12:13:14-00:00";
+        final long dateTimeAsNanoseconds = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX").parse(expectedDateTimeOfBirth).getTime() * 1000;
+        assertEquals(dateTimeAsNanoseconds, row.get(13, DataTypes.LongType), "For a timestamp, the Scala JacksonParser will " +
+            "return a long value in nanoseconds corresponding to the given timestamp; ");
+
         assertEquals("20:12:38", row.getString(14), "time");
         assertEquals("/John/IRI", row.getString(15), "IRI");
         assertEquals("/John/IRI", row.getString(16), "anyURI");
@@ -74,16 +68,20 @@ public class ReadAllDataTypesTest extends AbstractSparkReadTest{
         assertEquals("P30DT1H", row.getString(30), "dayTimeDuration");
         assertEquals("37.389965,-122.07858", row.getString(31), "point");
         assertEquals("37.389965,-122.07858", row.getString(32), "longLatPoint");
+        assertEquals("Dum spiro spero", new String(row.getBinary(33), "UTF-8"), "The Scala JacksonParser is able to " +
+            "convert the value of mottoBase64 into a byte array, so we can build a String with it and assert " +
+            "against the original value of that base64Binary value");
+        assertEquals("44756D20737069726F20737065726F", row.getString(34), "The Scala " +
+            "JacksonParser is not able to convert hexBinary values into a byte array, so the original value is " +
+            "returned as a string");
+    }
 
-        String base64BinaryString = new String(row.getBinary(33), "UTF-8");
-        String hexBinaryString = new String(row.getBinary(34), "UTF-8");
-        assertEquals("RHVtIHNwaXJvIHNwZXJv", base64BinaryString, "base64Binary");
-        assertEquals("44756D20737069726F20737065726F", hexBinaryString, "hexBinary");
-        String s1 = new String(Base64.decode(base64BinaryString), "UTF-8");
-        String s2 = new String(Hex.decodeHex(hexBinaryString.toCharArray()), "UTF-8");
-        assertEquals("Dum spiro spero", s1);
-        assertEquals(s1, s2);
-
-
+    private void writeSingleUserToFinal() {
+        InputStreamHandle handle = new InputStreamHandle(readInputStreamFromClasspath("entityInstances/User/user1.json"));
+        handle.withFormat(Format.JSON);
+        getHubClient().getFinalClient().newJSONDocumentManager().write("/doc1.json", new DocumentMetadataHandle()
+                .withCollections("User")
+                .withPermission("data-hub-operator", DocumentMetadataHandle.Capability.READ, DocumentMetadataHandle.Capability.UPDATE),
+            handle);
     }
 }
---
+++ marklogic-data-hub-spark-connector/src/test/java/com/marklogic/hub/spark/sql/sources/v2/reader/ReadArraysAndMapsTest.java
@@ -0,0 +1,151 @@
+package com.marklogic.hub.spark.sql.sources.v2.reader;
+
+import com.marklogic.hub.spark.sql.sources.v2.Options;
+import org.apache.spark.sql.catalyst.InternalRow;
+import org.apache.spark.sql.catalyst.util.ArrayData;
+import org.apache.spark.sql.catalyst.util.MapData;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import scala.collection.immutable.HashMap;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReadArraysAndMapsTest extends AbstractSparkReadTest {
+
+    // When building a Spark schema to be used, a Metadata object is needed; an empty one suffices for these tests
+    private Metadata emptySchemaMetadata = new Metadata(new HashMap<>());
+
+    @BeforeEach
+    void setup() {
+        runAsDataHubDeveloper();
+        loadSimpleCustomerTDE();
+        runAsDataHubOperator();
+    }
+
+    @Test
+    void arrayOfStrings() {
+        loadSimpleCustomers(1);
+
+        StructType sparkSchema = new StructType(new StructField[]{
+            new StructField("customerId", DataTypes.IntegerType, false, emptySchemaMetadata),
+            new StructField("myStrings", DataTypes.createArrayType(DataTypes.StringType), true, emptySchemaMetadata)
+        });
+
+        Options options = newOptions()
+            .withSerializedPlan(buildPlanForArrayOfStrings())
+            .withSparkSchema(sparkSchema.json());
+
+        List<InternalRow> rows = readRows(options);
+        assertEquals(1, rows.size());
+        assertEquals(0, rows.get(0).getInt(0));
+        Object[] strings = rows.get(0).getArray(1).toObjectArray(DataTypes.StringType);
+        assertEquals("hello", strings[0].toString(), "The type of the string is a UTF8String, so need to call " +
+            "toString on it to get the actual value");
+        assertEquals("Customer0", strings[1].toString());
+    }
+
+    @Test
+    void mapOfIntegers() {
+        loadSimpleCustomers(2);
+
+        StructType sparkSchema = new StructType(new StructField[]{
+            new StructField("myObject", DataTypes.createMapType(DataTypes.StringType, DataTypes.IntegerType), true, emptySchemaMetadata)
+        });
+
+        Options options = newOptions()
+            .withSerializedPlan(buildPlanForObjectOfStringsAndIntegers())
+            .withSparkSchema(sparkSchema.json());
+
+        List<InternalRow> rows = readRows(options);
+        assertEquals(1, rows.size(), "Expecting a single row containing myObject");
+        MapData mapData = rows.get(0).getMap(0);
+        Object[] keys = mapData.keyArray().toObjectArray(DataTypes.StringType);
+        assertEquals("customerIdCount", keys[0].toString());
+        assertEquals("otherCustomerIdCount", keys[1].toString());
+        Object[] values = mapData.valueArray().toObjectArray(DataTypes.IntegerType);
+        assertEquals(2, values[0]);
+        assertEquals(2, values[1]);
+    }
+
+    @Test
+    void arrayOfObjects() {
+        loadSimpleCustomers(3);
+
+        StructType sparkSchema = new StructType(new StructField[]{
+            new StructField("customerId", DataTypes.IntegerType, false, emptySchemaMetadata),
+            new StructField("myArrayOfObjects", DataTypes.createArrayType(
+                DataTypes.createMapType(DataTypes.StringType, DataTypes.StringType)
+            ), true, emptySchemaMetadata)
+        });
+
+        Options options = newOptions()
+            .withSerializedPlan(buildPlanForArrayOfObjects())
+            .withSparkSchema(sparkSchema.json());
+
+        List<InternalRow> rows = readRows(options);
+        assertEquals(3, rows.size(), "Expecting one row for each customer since customerId is one of the columns being selected");
+
+        for (InternalRow row : rows) {
+            ArrayData array = row.getArray(1);
+            MapData mapData = array.getMap(0);
+            Object[] keys = mapData.keyArray().toObjectArray(DataTypes.StringType);
+            assertEquals("hello", keys[0].toString());
+            assertEquals("and", keys[1].toString());
+            Object[] values = mapData.valueArray().toObjectArray(DataTypes.StringType);
+            assertEquals("world", values[0].toString());
+            assertEquals("hello again", values[1].toString());
+        }
+
+    }
+
+    private String buildPlanForArrayOfStrings() {
+        return exportQueryToPlan("const op = require('/MarkLogic/optic');\n" +
+            "op.fromView(null, 'Customer', '')\n" +
+            "  .select(['customerId', \n" +
+            "    op.as('myStrings', op.jsonArray([\n" +
+            "      op.jsonString('hello'),\n" +
+            "      op.col('name')\n" +
+            "    ]))\n" +
+            "  ])\n" +
+            "  .export()");
+    }
+
+    private String buildPlanForObjectOfStringsAndIntegers() {
+        return exportQueryToPlan("const op = require('/MarkLogic/optic');\n" +
+            "op.fromView(null, 'Customer', '')\n" +
+            "  .groupBy(null, op.count('customerIdCount'))\n" +
+            "  .select([\n" +
+            "    op.as('myObject', op.jsonObject([\n" +
+            "      op.prop('customerIdCount', op.col('customerIdCount')),\n" +
+            "      op.prop('otherCustomerIdCount', op.col('customerIdCount'))\n" +
+            "    ]))\n" +
+            "  ])\n" +
+            "  .export()");
+    }
+
+
+    private String buildPlanForArrayOfObjects() {
+        return exportQueryToPlan("const op = require('/MarkLogic/optic');\n" +
+            "op.fromView(null, 'Customer', '')\n" +
+            "  .groupBy('customerId', op.count('customerIdCount'))\n" +
+            "  .select(['customerId',\n" +
+            "    op.as('myArrayOfObjects', op.jsonArray([\n" +
+            "      op.jsonObject([\n" +
+            "        op.prop('hello', 'world'),\n" +
+            "        op.prop('and', 'hello again'),\n" +
+            "      ])\n" +
+            "    ]))\n" +
+            "  ])\n" +
+            "  .export()");
+    }
+
+    private String exportQueryToPlan(String query) {
+        return getHubClient().getFinalClient().newServerEval().javascript(query).evalAs(String.class);
+    }
+}
