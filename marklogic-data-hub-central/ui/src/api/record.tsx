69e5ab4dde46e335b17159eba88065d8e283cb9a
timur <timur38@gmail.com>
2020-12-08 13:43:42 -0800

    DHFPROD-6332: FE Implementation: Download a record or artifact from All Data View

---
+++ marklogic-data-hub-central/ui/src/api/record.tsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const getRecord = async (uri, database) => {
+    return await axios({
+      url: `/api/record/download?docUri=${uri}&database=${database}`,
+      method: "GET",
+      responseType: "blob"
+    });
+};
