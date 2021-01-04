382327c03e175f9d4bbd15c6573e772d43e5f824
Rob Rudin <rob.rudin@marklogic.com>
2021-01-04 10:01:43 -0500

    DHFPROD-6414: Added JSON schema for Content object
    Excluding "previousUri" for now, as that's only used internally. If we want to expose it to users, we will likely use a more meaningful name like "derivedFrom".

---
+++ marklogic-data-hub-central/src/main/java/com/marklogic/hub/central/schemas/Context.java
@@ -0,0 +1,198 @@
+
+package com.marklogic.hub.central.schemas;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+/**
+ * Defines properties that affect how a document is inserted
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "collections",
+    "originalCollections",
+    "permissions",
+    "metadata"
+})
+public class Context {
+
+    /**
+     * The collections to be included when the document is inserted; not populated when passed to a step module; intended to be populated by a step module
+     * 
+     */
+    @JsonProperty("collections")
+    @JsonPropertyDescription("The collections to be included when the document is inserted; not populated when passed to a step module; intended to be populated by a step module")
+    private List<String> collections = new ArrayList<String>();
+    /**
+     * The collections that exist on the document
+     * 
+     */
+    @JsonProperty("originalCollections")
+    @JsonPropertyDescription("The collections that exist on the document")
+    private List<String> originalCollections = new ArrayList<String>();
+    /**
+     * The permissions to be included when the document is inserted
+     * 
+     */
+    @JsonProperty("permissions")
+    @JsonPropertyDescription("The permissions to be included when the document is inserted")
+    private List<Permission> permissions = new ArrayList<Permission>();
+    /**
+     * The metadata keys and values to be included when the document is inserted
+     * 
+     */
+    @JsonProperty("metadata")
+    @JsonPropertyDescription("The metadata keys and values to be included when the document is inserted")
+    private Metadata metadata;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * The collections to be included when the document is inserted; not populated when passed to a step module; intended to be populated by a step module
+     * 
+     */
+    @JsonProperty("collections")
+    public List<String> getCollections() {
+        return collections;
+    }
+
+    /**
+     * The collections to be included when the document is inserted; not populated when passed to a step module; intended to be populated by a step module
+     * 
+     */
+    @JsonProperty("collections")
+    public void setCollections(List<String> collections) {
+        this.collections = collections;
+    }
+
+    /**
+     * The collections that exist on the document
+     * 
+     */
+    @JsonProperty("originalCollections")
+    public List<String> getOriginalCollections() {
+        return originalCollections;
+    }
+
+    /**
+     * The collections that exist on the document
+     * 
+     */
+    @JsonProperty("originalCollections")
+    public void setOriginalCollections(List<String> originalCollections) {
+        this.originalCollections = originalCollections;
+    }
+
+    /**
+     * The permissions to be included when the document is inserted
+     * 
+     */
+    @JsonProperty("permissions")
+    public List<Permission> getPermissions() {
+        return permissions;
+    }
+
+    /**
+     * The permissions to be included when the document is inserted
+     * 
+     */
+    @JsonProperty("permissions")
+    public void setPermissions(List<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    /**
+     * The metadata keys and values to be included when the document is inserted
+     * 
+     */
+    @JsonProperty("metadata")
+    public Metadata getMetadata() {
+        return metadata;
+    }
+
+    /**
+     * The metadata keys and values to be included when the document is inserted
+     * 
+     */
+    @JsonProperty("metadata")
+    public void setMetadata(Metadata metadata) {
+        this.metadata = metadata;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Context.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("collections");
+        sb.append('=');
+        sb.append(((this.collections == null)?"<null>":this.collections));
+        sb.append(',');
+        sb.append("originalCollections");
+        sb.append('=');
+        sb.append(((this.originalCollections == null)?"<null>":this.originalCollections));
+        sb.append(',');
+        sb.append("permissions");
+        sb.append('=');
+        sb.append(((this.permissions == null)?"<null>":this.permissions));
+        sb.append(',');
+        sb.append("metadata");
+        sb.append('=');
+        sb.append(((this.metadata == null)?"<null>":this.metadata));
+        sb.append(',');
+        sb.append("additionalProperties");
+        sb.append('=');
+        sb.append(((this.additionalProperties == null)?"<null>":this.additionalProperties));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = ((result* 31)+((this.metadata == null)? 0 :this.metadata.hashCode()));
+        result = ((result* 31)+((this.originalCollections == null)? 0 :this.originalCollections.hashCode()));
+        result = ((result* 31)+((this.additionalProperties == null)? 0 :this.additionalProperties.hashCode()));
+        result = ((result* 31)+((this.collections == null)? 0 :this.collections.hashCode()));
+        result = ((result* 31)+((this.permissions == null)? 0 :this.permissions.hashCode()));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Context) == false) {
+            return false;
+        }
+        Context rhs = ((Context) other);
+        return ((((((this.metadata == rhs.metadata)||((this.metadata!= null)&&this.metadata.equals(rhs.metadata)))&&((this.originalCollections == rhs.originalCollections)||((this.originalCollections!= null)&&this.originalCollections.equals(rhs.originalCollections))))&&((this.additionalProperties == rhs.additionalProperties)||((this.additionalProperties!= null)&&this.additionalProperties.equals(rhs.additionalProperties))))&&((this.collections == rhs.collections)||((this.collections!= null)&&this.collections.equals(rhs.collections))))&&((this.permissions == rhs.permissions)||((this.permissions!= null)&&this.permissions.equals(rhs.permissions))));
+    }
+
+}
---
+++ marklogic-data-hub-central/src/main/java/com/marklogic/hub/central/schemas/Permission.java
@@ -0,0 +1,129 @@
+
+package com.marklogic.hub.central.schemas;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "roleId",
+    "capability"
+})
+public class Permission {
+
+    /**
+     * MarkLogic-defined ID of the role associated with this permission
+     * 
+     */
+    @JsonProperty("roleId")
+    @JsonPropertyDescription("MarkLogic-defined ID of the role associated with this permission")
+    private String roleId;
+    /**
+     * Either read, update, insert, or execute
+     * 
+     */
+    @JsonProperty("capability")
+    @JsonPropertyDescription("Either read, update, insert, or execute")
+    private String capability;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * MarkLogic-defined ID of the role associated with this permission
+     * 
+     */
+    @JsonProperty("roleId")
+    public String getRoleId() {
+        return roleId;
+    }
+
+    /**
+     * MarkLogic-defined ID of the role associated with this permission
+     * 
+     */
+    @JsonProperty("roleId")
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    /**
+     * Either read, update, insert, or execute
+     * 
+     */
+    @JsonProperty("capability")
+    public String getCapability() {
+        return capability;
+    }
+
+    /**
+     * Either read, update, insert, or execute
+     * 
+     */
+    @JsonProperty("capability")
+    public void setCapability(String capability) {
+        this.capability = capability;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Permission.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("roleId");
+        sb.append('=');
+        sb.append(((this.roleId == null)?"<null>":this.roleId));
+        sb.append(',');
+        sb.append("capability");
+        sb.append('=');
+        sb.append(((this.capability == null)?"<null>":this.capability));
+        sb.append(',');
+        sb.append("additionalProperties");
+        sb.append('=');
+        sb.append(((this.additionalProperties == null)?"<null>":this.additionalProperties));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = ((result* 31)+((this.capability == null)? 0 :this.capability.hashCode()));
+        result = ((result* 31)+((this.additionalProperties == null)? 0 :this.additionalProperties.hashCode()));
+        result = ((result* 31)+((this.roleId == null)? 0 :this.roleId.hashCode()));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Permission) == false) {
+            return false;
+        }
+        Permission rhs = ((Permission) other);
+        return ((((this.capability == rhs.capability)||((this.capability!= null)&&this.capability.equals(rhs.capability)))&&((this.additionalProperties == rhs.additionalProperties)||((this.additionalProperties!= null)&&this.additionalProperties.equals(rhs.additionalProperties))))&&((this.roleId == rhs.roleId)||((this.roleId!= null)&&this.roleId.equals(rhs.roleId))));
+    }
+
+}
