bb9685fddec8cf8d0dcbcfc1cbf672b27f35f0e1
Akshay Sonvane <akshay.sonvane93@gmail.com>
2021-02-23 14:59:24 -0800

    DHFPROD-6962: Only enable hubDhs for aws/azure spring profile

--- marklogic-data-hub-central/src/test/java/com/marklogic/hub/central/HubCentralTest.java
+++ marklogic-data-hub-central/src/test/java/com/marklogic/hub/central/HubCentralTest.java
@@ -5,12 +5,12 @@ import com.marklogic.hub.DatabaseKind;
 import com.marklogic.hub.impl.HubConfigImpl;
 import com.marklogic.mgmt.ManageConfig;
 import com.marklogic.mgmt.util.PropertySource;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mock.env.MockEnvironment;
 
+import java.io.IOException;
 import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,44 +20,68 @@ public class HubCentralTest {
     HubCentral hubCentral;
     MockEnvironment mockEnvironment;
 
-    @BeforeEach
-    void beforeEach() throws Exception {
-        Properties defaultProps = new Properties();
-        defaultProps.load(new ClassPathResource("application.properties").getInputStream());
-        mockEnvironment = new MockEnvironment();
-        mockEnvironment.getPropertySources().addFirst(new PropertiesPropertySource("default", defaultProps));
-
-        hubCentral = new HubCentral();
-        hubCentral.environment = mockEnvironment;
+    @Test
+    void testNewDefaultHubConfig() throws IOException {
+        setupEnvironment("application.properties");
+        verifyDefaultPropertiesForSslAndAuthentication();
+        newDefaultHubConfig();
     }
 
     @Test
-    void verifyDefaultPropertiesForSslAndAuthentication() {
-        PropertySource props = hubCentral.buildPropertySource("anyone", "anyword");
-        assertEquals("anyone", props.getProperty("mlUsername"));
-        assertEquals("anyword", props.getProperty("mlPassword"));
-        assertEquals("localhost", props.getProperty("mlHost"),
-            "For convenience while doing local development, mlHost still defaults to localhost");
-        assertEquals("true", props.getProperty("hubDhs"));
-        assertEquals("true", props.getProperty("hubSsl"));
-    }
-
-    /**
-     * This is essentially tested via every HC test as well, which is using local defaults.
-     */
-    @Test
-    void verifyLocalDefaults() {
-        hubCentral.useLocalDefaults = true;
-        PropertySource props = hubCentral.buildPropertySource("anyone", "anyword");
-        assertEquals("anyone", props.getProperty("mlUsername"));
-        assertEquals("anyword", props.getProperty("mlPassword"));
-        assertEquals("localhost", props.getProperty("mlHost"));
-        assertEquals("false", props.getProperty("hubDhs"));
-        assertEquals("false", props.getProperty("hubSsl"));
+    void testNewHubConfigForDhsOnAWS() throws IOException {
+        setupEnvironment("application-aws.properties");
+        verifyPropertiesForSslAndAuthenticationOnDhs();
+        newHubConfigForDhs();
     }
 
     @Test
-    void newHubConfig() {
+    void testNewHubConfigForDhsOnAzure() throws IOException {
+        setupEnvironment("application-azure.properties");
+        verifyPropertiesForSslAndAuthenticationOnDhs();
+        newHubConfigForDhs();
+    }
+
+    void newDefaultHubConfig() {
+        Properties customProps = new Properties();
+        customProps.setProperty("mlHost", "somehost");
+        mockEnvironment.getPropertySources().addFirst(new PropertiesPropertySource("custom", customProps));
+
+        HubConfigImpl hubConfig = hubCentral.newHubConfig("anyone", "anyword");
+
+        assertEquals("somehost", hubConfig.getAppConfig().getHost());
+        assertEquals((Integer) 8000, hubConfig.getAppConfig().getAppServicesPort());
+        assertEquals("anyone", hubConfig.getAppConfig().getAppServicesUsername(),
+                "The Versions class used appConfig.getAppServicesDatabaseClient to get the ML version");
+        assertEquals("anyword", hubConfig.getAppConfig().getAppServicesPassword());
+        assertEquals(SecurityContextType.DIGEST, hubConfig.getAppConfig().getAppServicesSecurityContextType());
+        assertNull(hubConfig.getAppConfig().getAppServicesSslContext());
+
+        assertEquals("somehost", hubConfig.getHost());
+        assertEquals("anyone", hubConfig.getMlUsername());
+        assertEquals("anyword", hubConfig.getMlPassword());
+
+        assertEquals("digest", hubConfig.getAuthMethod(DatabaseKind.STAGING));
+        assertEquals("digest", hubConfig.getAuthMethod(DatabaseKind.FINAL));
+        assertEquals("digest", hubConfig.getAuthMethod(DatabaseKind.JOB));
+
+        assertNull(hubConfig.getSslContext(DatabaseKind.STAGING));
+        assertNull(hubConfig.getSslContext(DatabaseKind.FINAL));
+        assertNull(hubConfig.getSslContext(DatabaseKind.JOB));
+
+        assertFalse(hubConfig.getIsHostLoadBalancer());
+        assertFalse(hubConfig.getIsProvisionedEnvironment());
+
+        ManageConfig manageConfig = hubConfig.getManageConfig();
+        assertEquals("anyone", manageConfig.getUsername());
+        assertEquals("anyone", manageConfig.getSecurityUsername());
+        assertEquals("somehost", manageConfig.getHost());
+        assertEquals("anyword", manageConfig.getPassword());
+        assertEquals("anyword", manageConfig.getSecurityPassword());
+        assertFalse(manageConfig.isConfigureSimpleSsl());
+        assertEquals("http", manageConfig.getScheme());
+    }
+
+    void newHubConfigForDhs() {
         Properties customProps = new Properties();
         customProps.setProperty("mlHost", "somehost");
         mockEnvironment.getPropertySources().addFirst(new PropertiesPropertySource("custom", customProps));
@@ -66,9 +90,9 @@ public class HubCentralTest {
 
         assertEquals("somehost", hubConfig.getAppConfig().getHost());
         assertEquals((Integer) 8010, hubConfig.getAppConfig().getAppServicesPort(),
-            "8010 is needed by DHS, as 8000 is locked down; this should be defined in application.properties");
+                "8010 is needed by DHS, as 8000 is locked down; this should be defined in application.properties");
         assertEquals("anyone", hubConfig.getAppConfig().getAppServicesUsername(),
-            "The Versions class used appConfig.getAppServicesDatabaseClient to get the ML version");
+                "The Versions class used appConfig.getAppServicesDatabaseClient to get the ML version");
         assertEquals("anyword", hubConfig.getAppConfig().getAppServicesPassword());
         assertEquals(SecurityContextType.BASIC, hubConfig.getAppConfig().getAppServicesSecurityContextType());
         assertNotNull(hubConfig.getAppConfig().getAppServicesSslContext());
@@ -96,5 +120,32 @@ public class HubCentralTest {
         assertEquals("anyword", manageConfig.getSecurityPassword());
         assertTrue(manageConfig.isConfigureSimpleSsl());
         assertEquals("https", manageConfig.getScheme());
+    }
+
+    void verifyDefaultPropertiesForSslAndAuthentication() {
+        PropertySource props = hubCentral.buildPropertySource("anyone", "anyword");
+        assertEquals("anyone", props.getProperty("mlUsername"));
+        assertEquals("anyword", props.getProperty("mlPassword"));
+        assertEquals("localhost", props.getProperty("mlHost"),
+                "For convenience while doing local development, mlHost still defaults to localhost");
+        assertNull(props.getProperty("hubDhs"));
+        assertNull(props.getProperty("hubSsl"));
+    }
+
+    void verifyPropertiesForSslAndAuthenticationOnDhs() {
+        PropertySource props = hubCentral.buildPropertySource("anyone", "anyword");
+        assertEquals("anyone", props.getProperty("mlUsername"));
+        assertEquals("anyword", props.getProperty("mlPassword"));
+        assertEquals("true", props.getProperty("hubDhs"));
+    }
+
+    private void setupEnvironment(String propertyFileName) throws IOException {
+        Properties defaultProps = new Properties();
+        defaultProps.load(new ClassPathResource(propertyFileName).getInputStream());
+        mockEnvironment = new MockEnvironment();
+        mockEnvironment.getPropertySources().addFirst(new PropertiesPropertySource("default", defaultProps));
+
+        hubCentral = new HubCentral();
+        hubCentral.environment = mockEnvironment;
     }
 }
