5418785e5d39d37bd8ceb3e8a0cc741cef25d14b
Rob Rudin <rob.rudin@marklogic.com>
2021-03-03 18:47:37 -0500

    DHFPROD-7006: Cleaning up some duplication of CommandLineFlowInputs

--- marklogic-data-hub-client-jar/src/main/java/com/marklogic/hub/clientjar/ClientJarFlowInputs.java
+++ marklogic-data-hub-client-jar/src/main/java/com/marklogic/hub/clientjar/ClientJarFlowInputs.java
@@ -1,286 +1,98 @@
 package com.marklogic.hub.clientjar;
 
 import com.beust.jcommander.Parameter;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.marklogic.hub.flow.FlowInputs;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.tuple.Pair;
-import org.springframework.util.FileCopyUtils;
+import com.marklogic.hub.impl.CommandLineFlowInputs;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
- * Defines command-line-specific inputs for running a flow. JCommander Parameter annotations are used so that a
- * JCommander Command class can extend this to inherit all of the parameters. Usable in a Gradle context as well for
- * collecting Gradle properties and then constructing flow inputs.
- *
- * TODO This is duplicating CommandLineFlowInputs, will clean up soon.
+ * Extends CommandLineFlowInputs and adds JCommander Parameter annotations to setters so that they can be invoked via
+ * JCommander command-line arguments.
  */
-public class ClientJarFlowInputs {
+public class ClientJarFlowInputs extends CommandLineFlowInputs {
+
 
     @Parameter(names = "-flowName", required = true, description = "The name of the flow to run")
-    private String flowName;
+    @Override
+    public void setFlowName(String flowName) {
+        super.setFlowName(flowName);
+    }
 
     @Parameter(names = "-batchSize", description = "The number of items to process in each batch")
-    private Integer batchSize;
+    @Override
+    public void setBatchSize(Integer batchSize) {
+        super.setBatchSize(batchSize);
+    }
 
     @Parameter(names = "-threadCount", description = "The number of threads to process batches with")
-    private Integer threadCount;
+    @Override
+    public void setThreadCount(Integer threadCount) {
+        super.setThreadCount(threadCount);
+    }
 
     @Parameter(names = "-inputFilePath", description = "The directory path for an ingestion step to read from")
-    private String inputFilePath;
+    @Override
+    public void setInputFilePath(String inputFilePath) {
+        super.setInputFilePath(inputFilePath);
+    }
 
     @Parameter(names = "-inputFileType", description = "The type of files for an ingestion step to process")
-    private String inputFileType;
+    @Override
+    public void setInputFileType(String inputFileType) {
+        super.setInputFileType(inputFileType);
+    }
 
     @Parameter(names = "-outputURIReplacement", description = "The pattern for replacing a portion of a file-based URI during an ingestion step")
-    private String outputURIReplacement;
+    @Override
+    public void setOutputURIReplacement(String outputURIReplacement) {
+        super.setOutputURIReplacement(outputURIReplacement);
+    }
 
     @Parameter(names = "-outputURIPrefix", description = "The prefix of the URIs of documents during an ingestion step. It shouldn't be used if 'outputURIReplacement' is set")
-    private String outputURIPrefix;
+    @Override
+    public void setOutputURIPrefix(String outputURIPrefix) {
+        super.setOutputURIPrefix(outputURIPrefix);
+    }
 
     @Parameter(names = "-separator", description = "The separator value to use when processing a file during an ingestion step")
-    private String separator;
-
-    @Parameter(names = "-failHard", description = "If included, forces a job to stop once a batch fails (no parameter value allowed)")
-    private Boolean failHard = false;
-
-    @Parameter(names = "-steps", description = "Comma-delimited string of step numbers to run; e.g. -steps 2,3,5")
-    private List<String> steps;
-
-    @Parameter(names = "-jobId", description = "A user-specified job ID")
-    private String jobId;
-
-    @Parameter(names = "-optionsJson", description = "JSON object for overriding step options; e.g. -optionsJson \"{\\\"sourceQuery\\\":\\\"cts.collectionQuery('test')\\\"}\"")
-    private String optionsJSON;
-
-    @Parameter(names = "-optionsFile", description = "Path to a file containing a JSON object for overriding step options")
-    private String optionsFile;
+    @Override
+    public void setSeparator(String separator) {
+        super.setSeparator(separator);
+    }
 
     @Parameter(names = "-showOptions", description = "If included, prints the options JSON object set via '-optionsJson' or '-optionsFile' (no parameter value allowed)")
-    private Boolean showOptions = false;
-
-    public Pair<FlowInputs, String> buildFlowInputs() {
-        StringBuilder runFlowString = new StringBuilder("Running flow: [" + flowName + "]");
-
-        if (steps != null) {
-            runFlowString.append(", steps: " + steps);
-        }
-
-        FlowInputs flowInputs = new FlowInputs(flowName);
-        flowInputs.setSteps(steps);
-        flowInputs.setJobId(jobId);
-        flowInputs.setStepConfig(buildStepConfig(runFlowString));
-
-        Map<String, Object> flowOptions = buildFlowOptions();
-        flowInputs.setOptions(flowOptions);
-        if (showOptions && flowOptions != null) {
-            runFlowString.append("\n\tand options:");
-            for (String key : flowOptions.keySet()) {
-                runFlowString.append("\n\t\t" + key + " = " + flowOptions.get(key));
-            }
-        }
-
-        return Pair.of(flowInputs, runFlowString.toString());
-    }
-
-    protected Map<String, Object> buildStepConfig(StringBuilder runFlowString) {
-        Map<String, Object> stepConfig = new HashMap<>();
-
-        if (batchSize != null) {
-            runFlowString.append("\n\twith batch size: " + batchSize);
-            stepConfig.put("batchSize", batchSize);
-        }
-        if (threadCount != null) {
-            runFlowString.append("\n\twith thread count: " + threadCount);
-            stepConfig.put("threadCount", threadCount);
-        }
-        if (failHard) {
-            runFlowString.append("\n\t\twith fail hard: " + failHard);
-            stepConfig.put("stopOnFailure", failHard);
-        }
-
-        if (inputFileType != null || inputFilePath != null || outputURIReplacement != null || separator != null || outputURIPrefix != null) {
-            runFlowString.append("\n\tWith File Locations Settings:");
-            Map<String, String> fileLocations = new HashMap<>();
-            if (inputFileType != null) {
-                runFlowString.append("\n\t\tInput File Type: " + inputFileType);
-                fileLocations.put("inputFileType", inputFileType);
-            }
-            if (inputFilePath != null) {
-                runFlowString.append("\n\t\tInput File Path: " + inputFilePath);
-                fileLocations.put("inputFilePath", inputFilePath);
-            }
-
-            if (outputURIPrefix != null) {
-                runFlowString.append("\n\t\tOutput URI Prefix: " + outputURIPrefix);
-                fileLocations.put("outputURIPrefix", outputURIPrefix);
-            }
-
-            if (outputURIReplacement != null) {
-                runFlowString.append("\n\t\tOutput URI Replacement: " + outputURIReplacement);
-                fileLocations.put("outputURIReplacement", outputURIReplacement);
-            }
-            if (separator != null) {
-                if (inputFileType != null && !inputFileType.equalsIgnoreCase("csv")) {
-                    throw new IllegalArgumentException("Invalid argument for file type " + inputFileType + ". When specifying a separator, the file type must be 'csv'");
-                }
-                runFlowString.append("\n\t\tSeparator: " + separator);
-                fileLocations.put("separator", separator);
-            }
-
-            stepConfig.put("fileLocations", fileLocations);
-        }
-
-        return stepConfig;
-    }
-
-    protected Map<String, Object> buildFlowOptions() {
-        String optionsString;
-        if (StringUtils.isNotEmpty(optionsFile)) {
-            try {
-                optionsString = new String(FileCopyUtils.copyToByteArray(new File(optionsFile)));
-            } catch (IOException ex) {
-                throw new RuntimeException("Unable to read options file at: " + optionsFile, ex);
-            }
-        } else {
-            optionsString = optionsJSON;
-        }
-
-        Map<String, Object> optionsMap = null;
-        if (StringUtils.isNotEmpty(optionsString)) {
-            try {
-                optionsMap = new ObjectMapper().readValue(optionsString, new TypeReference<Map<String, Object>>() {});
-            } catch (IOException ex) {
-                throw new RuntimeException("Unable to parse JSON options string: " + optionsString, ex);
-            }
-        }
-
-        // Needed to force the flow to stop, not just the step
-        if (this.failHard) {
-            if (optionsMap == null) {
-                optionsMap = new HashMap<>();
-            }
-            optionsMap.put("stopOnError", true);
-        }
-
-        return optionsMap;
-    }
-
-    public String getFlowName() {
-        return flowName;
-    }
-
-    public void setFlowName(String flowName) {
-        this.flowName = flowName;
-    }
-
-    public Integer getBatchSize() {
-        return batchSize;
-    }
-
-    public void setBatchSize(Integer batchSize) {
-        this.batchSize = batchSize;
-    }
-
-    public Integer getThreadCount() {
-        return threadCount;
-    }
-
-    public void setThreadCount(Integer threadCount) {
-        this.threadCount = threadCount;
-    }
-
-    public String getInputFilePath() {
-        return inputFilePath;
-    }
-
-    public void setInputFilePath(String inputFilePath) {
-        this.inputFilePath = inputFilePath;
-    }
-
-    public String getInputFileType() {
-        return inputFileType;
-    }
-
-    public void setInputFileType(String inputFileType) {
-        this.inputFileType = inputFileType;
-    }
-
-    public String getOutputURIReplacement() {
-        return outputURIReplacement;
-    }
-
-    public void setOutputURIReplacement(String outputURIReplacement) {
-        this.outputURIReplacement = outputURIReplacement;
-    }
-
-    public String getOutputURIPrefix() {
-        return outputURIPrefix;
-    }
-
-    public void setOutputURIPrefix(String outputURIPrefix) {
-        this.outputURIPrefix = outputURIPrefix;
-    }
-
-    public String getSeparator() {
-        return separator;
-    }
-
-    public void setSeparator(String separator) {
-        this.separator = separator;
-    }
-
-    public Boolean getShowOptions() {
-        return showOptions;
-    }
-
+    @Override
     public void setShowOptions(Boolean showOptions) {
-        this.showOptions = showOptions;
+        super.setShowOptions(showOptions);
     }
 
-    public Boolean getFailHard() {
-        return failHard;
-    }
-
+    @Parameter(names = "-failHard", description = "If included, forces a job to stop once a batch fails (no parameter value allowed)")
+    @Override
     public void setFailHard(Boolean failHard) {
-        this.failHard = failHard;
+        super.setFailHard(failHard);
     }
 
-    public List<String> getSteps() {
-        return steps;
-    }
-
+    @Parameter(names = "-steps", description = "Comma-delimited string of step numbers to run; e.g. -steps 2,3,5")
+    @Override
     public void setSteps(List<String> steps) {
-        this.steps = steps;
+        super.setSteps(steps);
     }
 
-    public String getJobId() {
-        return jobId;
-    }
-
+    @Parameter(names = "-jobId", description = "A user-specified job ID")
+    @Override
     public void setJobId(String jobId) {
-        this.jobId = jobId;
+        super.setJobId(jobId);
     }
 
-    public String getOptionsFile() {
-        return optionsFile;
-    }
-
+    @Parameter(names = "-optionsFile", description = "Path to a file containing a JSON object for overriding step options")
+    @Override
     public void setOptionsFile(String optionsFile) {
-        this.optionsFile = optionsFile;
+        super.setOptionsFile(optionsFile);
     }
 
-    public String getOptionsJSON() {
-        return optionsJSON;
-    }
-
+    @Parameter(names = "-optionsJson", description = "JSON object for overriding step options; e.g. -optionsJson \"{\\\"sourceQuery\\\":\\\"cts.collectionQuery('test')\\\"}\"")
+    @Override
     public void setOptionsJSON(String optionsJSON) {
-        this.optionsJSON = optionsJSON;
+        super.setOptionsJSON(optionsJSON);
     }
 }
