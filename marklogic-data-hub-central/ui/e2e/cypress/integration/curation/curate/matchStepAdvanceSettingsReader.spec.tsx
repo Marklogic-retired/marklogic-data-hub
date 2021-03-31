71506a38905d64eb7fa062ed6362aa9c1cda6b8e
ngodugu-marklogic <nikhila.godugu@marklogic.com>
2021-03-30 21:30:37 -0700

    DHFPROD-6185: End to end test for match step advanced settings

---
+++ marklogic-data-hub-central/ui/e2e/cypress/integration/curation/curate/matchStepAdvanceSettingsReader.spec.tsx
@@ -0,0 +1,64 @@
+import "cypress-wait-until";
+import {Application} from "../../../support/application.config";
+import {
+  toolbar
+} from "../../../support/components/common/index";
+import curatePage from "../../../support/pages/curate";
+import LoginPage from "../../../support/pages/login";
+import loadPage from "../../../support/pages/load";
+import {
+  advancedSettingsDialog
+} from "../../../support/components/mapping/index";
+
+const matchStep = "match-person";
+
+describe("Validate Advance Settings for hub-central-match-merge-reader role", () => {
+  before(() => {
+    cy.visit("/");
+    cy.contains(Application.title);
+    cy.loginAsTestUserWithRoles("hub-central-match-merge-reader").withRequest();
+    LoginPage.postLogin();
+    cy.waitForAsyncRequest();
+  });
+  beforeEach(() => {
+    cy.loginAsTestUserWithRoles("hub-central-match-merge-reader").withRequest();
+    cy.waitForAsyncRequest();
+  });
+  afterEach(() => {
+    cy.resetTestUser();
+    cy.waitForAsyncRequest();
+  });
+  it("Navigate to curate tab and Open Customer entity", () => {
+    cy.waitUntil(() => toolbar.getCurateToolbarIcon()).click();
+    cy.waitUntil(() => curatePage.getEntityTypePanel("Person").should("be.visible"));
+    curatePage.toggleEntityTypeId("Person");
+    curatePage.selectMatchTab("Person");
+    cy.waitUntil(() => curatePage.addNewStep());
+  });
+  it("Validate the default Advanced settings are disabled", () => {
+    loadPage.editStepInCardView(matchStep).click({force: true});
+    loadPage.switchEditAdvanced().click();
+    cy.get("#sourceDatabase").invoke("attr", "class").should("contain", "ant-select-disabled");
+    cy.get("#targetDatabase").invoke("attr", "class").should("contain", "ant-select-disabled");
+    cy.get("div[id=\"additionalColl\"]").invoke("attr", "class").should("contain", "ant-select-disabled");
+    cy.get("#targetPermissions").should("be.disabled");
+    cy.get("#provGranularity").invoke("attr", "class").should("contain", "ant-select-disabled");
+    cy.get("#batchSize").should("be.disabled");
+    advancedSettingsDialog.toggleInterceptors();
+    cy.get("#interceptors").should("be.disabled");
+    advancedSettingsDialog.toggleCustomHook();
+    cy.get("#customHook").should("be.disabled");
+  });
+  it("Validate the Advanced settings options are not displayed", () => {
+    cy.get("#sourceDatabase").click();
+    cy.findByTestId("sourceDbOptions-data-hub-STAGING").should("not.exist");
+    cy.findByTestId("sourceDbOptions-data-hub-FINAL").should("not.exist");
+    cy.get("#targetDatabase").click();
+    cy.findByTestId("targetDbOptions-data-hub-STAGING").should("not.exist");
+    cy.findByTestId("targetDbOptions-data-hub-FINAL").should("not.exist");
+    cy.get("#provGranularity").click();
+    cy.findByTestId("provOptions-Coarse-grained").should("not.exist");
+    cy.findByTestId("provOptions-Fine-grained").should("not.exist");
+    cy.findByTestId("provOptions-Off").should("not.exist");
+  });
+});
---
+++ marklogic-data-hub-central/ui/e2e/cypress/integration/curation/curate/matchStepAdvanceSettingsWriter.spec.tsx
@@ -0,0 +1,93 @@
+import "cypress-wait-until";
+import {Application} from "../../../support/application.config";
+import {
+  toolbar,
+  createEditStepDialog
+} from "../../../support/components/common/index";
+import curatePage from "../../../support/pages/curate";
+import LoginPage from "../../../support/pages/login";
+import loadPage from "../../../support/pages/load";
+import {
+  advancedSettingsDialog
+} from "../../../support/components/mapping/index";
+
+const matchStep = "matchCustomerTest";
+
+describe("Validate Advance Settings for hub-central-match-merge-writer role", () => {
+  before(() => {
+    cy.visit("/");
+    cy.contains(Application.title);
+    cy.loginAsTestUserWithRoles("hub-central-match-merge-writer").withRequest();
+    LoginPage.postLogin();
+    cy.waitForAsyncRequest();
+  });
+  beforeEach(() => {
+    cy.loginAsTestUserWithRoles("hub-central-match-merge-writer").withRequest();
+    cy.waitForAsyncRequest();
+  });
+  afterEach(() => {
+    cy.resetTestUser();
+    cy.waitForAsyncRequest();
+  });
+  after(() => {
+    cy.loginAsDeveloper().withRequest();
+    cy.deleteSteps("matching", "matchCustomerTest");
+    cy.resetTestUser();
+    cy.waitForAsyncRequest();
+  });
+  it("Navigate to curate tab and Open Customer entity", () => {
+    cy.waitUntil(() => toolbar.getCurateToolbarIcon()).click();
+    cy.waitUntil(() => curatePage.getEntityTypePanel("Customer").should("be.visible"));
+    curatePage.toggleEntityTypeId("Customer");
+    curatePage.selectMatchTab("Customer");
+    cy.waitUntil(() => curatePage.addNewStep());
+  });
+  it("Creating a new match step", () => {
+    curatePage.addNewStep().should("be.visible").click();
+    createEditStepDialog.stepNameInput().type(matchStep);
+    createEditStepDialog.stepDescriptionInput().type("match customer step example", {timeout: 2000});
+    createEditStepDialog.setSourceRadio("Query");
+    createEditStepDialog.setQueryInput(`cts.collectionQuery(['${matchStep}'])`);
+    createEditStepDialog.saveButton("matching").click();
+    cy.waitForAsyncRequest();
+    curatePage.verifyStepNameIsVisible(matchStep);
+  });
+  it("Validate the default Advanced settings values", () => {
+    loadPage.editStepInCardView(matchStep).click({force: true});
+    loadPage.switchEditAdvanced().click();
+    cy.get("[id=\"sourceDatabase\"] [class=\"ant-select-selection-selected-value\"]").should("have.text", "data-hub-FINAL");
+    cy.get("[id=\"targetDatabase\"] [class=\"ant-select-selection-selected-value\"]").should("have.text", "data-hub-FINAL");
+    cy.findByText("Please add target collections").should("exist");
+    loadPage.defaultCollections(matchStep).should("be.visible");
+    cy.get("#targetPermissions").should("have.value", "data-hub-common,read,data-hub-common,update");
+    cy.get("[id=\"provGranularity\"] [class=\"ant-select-selection-selected-value\"]").should("have.text", "Coarse-grained");
+    cy.get("#batchSize").should("have.value", "100");
+    advancedSettingsDialog.toggleInterceptors();
+    cy.get("#interceptors").should("have.text", "[]");
+    advancedSettingsDialog.toggleCustomHook();
+    cy.get("#customHook").should("have.text", "{}");
+  });
+  it("Validate the default Advanced settings are enabled", () => {
+    cy.get("#sourceDatabase").invoke("attr", "class").should("contain", "ant-select-enabled");
+    cy.get("#targetDatabase").invoke("attr", "class").should("contain", "ant-select-enabled");
+    cy.get("div[id=\"additionalColl\"]").invoke("attr", "class").should("contain", "ant-select-enabled");
+    cy.get("#targetPermissions").should("be.enabled");
+    cy.get("#provGranularity").invoke("attr", "class").should("contain", "ant-select-enabled");
+    cy.get("#batchSize").should("be.enabled");
+    cy.get("#interceptors").should("be.enabled");
+    cy.get("#customHook").should("be.enabled");
+  });
+  it("Validate the Advanced settings options", () => {
+    cy.get("#sourceDatabase").click();
+    cy.findByTestId("sourceDbOptions-data-hub-STAGING").should("exist");
+    cy.findByTestId("sourceDbOptions-data-hub-FINAL").should("exist");
+    cy.get("#sourceDatabase").click();
+    cy.get("#targetDatabase").click();
+    cy.findByTestId("targetDbOptions-data-hub-STAGING").should("exist");
+    cy.findByTestId("targetDbOptions-data-hub-FINAL").should("exist");
+    cy.get("#provGranularity").click();
+    cy.findByTestId("provOptions-Coarse-grained").should("exist");
+    cy.findByTestId("provOptions-Fine-grained").should("exist");
+    cy.findByTestId("provOptions-Off").should("exist");
+  });
+});
