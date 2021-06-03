4ef01e4e9e8ddb753f75493fb3897bc75a70b50a
sanjeevani vishaka <sanjeevani.vishaka@marklogic.com>
2021-06-03 16:33:52 -0500

    DHFPROD-7361 e2e tests for monitoring tile

--- marklogic-data-hub-central/ui/e2e/cypress/integration/monitor/monitorTableValidations.spec.tsx
+++ marklogic-data-hub-central/ui/e2e/cypress/integration/monitor/monitorTableValidations.spec.tsx
@@ -3,6 +3,7 @@ import {Application} from "../../support/application.config";
 import "cypress-wait-until";
 import {toolbar} from "../../support/components/common";
 import LoginPage from "../../support/pages/login";
+import browsePage from "../../support/pages/browse";
 
 describe("Monitor Tile", () => {
 
@@ -24,9 +25,9 @@ describe("Monitor Tile", () => {
     cy.resetTestUser();
     cy.waitForAsyncRequest();
   });
-/*
+
   it("Navigate to Monitor Tile and verify pagination works", () => {
-    monitorPage.getTableRows().should("have.length", 20);
+    monitorPage.getTableRows().its("length").should("be.gte", 10);
     monitorPage.getPaginationPageSizeOptions().then(attr => {
       attr[0].click();
       monitorPage.getPageSizeOption("10 / page").click();
@@ -35,25 +36,104 @@ describe("Monitor Tile", () => {
   });
 
   it("apply facet search and verify docs", () => {
-    browsePage.getShowMoreLink("flow").click();
-    browsePage.getFacetItemCheckbox("flow", "convertedFlow").click();
-    browsePage.getSelectedFacets().should("exist");
-    browsePage.getGreySelectedFacets("convertedFlow").should("exist");
-    browsePage.getFacetApplyButton().should("exist");
-    browsePage.getClearGreyFacets().should("exist");
-    browsePage.getFacetApplyButton().click();
-    monitorPage.getTableRows().should("have.length", 2);
-    browsePage.getClearAllFacetsButton().should("exist");
-    browsePage.getFacetSearchSelectionCount("flow").should("contain", "1");
-    browsePage.clickClearFacetSearchSelection("convertedFlow");
+    monitorPage.validateAppliedFacetTableRows("step-type", 1);
   });
+
   it("apply facet search and clear individual grey facet", () => {
-    monitorPage.getTableRows().should("have.length", 10);
-    browsePage.getShowMoreLink("flow").click();
-    browsePage.getFacetItemCheckbox("flow", "convertedFlow").click();
-    browsePage.getGreySelectedFacets("convertedFlow").click();
-    monitorPage.getTableRows().should("have.length", 10);
+    monitorPage.validateClearGreyFacet("step-type", 0);
   });
-*/
+
+  it("apply facet search and clear all grey facets", () => {
+    monitorPage.validateGreyFacet("step-type", 0);
+    monitorPage.validateGreyFacet("flow", 0);
+    browsePage.getClearGreyFacets().click();
+  });
+
+  it("Verify functionality of clear and apply facet buttons", () => {
+    //verify no facets selected case.
+    browsePage.getClearAllFacetsButton().should("be.disabled");
+    browsePage.getApplyFacetsButton().should("be.disabled");
+    //verify selecting facets case.
+    monitorPage.validateGreyFacet("step-type", 0);
+    browsePage.getClearAllFacetsButton().should("not.be.disabled");
+    browsePage.getApplyFacetsButton().should("not.be.disabled");
+    //verify facets applied case.
+    browsePage.getApplyFacetsButton().click();
+    browsePage.getClearAllFacetsButton().should("not.be.disabled");
+    browsePage.getApplyFacetsButton().should("be.disabled");
+    // verify selecting additional facets case.
+    monitorPage.validateGreyFacet("step", 0);
+    browsePage.getClearAllFacetsButton().should("not.be.disabled");
+    browsePage.getApplyFacetsButton().should("not.be.disabled");
+    browsePage.getClearAllFacetsButton().click();
+    browsePage.getClearAllFacetsButton().should("be.disabled");
+    browsePage.getApplyFacetsButton().should("be.disabled");
+  });
+
+
+  it("apply multiple facets, deselect them, apply changes, apply multiple, clear them, verify no facets checked", () => {
+    browsePage.getShowMoreLink("step").click();
+    browsePage.getFacetItemCheckbox("step", "loadPersonJSON").click();
+    cy.get("#monitorContent").scrollTo("top",  {ensureScrollable: false});
+    browsePage.getGreySelectedFacets("loadPersonJSON").should("exist");
+    browsePage.getFacetItemCheckbox("step", "loadPersonJSON").should("be.checked");
+    browsePage.getFacetApplyButton().click();
+    browsePage.getFacetItemCheckbox("flow", "personJSON").click();
+    cy.get("#monitorContent").scrollTo("top",  {ensureScrollable: false});
+    browsePage.getGreySelectedFacets("personJSON").should("exist");
+    browsePage.getFacetItemCheckbox("step-type", "ingestion").click();
+    browsePage.getGreySelectedFacets("ingestion").should("exist");
+    browsePage.getFacetApplyButton().click();
+    browsePage.clickClearFacetSearchSelection("personJSON");
+    cy.get("#monitorContent").scrollTo("top",  {ensureScrollable: false});
+    browsePage.getFacetItemCheckbox("step-type", "ingestion").should("be.checked");
+    browsePage.getFacetItemCheckbox("step", "loadPersonJSON").click();
+    browsePage.getFacetItemCheckbox("step-type", "ingestion").click();
+    cy.get("#monitorContent").scrollTo("top",  {ensureScrollable: false});
+    browsePage.getFacetItemCheckbox("step", "loadPersonJSON").should("not.be.checked");
+    browsePage.getFacetItemCheckbox("step-type", "ingestion").should("not.be.checked");
+    browsePage.getGreySelectedFacets("loadPersonJSON").should("not.exist");
+    browsePage.getGreySelectedFacets("ingestion").should("not.exist");
+    cy.waitForAsyncRequest();
+    browsePage.getFacetItemCheckbox("step", "loadPersonJSON").click();
+    browsePage.getFacetItemCheckbox("step-type", "ingestion").click();
+    cy.get("#monitorContent").scrollTo("top",  {ensureScrollable: false});
+    browsePage.getFacetApplyButton().click();
+    browsePage.clickClearFacetSearchSelection("loadPersonJSON");
+    browsePage.clickClearFacetSearchSelection("ingestion");
+    browsePage.getFacetItemCheckbox("step", "loadPersonJSON").should("not.be.checked");
+    browsePage.getFacetItemCheckbox("step-type", "ingestion").should("not.be.checked");
+    browsePage.getGreySelectedFacets("loadPersonJSON").should("not.exist");
+    browsePage.getGreySelectedFacets("ingestion").should("not.exist");
+  });
+
+
+  it("Verify facets can be selected, applied and cleared using clear text", () => {
+    monitorPage.validateAppliedFacet("step", 0);
+    browsePage.getFacetSearchSelectionCount("step").should("contain", "1");
+    browsePage.getClearFacetSelection("step").click();
+    browsePage.waitForSpinnerToDisappear();
+  });
+
+  it("Apply facets, unchecking them should not recheck original facets", () => {
+    browsePage.getShowMoreLink("step").click();
+    browsePage.getFacetItemCheckbox("step", "mapPersonJSON").click();
+    browsePage.getFacetItemCheckbox("step", "loadPersonJSON").click();
+    browsePage.getGreySelectedFacets("mapPersonJSON").should("exist");
+    browsePage.getGreySelectedFacets("loadPersonJSON").should("exist");
+    browsePage.getFacetApplyButton().click();
+    browsePage.getFacetItemCheckbox("step", "mapPersonJSON").should("be.checked");
+    browsePage.getFacetItemCheckbox("step", "loadPersonJSON").should("be.checked");
+    browsePage.getFacetItemCheckbox("status", "finished").click();
+    browsePage.getFacetItemCheckbox("step", "mapPersonJSON").click();
+    browsePage.waitForSpinnerToDisappear();
+    browsePage.getFacetItemCheckbox("step", "loadPersonJSON").click();
+    browsePage.getFacetItemCheckbox("status", "finished").click();
+    browsePage.getFacetItemCheckbox("step", "mapPersonJSON").should("not.be.checked");
+    browsePage.getFacetItemCheckbox("step", "loadPersonJSON").click();
+    browsePage.getFacetItemCheckbox("step", "loadPersonJSON").should("not.be.checked");
+    browsePage.getFacetItemCheckbox("status", "finished").should("not.be.checked");
+  });
+
 });
 
