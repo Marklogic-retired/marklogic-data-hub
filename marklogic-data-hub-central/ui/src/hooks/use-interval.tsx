1ea049ba5748d50d99f79fda7d8b54bdd93b49f6
Ryan Dew <ryan.j.dew@gmail.com>
2021-06-23 17:36:37 -0700

    DHFPROD-7501: Use Worker for interval to avoid sleep

--- marklogic-data-hub-central/ui/src/hooks/use-interval.tsx
+++ marklogic-data-hub-central/ui/src/hooks/use-interval.tsx
@@ -1,6 +1,9 @@
 import {useEffect, useRef} from "react";
+import intervalWorkerJS from "./use-interval-worker";
 
 export const useInterval = (callback, delay) => {
+  const code = intervalWorkerJS.toString();
+
   const savedCallback = useRef<any>(null);
 
   // Remember the last callback ref
@@ -15,8 +18,24 @@ export const useInterval = (callback, delay) => {
     }
 
     if (delay !== null) {
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
+      // Check for Worker available in modern browsers. If not, provide degraded experience (primarily for unit tests)
+      if (window.Worker) {
+        // Create an Object URL for the Worker JS file
+        const blobUrl = URL.createObjectURL(new Blob(["(" + code + ")()"]));
+        const intervalWorker = new Worker(blobUrl);
+        // tell the to start and pass the delay
+        intervalWorker.postMessage({action: "start", delay});
+        // add a listener on worker to call the callback
+        intervalWorker.addEventListener("message", tick);
+        // tell the worker to stop the interval and remove listener
+        return () => {
+          intervalWorker.postMessage({action: "stop"});
+          intervalWorker.removeEventListener("message", tick);
+        };
+      } else {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
     }
   }, [delay]);
 };
