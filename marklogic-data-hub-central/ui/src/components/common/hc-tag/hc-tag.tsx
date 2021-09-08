c95c3ca2ecde16c8f83109f3049901d0ff3dbf44
MSBrignoni <martin.brignoni@marklogic.com>
2021-09-08 15:08:59 -0300

    DHFPROD-7740: Component Replacement: Tag

---
+++ marklogic-data-hub-central/ui/src/components/common/hc-tag/hc-tag.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./hc-tag.module.scss";
+import iconCloseTag from "../../../assets/icon_closeTag.svg";
+interface Props {
+  label: string;
+  ariaLabel?: string;
+  style?: React.CSSProperties;
+  className?: string;
+  dashed?: boolean;
+  color?: "black" | "blue" | "green" | "grey" | "magenta" | "red" | "yellow";
+  closable?: boolean;
+  visible?: boolean;
+  onClose?: () => void;
+}
+
+const colors = {
+  "black": styles.spanTagBlack, "blue": styles.spanTagBlue, "green": styles.spanTagGreen, "grey": styles.spanTagGrey,
+  "magenta": styles.spanTagMagenta, "red": styles.spanTagRed, "yellow": styles.spanTagYellow
+};
+
+const HCTag: React.FC<Props> = (props) => {
+  return (
+    <span aria-label={props.ariaLabel}
+      data-testid="tag-component"
+      className={[styles.spanTag, props.className ? props.className : "", props?.color && colors[props.color],
+        props.dashed && styles.spanTagDashed, props.visible === false && styles.spanTagInvisible].join(" ")}
+      style={props.style}>
+      {props.label}
+      {(props?.closable || props?.closable === undefined) &&
+        <img src={iconCloseTag} alt={""} data-testid="iconClose-tagComponent" onClick={props?.onClose} className={styles.spanIcon} />
+      }
+    </span>
+  );
+};
+
+export default HCTag;
