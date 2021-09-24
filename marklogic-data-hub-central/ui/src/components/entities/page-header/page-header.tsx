e077f1890179a0cd7b644a5a086b39c063a846e6
Yubisel Vega <yubisel.vega@marklogic.com>
2021-09-24 12:29:21 -0700

    DHFPROD-7828: Component Replacement: PageHeader

--- marklogic-data-hub-central/ui/src/components/entities/page-header/page-header.tsx
+++ marklogic-data-hub-central/ui/src/components/entities/page-header/page-header.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import {PageHeader as PageHeaderAntd} from "antd";
+import {Row, Col} from "react-bootstrap";
 import styles from "./page-header.module.scss";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
+import {ArrowLeftShort} from "react-bootstrap-icons";
 
 interface Props {
   title: any;
@@ -13,12 +12,11 @@ const PageHeader: React.FC<Props> = (props) => {
 
   return (
     <div className={styles.customHeader}>
-      <PageHeaderAntd
-        className={styles.pageHeader}
-        onBack={props.handleOnBack}
-        backIcon={<FontAwesomeIcon data-testid="arrow-left" className={styles.arrowLeftIcon} icon={faArrowLeft} />}
-        title={props.title}
-      />
+      <Row id="back-button" className={"py-3 px-3 header-heading-title"} onClick={props.handleOnBack}>
+        <Col>
+          <span className={`d-flex align-items-center cursor-pointer`} data-testid="arrow-left"><ArrowLeftShort aria-label="Back" className={"d-inline-block me-2 fs-1 header-back-button"} />{props.title}</span>
+        </Col>
+      </Row>
     </div>
   );
 };
