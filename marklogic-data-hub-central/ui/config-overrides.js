df66b3e257e231535f6261c57f49c2e29702594a
Yubisel Vega <yubisel.vega@marklogic.com>
2022-01-18 11:11:58 -0300

    DHFPROD-8252/Removing-ant-lib-dependency-from-hub-central-repo

--- marklogic-data-hub-central/ui/config-overrides.js
+++ marklogic-data-hub-central/ui/config-overrides.js
@@ -1,42 +1,15 @@
-const {override, fixBabelImports, addLessLoader, babelInclude, addWebpackAlias} = require("customize-cra");
+const {override, addLessLoader, babelInclude, addWebpackAlias} = require("customize-cra");
 const path = require("path");
-const themeVariables = require("@marklogic/design-system/src/theme-variables.json");
 
 module.exports = override(
   babelInclude([
     path.resolve(__dirname, "src"),
-    /@marklogic\/design-system/
   ]),
-  fixBabelImports("@marklogic/design-system",
-    {
-      libraryDirectory: "es",
-      camel2DashComponentName: false,
-      style: true
-    }
-  ),
-  fixBabelImports("@marklogic/design-system/es/MLIcon",
-    {
-      libraryDirectory: "",
-      camel2DashComponentName: false,
-      customName: function (name) {
-        return `@marklogic/design-system/es/MLIcon/${name}`;
-      },
-      style: function () {
-        return "@marklogic/design-system/es/MLIcon/style";
-      }
-    }
-  ),
-  fixBabelImports("import", {
-    libraryName: "antd",
-    libraryDirectory: "es",
-    style: true
-  }),
   //Configure path alias
   addWebpackAlias({
     ["@components"]: path.resolve(__dirname, "src/components"),
   }),
   addLessLoader({
     javascriptEnabled: true,
-    modifyVars: themeVariables
   })
 );
