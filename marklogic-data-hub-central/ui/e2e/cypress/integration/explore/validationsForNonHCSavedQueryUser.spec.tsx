36b3d840a6a02bf62b61170da3dce6bae5794f14
rolonPablo <pablo.rolon@marklogic.com>
2022-01-25 10:51:57 -0300

    DHFPROD-8371: Graph search endpoint requires more role privileges than we want

--- marklogic-data-hub-central/ui/e2e/cypress/integration/explore/validationsForNonHCSavedQueryUser.spec.tsx
+++ marklogic-data-hub-central/ui/e2e/cypress/integration/explore/validationsForNonHCSavedQueryUser.spec.tsx
@@ -26,17 +26,16 @@ describe("User without hub-central-saved-query-user role should not see saved qu
     cy.resetTestUser();
     cy.waitForAsyncRequest();
   });
-  //Skipping these for DHFPROD-8230, revert it back (remove skips) with DHFPROD-8371
-  it.skip("verifies saved queries drop down does not exist", () => {
+  it("verifies saved queries drop down does not exist", () => {
     cy.waitUntil(() => toolbar.getExploreToolbarIcon()).click();
     browsePage.getSaveQueriesDropdown().should("not.exist");
   });
-  it.skip("verifies user without hub-central-saved-query-user role can explore data", () => {
+  it("verifies user without hub-central-saved-query-user role can explore data", () => {
     browsePage.getSaveQueriesDropdown().should("not.exist");
     browsePage.selectEntity("Customer");
     browsePage.getSelectedEntity().should("contain", "Customer");
   });
-  it.skip("verifies user without hub-central-saved-query-user can not save query", () => {
+  it("verifies user without hub-central-saved-query-user can not save query", () => {
     browsePage.getSaveQueriesDropdown().should("not.exist");
     browsePage.getFacetItemCheckbox("name", "Adams Cole").click();
     browsePage.getSelectedFacets().should("exist");
