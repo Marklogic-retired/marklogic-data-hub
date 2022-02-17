6f94de01432dbd78e482dda145d32ea7fc6fed95
wooldridge <mike@hellion.org>
2022-02-17 12:29:22 -0800

    DHFPROD-8428: Add tests for Dashboard view

---
+++ marklogic-data-hub-central/ui-custom/src/views/Dashboard.test.tsx
@@ -0,0 +1,47 @@
+import Dashboard from "./Dashboard";
+import {render, act} from "@testing-library/react";
+import { UserContext } from "../store/UserContext";
+import config from '../../../src/main/resources/explore-data/ui-config/config.json';
+
+const userContextValue = {
+    userid: "",
+    proxy: "",
+    config: config,
+    handleGetProxy: jest.fn(),
+    handleGetUserid: jest.fn(),
+    handleLogin: jest.fn(),
+    handleGetConfig: jest.fn()
+};
+
+const userContextValueEmptyConfig = {...userContextValue, config: {}};
+
+describe("Dashboard view", () => {
+
+    test("Renders configured content with non-empty config", async () => {
+        let getByText;
+        await act(async () => {
+            const renderResults = render(
+                <UserContext.Provider value={userContextValue}>
+                    <Dashboard />
+                </UserContext.Provider>
+            );
+            getByText = renderResults.getByText;
+        });
+        expect(getByText(config.dashboard.metrics[0].title)).toBeInTheDocument();
+        expect(getByText(config.dashboard.saved.cols[1].title!)).toBeInTheDocument();
+    });
+
+    test("Renders loading content with empty config", async () => {
+        let getByText;
+        await act(async () => {
+            const renderResults = render(
+                <UserContext.Provider value={userContextValueEmptyConfig}>
+                    <Dashboard />
+                </UserContext.Provider>
+            );
+            getByText = renderResults.getByText;
+        });
+        expect(getByText("Loading...")).toBeInTheDocument();
+    });
+
+});
