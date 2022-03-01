c88f745621f1f0c936aaff3ae405069702126daa
wooldridge <mike@hellion.org>
2022-02-28 16:24:00 -0800

    DHFPROD-8533: Componentize ResultsList and Detail views
    Pass config info into ResultsList items and the Detail personal section consistently:
    - "component" property that specifies the component
    - "config" property that specfies the configuration for that component, with cosistent properties (arrayPath, path, etc.)
    
    Create an Image widget for thumbnails, etc.
    Add a default color to Chiclet widget. Displayed when no color found in widget config.
    Fix.tests.

--- marklogic-data-hub-central/ui-custom/src/components/DataTableValue/DataTableValue.test.tsx
+++ marklogic-data-hub-central/ui-custom/src/components/DataTableValue/DataTableValue.test.tsx
@@ -4,84 +4,99 @@ import {render} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 const configMultiple = {
-    id: "phone", 
-    title: "Phone Number", 
-    path: "result[0].extracted.person.phone", 
-    icon: "phone",
-    metadata: [
-        {
-            type: "block",
-            color: "#96bde4",
-            value: "B"
-        },
-        {
-            type: "block",
-            color: "#5d6aaa",
-            value: "4"
-        }
-    ]
+    component: "DataTableValue",
+    config: {
+        id: "phone", 
+        title: "Phone Number", 
+        path: "result[0].extracted.person.phone", 
+        icon: "phone",
+        metadata: [
+            {
+                type: "block",
+                color: "#96bde4",
+                value: "B"
+            },
+            {
+                type: "block",
+                color: "#5d6aaa",
+                value: "4"
+            }
+        ]
+    }
 };
 
 const configSingular = {
-    id: "ssn", 
-    title: "SSN", 
-    path: "result[0].extracted.person.ssn",
-    metadata: [
-        {
-            type: "block",
-            color: "#96bde4",
-            value: "B"
-        },
-        {
-            type: "block",
-            color: "#5d6aaa",
-            value: "4"
-        }
-    ]
+    component: "DataTableValue",
+    config: {
+        id: "ssn", 
+        title: "SSN", 
+        path: "result[0].extracted.person.ssn",
+        metadata: [
+            {
+                type: "block",
+                color: "#96bde4",
+                value: "B"
+            },
+            {
+                type: "block",
+                color: "#5d6aaa",
+                value: "4"
+            }
+        ]
+    }
 };
 
 const configArrayPathMultiple = {
-    id: "email", 
-    title: "Email", 
-    arrayPath: "result[0].extracted.person.email",
-    path: "value",
-    metadata: [
-        {
-            type: "block",
-            color: "#96bde4",
-            value: "B"
-        },
-        {
-            type: "block",
-            color: "#5d6aaa",
-            value: "4"
-        }
-    ]
+    component: "DataTableValue",
+    config: {
+        id: "email", 
+        title: "Email", 
+        arrayPath: "result[0].extracted.person.email",
+        path: "value",
+        metadata: [
+            {
+                type: "block",
+                color: "#96bde4",
+                value: "B"
+            },
+            {
+                type: "block",
+                color: "#5d6aaa",
+                value: "4"
+            }
+        ]
+    }
 };
 
 const configArrayPathSingular = {
-    id: "email", 
-    title: "Email", 
-    arrayPath: "result[0].extracted.person.image",
-    path: "value",
-    metadata: [
-        {
-            type: "block",
-            color: "#96bde4",
-            value: "B"
-        },
-        {
-            type: "block",
-            color: "#5d6aaa",
-            value: "4"
-        }
-    ]
+    component: "DataTableValue",
+    config: {
+        id: "email", 
+        title: "Email", 
+        arrayPath: "result[0].extracted.person.image",
+        path: "value",
+        metadata: [
+            {
+                type: "block",
+                color: "#96bde4",
+                value: "B"
+            },
+            {
+                type: "block",
+                color: "#5d6aaa",
+                value: "4"
+            }
+        ]
+    }
 };
 
 const configNoExist = {
-    id: "noexist", 
-    title: "No Exist", 
-    dataPath: "result[0].extracted.person.noexist",
+    component: "DataTableValue",
+    config: {
+        id: "noexist", 
+        title: "No Exist", 
+        path: "result[0].extracted.person.noexist"
+    }
 };
 
 const detail = {
@@ -125,17 +140,15 @@ describe("DataTableValue component", () => {
 
     test("Verify data table renders with a property with multiple values and an icon", () => {
         const {getByText, queryAllByText, getByTestId} = render(
-            <DetailContext.Provider value={detailContextValue}>
-                <DataTableValue config={configMultiple} />
-            </DetailContext.Provider>
+            <DataTableValue config={configMultiple.config} data={detail} />
         );
-        expect(getByText(configMultiple.title)).toBeInTheDocument();
+        expect(getByText(configMultiple.config.title)).toBeInTheDocument();
         expect(getByTestId("hideUp")).toBeInTheDocument();
-        expect(getByTestId("icon-" + configMultiple.icon)).toBeInTheDocument();
+        expect(getByTestId("icon-" + configMultiple.config.icon)).toBeInTheDocument();
         expect(getByText("123-456-7890")).toBeInTheDocument();
         expect(getByText("321-654-9876")).toBeInTheDocument();
-        expect(queryAllByText(configMultiple.metadata[0].value).length > 0);
-        expect(queryAllByText(configMultiple.metadata[1].value).length > 0);
+        expect(queryAllByText(configMultiple.config.metadata[0].value).length > 0);
+        expect(queryAllByText(configMultiple.config.metadata[1].value).length > 0);
         userEvent.click(getByTestId("hideUp"));
         expect(getByTestId("hideDown")).toBeInTheDocument();
         userEvent.click(getByTestId("hideDown"));
@@ -145,23 +158,23 @@ describe("DataTableValue component", () => {
     test("Verify data table renders with a property with a single value", () => {
         const {getByText, queryAllByText, queryByTestId} = render(
             <DetailContext.Provider value={detailContextValue}>
-                <DataTableValue config={configSingular} />
+                <DataTableValue config={configSingular.config} data={detail} />
             </DetailContext.Provider>
         );
-        expect(getByText(configSingular.title)).toBeInTheDocument();
+        expect(getByText(configSingular.config.title)).toBeInTheDocument();
         expect(queryByTestId("hideUp")).not.toBeInTheDocument();
         expect(getByText("123-45-6789")).toBeInTheDocument();
-        expect(queryAllByText(configMultiple.metadata[0].value).length > 0);
-        expect(queryAllByText(configMultiple.metadata[1].value).length > 0);
+        expect(queryAllByText(configMultiple.config.metadata[0].value).length > 0);
+        expect(queryAllByText(configMultiple.config.metadata[1].value).length > 0);
     });
 
     test("Verify data table renders with a property in an array of objects using arrayPath", () => {
         const {getByText, getByTestId} = render(
             <DetailContext.Provider value={detailContextValue}>
-                <DataTableValue config={configArrayPathMultiple} />
+                <DataTableValue config={configArrayPathMultiple.config} data={detail} />
             </DetailContext.Provider>
         );
-        expect(getByText(configArrayPathMultiple.title)).toBeInTheDocument();
+        expect(getByText(configArrayPathMultiple.config.title)).toBeInTheDocument();
         expect(getByTestId("hideUp")).toBeInTheDocument();
         expect(getByText("jdoe1@example.org")).toBeInTheDocument();
         expect(getByText("jdoe2@example.org")).toBeInTheDocument();
@@ -170,10 +183,10 @@ describe("DataTableValue component", () => {
     test("Verify data table renders with a property in a single object using arrayPath", () => {
         const {getByText, queryByTestId} = render(
             <DetailContext.Provider value={detailContextValue}>
-                <DataTableValue config={configArrayPathSingular} />
+                <DataTableValue config={configArrayPathSingular.config} data={detail} />
             </DetailContext.Provider>
         );
-        expect(getByText(configArrayPathSingular.title)).toBeInTheDocument();
+        expect(getByText(configArrayPathSingular.config.title)).toBeInTheDocument();
         expect(queryByTestId("hideUp")).not.toBeInTheDocument();
         expect(getByText("http://example.org/entity1.jpg")).toBeInTheDocument();
     });
@@ -181,11 +194,11 @@ describe("DataTableValue component", () => {
     test("Verify data table does not render with a property that does not exist in the results", () => {
         const {queryByText, queryByTestId} = render(
             <DetailContext.Provider value={detailContextValue}>
-                <DataTableValue config={configNoExist} />
+                <DataTableValue config={configNoExist.config} data={detail} />
             </DetailContext.Provider>
         );
-        expect(queryByText(configNoExist.title)).not.toBeInTheDocument();
-        expect(queryByTestId(configNoExist.id)).not.toBeInTheDocument();
+        expect(queryByText(configNoExist.config.title)).not.toBeInTheDocument();
+        expect(queryByTestId(configNoExist.config.id)).not.toBeInTheDocument();
     });
 
 });
