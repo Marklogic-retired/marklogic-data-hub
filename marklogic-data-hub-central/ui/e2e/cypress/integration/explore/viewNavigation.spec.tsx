b0522daa8028423a6f741ed516bedf5f735b399f
Yubisel Vega <yubisel.vega@marklogic.com>
2022-03-21 09:34:40 -0300

    DHFPROD-8595: Incorrect Active Tab in Record XML/JSON View

--- marklogic-data-hub-central/ui/e2e/cypress/integration/explore/viewNavigation.spec.tsx
+++ marklogic-data-hub-central/ui/e2e/cypress/integration/explore/viewNavigation.spec.tsx
@@ -1,9 +1,10 @@
 import {toolbar} from "../../support/components/common";
 import browsePage from "../../support/pages/browse";
+import detailPage from "../../support/pages/detail";
 import graphExplore from "../../support/pages/graphExplore";
 import LoginPage from "../../support/pages/login";
 
-describe("Navigation through all the Explore views (Table, Snippet, Graph)", () => {
+describe("Navigation through all the Explore views (Table, Snippet, Graph and Details)", () => {
   before(() => {
     cy.visit("/");
 
@@ -13,20 +14,19 @@ describe("Navigation through all the Explore views (Table, Snippet, Graph)", () 
     //Saving Local Storage to preserve session
     cy.saveLocalStorage();
   });
-  before(() => {
-    cy.log("**Navigate to Explore**");
-    toolbar.getExploreToolbarIcon().click();
-    browsePage.waitForSpinnerToDisappear();
-  });
 
   beforeEach(() => {
     //Restoring Local Storage to Preserve Session
     Cypress.Cookies.preserveOnce("HubCentralSession");
     cy.restoreLocalStorage();
+
+    cy.log("**Navigate to Explore**");
+    toolbar.getExploreToolbarIcon().click();
+    browsePage.waitForSpinnerToDisappear();
+    cy.wait(2000);
   });
 
   it("Switch views and validate they get updated correctly", () => {
-    cy.wait(2000);
     cy.log("**Select Table view and validate it switches correctly**");
     browsePage.clickSwitchToTableView();
     //Graph and table should not appear anymore
@@ -48,6 +48,39 @@ describe("Navigation through all the Explore views (Table, Snippet, Graph)", () 
     //Table and snippet view should not appear anymore
     browsePage.getSnippetViewResult().should("not.exist");
     browsePage.getHCTableRows().should("not.exist");
+  });
 
+  it("Switch views, go to details and back", () => {
+    cy.log("**Go to Snippet View**");
+    browsePage.clickSnippetView();
+    browsePage.getSnippetViewResult().should("be.visible");
+
+    cy.log("**Click on source icon in first row and check in details page that source tab icon should be active**");
+    browsePage.getSourceViewIcon().first().click();
+    detailPage.getInstanceView().should("not.have.class", "active");
+    detailPage.getSourceView().should("have.class", "active");
+    detailPage.getMetadataView().should("not.have.class", "active");
+
+    cy.log("**Click back button and check in explore view that snippet tab icon should be active**");
+    detailPage.clickBackButton();
+    browsePage.viewSelector("graph").prev().should("not.be.checked");
+    browsePage.viewSelector("table").prev().should("not.be.checked");
+    browsePage.viewSelector("snippet").prev().should("be.checked");
+
+    cy.log("**Switch to table view**");
+    browsePage.clickTableView();
+    browsePage.waitForSpinnerToDisappear();
+
+    cy.log("**Click on instance icon in first row and check in details page that instance tab icon should be active**");
+    browsePage.getInstanceViewIcon().first().click();
+    detailPage.getInstanceView().should("have.class", "active");
+    detailPage.getSourceView().should("not.have.class", "active");
+    detailPage.getMetadataView().should("not.have.class", "active");
+
+    cy.log("**Click back button and check in explore view that table tab icon should be active**");
+    detailPage.clickBackButton();
+    browsePage.viewSelector("graph").prev().should("not.be.checked");
+    browsePage.viewSelector("table").prev().should("be.checked");
+    browsePage.viewSelector("snippet").prev().should("not.be.checked");
   });
 });
