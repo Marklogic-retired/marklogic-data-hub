de259ef262203eaf8e16a8b626a391ca4822a954
iveyMarklogic <Ivey.Leon@marklogic.com>
2022-03-22 14:00:49 -0300

    DHFPROD-8611: image gallery metadata

--- marklogic-data-hub-central/ui-custom/src/components/ImageGallery/ImageGallery.tsx
+++ marklogic-data-hub-central/ui-custom/src/components/ImageGallery/ImageGallery.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Carousel} from 'react-bootstrap';
 import {getValByConfig} from '../../util/util';
 import "./ImageGallery.scss";
-
+import DateTime from '../DateTime/DateTime';
+import {Modal} from "react-bootstrap";
+import _ from "lodash";
+import Value from '../Value/Value';
 
 type Props = {
   data?: any;
   config?: any;
   style?: React.CSSProperties
+};
+
+const COMPONENTS = {
+  DateTime: DateTime,
+  Value: Value
 };
 
 /**
@@ -30,25 +38,129 @@ type Props = {
             "path": "url"
           }
         },
-        "metadata": {},
+        "modal": {
+          "title": {
+            "component": "Value",
+            "path": "url",
+            "config": {
+              "style": {
+                "fontStyle": "bold"
+              }
+            }
+          },
+          "items": [
+            {
+              "component": "Value",
+              "label": "Source",
+              "path": "source.name",
+              "config": {}
+            },
+            {
+              "component": "DateTime",
+              "label": "Uploaded on",
+              "path": "source.ts",
+              "config": {
+                "format": "MMMM dd, yyyy"
+              }
+            },
+            {
+              "component": "Value",
+              "label": "Uploaded by",
+              "path": "source.uploadedBy",
+              "config": {
+                "className": "foo"
+              }
+            }
+          ]
+        },
         "download": true
       }
     }
  */
 const ImageGallery: React.FC<Props> = (props) => {
+  const [selectedImage, setSelectedImage] = useState(null);
   const {data, config} = props;
-  const uris = getValByConfig(data, config?.images?.url);
-  const singleImage = uris && uris?.length === 1;
+  let images = getValByConfig(data, config?.images);
+  images = _.isNil(images) ? null : (Array.isArray(images) ? images : [images]);
+  const singleImage = images && images?.length === 1;
   const carouselContainerStyle = config?.style ? config.style : {}
+
+  const nodalDataValues = () => {
+    const {modal: {items}} = config;
+    let values = [];
+    values = items.map((item, index) => {
+      if (!item) return null;
+      const {component, label, path, config} = item
+      let value: any = path ? _.get(selectedImage, path, null) : null;
+      if (!value) return null;
+      return (
+        <div className="metadata-row">
+          <span className="metadata-label">{label}</span>
+          <span className="metadata-value">
+            {React.createElement(
+              COMPONENTS[component],
+              {config: config}, value
+            )}
+          </span>
+        </div>
+      )
+    })
+    return values;
+  }
+
+  const CenteredModal = (props) => {
+    const {images: {url}, modal: {title: {component, path, config: titleConfig}}} = config;
+    let urlValue: any = _.get(selectedImage, url, null);
+    let titleValue: any = path ? _.get(selectedImage, path, null) : null;
+    return (
+      <Modal
+        {...props}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        contentClassName="image-gallery-modal"
+
+      >
+        <Modal.Header closeButton>
+          {titleValue && <Modal.Title id="contained-modal-title-vcenter">
+            {React.createElement(
+              COMPONENTS[component],
+              {config: titleConfig}, titleValue
+            )}
+          </Modal.Title>}
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            {selectedImage && <img
+              className="d-block w-100"
+              src={urlValue}
+              alt="First slide"
+            />}
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <div className="metadata-container">
+            {nodalDataValues()}
+          </div>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
+
+  const handleSelectedImage = (image) => {
+    setSelectedImage(image);
+  };
+
   const getItems = () => {
-    const carouselItems = uris?.map((item, index) => {
+    const carouselItems = images?.map((item, index) => {
+      let urlValue: any = _.get(item, config?.images?.url, []);
       return (
         <Carousel.Item key={index}>
           <div data-testid={`item-${index}`}>
             <img
               className="d-block w-100"
-              src={item}
+              src={urlValue}
               alt="First slide"
+              onClick={() => handleSelectedImage(item)}
             />
           </div>
         </Carousel.Item>
@@ -63,6 +175,10 @@ const ImageGallery: React.FC<Props> = (props) => {
           {getItems()}
         </Carousel>
       </div>
+      <CenteredModal
+        show={selectedImage !== null ? true : false}
+        onHide={() => setSelectedImage(null)}
+      />
     </div>)
 }
 
--- marklogic-data-hub-central/ui-custom/src/components/ImageGalleryMulti/ImageGalleryMulti.test.tsx
+++ marklogic-data-hub-central/ui-custom/src/components/ImageGalleryMulti/ImageGalleryMulti.test.tsx
@@ -1,5 +1,5 @@
 
-import {render, waitFor} from "@testing-library/react";
+import {fireEvent, render, waitFor} from "@testing-library/react";
 import {DetailContext} from "../../store/DetailContext";
 import ImageGalleryMulti from "./ImageGalleryMulti";
 
@@ -18,7 +18,41 @@ const configMultiple = {
         path: "url"
       }
     },
-    metadata: {},
+    modal: {
+      title: {
+        component: "Value",
+        path: "url",
+        config: {
+          style: {
+            fontStyle: "bold"
+          }
+        }
+      },
+      items: [
+        {
+          component: "Value",
+          label: "Source",
+          path: "source.name",
+          config: {}
+        },
+        {
+          component: "DateTime",
+          label: "Uploaded on",
+          path: "source.ts",
+          config: {
+            format: "MMMM dd, yyyy"
+          }
+        },
+        {
+          component: "Value",
+          label: "Uploaded by",
+          path: "source.uploadedBy",
+          config: {
+            className: "foo"
+          }
+        }
+      ]
+    },
     download: true
   }
 };
@@ -29,6 +63,7 @@ const detail = {
       "image": [
         {
           "url": "http://example.org/image1.jpg",
+          title: "image1.jpg",
           "source": {
             "name": "Washington Post",
             "ts": "2011-09-29T17:38:02Z"
@@ -61,6 +96,21 @@ describe("ImageGalleryMulti component", () => {
       expect(getByTestId("ImageGalleryMulti-component")).toBeInTheDocument();
       expect(getByTestId("item-0")).toBeInTheDocument();
       expect(getByTestId("item-1")).toBeInTheDocument();
+    })
+  });
+  test("Verify Image metadata modal renders correctly", () => {
+    const {getByTestId, getByText} = render(
+      <DetailContext.Provider value={detailContextValue}>
+        <ImageGalleryMulti config={configMultiple.config} data={detail} />
+      </DetailContext.Provider>
+    );
+    waitFor(() => {
+      expect(getByTestId("ImageGalleryMulti-component")).toBeInTheDocument();
+      expect(getByTestId("item-0")).toBeInTheDocument();
+      fireEvent.click(getByTestId("item-0"));
+      expect(getByText("image1.jpg")).toBeInTheDocument();
+      expect(getByText("Washington Post")).toBeInTheDocument();
+
     })
   });
 
