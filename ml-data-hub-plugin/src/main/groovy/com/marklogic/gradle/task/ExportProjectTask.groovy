563179eedbdfc37f329821d744286e35aefbce0a
Ryan Dew <ryan.j.dew@gmail.com>
2022-03-29 17:29:58 -0700

    DHFPROD-8187: Pull changes from HC when exporting project

--- ml-data-hub-plugin/src/main/groovy/com/marklogic/gradle/task/ExportProjectTask.groovy
+++ ml-data-hub-plugin/src/main/groovy/com/marklogic/gradle/task/ExportProjectTask.groovy
@@ -1,13 +1,28 @@
 package com.marklogic.gradle.task
 
+import com.marklogic.hub.hubcentral.HubCentralManager
+import org.apache.commons.io.IOUtils
 import org.gradle.api.tasks.TaskAction
 
 class ExportProjectTask extends HubTask {
 
     @TaskAction
     void exportJobs() {
+        HubCentralManager hubCentralManager = new HubCentralManager()
         File exportFile = getHubProject().projectDir.resolve("build").resolve("datahub-project.zip").toFile();
-        getHubConfig().getHubProject().exportProject(exportFile);
+        if (!exportFile.getParentFile().exists()) {
+            exportFile.getParentFile().mkdirs()
+        }
+        if (!exportFile.exists()) {
+            exportFile.createNewFile()
+        }
+        FileOutputStream fos
+        try {
+            fos = new FileOutputStream(exportFile)
+            hubCentralManager.writeProjectFilesAsZip(getHubConfig().newHubClient(), fos)
+        } finally {
+            IOUtils.closeQuietly(fos)
+        }
         println("The project has been exported to \"./build/datahub-project.zip\"")
     }
 }
