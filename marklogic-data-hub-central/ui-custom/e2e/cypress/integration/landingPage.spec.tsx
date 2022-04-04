77d84a36e30bfd929d96bbb6b09f53bf341257c0
ngodugu-marklogic <nikhila.godugu@marklogic.com>
2022-04-04 13:44:25 -0700

    DHFPROD-8634: E2E tests for validating landing page of Twizzlers

--- marklogic-data-hub-central/ui-custom/e2e/cypress/integration/landingPage.spec.tsx
+++ marklogic-data-hub-central/ui-custom/e2e/cypress/integration/landingPage.spec.tsx
@@ -1,6 +1,43 @@
+import landingPage from "../support/pages/landing";
+import searchPage from "../support/pages/search";
+
 describe("Visit Entity Viewer Landing page", () => {
   it("Greets with Entity Viewer title", () => {
-	cy.visit("/");
+    cy.visit("/");
     cy.title().should("eq", "Entity Viewer");
+    landingPage.dashboard().should("be.visible");
+    cy.waitUntil(() => landingPage.whatsNewChart().should("be.visible",{timeout:5000}));
+  });
+  it("Validate the Header Menus", () => {
+    landingPage.menuOptions().eq(0).should("be.visible");
+    landingPage.menuOptions().eq(1).should("be.visible").should("have.attr", "href", "http://www.marklogic.com");
+    landingPage.subMenu().click();
+    landingPage.subMenuMlDocs().should("be.visible").should("have.attr", "href", "https://docs.marklogic.com/");
+
+    cy.log("***Clicking on search sub menu option to validate the navigation to Search page***");
+    landingPage.subMenuSearch().scrollIntoView().click("topLeft");
+    searchPage.summaryMeter().should("exist");
+    landingPage.entityViewerTitle().click();
+    landingPage.dashboard().should("be.visible");
+    searchPage.menuSearchBox().should("exist");
+    searchPage.menuEntityDropdown().should("exist");
+
+    cy.log("***Clicking on searchMenu to validate the navigation to Search page***");
+    landingPage.menuOptions().eq(0).click();
+    searchPage.summaryMeter().should("exist");
+    landingPage.entityViewerTitle().click();
+    landingPage.dashboard().should("be.visible");
+  });
+  it("Validate the Metrics section", () => {
+    cy.contains("New entities this week");
+    cy.contains("Sources added this week");
+    cy.contains("Tasks created today");
+    cy.contains("Activities unassigned");
+  });
+  it("Validate the default sections are displayed", () => {
+    cy.contains("New Search");
+    cy.contains("Recent Searches");
+    cy.contains("What's New with Entities");
+    cy.contains("Recently Visited");
   });
 });
