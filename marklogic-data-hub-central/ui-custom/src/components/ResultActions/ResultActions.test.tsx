c23bd60f1ce3e73f138596d893854e6c5ac6a17d
iveyMarklogic <Ivey.Leon@marklogic.com>
2022-04-08 12:39:32 -0300

    DHFPROD-8714: Widget: ResultActions

---
+++ marklogic-data-hub-central/ui-custom/src/components/ResultActions/ResultActions.test.tsx
@@ -0,0 +1,55 @@
+import {render} from "@testing-library/react";
+import {DetailContext} from "../../store/DetailContext";
+import ResultActions from "./ResultActions";
+
+const configuration = {
+  resultActions: {
+    component: "ResultActions",
+    config: {
+      id: "resultActions",
+      arrayPath: "person.actions.action",
+      action: {
+        icon: "icon",
+        color: "color",
+        url: "url"
+      }
+    }
+  }
+}
+const detail = {
+  person: {
+    actions: {
+      action: [
+        {
+          url: "http://oaic.gov.au",
+          icon: "faCog"
+        },
+        {
+          url: "https://multiply.com",
+          icon: "faCode"
+        },
+        {
+          url: "http://addthis.com",
+          icon: "faSync"
+        }
+      ]
+    }
+  }
+}
+const detailContextValue = {
+  detail: detail,
+  handleDetail: jest.fn()
+};
+
+describe("LinkList component", () => {
+  test("Verify LinkList widget renders correctly", () => {
+    const {getByTestId} = render(
+      <DetailContext.Provider value={detailContextValue}>
+        <ResultActions config={configuration.resultActions.config} data={detail} />
+      </DetailContext.Provider>
+    );
+    expect(getByTestId("action-item-0")).toBeInTheDocument();
+    expect(getByTestId("action-item-1")).toBeInTheDocument();
+    expect(getByTestId("action-item-2")).toBeInTheDocument();
+  });
+});
