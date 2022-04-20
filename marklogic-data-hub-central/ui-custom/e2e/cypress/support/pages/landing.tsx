267b760a9bec0d7b998cdd28c738fa7aa87d316a
ngodugu-marklogic <nikhila.godugu@marklogic.com>
2022-04-20 09:29:34 -0700

    DHFPROD-8636: E2E tests for validating all the widgets in Twizzlers

--- marklogic-data-hub-central/ui-custom/e2e/cypress/support/pages/landing.tsx
+++ marklogic-data-hub-central/ui-custom/e2e/cypress/support/pages/landing.tsx
@@ -24,6 +24,42 @@ class LandingPage {
   searchBox() {
     return cy.get(".section").findByTestId("searchBox");
   }
+  recentSearchesClear() {
+    return cy.findByTestId("recentSearches-clearButton");
+  }
+  noRecentSearches() {
+    return cy.get(".recentSearches .none-found");
+  }
+  recentSearchesText() {
+    return cy.get(".recentSearches span.qtext");
+  }
+  recentSearchesRows() {
+    return cy.get(".recentSearches").find("tbody tr");
+  }
+  recentSearchesConfirmationModal() {
+    return cy.findByTestId("recent search-resetConfirmationModal");
+  }
+  recentSearchesConfirmationYes() {
+    return cy.findByTestId("yesButton");
+  }
+  recentSearchesConfirmationNo() {
+    return cy.findByTestId("noButton");
+  }
+  recentlyVisitedClear() {
+    return cy.findByTestId("recentRecords-clearButton");
+  }
+  noRecentlyVisited() {
+    return cy.get(".section").eq(2).find(".none-found");
+  }
+  recentlyVisitedText() {
+    return cy.get(".recentRecords .title span");
+  }
+  recentlyVisitedRows() {
+    return cy.get(".recentRecords").find(".result");
+  }
+  recentlyVisitedConfirmationModal() {
+    return cy.findByTestId("recently visited record-resetConfirmationModal");
+  }
 
 }
 
