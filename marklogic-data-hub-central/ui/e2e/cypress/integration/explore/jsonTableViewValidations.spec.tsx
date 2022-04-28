d3b37cbfb816a2c8198cb100ae1491fd6ba2542c
csilva-ml <Nadia.Silva@endava.com>
2022-04-28 00:35:46 -0300

    DHFPROD-8762-e2eFixes-pt2

--- marklogic-data-hub-central/ui/e2e/cypress/integration/explore/jsonTableViewValidations.spec.tsx
+++ marklogic-data-hub-central/ui/e2e/cypress/integration/explore/jsonTableViewValidations.spec.tsx
@@ -84,6 +84,7 @@ describe("json scenario for table on browse documents page", () => {
 
   it("verify instance view of the document without pk", () => {
     cy.wait(2000);
+    entitiesSidebar.showMoreEntities();
     entitiesSidebar.openBaseEntityFacets(BaseEntityTypes.PERSON);
     browsePage.getFacetItemCheckbox("fname", "Alice").click();
     browsePage.getGreySelectedFacets("Alice").should("exist");
