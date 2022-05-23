f5434b9eab3e2fe316919788ebb16407e3b7c2cb
lucianaabc-ml <Luciana.Abecasis@marklogic.com>
2022-05-23 11:20:40 -0300

    DHFPROD-8901: Error handling: Redirect screen

--- marklogic-data-hub-central/ui/src/pages/noMatchRedirect.tsx
+++ marklogic-data-hub-central/ui/src/pages/noMatchRedirect.tsx
@@ -1,11 +1,12 @@
+import {Col, Row} from "react-bootstrap";
 import React, {useContext, useEffect, useState} from "react";
-import {withRouter} from "react-router-dom";
-import {UserContext} from "@util/user-context";
-import {HCButton} from "@components/common";
-import styles from "./noMatchRedirect.module.scss";
-import oopsIcon from "../assets/oopsIcon.png";
-import {Row, Col} from "react-bootstrap";
+
 import {ArrowLeftShort} from "react-bootstrap-icons";
+import {HCButton} from "@components/common";
+import {UserContext} from "@util/user-context";
+import oopsIcon from "../assets/oopsIcon.png";
+import styles from "./noMatchRedirect.module.scss";
+import {withRouter} from "react-router-dom";
 
 const NoMatchRedirect = ({history}) => {
 
@@ -14,12 +15,14 @@ const NoMatchRedirect = ({history}) => {
   const [errorBodyText, setErrorBodyText] = useState("");
 
   useEffect(() => {
-    clearErrorMessage();
     setErrorBodyText(user.error.message);
+    return () => {
+      clearErrorMessage();
+    };
   }, []);
 
   const goBack = () => {
-    return user.authenticated ? history.push("/tiles") : history.push("/");
+    return user.authenticated ? history.goBack() : history.push("/");
   };
 
   const copyToClipboard = () => {
@@ -36,7 +39,6 @@ const NoMatchRedirect = ({history}) => {
             <ArrowLeftShort aria-label="Back" className={"d-inline-block me-2 fs-2 header-back-button"} /><div className={styles.backOperationFailed}>
               Back</div></div>
         </Col>
-
         <Col className="col-md-6">
           <Row className={styles.superiorMiddleContainer}>
             <Col>
@@ -48,40 +50,31 @@ const NoMatchRedirect = ({history}) => {
                   <div className="px-2">
                     <div className={styles.title}><h1><strong>Operation failed.</strong></h1></div>
                     <div className={styles.spacer}>
-                      <strong>The operation failed due to unknown reasons.</strong>
+                      <strong>The operation failed because of {user.error.type ? "the following errors:" : "an unknown error."}</strong>
                     </div>
+                    {user.error.message && <>
+                      <div className={styles.errorContainer}>
+                        {user.error.title && <h3>user.error.title</h3>}
+                        {user.error.encounteredErrors && user.error.encounteredErrors}
+                        {user.error.message}
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget libero vitae turpis scelerisque tristique eget quis nibh. Maecenas luctus condimentum ligula, quis tincidunt lorem viverra dictum. Aliquam erat volutpat. Mauris malesuada consequat erat eget malesuada. Etiam quis scelerisque risus. Fusce rhoncus condimentum sapien, sed iaculis sapien scelerisque porttitor. Morbi imperdiet nisl non neque pharetra tempor. Phasellus eu purus lorem. Morbi ultrices nunc et enim laoreet commodo. Nam quis orci scelerisque, fringilla magna id, eleifend libero.
+                      </div>
+                      <div className={styles.buttonCopyCol}>
+                        <HCButton data-testid={`copy-button-error`} className={styles.copyButton} onClick={copyToClipboard}>{copyClicked}</HCButton>
+                      </div>
 
-                    <div className={styles.spacer}>
-                      <strong>{user.error.message ? "Copy the log below and send it to" : "Contact"} <a className={styles.contactLink} target="_blank" href="https://docs.marklogic.com/cloudservices/contact-support.html">MarkLogic Support</a>.</strong>
+                    </>}
+                    <div className={styles.contactSupport}>
+                      <strong> Contact <a className={styles.contactLink} target="_blank" href="https://docs.marklogic.com/cloudservices/contact-support.html">MarkLogic Support</a>.</strong>
                     </div>
                   </div>
                 </Col>
               </Row>
             </Col>
           </Row>
-
-          {user.error.message ? <>
-            <Row>
-              <Col className={styles.buttonCopyCol} style={{paddingBottom: 20}}>
-                <HCButton data-testid={`copy-button-error`} className={styles.copyButton} onClick={copyToClipboard}>{copyClicked}</HCButton>
-              </Col>
-            </Row>
-
-            <Row>
-              <Col>
-                <div className={styles.errorContainer}>
-                  {errorBodyText}
-                </div>
-              </Col>
-            </Row>
-          </> : ""
-          }
         </Col>
-
-        <Col className="col-md-3">
-        </Col>
-      </Row>
-    </div>
+      </Row >
+    </div >
   );
 };
 
