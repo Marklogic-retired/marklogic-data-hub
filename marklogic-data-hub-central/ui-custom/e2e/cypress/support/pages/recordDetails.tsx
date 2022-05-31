5fac809bc5550ed741e31096aa5e51d38b19e50f
nikhila.godugu@marklogic.com <nikhila.godugu@marklogic.com>
2022-05-31 07:08:11 -0700

    DHFPROD-8751: E2E tests for validate Record Details Page in Twizzlers

--- marklogic-data-hub-central/ui-custom/e2e/cypress/support/pages/recordDetails.tsx
+++ marklogic-data-hub-central/ui-custom/e2e/cypress/support/pages/recordDetails.tsx
@@ -3,7 +3,51 @@ class RecordDetailsPage {
   backToSearch() {
     return cy.get(".title .icon svg");
   }
-
+  recordTitle() {
+    return cy.get(".title .text span");
+  }
+  thumbNail() {
+    return cy.get(".heading .thumbnail img");
+  }
+  recordValue(value: string) {
+    return cy.get(`[data-testid="table-${value}"] span[data-testid="valueId"]`);
+  }
+  recordText(value: string, textVal: string) {
+    return cy.get(`[data-testid="table-${value}"]`).findAllByText(textVal);
+  }
+  socialItems() {
+    return cy.get(".social-items a");
+  }
+  socialHandle() {
+    return cy.get(`[class="handle"]`);
+  }
+  metaDataVal() {
+    return cy.get(".MetadataValue .hasPopover");
+  }
+  expandSections() {
+    return cy.get(".expand");
+  }
+  expandButton() {
+    return cy.findAllByTestId("expandButton");
+  }
+  collapseButton() {
+    return cy.findAllByTestId("collapseButton");
+  }
+  membershipContainer() {
+    return cy.findByTestId("membership-component");
+  }
+  relationships() {
+    return cy.get(".relationships");
+  }
+  imageGallery() {
+    return cy.findByTestId("ImageGalleryMulti-component");
+  }
+  timeline() {
+    return cy.findByTestId("activity-info-timeline");
+  }
+  recordId() {
+    return cy.get(`[class="details"] [class="title"] span`);
+  }
 }
 
 const recordDetailsPage = new RecordDetailsPage();
