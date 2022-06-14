9790dabeaf0738d8edf8f27d94b06d17e8ae670e
Yubisel Vega <yubisel.vega@marklogic.com>
2022-06-14 17:48:08 -0300

    DHFPROD-8932: Define entity display settings in explore when hub central config file doesn't exist

--- marklogic-data-hub-central/ui/src/util/hubCentralConfig-context.tsx
+++ marklogic-data-hub-central/ui/src/util/hubCentralConfig-context.tsx
@@ -1,6 +1,9 @@
 import React, {useState, useContext} from "react";
 import {UserContext} from "@util/user-context";
-import {getHubCentralConfig, updateHubCentralConfig} from "@api/modeling";
+import {getHubCentralConfig, updateHubCentralConfig, primaryEntityTypes} from "@api/modeling";
+import {defaultHubCentralConfig} from "@config/modeling.config";
+import {themeColors} from "@config/themes.config";
+import {defaultIcon} from "@config/explore.config";
 
 const defaultContextOptions = {
   hubCentralConfig: {},
@@ -20,13 +23,25 @@ export const HubCentralConfigContext = React.createContext<HubCentralConfigConte
 
 const HubCentralConfigProvider: React.FC<{children: any}> = ({children}) => {
   const {handleError} = useContext(UserContext);
-  const [hubCentralConfig, setConfig] = useState<HubCentralConfigContextInterface>(defaultContextOptions);
+  const [hubCentralConfig, setConfig] = useState<any>({});
 
   const getHubCentralConfigFromServer = async () => {
     try {
       const response = await getHubCentralConfig();
       if (response["status"] === 200) {
-        setConfig(response.data);
+        if (response.data?.modeling) {
+          setConfig(response.data);
+        } else {
+          const responsePrimaryEntityTypes = await primaryEntityTypes();
+          const updatedHubCentralConfig: any = defaultHubCentralConfig;
+          responsePrimaryEntityTypes.data.forEach(model => {
+            updatedHubCentralConfig["modeling"]["entities"][model.entityName] = {
+              color: themeColors.defaults.entityColor,
+              icon: defaultIcon
+            };
+          });
+          setConfig(updatedHubCentralConfig);
+        }
       }
       return response;
     } catch (error) {
