5ad387a524c6cce9c80a57692fdaf7754f39f0fd
Snigdha Enumulapally <e.snigdharao@gmail.com>
2022-07-29 11:26:34 -0700

    DHFPROD-9125: Step Details Should Not Exit on Step Name Click

--- marklogic-data-hub-central/ui/src/components/entities/page-header/page-header.tsx
+++ marklogic-data-hub-central/ui/src/components/entities/page-header/page-header.tsx
@@ -12,9 +12,9 @@ const PageHeader: React.FC<Props> = (props) => {
 
   return (
     <div className={styles.customHeader}>
-      <Row id="back-button" className={"py-3 px-3 header-heading-title"} onClick={props.handleOnBack}>
+      <Row id="back-button" className={"py-3 px-3 header-heading-title"}>
         <Col>
-          <span className={`d-flex align-items-center cursor-pointer`} data-testid="arrow-left"><ArrowLeftShort aria-label="Back" className={"d-inline-block me-2 fs-1 header-back-button"} />{props.title}</span>
+          <span className={`d-flex align-items-center`} data-testid="arrow-left"><ArrowLeftShort aria-label="Back" className={"d-inline-block me-2 fs-1 header-back-button cursor-pointer"} onClick={props.handleOnBack}/>{props.title}</span>
         </Col>
       </Row>
     </div>
