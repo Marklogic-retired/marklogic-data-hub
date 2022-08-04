c665f4066f6465105e5b01fdc1cd1ab5c17bc2b4
lucianaabc-ml <Luciana.Abecasis@marklogic.com>
2022-08-04 14:10:59 -0300

    DHFPROD-9093: Refactor Toolbar component
    DHFPROD-9093: Refactor Toolbar component

---
+++ marklogic-data-hub-central/ui/src/components/tiles/toolbar-icon.test.tsx
@@ -0,0 +1,40 @@
+import React, {createRef} from "react";
+import {Router} from "react-router";
+import {createMemoryHistory} from "history";
+const history = createMemoryHistory();
+import {render, fireEvent} from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import ToolbarIcon from "./toolbar-icon";
+import tiles from "../../config/tiles.config";
+
+describe("Toolbar Icon test suite", () => {
+
+  it("should render the enabled icon", () => {
+    const ref = createRef();
+    const onClick = jest.fn();
+    const onKeyDown = jest.fn();
+
+    const {getByLabelText} = render(<Router history={history}><ToolbarIcon tile={tiles.load} tileId={"load"} tileRef={ref} i={1} isActive={false} enabled={true} onClick={onClick}
+      onKeyDown={onKeyDown} /> </Router>);
+
+    const icon = getByLabelText("tool-load");
+    expect(icon).toBeInTheDocument();
+
+    fireEvent.click(icon);
+    expect(onClick).toBeCalled();
+  });
+  it("should render the icon disabled", () => {
+    const ref = createRef();
+    const onClick = jest.fn();
+    const onKeyDown = jest.fn();
+
+    const {getByLabelText} = render(<Router history={history}>
+      <ToolbarIcon tile={tiles.load} tileId={"load"} tileRef={ref} i={1} isActive={false} enabled={false} onClick={onClick}
+        onKeyDown={onKeyDown} /></Router>);
+
+    const icon = getByLabelText("tool-load");
+
+    fireEvent.click(icon);
+    expect(onClick).not.toBeCalled();
+  });
+});
---
+++ marklogic-data-hub-central/ui/src/components/tiles/toolbar-icon.tsx
@@ -0,0 +1,58 @@
+import React, {FC, RefObject} from "react";
+import {HCTooltip} from "@components/common";
+import {Link} from "react-router-dom";
+import {TileItem, TileId} from "@config/tiles.config";
+
+import styles from "./toolbar.module.scss";
+
+interface Props {
+  tile: TileItem;
+  tileId: TileId;
+  tileRef: RefObject<any>;
+  i: number;
+  isActive: boolean;
+  enabled: boolean;
+  onClick: (tileId: TileId, i: number) => void;
+  onKeyDown: (e: React.KeyboardEvent<any>, i: number) => void;
+}
+
+const ToolbarIcon: FC<Props> = ({tile, tileRef, tileId, i, isActive, enabled, onClick, onKeyDown}) => {
+  let tooltipText = enabled ? tile["title"] : `${tile["title"]}: Contact your security administrator to get the roles and permissions required to access this functionality.`;
+
+  const linkOnClickHandler = (event) => {
+    if (!enabled) event.preventDefault();
+  };
+
+  const handleClick = () => {
+    return enabled ? onClick(tileId, i) : null;
+  };
+
+  return (
+
+    <HCTooltip text={tooltipText} id={tooltipText + "-tooltip"} placement="left-start" key={i}>
+      <div
+        className={`${styles.toolbarIcon} ${styles[tile["icon"]]} ${isActive && styles.selected} ${!enabled && styles.disabled}`}
+        aria-label={"tool-" + tileId}
+        tabIndex={-1}
+        onClick={handleClick}
+      >
+        <Link to={
+          {
+            pathname: `/tiles/${tileId}`,
+            state: {
+              tileIconClicked: true
+            }
+          }}
+        aria-label={"tool-" + tileId + "-link"}
+        tabIndex={0}
+        ref={tileRef}
+        onClick={linkOnClickHandler}
+        onKeyDown={(e) => onKeyDown(e, i)}
+        />
+      </div>
+    </HCTooltip>
+
+  );
+};
+
+export default ToolbarIcon;
