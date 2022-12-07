353425ae088190bcd1d8b94ba91cefa42adf34fb
Snigdha Enumulapally <e.snigdharao@gmail.com>
2022-12-07 07:54:11 -0800

    DHFPROD-9618: Migrate to RTL - search

--- marklogic-data-hub-central/ui/src/components/search-pagination/search-pagination.test.tsx
+++ marklogic-data-hub-central/ui/src/components/search-pagination/search-pagination.test.tsx
@@ -1,29 +1,31 @@
 import React from "react";
-import {mount} from "enzyme";
-import SearchPagination from "./search-pagination";
+import {render, cleanup} from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import SearchPagination from "@components/search-pagination/search-pagination";
 
-describe("Search Pagination component", () => {
-  let wrapper;
+describe("Search Pagination Simple Component", () => {
 
+  afterEach(cleanup);
   describe("change current page", () => {
 
     test("renders", () => {
-      wrapper = mount(
+      const {getByTestId, getByText} = render(
         <SearchPagination
           total={11}
-          pageSize={20}
+          pageSize={10}
           pageNumber={1}
           pageLength={1}
           maxRowsPerPage={20}
         />);
 
-      expect(wrapper.find(`.pagination`)).toHaveLength(1);
-      const pageSize = wrapper.find(`select[data-testid="pageSizeSelect"]`);
-      expect(pageSize.props().value).toBe(20);
+      // pagination will have 'Previous', '1', '2', 'Next' values. Hence, verifying the length to be 4.
+      expect(getByTestId("pagination").children.length).toBe(4);
+      // checking if the pageSize is 10 rows per page
+      expect(getByText("10 / page")).toBeInTheDocument();
     });
 
-    test("verify not showing pagination controls if 0 or 1 pages ", () => {
-      wrapper = mount(
+    test("No pagination when there is 0 or 1 page", () => {
+      const {queryByTestId} = render(
         <SearchPagination
           total={10}
           pageSize={20}
@@ -31,8 +33,7 @@ describe("Search Pagination component", () => {
           pageLength={1}
           maxRowsPerPage={20}
         />);
-      expect(wrapper.find(".pagination")).toHaveLength(0);
+      expect(queryByTestId("pagination")).toBeNull();
     });
-
   });
 });
