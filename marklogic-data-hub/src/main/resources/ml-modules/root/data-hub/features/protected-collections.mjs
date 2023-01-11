eb87f69e6c2331916f4aab6f958e9e4b1e8f9279
Aniela Amy <aniela.amy@marklogic.com>
2023-01-11 14:51:55 -0300

    DHFPROD-9447: Protected collections feature

--- marklogic-data-hub/src/main/resources/ml-modules/root/data-hub/features/protected-collections.mjs
+++ marklogic-data-hub/src/main/resources/ml-modules/root/data-hub/features/protected-collections.mjs
@@ -15,15 +15,132 @@
  */
 'use strict';
 
-function onArtifactPublish (artifactType, artifactName) {
+import core from "/data-hub/5/artifacts/core.mjs";
+import consts from "/data-hub/5/impl/consts.mjs";
+import hubUtils from "/data-hub/5/impl/hub-utils.mjs";
+import sjsProxy from "/data-hub/core/util/sjsProxy.mjs";
+const sec = sjsProxy.requireSjsModule("/MarkLogic/security.xqy");
 
+const INFO_EVENT = consts.TRACE_CORE;
+const DEBUG_EVENT = consts.TRACE_CORE_DEBUG;
+const DEBUG_ENABLED = xdmp.traceEnabled(DEBUG_EVENT);
+
+function protectCollections(collections, permissions) {
+    collections.forEach(coll => {
+        try {
+            if (DEBUG_ENABLED) {
+                hubUtils.hubTrace(DEBUG_EVENT, `Protecting collection ${coll}`);
+            }
+            let permissionsSplit = permissions.split(",");
+            if (permissionsSplit.length % 2 !== 0) {
+                hubUtils.hubTrace(INFO_EVENT, `While adding protected collections, cannot protect collection ${coll}. Invalid permissions: ${permissions}`);
+            } else {
+                let permissionsSec = [];
+                for (var i = 0; i < permissionsSplit.length; i++) {
+                    permissionsSec.push(xdmp.permission(permissionsSplit[i], permissionsSplit[++i], "element"));
+                }
+                sec.protectCollection(coll, permissionsSec);
+            }
+        } catch(e) {
+            hubUtils.hubTrace(INFO_EVENT, `Could not protect collection ${coll}; Reason: ${e.message}`);
+        }
+    })
+}
+
+function onArtifactPublish (artifactType, artifactName) {
+    const artifact = core.getArtifact(artifactType, artifactName);
+
+    //we need to get the permissions from somewhere else later
+    const permissions = "data-hub-common,read,data-hub-common,update";
+    const collection = artifactName;
+    if(artifact.collections && !artifact.collections.includes(collection)) {
+        artifact.collections.push(collection);
+    }
+
+    if (!artifact.features) {
+        artifact.features = [];
+    }
+
+    let protectedFeature = {};
+    protectedFeature.collections = [collection];
+    protectedFeature.permissions = permissions;
+
+    artifact.features["protectedCollections"] = protectedFeature;
+    //confirm that the collection is protected
+    protectCollections([collection], permissions);
+    hubUtils.hubTrace(INFO_EVENT, `Finished processing protected collections feature for ${artifactName}`);
+
+    return artifact;
 }
 
 function onInstanceSave(stepContext, model, contentObject) {
+    let permissions = [];
+    let collections = [];
 
+    if(featureEnabled(stepContext)) {
+        hubUtils.hubTrace(INFO_EVENT, `Processing protected collections feature for an instance while running ${stepContext.name}`);
+
+        const stepFeature = stepContext.features["protectedCollections"]
+        if (stepFeature.permissions) {
+            permissions = stepFeature.permissions;
+        }
+        if (stepFeature.collections) {
+            collections = stepFeature.collections;
+        }
+        addCollectionsToObject(collections, permissions, contentObject);
+
+    }
+
+    const modelName = model.info.title;
+    if(featureEnabled(model.definitions[modelName]))
+    {
+        hubUtils.hubTrace(INFO_EVENT, `Processing protected collections feature for an instance of ${modelName}`);
+
+        const modelFeature = model.definitions[modelName].features["protectedCollections"];
+        if (modelFeature.permissions) {
+            permissions = modelFeature.permissions;
+        }
+        if (modelFeature.collections) {
+            collections = modelFeature.collections;
+        }
+        addCollectionsToObject(collections, permissions, contentObject);
+    }
+
+    hubUtils.hubTrace(INFO_EVENT, `Finished processing protected collections feature for an instance of ${modelName} while running ${stepContext.name}`);
+
+    return contentObject;
 }
 
-export {
+function featureEnabled(artifact) {
+    if (artifact.features && artifact.features["protectedCollections"]) {
+        return artifact.features["protectedCollections"].enabled ? artifact.features["protectedCollections"].enabled : false
+    }
+    return false;
+}
+
+function addCollectionsToObject(collections, permissions, contentObject) {
+    if(Array.isArray(collections) && collections.length > 0) {
+
+        // add collections to the contentObject if it doesn't have them
+        const contentCollections = contentObject.context.collections || [];
+        let newCollections = [];
+        if (Array.isArray(contentCollections)) {
+            collections.forEach(coll => {
+                if( !contentObject.context.collections.includes(coll) ) {
+                    newCollections.push(coll);
+                }
+            })
+            contentObject.context.collections = contentCollections.concat(newCollections);
+        } else {
+            contentObject.context.collections = collections;
+        }
+
+        //confirm that the collections are protected
+        protectCollections(collections, permissions);
+    }
+}
+
+export default {
     onArtifactPublish,
     onInstanceSave
 };
