4597502e5fc6920d31c3372bfee4f69a29d6ff80
Andres Mejia <andres.mejia@endava.com>
2023-03-09 11:34:46 -0500

    DHFPROD-10005: Entity modal closes even if there is an error

---
+++ marklogic-data-hub-central/ui/e2e/cypress/e2e/modeling/editEntityValidations.cy.tsx
@@ -0,0 +1,54 @@
+/// <reference types="cypress"/>
+
+import {Application} from "../../support/application.config";
+import {confirmationModal, toolbar} from "../../support/components/common/index";
+import {entityTypeModal, entityTypeTable} from "../../support/components/model";
+import loginPage from "../../support/pages/login";
+import modelPage from "../../support/pages/model";
+import {ConfirmationType} from "../../support/types/modeling-types";
+
+describe("Entity validations ", () => {
+
+  before(() => {
+    cy.visit("/");
+    cy.contains(Application.title);
+    cy.loginAsTestUserWithRoles("hub-central-entity-model-reader", "hub-central-entity-model-writer", "hub-central-saved-query-user").withRequest();
+    loginPage.postLogin();
+  });
+
+  afterEach(() => {
+    cy.clearAllSessionStorage();
+    cy.clearAllLocalStorage();
+  });
+
+  it("Check Namespace URI and prefix", () => {
+    cy.waitUntil(() => toolbar.getModelToolbarIcon()).click();
+    modelPage.selectView("table");
+    entityTypeTable.waitForTableToLoad();
+    modelPage.getAddButton().click();
+    modelPage.getAddEntityTypeOption().should("be.visible").click({force: true});
+    entityTypeModal.newEntityName("TestEdit");
+    entityTypeModal.getAddButton().click();
+
+
+    cy.log("**Check error handling with wrong URI**");
+    entityTypeTable.getEntity("TestEdit").click();
+    entityTypeModal.getNamespaceInput().type("....");
+    entityTypeModal.getAddButton().click();
+    cy.findByText("You must define a prefix because you defined a namespace URI.");
+
+    cy.log("**Check error handling prefix without nameURI**");
+    entityTypeModal.getNamespaceInput().clear();
+    entityTypeModal.getPrefixInput().type("...");
+    entityTypeModal.getAddButton().click();
+    cy.findByText("You must define a namespace URI because you defined a prefix.");
+
+    cy.log("**Check error handling invalid syntax URI**");
+    entityTypeModal.getNamespaceInput().type("....");
+    entityTypeModal.getAddButton().click();
+    cy.findByText("Invalid syntax: Namespace property must be a valid absolute URI. Example: http://example.org/es/gs");
+    entityTypeModal.getCancelButton().click();
+    entityTypeTable.getDeleteEntityIcon("TestEdit").click();
+    confirmationModal.getYesButton(ConfirmationType.DeleteEntity);
+  });
+});
