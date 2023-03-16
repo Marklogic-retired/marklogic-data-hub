4ff944e5f06569122b4ad3a707719d6f60598967
rolonPablo <pablo.rolon@marklogic.com>
2023-03-16 15:30:11 -0300

    DHFPROD-9869: schema validation

--- marklogic-data-hub/src/test/ml-modules/root/test/suites/data-hub/features/schemaValidation/schema-validation.mjs
+++ marklogic-data-hub/src/test/ml-modules/root/test/suites/data-hub/features/schemaValidation/schema-validation.mjs
@@ -1,23 +1,22 @@
 import schemaValidation from "/data-hub/features/schema-validation.mjs";
 const test = require("/test/test-helper.xqy");
+const config = require("/com.marklogic.hub/config.sjs");
 
-try{
-    schemaValidation.onArtifactSave("model", "Customer");
-} catch (error) {
-    xdmp.log("Unable to generate schema " +  error.message);
-}
+const assertions = [];
 
-const assertions = [
-test.assertTrue(checkForSchema("/entities/Customer.entity.schema.json"), "Customer xml schema should exist"),
-test.assertTrue(checkForSchema("/entities/Customer.entity.xsd"), "Customer xml schema should exist")
-];
+schemaValidation.onArtifactSave("model", "Customer");
 
-function checkForSchema(uri){
-    return fn.head(xdmp.eval(
-        `fn.docAvailable('${uri}') `,
-        {uri:uri}, {database: xdmp.schemaDatabase()}
-    ));
-}
+// assert
+xdmp.invokeFunction(function() {
+
+    assertions.push(test.assertTrue(fn.head(xdmp.eval(  "fn.docAvailable('/entities/Customer.entity.schema.json') "
+        ,  {}, {database: xdmp.schemaDatabase()}))), `Customer json schema should exist.`);
+    assertions.push(test.assertTrue(fn.head(xdmp.eval(  " fn.docAvailable('/entities/Customer.entity.xsd') "
+        ,  {}, {database: xdmp.schemaDatabase()}))), `Customer xml schema should exist.`)
+
+},{database: xdmp.database(config.FINALDATABASE)});
+
+
 
 
 assertions;
