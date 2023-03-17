4489a6c1f16fdb35aec7ab1aeaf715400882e25c
Aniela Amy <aniela.amy@marklogic.com>
2023-03-17 10:23:16 -0300

    DHFPROD-9195: Fixing schema tests.

--- marklogic-data-hub/src/test/ml-modules/root/test/suites/data-hub/features/schemaValidation/schema-validation.mjs
+++ marklogic-data-hub/src/test/ml-modules/root/test/suites/data-hub/features/schemaValidation/schema-validation.mjs
@@ -2,21 +2,19 @@ import schemaValidation from "/data-hub/features/schema-validation.mjs";
 const test = require("/test/test-helper.xqy");
 const config = require("/com.marklogic.hub/config.sjs");
 
-const assertions = [];
-
 schemaValidation.onArtifactSave("model", "Customer");
 
 // assert
-xdmp.invokeFunction(function() {
+const assertions = [
+test.assertTrue(checkForSchema("/entities/Customer.entity.schema.json"), "Customer json schema should exist"),
+test.assertTrue(checkForSchema("/entities/Customer.entity.xsd"), "Customer xml schema should exist")
+];
 
-    assertions.push(test.assertTrue(fn.head(xdmp.eval(  "fn.docAvailable('/entities/Customer.entity.schema.json') "
-        ,  {}, {database: xdmp.schemaDatabase()})), `Customer json schema should exist.`));
-    assertions.push(test.assertTrue(fn.head(xdmp.eval(  " fn.docAvailable('/entities/Customer.entity.xsd') "
-        ,  {}, {database: xdmp.schemaDatabase()})), `Customer xml schema should exist.`))
-
-},{database: xdmp.database(config.FINALSCHEMASDATABASE)});
-
-
-
+function checkForSchema(uri){
+    return fn.head(xdmp.eval(
+        `fn.docAvailable('${uri}') `,
+        {uri:uri}, {database: xdmp.database(config.FINALSCHEMASDATABASE)}
+    ));
+}
 
 assertions;
