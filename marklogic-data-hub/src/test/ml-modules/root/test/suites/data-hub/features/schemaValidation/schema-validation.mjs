1b6d06245a629992c41b8eb97e88e2d34ac930f9
Aniela Amy <aniela.amy@marklogic.com>
2023-03-21 13:57:02 -0300

    rework on schema tests

--- marklogic-data-hub/src/test/ml-modules/root/test/suites/data-hub/features/schemaValidation/schema-validation.mjs
+++ marklogic-data-hub/src/test/ml-modules/root/test/suites/data-hub/features/schemaValidation/schema-validation.mjs
@@ -1,13 +1,15 @@
 import schemaValidation from "/data-hub/features/schema-validation.mjs";
 const test = require("/test/test-helper.xqy");
 
-xdmp.invokeFunction(() => schemaValidation.onArtifactSave("model", "Customer"), {update: "true"});
-
 function checkForSchema(uri){
-  return fn.head(xdmp.invokeFunction(() => fn.docAvailable(uri), {database: xdmp.schemaDatabase()}));
+  return fn.head(xdmp.eval(
+      `fn.docAvailable('${uri}') `,
+      {uri:uri}, {database: xdmp.schemaDatabase()}
+  ));
 }
 
 function verifySchemaGeneration() {
+  schemaValidation.onArtifactSave("model", "Customer");
   return [
     test.assertTrue(checkForSchema("/entities/Customer.entity.schema.json"), "Customer json schema should exist"),
     test.assertTrue(checkForSchema("/entities/Customer.entity.xsd"), "Customer xml schema should exist")
