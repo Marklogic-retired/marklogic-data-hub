250c717b9ecb1000a8f9037187e31c3b95a11b78
Sameera Priyatham Tadikonda <preethu4476@gmail.com>
2023-03-23 14:57:36 -0700

    DEVO-774: Updating the version to 5.8.1

--- marklogic-data-hub/src/main/java/com/marklogic/hub/impl/VersionInfo.java
+++ marklogic-data-hub/src/main/java/com/marklogic/hub/impl/VersionInfo.java
@@ -82,6 +82,6 @@ public class VersionInfo {
         }
 
         String version = properties.getProperty("version");
-        return "${project.version}".equals(version) ? "5.8-SNAPSHOT" : version;
+        return "${project.version}".equals(version) ? "5.8.1" : version;
     }
 }
