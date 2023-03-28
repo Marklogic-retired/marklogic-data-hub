98a048d39db183e1624b9c1f80276600187b3c1e
Aniela Amy <aniela.amy@marklogic.com>
2023-03-28 18:38:01 -0300

    DHFPROD-9448: Temporal feature.

--- marklogic-data-hub/src/main/resources/ml-modules/root/data-hub/features/temporal.mjs
+++ marklogic-data-hub/src/main/resources/ml-modules/root/data-hub/features/temporal.mjs
@@ -19,20 +19,143 @@
  * Feature that handles the temporal logic of the artifacts and instances.
  */
 
+import temporalLib from "/data-hub/5/temporal/hub-temporal.mjs";
+import hubUtils from "/data-hub/5/impl/hub-utils.mjs";
+import flowUtils from '/data-hub/5/impl/flow-utils.mjs';
+import consts from "/data-hub/5/impl/consts.mjs";
+import core from "/data-hub/5/artifacts/core.mjs";
+
+const temporal = require("/MarkLogic/temporal.xqy");
+
+const INFO_EVENT = consts.TRACE_CORE;
+const DEBUG_EVENT = consts.TRACE_CORE_DEBUG;
+const DEBUG_ENABLED = xdmp.traceEnabled(DEBUG_EVENT);
+
+function logDebug(message) {
+    if (DEBUG_ENABLED) {
+        hubUtils.hubTrace(DEBUG_EVENT, message);
+    }
+}
+
+function getModelFeatures(model, artifactName) {
+    if(model.definitions && model.definitions[artifactName] && model.definitions[artifactName].features) {
+        return model.definitions[artifactName].features;
+    }
+    return undefined;
+}
 function onArtifactPublish (artifactType, artifactName) {
-    return true;
+  const artifact = core.getArtifact(artifactType, artifactName);
+  let features = artifact.features;
+  if("model" === artifactType) {
+    const modelFeature = getModelFeatures(artifact, artifactName);
+    features = modelFeature? modelFeature : features;
+  }
+
+  if (!features || !features["temporal"]) {
+      logDebug(`Temporal feature: Disabled for artifact ${artifactName} of type ${artifactType}.`);
+      return;
+  } else if (!features["temporal"].collection) {
+      logDebug( `Temporal feature: Missing temporal collection for artifact ${artifactName} of type ${artifactType}.`);
+      return;
+  }
+
+  const temporalCollection = features["temporal"].collection;
+  if (getTemporalCollection(temporalCollection).length === 0){
+    hubUtils.hubTrace(INFO_EVENT, `Temporal feature: Creating temporal collection ${temporalCollection}.`);
+
+    createIndexes();
+    hubUtils.hubTrace(INFO_EVENT,`Temporal feature: Created indexes for temporal collection ${temporalCollection}.`);
+    createAxes();
+    hubUtils.hubTrace(INFO_EVENT, `Temporal feature: Created temporal axis for temporal collection ${temporalCollection}.`);
+
+    xdmp.invokeFunction(function () {
+      temporal.collectionCreate(temporalCollection, "system", "valid");
+    }, {update: "true"});
+
+    hubUtils.hubTrace(INFO_EVENT, `Temporal feature: Created temporal collection ${temporalCollection}.`);
+  }
+
+}
+
+function createIndexes() {
+  xdmp.invokeFunction(function () {
+    const admin = require("/MarkLogic/admin");
+    let config = admin.getConfiguration();
+    let dbid = xdmp.database();
+    let rangespecs = [
+      admin.databaseRangeElementIndex("dateTime", "", "validStart", "", fn.false()),
+      admin.databaseRangeElementIndex("dateTime", "", "validEnd", "", fn.false()),
+      admin.databaseRangeElementIndex("dateTime", "", "systemStart", "", fn.false()),
+      admin.databaseRangeElementIndex("dateTime", "", "systemEnd", "", fn.false())];
+    rangespecs.forEach((rangespec) => {
+      try {
+        config = admin.databaseAddRangeElementIndex(config, dbid, rangespec);
+      } catch (e) {
+        hubUtils.hubTrace(INFO_EVENT, `Temporal feature: There was an issue adding an index: ${e}.`);
+      }
+    });
+    admin.saveConfiguration(config);
+  });
+}
+
+function createAxes() {
+  xdmp.invokeFunction(function () {
+    try {
+      temporal.axisCreate("valid",
+        cts.elementReference("systemStart", "type=dateTime"), cts.elementReference("systemEnd", "type=dateTime"));
+    } catch (e) {
+      hubUtils.hubTrace(INFO_EVENT, `Temporal feature: There was an issue creating axis 'valid': ${e}.`);
+    }
+    try {
+      temporal.axisCreate("system",
+        cts.elementReference("systemStart", "type=dateTime"), cts.elementReference("systemEnd", "type=dateTime"));
+    } catch (e) {
+      hubUtils.hubTrace(INFO_EVENT, `Temporal feature: There was an issue creating axis 'system': ${e}.`);
+    }
+  });
+
 }
 
 function onBuildInstanceQuery(stepContext, model, sourceQuery) {
-    return true;
+  if (!stepContext.features || !stepContext.features["temporal"]) {
+    return;
+  }
+  return cts.andQuery([sourceQuery, cts.collectionQuery("latest")]);
 }
 
-function onInstanceSave(stepContext, model, contentObject) {
-    return true;
+function onInstanceSave(stepContext, model, contentArray) {
+  const flowStep = stepContext.flowStep;
+  if (!flowStep.features || !flowStep.features["temporal"]) {
+      return;
+  }
+  const temporalCollection = flowStep.features["temporal"].collection;
+  hubUtils.hubTrace(INFO_EVENT, `Temporal feature: Updating temporal for documents of model ${model} with collection ${temporalCollection}.`);
+
+  try {
+      flowUtils.writeContentArray(contentArray);
+  } catch (e) {
+      hubUtils.hubTrace(INFO_EVENT, `Temporal feature: There was an issue inserting documents in step ${flowStep.name} using collection ${temporalCollection}: ${e}`);
+  }
 }
 
-function onInstanceDelete(stepContext, model, contentObject) {
-    return true;
+function onInstanceDelete(stepContext, model, contentArray) {
+  const flowStep = stepContext.flowStep;
+  if (!flowStep.features || !flowStep.features["temporal"]) {
+      return;
+  }
+  const temporalCollection = flowStep.features["temporal"].collection;
+  hubUtils.hubTrace(INFO_EVENT, `Temporal feature: Deleting temporal for documents of model ${model} with collection ${temporalCollection}.`);
+
+  try {
+      flowUtils.writeContentArray(contentArray);
+  } catch (e) {
+      hubUtils.hubTrace(INFO_EVENT, `Temporal feature: There was an issue deleting documents from temporal collection ${temporalCollection}: ${e}`);
+  }
+}
+
+function getTemporalCollection(tempCollection) {
+    const temporalCollections = temporalLib.getTemporalCollections().toArray();
+    return temporalCollections.filter((col) => fn.string(col) === tempCollection);
 }
 
 export default {
---
+++ marklogic-data-hub/src/test/ml-modules/root/test/suites/data-hub/features/temporal/temporal.mjs
@@ -0,0 +1,60 @@
+import temporalFeature from "/data-hub/features/temporal.mjs";
+const test = require("/test/test-helper.xqy");
+import temporalLib from "/data-hub/5/temporal/hub-temporal.mjs";
+
+const myTempCollection = "koolTest";
+function getTemporalCollection(tempCollection) {
+  const temporalCollections = temporalLib.getTemporalCollections().toArray();
+  return temporalCollections.filter((col) => fn.string(col) === tempCollection);
+}
+
+const assertions = [];
+assertions.push(test.assertTrue(getTemporalCollection(myTempCollection).length === 0, "koolTest should not be a temporal collection"));
+
+temporalFeature.onArtifactPublish("model", "Person");
+assertions.push(test.assertTrue(getTemporalCollection(myTempCollection).length === 0, "koolTest should not be a temporal collection"));
+
+temporalFeature.onArtifactPublish("model", "AffiliateCustomer");
+assertions.push(test.assertTrue(getTemporalCollection(myTempCollection).length === 1, "koolTest should be a temporal collection"));
+
+
+const stepContext = {flowStep: {features: {
+  "temporal": { "enabled" : true, "collection": myTempCollection}
+  }}};
+
+const model = undefined;
+const sourceQuery = "cts.collectionQuery(['raw-content'])";
+
+const resultQuery = temporalFeature.onBuildInstanceQuery(stepContext, model, sourceQuery);
+assertions.push(test.assertTrue(resultQuery, "cts.andQuery[cts.collectionQuery(['latest']),cts.collectionQuery(['raw-content'])]"));
+
+const contentArray = [];
+contentArray.push({
+  "uri": "/affiliate1.json",
+  "value": { "meta": { "systemStart": "1601-01-01T13:59:00Z", "systemEnd": "9999-12-31T11:59:59Z", "validStart": fn.currentDateTime() , "validEnd": fn.currentDateTime().add(xs.yearMonthDuration('P1Y'))  },"customerId": "1"} ,
+  "context": { "collections":[myTempCollection]}});
+xdmp.invokeFunction(() =>{
+temporalFeature.onInstanceSave(stepContext, model, contentArray);
+});
+xdmp.invokeFunction(() => {
+  assertions.push(test.assertEqual(1,
+    cts.estimate(cts.andQuery([cts.collectionQuery(["koolTest"]), cts.collectionQuery(["latest"])])),
+    `One document must be found with collection: ${myTempCollection}.`));
+});
+const contentArrayDelete = [];
+contentArrayDelete.push({
+  "uri": "/affiliate1.json",
+  "value": { "meta": { "systemStart": "1601-01-01T13:59:00Z", "systemEnd": "9999-12-31T11:59:59Z", "validStart": fn.currentDateTime() , "validEnd": fn.currentDateTime().add(xs.yearMonthDuration('P1Y'))  },
+    "customerId": "1", "active":false} ,
+  "context": { "collections":[myTempCollection]},
+  "$delete" : true});
+xdmp.invokeFunction(() => {
+  temporalFeature.onInstanceDelete(stepContext, model, contentArrayDelete);
+});
+xdmp.invokeFunction(() => {
+  assertions.push(test.assertEqual(0,
+    cts.estimate(cts.andQuery([cts.collectionQuery(["koolTest"]), cts.collectionQuery(["latest"])])),
+    `No document must be found with collection: ${myTempCollection}.`));
+});
+
+assertions;
