cb81e85da94ccc0d9a9a38d41b833f0047b9b489
rolonPablo <pablo.rolon@marklogic.com>
2023-04-10 12:15:58 -0300

    DHFPROD-9446: Add document permissions to Artifact Events and Instance Events

--- marklogic-data-hub/src/main/resources/ml-modules/root/data-hub/features/doc-permissions.mjs
+++ marklogic-data-hub/src/main/resources/ml-modules/root/data-hub/features/doc-permissions.mjs
@@ -19,17 +19,67 @@
  * Feature that handles the document permissions of the artifacts and instances.
  */
 
+import consts from "/data-hub/5/impl/consts.mjs";
+import hubUtils from "/data-hub/5/impl/hub-utils.mjs";
+
+const INFO_EVENT = consts.TRACE_CORE;
+
 function onArtifactSave(artifactType, artifactName) {
+    //Complete in upcoming stories
     return true;
 }
 
 function onArtifactPublish(artifactType, artifactName) {
+    //Complete in upcoming stories
     return true;
 }
 
-function onInstanceSave(stepContext, model, contentObject) {
-    return true;
+function onInstanceSave(context, model, contentArray) {
+    let flagAddPermissionObject = false;
+    let permissions = [];
+    const stepContext = context.flowStep;
+    if (!model) {
+        return contentArray;
+    }
+    const modelName = model.info.title;
+    if(featureEnabled(model.definitions[modelName])) {
+        hubUtils.hubTrace(INFO_EVENT, `Processing doc permission feature for an instance of ${modelName}`);
+        const docPermissionObj = model.definitions[modelName].features.find(item => item['docPermission']);
+        permissions = docPermissionObj['docPermission'].permissions;
+        flagAddPermissionObject = true;
+    }
+    if(featureEnabled(stepContext)) {
+        hubUtils.hubTrace(INFO_EVENT, `Processing doc permission feature for an instance while running ${stepContext.name}`);
+        const docPermissionObj = stepContext.features.find(item => item['docPermission']);
+
+        permissions = docPermissionObj["docPermission"].permissions;
+        flagAddPermissionObject = true;
+    }
+    if (flagAddPermissionObject){
+        addPermissionsToObject(permissions, contentArray);
+    }
+    hubUtils.hubTrace(INFO_EVENT, `Finished processing doc permission feature `);
+    return contentArray;
 }
+
+function featureEnabled(artifact) {
+   if (artifact.features) {
+       const docPermissionObj = artifact.features.find(item => item['docPermission']);
+       if (docPermissionObj) {
+           return docPermissionObj['docPermission'].enabled;
+       }
+   }
+    return false;
+}
+
+function addPermissionsToObject(permissions, contentArray) {
+    // add permissions to the contentObject
+    contentArray.forEach(contentObject => {
+    const permissionsArray = hubUtils.parsePermissions(permissions);
+    contentObject.context.permissions = contentObject.context.permissions.concat(permissionsArray);
+    });
+}
+
 
 export default {
     onArtifactSave,
