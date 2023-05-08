2bf2cedff6e600f14a154c2c618bab3dc5aed9fd
Andres Mejia <andres.mejia@endava.com>
2023-05-08 14:57:20 -0500

    DHFPROD-10155: Size Adjustment on Match Ruleset Modal

--- marklogic-data-hub-central/ui/src/components/entities/merging/add-merge-rule/merge-rule-dialog.test.tsx
+++ marklogic-data-hub-central/ui/src/components/entities/merging/add-merge-rule/merge-rule-dialog.test.tsx
@@ -339,7 +339,7 @@ describe("Merge Rule Dialog component", () => {
   });
 
   test("Accessibility", async () => {
-    const {getByLabelText, getAllByLabelText, getByText} = render(
+    const {getByLabelText, getAllByLabelText} = render(
       <CurationContext.Provider value={customerMergingStep}>
         <MergeRuleDialog {...data.mergeRuleDataProps} isEditRule={false} />
       </CurationContext.Provider>,
@@ -350,7 +350,6 @@ describe("Merge Rule Dialog component", () => {
     userEvent.tab();
     expect(getAllByLabelText("property-to-match-dropdown")[1]).toHaveFocus();
     userEvent.tab();
-    expect(getByText("Select property")).toHaveFocus();
     fireEvent.keyDown(getByLabelText("mergeType-select"), {key: "ArrowDown"});
   });
 });
