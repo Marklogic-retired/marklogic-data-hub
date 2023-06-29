c223808b3f187d1b86ff4df38f9c041f2e3213cc
rahul vudutala <rahulbharadwaj.v@gmail.com>
2023-06-29 08:56:47 -0700

    DHFPROD-10359: Removing content object created when converting from csv to json/xml

--- marklogic-data-hub/src/main/resources/scaffolding/custom-module/mjs/main-ingestion-legacy-upgrade.mjs
+++ marklogic-data-hub/src/main/resources/scaffolding/custom-module/mjs/main-ingestion-legacy-upgrade.mjs
@@ -1,8 +1,10 @@
 function main(content, options) {
   let legacyOptions = options.options;
   let result = {};
+  let contentObj = content["value"].toObject();
+  contentObj = (contentObj && contentObj.content) ? contentObj.content : contentObj;
   result["uri"] = content.uri;
-  result["value"] = require(legacyOptions["mainModuleUri"]).main(content.uri, content.value, legacyOptions);
+  result["value"] = require(legacyOptions["mainModuleUri"]).main(content.uri, contentObj, legacyOptions);
   return result;
 }
 
